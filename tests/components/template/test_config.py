a70fe2e630d72c7560e92ed73fa696855b09087b
Petro31 <35082313+Petro31@users.noreply.github.com>
2025-10-23 15:02:18 -0400

    Fix variables in icon, picture, and name for state based template entities (#154994)

--- tests/components/template/test_config.py
+++ tests/components/template/test_config.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import pytest
 import voluptuous as vol
 
+from homeassistant.components.template import DOMAIN
 from homeassistant.components.template.config import (
     CONFIG_SECTION_SCHEMA,
     async_validate_config_section,
@@ -12,6 +13,7 @@ from homeassistant.components.template.config import (
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.script_variables import ScriptVariables
 from homeassistant.helpers.template import Template
+from homeassistant.setup import async_setup_component
 
 
 @pytest.mark.parametrize(
@@ -256,3 +258,59 @@ async def test_combined_trigger_variables(
     assert root_variables.as_dict() == expected_root
     variables: ScriptVariables = validated["binary_sensor"][0].get("variables", empty)
     assert variables.as_dict() == expected_entity
+
+
+async def test_state_init_attribute_variables(
+    hass: HomeAssistant,
+) -> None:
+    """Test a state based template entity initializes icon, name, and picture with variables."""
+    source = "switch.foo"
+    entity_id = "sensor.foo"
+
+    hass.states.async_set(source, "on", {"friendly_name": "Foo"})
+    config = {
+        "template": [
+            {
+                "variables": {
+                    "switch": "switch.foo",
+                    "on_icon": "mdi:lightbulb",
+                    "on_picture": "on.png",
+                },
+                "sensor": {
+                    "variables": {
+                        "off_icon": "mdi:lightbulb-off",
+                        "off_picture": "off.png",
+                    },
+                    "name": "{{ state_attr(switch, 'friendly_name') }}",
+                    "icon": "{{ on_icon if is_state(switch, 'on') else off_icon }}",
+                    "picture": "{{ on_picture if is_state(switch, 'on') else off_picture }}",
+                    "state": "{{ is_state(switch, 'on') }}",
+                },
+            }
+        ],
+    }
+    assert await async_setup_component(
+        hass,
+        DOMAIN,
+        config,
+    )
+    await hass.async_block_till_done()
+
+    # Check initial state
+    sensor = hass.states.get(entity_id)
+    assert sensor
+    assert sensor.state == "True"
+    assert sensor.attributes["icon"] == "mdi:lightbulb"
+    assert sensor.attributes["entity_picture"] == "on.png"
+    assert sensor.attributes["friendly_name"] == "Foo"
+
+    hass.states.async_set(source, "off", {"friendly_name": "Foo"})
+    await hass.async_block_till_done()
+
+    # Check to see that the template light works
+    sensor = hass.states.get(entity_id)
+    assert sensor
+    assert sensor.state == "False"
+    assert sensor.attributes["icon"] == "mdi:lightbulb-off"
+    assert sensor.attributes["entity_picture"] == "off.png"
+    assert sensor.attributes["friendly_name"] == "Foo"
