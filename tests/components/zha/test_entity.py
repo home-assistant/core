ea421343fc87359a8b63cda631a31aca20816e1b
Markus Tuominen <3738613+Markus98@users.noreply.github.com>
2025-10-25 21:30:10 +0300

    Split zha test entity postfix into two

--- tests/components/zha/test_entity.py
+++ tests/components/zha/test_entity.py
@@ -70,7 +70,6 @@ async def test_device_registry_via_device(
     assert reg_device.via_device_id == reg_coordinator_device.id
 
 
-@pytest.mark.parametrize("n_endpoints", [1, 2])
 @pytest.mark.parametrize(
     (
         "cluster_id",
@@ -102,9 +101,9 @@ async def test_entity_postfix(
     cluster_id: t.uint16_t,
     entity_prefix: str,
     entity_suffix: str,
-    n_endpoints: int,
 ) -> None:
-    """Test postfix being present in entity name."""
+    """Test postfix when a device has several entities of the same type."""
+    n_endpoints = 2
 
     await setup_zha()
     gateway = get_zha_gateway(hass)
@@ -124,14 +123,61 @@ async def test_entity_postfix(
     await gateway.async_device_initialized(zigpy_device)
     await hass.async_block_till_done(wait_background_tasks=True)
 
-    if n_endpoints > 1:
-        for n in range(1, n_endpoints + 1):
-            state = hass.states.get(
-                ENTITY_ID_PREFIX_NUM.format(entity_prefix, entity_suffix, n)
-            )
-            assert state is not None
-            assert state.name.endswith(f" ({n})")
-    else:
-        state = hass.states.get(ENTITY_ID_NO_PREFIX.format(entity_prefix))
+    for n in range(1, n_endpoints + 1):
+        state = hass.states.get(
+            ENTITY_ID_PREFIX_NUM.format(entity_prefix, entity_suffix, n)
+        )
         assert state is not None
-        assert not state.name.endswith(")")
+        assert state.name.endswith(f" ({n})")
+
+
+@pytest.mark.parametrize(
+    (
+        "cluster_id",
+        "entity_prefix",
+    ),
+    [
+        (
+            measurement.TemperatureMeasurement.cluster_id,
+            "sensor",
+        ),
+        (
+            measurement.PressureMeasurement.cluster_id,
+            "sensor",
+        ),
+        (
+            general.OnOff.cluster_id,
+            "switch",
+        ),
+    ],
+)
+async def test_single_entity_no_postfix(
+    hass: HomeAssistant,
+    setup_zha: Callable[..., Coroutine[None]],
+    zigpy_device_mock: Callable[..., Device],
+    cluster_id: t.uint16_t,
+    entity_prefix: str,
+) -> None:
+    """Test that postfix is not in the name of singular entities."""
+
+    await setup_zha()
+    gateway = get_zha_gateway(hass)
+
+    zigpy_device = zigpy_device_mock(
+        {
+            1: {
+                SIG_EP_INPUT: [cluster_id, general.Basic.cluster_id],
+                SIG_EP_OUTPUT: [],
+                SIG_EP_TYPE: zha.DeviceType.ON_OFF_SWITCH,
+            },
+        }
+    )
+
+    gateway.get_or_create_device(zigpy_device)
+    await gateway.async_device_initialized(zigpy_device)
+    await hass.async_block_till_done(wait_background_tasks=True)
+
+    state = hass.states.get(ENTITY_ID_NO_PREFIX.format(entity_prefix))
+    assert state is not None
+    # Name should not be "Entity (<NUM>)", check for trailing parenthesis
+    assert not state.name.endswith(")")
