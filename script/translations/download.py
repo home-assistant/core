a930a4250b607ef23193c8afc9c36390ccfe4ef3
Artur Pragacz <artur@pragacz.com>
2025-10-25 21:39:08 +0200

    Fix translation references to unverified translations

--- script/translations/download.py
+++ script/translations/download.py
@@ -10,12 +10,17 @@ from typing import Any
 
 from .const import CLI_2_DOCKER_IMAGE, CORE_PROJECT_ID, INTEGRATIONS_DIR
 from .error import ExitApp
-from .util import get_lokalise_token, load_json_from_path
+from .util import (
+    flatten_translations,
+    get_lokalise_token,
+    load_json_from_path,
+    substitute_references,
+)
 
 DOWNLOAD_DIR = Path("build/translations-download").absolute()
 
 
-def run_download_docker():
+def run_download_docker() -> None:
     """Run the Docker image to download the translations."""
     print("Running Docker to download latest translations.")
     result = subprocess.run(
@@ -39,6 +44,7 @@ def run_download_docker():
             "--replace-breaks=false",
             "--filter-data",
             "nonfuzzy",
+            "--disable-references",
             "--export-empty-as",
             "skip",
             "--format",
@@ -76,9 +82,12 @@ def filter_translations(translations: dict[str, Any], strings: dict[str, Any]) -
                 continue
 
 
-def save_language_translations(lang, translations):
+def save_language_translations(lang: str, translations: dict[str, Any]) -> None:
     """Save translations for a single language."""
     components = translations.get("component", {})
+
+    flattened_all_translations = flatten_translations(translations)
+
     for component, component_translations in components.items():
         # Remove legacy platform translations
         component_translations.pop("platform", None)
@@ -104,12 +113,16 @@ def save_language_translations(lang, translations):
         path = component_path / "translations" / f"{lang}.json"
         path.parent.mkdir(parents=True, exist_ok=True)
 
+        component_translations = substitute_references(
+            component_translations, flattened_all_translations, fail_on_missing=False
+        )
+
         filter_translations(component_translations, strings)
 
         save_json(path, component_translations)
 
 
-def save_integrations_translations():
+def save_integrations_translations() -> None:
     """Save integrations translations."""
     for lang_file in DOWNLOAD_DIR.glob("*.json"):
         lang = lang_file.stem
@@ -117,13 +130,13 @@ def save_integrations_translations():
         save_language_translations(lang, translations)
 
 
-def delete_old_translations():
+def delete_old_translations() -> None:
     """Delete old translations."""
     for fil in INTEGRATIONS_DIR.glob("*/translations/*"):
         fil.unlink()
 
 
-def run():
+def run() -> None:
     """Run the script."""
     DOWNLOAD_DIR.mkdir(parents=True, exist_ok=True)
 
