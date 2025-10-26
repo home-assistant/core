b7e7a9cc1df1496a4dbc5d084dfdc2428fa6c4e3
Shay Levy <levyshay1@gmail.com>
2025-10-26 19:57:05 +0000

    Fix typo in API_SET_POSITION

--- homeassistant/components/switcher_kis/cover.py
+++ homeassistant/components/switcher_kis/cover.py
@@ -23,7 +23,7 @@ from .entity import SwitcherEntity
 
 PARALLEL_UPDATES = 1
 
-API_SET_POSITON = "set_position"
+API_SET_POSITION = "set_position"
 API_STOP = "stop_shutter"
 
 
@@ -85,16 +85,16 @@ class SwitcherBaseCoverEntity(SwitcherEntity, CoverEntity):
 
     async def async_close_cover(self, **kwargs: Any) -> None:
         """Close cover."""
-        await self._async_call_api(API_SET_POSITON, 0, self._cover_id)
+        await self._async_call_api(API_SET_POSITION, 0, self._cover_id)
 
     async def async_open_cover(self, **kwargs: Any) -> None:
         """Open cover."""
-        await self._async_call_api(API_SET_POSITON, 100, self._cover_id)
+        await self._async_call_api(API_SET_POSITION, 100, self._cover_id)
 
     async def async_set_cover_position(self, **kwargs: Any) -> None:
         """Move the cover to a specific position."""
         await self._async_call_api(
-            API_SET_POSITON, kwargs[ATTR_POSITION], self._cover_id
+            API_SET_POSITION, kwargs[ATTR_POSITION], self._cover_id
         )
 
     async def async_stop_cover(self, **kwargs: Any) -> None:
