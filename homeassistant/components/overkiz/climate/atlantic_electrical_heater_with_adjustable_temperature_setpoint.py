home-assistant/core

commit 3d64b72308db0942c0c45390d753dc1227ed8210

Add manual mode to the map of Overkiz to HVAC modes (#151438)

homeassistant/components/overkiz/climate/atlantic_electrical_heater_with_adjustable_temperature_setpoint.py
@@ -52,6 +52,7 @@ OVERKIZ_TO_HVAC_MODE: dict[str, HVACMode] = {
     OverkizCommandParam.OFF: HVACMode.OFF,
     OverkizCommandParam.AUTO: HVACMode.AUTO,
     OverkizCommandParam.BASIC: HVACMode.HEAT,
+    OverkizCommandParam.MANUAL: HVACMode.HEAT,
     OverkizCommandParam.STANDBY: HVACMode.OFF,
     OverkizCommandParam.EXTERNAL: HVACMode.AUTO,
     OverkizCommandParam.INTERNAL: HVACMode.AUTO,

commit b37496c77c1275192bfe450be1705158f7db98e8

Fix regression caused by #151438

homeassistant/components/overkiz/climate/atlantic_electrical_heater_with_adjustable_temperature_setpoint.py
@@ -51,8 +51,8 @@ OVERKIZ_TO_HVAC_MODE: dict[str, HVACMode] = {
     OverkizCommandParam.ON: HVACMode.HEAT,
     OverkizCommandParam.OFF: HVACMode.OFF,
     OverkizCommandParam.AUTO: HVACMode.AUTO,
-    OverkizCommandParam.BASIC: HVACMode.HEAT,
     OverkizCommandParam.MANUAL: HVACMode.HEAT,
+    OverkizCommandParam.BASIC: HVACMode.HEAT,
     OverkizCommandParam.STANDBY: HVACMode.OFF,
     OverkizCommandParam.EXTERNAL: HVACMode.AUTO,
     OverkizCommandParam.INTERNAL: HVACMode.AUTO,