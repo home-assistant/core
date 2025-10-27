99ea7eb9773e4452138f65c5d576ff88ffa7df4f
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-10-27 14:19:39 +0000

    changes

--- homeassistant/components/xbox/sensor.py
+++ homeassistant/components/xbox/sensor.py
@@ -48,31 +48,48 @@ class XboxSensorEntityDescription(XboxBaseEntityDescription, SensorEntityDescrip
 
 def now_playing_attributes(_: Person, title: Title | None) -> dict[str, Any]:
     """Attributes of the currently played title."""
-    attributes: dict[str, Any] = {}
+    attributes: dict[str, Any] = {
+        "short_description": None,
+        "genres": None,
+        "developer": None,
+        "publisher": None,
+        "release_date": None,
+        "min_age": None,
+        "achievements": None,
+        "gamerscore": None,
+        "progress": None,
+    }
     if not title:
         return attributes
     if title.detail is not None:
-        attributes["short_description"] = title.detail.short_description
-        attributes["genres"] = (
-            ", ".join(title.detail.genres) if title.detail.genres else None
+        attributes.update(
+            {
+                "short_description": title.detail.short_description,
+                "genres": (
+                    ", ".join(title.detail.genres) if title.detail.genres else None
+                ),
+                "developer": title.detail.developer_name,
+                "publisher": title.detail.publisher_name,
+                "release_date": (
+                    title.detail.release_date.replace(tzinfo=UTC).date()
+                    if title.detail.release_date
+                    else None
+                ),
+                "min_age": title.detail.min_age,
+            }
         )
-        attributes["developer"] = title.detail.developer_name
-        attributes["publisher"] = title.detail.publisher_name
-        attributes["min_age"] = title.detail.min_age
-        attributes["release_date"] = (
-            title.detail.release_date.replace(tzinfo=UTC).date()
-            if title.detail.release_date
-            else None
-        )
-        attributes["min_age"] = title.detail.min_age
     if (achievement := title.achievement) is not None:
-        attributes["achievements"] = (
-            f"{achievement.current_achievements} / {achievement.total_achievements}"
+        attributes.update(
+            {
+                "achievements": (
+                    f"{achievement.current_achievements} / {achievement.total_achievements}"
+                ),
+                "gamerscore": (
+                    f"{achievement.current_gamerscore} / {achievement.total_gamerscore}"
+                ),
+                "progress": f"{int(achievement.progress_percentage)} %",
+            }
         )
-        attributes["gamerscore"] = (
-            f"{achievement.current_gamerscore} / {achievement.total_gamerscore}"
-        )
-        attributes["progress"] = f"{int(achievement.progress_percentage)} %"
 
     return attributes
 
@@ -81,7 +98,7 @@ def title_logo(_: Person, title: Title | None) -> str | None:
     """Get the game logo."""
 
     return (
-        next((i.url for i in title.images if i.type == "Logo"), None)
+        next((i.url for i in title.images if i.type == "Tile"), None)
         if title and title.images
         else None
     )
