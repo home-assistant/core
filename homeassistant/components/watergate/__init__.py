470471f8cd8073d0121295aab8f6ce6ea54cb4a2
adam-the-hero <adam.k@watergate.ai>
2025-10-28 16:53:36 +0000

    Update Watergate dependency. Update aioohttp session usage. Update Quality Scale.

--- homeassistant/components/watergate/__init__.py
+++ homeassistant/components/watergate/__init__.py
@@ -20,6 +20,7 @@ from homeassistant.components.webhook import (
 from homeassistant.const import CONF_IP_ADDRESS, CONF_WEBHOOK_ID, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.dispatcher import async_dispatcher_send
 
 from .const import AUTO_SHUT_OFF_EVENT_NAME, DOMAIN
@@ -51,11 +52,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: WatergateConfigEntry) ->
     )
 
     watergate_client = WatergateLocalApiClient(
-        base_url=(
-            sonic_address
-            if sonic_address.startswith("http")
-            else f"http://{sonic_address}"
-        ),
+        base_url=sonic_address
+        if sonic_address.startswith("http")
+        else f"http://{sonic_address}",
         session=async_get_clientsession(hass),
     )
 
