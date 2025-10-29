62d66da665e32a40ef3463e6b1fdf229cc6d394f
David Rapan <david@rapan.cz>
2025-10-29 12:44:00 +0100

    Add test_sensor.test_async_session_rollback_on_error
    Signed-off-by: David Rapan <david@rapan.cz>

--- tests/components/sql/test_sensor.py
+++ tests/components/sql/test_sensor.py
@@ -792,6 +792,47 @@ async def test_session_rollback_on_error(
     assert mock_session_rollback.call_count == 1
 
 
+async def test_async_session_rollback_on_error(
+    recorder_mock: Recorder,
+    hass: HomeAssistant,
+    freezer: FrozenDateTimeFactory,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    """Test the SQL sensor."""
+    config = {CONF_DB_URL: "sqlite+aiosqlite:///"}
+    options = {
+        CONF_QUERY: "SELECT 5 as value",
+        CONF_COLUMN_NAME: "value",
+        CONF_UNIQUE_ID: "very_unique_id",
+    }
+    await init_integration(
+        hass, title="Select value SQL query", config=config, options=options
+    )
+    platforms = async_get_platforms(hass, "sql")
+    sql_entity = platforms[0].entities["sensor.select_value_sql_query"]
+
+    state = hass.states.get("sensor.select_value_sql_query")
+    assert state.state == "5"
+    assert state.attributes["value"] == 5
+
+    with (
+        patch.object(
+            sql_entity,
+            "_lambda_stmt",
+            generate_lambda_stmt("Faulty syntax create operational issue"),
+        ),
+        patch(
+            "sqlalchemy.ext.asyncio.session.AsyncSession.rollback"
+        ) as mock_async_session_rollback,
+    ):
+        freezer.tick(timedelta(minutes=1))
+        async_fire_time_changed(hass)
+        await hass.async_block_till_done(wait_background_tasks=True)
+        assert "sqlite3.OperationalError" in caplog.text
+
+    assert mock_async_session_rollback.call_count == 1
+
+
 async def test_query_recover_from_rollback(
     recorder_mock: Recorder,
     hass: HomeAssistant,
