12b70f743676606a9fe0d53a2b77edf15c3ff127
Matthew Gibson <matthew.gibzon@gmail.com>
2025-10-30 18:05:01 +0000

    Finished unit testing for config_flow
    Finished adding the final tests to the test_config_flow program.
    Added required strings to the strings.json file needed for the
    config flow.
    Added a new mocks for the config tests that allow for mocked ptdevice
    responses.

--- tests/components/ptdevices/conftest.py
+++ tests/components/ptdevices/conftest.py
@@ -1,6 +1,6 @@
 """Common fixtures for the PTDevices tests."""
 
-from collections.abc import Generator
+from collections.abc import AsyncGenerator, Generator
 import json
 from unittest.mock import AsyncMock, patch
 
@@ -8,12 +8,14 @@ from aioptdevices.interface import PTDevicesResponse
 import pytest
 
 from homeassistant.components.ptdevices.const import DOMAIN
+from homeassistant.config_entries import SOURCE_USER
+from homeassistant.const import CONF_API_TOKEN, CONF_DEVICE_ID
 
-from tests.common import load_fixture
+from tests.common import MockConfigEntry, load_fixture
 
 
 @pytest.fixture
-def mock_setup_entry() -> Generator[AsyncMock]:
+def mock_ptdevices_setup_entry() -> Generator[AsyncMock]:
     """Override async_setup_entry."""
     with patch(
         "homeassistant.components.ptdevices.async_setup_entry", return_value=True
@@ -21,8 +23,8 @@ def mock_setup_entry() -> Generator[AsyncMock]:
         yield mock_setup_entry
 
 
-@pytest.fixture(name="ptdevices_level")
-def mock_ptdevices_level():
+@pytest.fixture
+def mock_ptdevices_level() -> PTDevicesResponse:
     """Mock a PTLevel device."""
     data = json.loads(load_fixture("ptdevices_level.json", integration=DOMAIN))
     return PTDevicesResponse(
@@ -31,8 +33,8 @@ def mock_ptdevices_level():
     )
 
 
-@pytest.fixture(name="ptdevices_level_missing_title")
-def mock_ptdevices_level_missing_title():
+@pytest.fixture
+def mock_ptdevices_level_missing_title() -> PTDevicesResponse:
     """Mock a malformed PTLevel response."""
     data = json.loads(load_fixture("ptdevices_level.json", integration=DOMAIN))
     data.pop("title")
@@ -42,8 +44,8 @@ def mock_ptdevices_level_missing_title():
     )
 
 
-@pytest.fixture(name="ptdevices_level_missing_device_id")
-def mock_ptdevices_level_missing_device_id():
+@pytest.fixture
+def mock_ptdevices_level_missing_device_id() -> PTDevicesResponse:
     """Mock a malformed PTLevel response."""
     data = json.loads(load_fixture("ptdevices_level.json", integration=DOMAIN))
     data.pop("id")
@@ -53,22 +55,34 @@ def mock_ptdevices_level_missing_device_id():
     )
 
 
-# @pytest.fixture
-# def mock_config_entry(
-#     request: pytest.FixtureRequest,
-#     mock_request_status: AsyncMock,
-# ) -> MockConfigEntry:
-#     """Mock setting up a config entry."""
-#     entry_id = getattr(request, "param", None)
-#     return MockConfigEntry(
-#         entry_id=entry_id,
-#         version=1,
-#         domain=DOMAIN,
-#         title="Home",
-#         data={
-#             CONF_API_TOKEN: "test-api-token",
-#             CONF_DEVICE_ID: "test-device-id",
-#         },
-#         unique_id="test-device-id",
-#         source=SOURCE_USER,
-#     )
+@pytest.fixture
+async def mock_ptdevices_get_data(
+    mock_ptdevices_level: PTDevicesResponse,
+) -> AsyncGenerator[AsyncMock]:
+    """Return a mocked ptdevices.request_status function."""
+
+    with patch("aioptdevices.interface.Interface.get_data") as mock_request_status:
+        mock_request_status.return_value = mock_ptdevices_level
+        yield mock_request_status
+
+
+@pytest.fixture
+def mock_ptdevices_config_entry(
+    request: pytest.FixtureRequest,
+    mock_ptdevices_get_data: AsyncMock,
+) -> MockConfigEntry:
+    """Return a mocked ptdevice configuration entry."""
+    entry_id = getattr(request, "param", None)
+
+    return MockConfigEntry(
+        entry_id=entry_id,
+        version=1,
+        domain=DOMAIN,
+        title="Home",
+        data={
+            CONF_API_TOKEN: "test-api-token",
+            CONF_DEVICE_ID: "test-device-id",
+        },
+        unique_id="test-device-id",
+        source=SOURCE_USER,
+    )
