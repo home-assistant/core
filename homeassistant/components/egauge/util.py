410160bd82648dfe9dae7e59d4cfe5d1ed21023f
neggert <nic@eggert.io>
2025-10-31 13:26:42 -0500

    Removed unused helper function

--- homeassistant/components/egauge/util.py
+++
@@ -1,7 +0,0 @@
-"""Utilities for the eGauge integration."""
-
-
-def _build_client_url(host: str, use_ssl: bool) -> str:
-    """Builds the base URL for EgaugeJsonClient."""
-    protocol = "https://" if use_ssl else "http://"
-    return protocol + host
