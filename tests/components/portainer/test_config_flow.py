6ffe5800814a38346fa33948a9de084dce830eb9
Erwin Douna <e.douna@gmail.com>
2025-10-31 12:05:48 +0100

    Portainer expand reconfigure check (#155544)

--- tests/components/portainer/test_config_flow.py
+++ tests/components/portainer/test_config_flow.py
@@ -255,6 +255,31 @@ async def test_full_flow_reconfigure(
     assert len(mock_setup_entry.mock_calls) == 1
 
 
+async def test_full_flow_reconfigure_unique_id(
+    hass: HomeAssistant,
+    mock_portainer_client: AsyncMock,
+    mock_setup_entry: MagicMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test the full flow of the config flow, this time with a known unique ID."""
+    mock_config_entry.add_to_hass(hass)
+    result = await mock_config_entry.start_reconfigure_flow(hass)
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "reconfigure"
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input=MOCK_USER_SETUP,
+    )
+
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "already_configured"
+    assert mock_config_entry.data[CONF_API_TOKEN] == "test_api_token"
+    assert mock_config_entry.data[CONF_URL] == "https://127.0.0.1:9000/"
+    assert mock_config_entry.data[CONF_VERIFY_SSL] is True
+    assert len(mock_setup_entry.mock_calls) == 0
+
+
 @pytest.mark.parametrize(
     ("exception", "reason"),
     [
