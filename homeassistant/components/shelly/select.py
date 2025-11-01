cc4c76642faec4db38f15acbd6c4f34536cda883
Maciej Bieniek <bieniu@users.noreply.github.com>
2025-11-01 10:29:09 +0100

    Add Mode select entity

--- homeassistant/components/shelly/select.py
+++ homeassistant/components/shelly/select.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Final
+from typing import TYPE_CHECKING, Final
 
 from aioshelly.const import RPC_GENERATIONS
 
@@ -37,14 +37,33 @@ PARALLEL_UPDATES = 0
 class RpcSelectDescription(RpcEntityDescription, SelectEntityDescription):
     """Class to describe a RPC select entity."""
 
+    method: str
+
 
 RPC_SELECT_ENTITIES: Final = {
+    "cury_mode": RpcSelectDescription(
+        key="cury",
+        sub_key="mode",
+        name="Mode",
+        translation_key="cury_mode",
+        options=[
+            "hall",
+            "bedroom",
+            "living_room",
+            "lavatory_room",
+            "reception",
+            "workplace",
+        ],
+        method="cury_set_mode",
+        available=lambda status: status is not None,
+    ),
     "enum_generic": RpcSelectDescription(
         key="enum",
         sub_key="value",
         removal_condition=lambda config, _status, key: not is_view_for_platform(
             config, key, SELECT_PLATFORM
         ),
+        method="enum_set",
         role=ROLE_GENERIC,
     ),
 }
@@ -107,7 +126,8 @@ class RpcSelect(ShellyRpcAttributeEntity, SelectEntity):
         """Initialize select."""
         super().__init__(coordinator, key, attribute, description)
 
-        self._attr_options = list(self.option_map.values())
+        if self.option_map:
+            self._attr_options = list(self.option_map.values())
 
     @property
     def current_option(self) -> str | None:
@@ -115,11 +135,20 @@ class RpcSelect(ShellyRpcAttributeEntity, SelectEntity):
         if not isinstance(self.attribute_value, str):
             return None
 
-        return self.option_map[self.attribute_value]
+        if self.option_map:
+            return self.option_map[self.attribute_value]
+
+        return self.attribute_value
 
     @rpc_call
     async def async_select_option(self, option: str) -> None:
         """Change the value."""
-        await self.coordinator.device.enum_set(
-            self._id, self.reversed_option_map[option]
-        )
+        method = getattr(self.coordinator.device, self.entity_description.method)
+
+        if TYPE_CHECKING:
+            assert method is not None
+
+        if self.reversed_option_map:
+            await method(self._id, self.reversed_option_map[option])
+        else:
+            await method(self._id, option)
