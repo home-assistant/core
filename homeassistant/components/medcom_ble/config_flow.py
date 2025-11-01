507b2329bf505011788eb566dd5dc69a54d3a3c5
J. Nick Koston <nick@koston.org>
2025-11-01 11:04:05 -0500

    Allow ignored medcom_ble devices to be set up from the user flow (#155622)

--- homeassistant/components/medcom_ble/config_flow.py
+++ homeassistant/components/medcom_ble/config_flow.py
@@ -93,7 +93,7 @@ class InspectorBLEConfigFlow(ConfigFlow, domain=DOMAIN):
             self._discovery_info = self._discovered_devices[address]
             return await self.async_step_check_connection()
 
-        current_addresses = self._async_current_ids()
+        current_addresses = self._async_current_ids(include_ignore=False)
         for discovery_info in async_discovered_service_info(self.hass):
             address = discovery_info.address
             if address in current_addresses or address in self._discovered_devices:
