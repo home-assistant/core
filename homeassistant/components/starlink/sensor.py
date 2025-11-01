d69832466401b936bcbcca95a54dad0962f8d82e
David Rapan <david@rapan.cz>
2025-11-01 11:24:46 +0100

    Rename Last boot time to Last restart
    Signed-off-by: David Rapan <david@rapan.cz>

--- homeassistant/components/starlink/sensor.py
+++ homeassistant/components/starlink/sensor.py
@@ -146,7 +146,7 @@ SENSORS: tuple[StarlinkSensorEntityDescription, ...] = (
     ),
     StarlinkSensorEntityDescription(
         key="last_boot_time",
-        translation_key="last_boot_time",
+        translation_key="last_restart",
         device_class=SensorDeviceClass.TIMESTAMP,
         entity_category=EntityCategory.DIAGNOSTIC,
         value_fn=lambda data: (
