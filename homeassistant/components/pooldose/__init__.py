3668eeecd7c89ca66cbe77fc1755c7ab0cd5e7aa
Lukas <12813107+lmaertin@users.noreply.github.com>
2025-11-02 09:10:05 +0100

    __init__.py aktualisieren
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/pooldose/__init__.py
+++ homeassistant/components/pooldose/__init__.py
@@ -25,7 +25,7 @@ async def async_migrate_entry(hass: HomeAssistant, entry: PooldoseConfigEntry) -
     # Version 1.0 -> 1.1: Migrate entity unique IDs
     # - ofa_orp_value -> ofa_orp_time
     # - ofa_ph_value -> ofa_ph_time
-    if entry.version == 1 and entry.minor_version == 0:
+    if entry.version == 1 and entry.minor_version == 1:
 
         @callback
         def migrate_unique_id(entity_entry: er.RegistryEntry) -> dict[str, Any] | None:
