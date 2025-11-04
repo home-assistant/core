bf79e410e33bbcd09dcde25b46e93b1d6c70c8fb
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-11-04 21:09:32 +0100

    Parametrize climate service tests
    Refactored multiple individual tests for climate entity service calls into a single parameterized test using pytest.mark.parametrize. This reduces code duplication and improves maintainability.

--- tests/components/niko_home_control/test_climate.py
+++ tests/components/niko_home_control/test_climate.py
@@ -1,7 +1,9 @@
 """Tests for the Niko Home Control Climate platform."""
 
+from typing import Any
 from unittest.mock import AsyncMock, patch
 
+import pytest
 from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.components.climate import ATTR_HVAC_MODE, ATTR_PRESET_MODE, HVACMode
@@ -30,120 +32,38 @@ async def test_entities(
     await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
 
 
-async def test_set_temperature(
+@pytest.mark.parametrize(
+    ("service", "service_parameters", "api_method", "api_parameters"),
+    [
+        ("set_temperature", {"temperature": 25}, "set_temperature", (25,)),
+        ("set_preset_mode", {ATTR_PRESET_MODE: "eco"}, "set_mode", (2,)),
+        ("set_hvac_mode", {ATTR_HVAC_MODE: HVACMode.COOL}, "set_mode", (4,)),
+        ("set_hvac_mode", {ATTR_HVAC_MODE: HVACMode.OFF}, "set_mode", (3,)),
+        ("set_hvac_mode", {ATTR_HVAC_MODE: HVACMode.AUTO}, "set_mode", (5,)),
+    ],
+)
+async def test_set(
     hass: HomeAssistant,
     mock_niko_home_control_connection: AsyncMock,
     mock_config_entry: MockConfigEntry,
     climate: AsyncMock,
+    service: str,
+    service_parameters: dict[str, Any],
+    api_method: str,
+    api_parameters: tuple[Any, ...],
 ) -> None:
-    """Test setting a temperature."""
+    """Test setting a value on the climate entity."""
     await setup_integration(hass, mock_config_entry)
     await hass.services.async_call(
         "climate",
-        "set_temperature",
-        {ATTR_ENTITY_ID: "climate.thermostat", "temperature": 25},
+        service,
+        {ATTR_ENTITY_ID: "climate.thermostat"} | service_parameters,
         blocking=True,
     )
-    mock_niko_home_control_connection.thermostats[
-        "thermostat-5"
-    ].set_temperature.assert_called_once_with(25)
-
-
-async def test_set_preset(
-    hass: HomeAssistant,
-    mock_niko_home_control_connection: AsyncMock,
-    mock_config_entry: MockConfigEntry,
-    climate: AsyncMock,
-) -> None:
-    """Test setting a preset."""
-    await setup_integration(hass, mock_config_entry)
-    await hass.services.async_call(
-        "climate",
-        "set_preset_mode",
-        {ATTR_ENTITY_ID: "climate.thermostat", ATTR_PRESET_MODE: "eco"},
-        blocking=True,
-    )
-    mock_niko_home_control_connection.thermostats[
-        "thermostat-5"
-    ].set_mode.assert_called_once_with(2)
-
-
-async def test_set_hvac_cool_mode(
-    hass: HomeAssistant,
-    mock_niko_home_control_connection: AsyncMock,
-    mock_config_entry: MockConfigEntry,
-    climate: AsyncMock,
-) -> None:
-    """Test setting cool mode."""
-    await setup_integration(hass, mock_config_entry)
-
-    await hass.services.async_call(
-        "climate",
-        "set_hvac_mode",
-        {ATTR_ENTITY_ID: "climate.thermostat", ATTR_HVAC_MODE: HVACMode.COOL},
-        blocking=True,
-    )
-    mock_niko_home_control_connection.thermostats[
-        "thermostat-5"
-    ].set_mode.assert_called_once_with(4)
-
-
-async def test_set_hvac_off_mode(
-    hass: HomeAssistant,
-    mock_niko_home_control_connection: AsyncMock,
-    mock_config_entry: MockConfigEntry,
-    climate: AsyncMock,
-) -> None:
-    """Test setting off mode."""
-    await setup_integration(hass, mock_config_entry)
-
-    await hass.services.async_call(
-        "climate",
-        "set_hvac_mode",
-        {ATTR_ENTITY_ID: "climate.thermostat", ATTR_HVAC_MODE: HVACMode.OFF},
-        blocking=True,
-    )
-    mock_niko_home_control_connection.thermostats[
-        "thermostat-5"
-    ].set_mode.assert_called_once_with(3)
-
-
-async def test_set_hvac_auto_mode(
-    hass: HomeAssistant,
-    mock_niko_home_control_connection: AsyncMock,
-    mock_config_entry: MockConfigEntry,
-    climate: AsyncMock,
-) -> None:
-    """Test setting auto mode."""
-    await setup_integration(hass, mock_config_entry)
-    await hass.services.async_call(
-        "climate",
-        "set_hvac_mode",
-        {ATTR_ENTITY_ID: "climate.thermostat", ATTR_HVAC_MODE: HVACMode.AUTO},
-        blocking=True,
-    )
-    mock_niko_home_control_connection.thermostats[
-        "thermostat-5"
-    ].set_mode.assert_called_once_with(5)
-
-
-async def test_turn_off(
-    hass: HomeAssistant,
-    mock_niko_home_control_connection: AsyncMock,
-    mock_config_entry: MockConfigEntry,
-    climate: AsyncMock,
-) -> None:
-    """Test turning off the thermostat."""
-    await setup_integration(hass, mock_config_entry)
-    await hass.services.async_call(
-        "climate",
-        "turn_off",
-        {ATTR_ENTITY_ID: "climate.thermostat"},
-        blocking=True,
-    )
-    mock_niko_home_control_connection.thermostats[
-        "thermostat-5"
-    ].set_mode.assert_called_once_with(3)
+    getattr(
+        mock_niko_home_control_connection.thermostats["thermostat-5"],
+        api_method,
+    ).assert_called_once_with(*api_parameters)
 
 
 async def test_updating(
