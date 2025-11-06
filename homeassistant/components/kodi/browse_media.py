83d5c00c44a95e457a6a8130314f42cc6e115a80
Charlie Rusbridger <charrus@users.noreply.github.com>
2025-11-06 19:18:07 +0000

    Fix wrong BrowseError module in Kode (#155971)

--- homeassistant/components/kodi/browse_media.py
+++ homeassistant/components/kodi/browse_media.py
@@ -221,7 +221,7 @@ async def library_payload(hass):
     for child in library_info.children:
         child.thumbnail = "https://brands.home-assistant.io/_/kodi/logo.png"
 
-    with contextlib.suppress(media_source.BrowseError):
+    with contextlib.suppress(BrowseError):
         item = await media_source.async_browse_media(
             hass, None, content_filter=media_source_content_filter
         )
