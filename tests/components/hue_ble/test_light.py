da12230c35e530c1401d0bba871aa226de085299
Harvey Lelliott <42912136+flip-dots@users.noreply.github.com>
2025-11-06 16:33:32 +0000

    Add tests for color modes

--- tests/components/hue_ble/test_light.py
+++ tests/components/hue_ble/test_light.py
@@ -1,14 +1,22 @@
 """Hue BLE light tests."""
 
-from unittest.mock import AsyncMock
+from unittest.mock import AsyncMock, PropertyMock, patch
+
+from HueBLE import HueBleLight
+import pytest
 
 from homeassistant.components.hue_ble.light import HueBLELight
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
     ATTR_COLOR_TEMP_KELVIN,
     ATTR_XY_COLOR,
+    ColorMode,
 )
 from homeassistant.util import color as color_util
+
+from . import TEST_DEVICE_MAC, TEST_DEVICE_NAME
+
+from tests.components.bluetooth import generate_ble_device
 
 
 async def test_lights() -> None:
@@ -32,3 +40,69 @@ async def test_lights() -> None:
 
     await light.async_turn_off()
     mock_light.set_power.assert_called_with(False)
+
+
+@pytest.mark.parametrize(
+    (
+        "mock_supports_brightness",
+        "mock_supports_color_temp",
+        "mock_supports_color_xy",
+        "mock_color_temp_mode",
+        "color_mode",
+    ),
+    [
+        (True, True, True, False, ColorMode.XY),
+        (True, True, True, True, ColorMode.COLOR_TEMP),
+        (True, True, False, True, ColorMode.COLOR_TEMP),
+        (True, False, False, False, ColorMode.BRIGHTNESS),
+        (False, False, False, False, ColorMode.ONOFF),
+    ],
+    ids=[
+        "xy_light_in_xy",
+        "xy_light_in_color_temp",
+        "ct_light_in_color_temp",
+        "brightness_only_light",
+        "on_off_only_light",
+    ],
+)
+async def test_color(
+    mock_supports_brightness: bool,
+    mock_supports_color_temp: bool,
+    mock_supports_color_xy: bool,
+    mock_color_temp_mode: bool,
+    color_mode: ColorMode,
+) -> None:
+    """Test the color mode behavior."""
+
+    light = HueBLELight(
+        HueBleLight(generate_ble_device(TEST_DEVICE_NAME, TEST_DEVICE_MAC))
+    )
+
+    with (
+        patch(
+            "homeassistant.components.hue_ble.light.HueBleLight.supports_on_off",
+            new_callable=PropertyMock,
+            return_value=True,
+        ),
+        patch(
+            "homeassistant.components.hue_ble.light.HueBleLight.supports_brightness",
+            new_callable=PropertyMock,
+            return_value=mock_supports_brightness,
+        ),
+        patch(
+            "homeassistant.components.hue_ble.light.HueBleLight.supports_colour_temp",
+            new_callable=PropertyMock,
+            return_value=mock_supports_color_temp,
+        ),
+        patch(
+            "homeassistant.components.hue_ble.light.HueBleLight.supports_colour_xy",
+            new_callable=PropertyMock,
+            return_value=mock_supports_color_xy,
+        ),
+        patch(
+            "homeassistant.components.hue_ble.light.HueBleLight.colour_temp_mode",
+            new_callable=PropertyMock,
+            return_value=mock_color_temp_mode,
+        ),
+    ):
+        assert light.color_mode is color_mode
