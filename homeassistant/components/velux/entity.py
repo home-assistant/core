bd1304bf523d1e115f5599a50703dee0bb1977fe
Wolfgang Miller-Reichling <wolfgang@miller-reichling.de>
2025-11-06 21:10:43 +0000

    cleanup registered callbacks before removing entities

--- homeassistant/components/velux/entity.py
+++ homeassistant/components/velux/entity.py
@@ -1,8 +1,9 @@
 """Support for VELUX KLF 200 devices."""
 
+from collections.abc import Awaitable, Callable
+
 from pyvlx import Node
 
-from homeassistant.core import callback
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
 
@@ -14,6 +15,7 @@ class VeluxEntity(Entity):
 
     _attr_should_poll = False
     _attr_has_entity_name = True
+    update_callback: Callable[["Node"], Awaitable[None]] | None = None
 
     def __init__(self, node: Node, config_entry_id: str) -> None:
         """Initialize the Velux device."""
@@ -37,16 +39,18 @@ class VeluxEntity(Entity):
             via_device=(DOMAIN, f"gateway_{config_entry_id}"),
         )
 
-    @callback
-    def async_register_callbacks(self):
-        """Register callbacks to update hass after device was changed."""
+    async def async_added_to_hass(self) -> None:
+        """Register callback and store reference for cleanup."""
 
-        async def after_update_callback(device):
+        async def after_update_callback(node) -> None:
             """Call after device was updated."""
             self.async_write_ha_state()
 
         self.node.register_device_updated_cb(after_update_callback)
+        self.update_callback = after_update_callback
 
-    async def async_added_to_hass(self) -> None:
-        """Store register state change callback."""
-        self.async_register_callbacks()
+    async def async_will_remove_from_hass(self) -> None:
+        """Clean up registered callbacks."""
+        if self.update_callback:
+            self.node.unregister_device_updated_cb(self.update_callback)
+            self.update_callback = None
