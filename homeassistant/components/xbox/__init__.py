ccf181feb99a8e020c4742c9f07f6a9401dfb3f5
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-11-07 19:06:46 +0000

    Migrate friends in Xbox integration to subentries

--- homeassistant/components/xbox/__init__.py
+++ homeassistant/components/xbox/__init__.py
@@ -4,9 +4,10 @@ from __future__ import annotations
 
 import logging
 
+from homeassistant.config_entries import ConfigSubentry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers import config_validation as cv, device_registry as dr
 
 from .const import DOMAIN
 from .coordinator import (
@@ -41,8 +42,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: XboxConfigEntry) -> bool
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    await async_migrate_unique_id(hass, entry)
+    await async_migrate_config_entry(hass, entry)
+
+    entry.async_on_unload(entry.add_update_listener(_async_update_listener))
+
     return True
+
+
+async def _async_update_listener(hass: HomeAssistant, entry: XboxConfigEntry) -> None:
+    """Handle update."""
+    await hass.config_entries.async_reload(entry.entry_id)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: XboxConfigEntry) -> bool:
@@ -51,24 +60,48 @@ async def async_unload_entry(hass: HomeAssistant, entry: XboxConfigEntry) -> boo
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
 
-async def async_migrate_unique_id(hass: HomeAssistant, entry: XboxConfigEntry) -> bool:
+async def async_migrate_config_entry(
+    hass: HomeAssistant, entry: XboxConfigEntry
+) -> bool:
     """Migrate config entry.
 
     Migration requires runtime data
     """
+    coordinator = entry.runtime_data.status
 
     if entry.version == 1 and entry.minor_version < 2:
         # Migrate unique_id from `xbox` to account xuid and
         # change generic entry name to user's gamertag
-        coordinator = entry.runtime_data.status
         xuid = coordinator.client.xuid
         gamertag = coordinator.data.presence[xuid].gamertag
 
-        return hass.config_entries.async_update_entry(
+        hass.config_entries.async_update_entry(
             entry,
             unique_id=xuid,
             title=(gamertag if entry.title == "Home Assistant Cloud" else entry.title),
             minor_version=2,
         )
+    if entry.version == 1 and entry.minor_version < 3:
+        # Migrate favorite friends to friend subentries
+        dev_reg = dr.async_get(hass)
+        for friend in coordinator.data.presence.values():
+            if not friend.is_favorite:
+                continue
+            subentry = ConfigSubentry(
+                subentry_type="friend",
+                title=friend.gamertag,
+                unique_id=friend.xuid,
+                data={},  # type: ignore[arg-type]
+            )
+            hass.config_entries.async_add_subentry(entry, subentry)
 
+            if device := dev_reg.async_get_device({(DOMAIN, friend.xuid)}):
+                dev_reg.async_update_device(
+                    device.id,
+                    remove_config_entry_id=entry.entry_id,
+                    add_config_subentry_id=subentry.subentry_id,
+                    add_config_entry_id=entry.entry_id,
+                )
+        hass.config_entries.async_update_entry(entry, minor_version=3)
+        hass.config_entries.async_schedule_reload(entry.entry_id)
     return True
