d768929391095bd4123e34a6c2c1a15281b9ace1
Eric Hylands <eric.hylands@gmail.com>
2025-11-07 20:29:36 -0500

    Enable BG Panels Night Mode
    Enable Bosch B/G Banel Night Mode

--- homeassistant/components/bosch_alarm/alarm_control_panel.py
+++ homeassistant/components/bosch_alarm/alarm_control_panel.py
@@ -36,6 +36,17 @@ async def async_setup_entry(
 
 PARALLEL_UPDATES = 0
 
+BG_PANELS = {
+    "D7412GV4",
+    "D9412GV4",
+    "B3512 (US1B)",
+    "B4512 (US1B)",
+    "B5512 (US1B)",
+    "B6512 (US1B)",
+    "B8512G (US1A)",
+    "B9512G (US1A)",
+}
+
 
 class AreaAlarmControlPanel(BoschAlarmAreaEntity, AlarmControlPanelEntity):
     """An alarm control panel entity for a bosch alarm panel."""
@@ -45,6 +56,7 @@ class AreaAlarmControlPanel(BoschAlarmAreaEntity, AlarmControlPanelEntity):
         AlarmControlPanelEntityFeature.ARM_HOME
         | AlarmControlPanelEntityFeature.ARM_AWAY
     )
+
     _attr_code_arm_required = False
     _attr_name = None
 
@@ -52,6 +64,10 @@ class AreaAlarmControlPanel(BoschAlarmAreaEntity, AlarmControlPanelEntity):
         """Initialise a Bosch Alarm control panel entity."""
         super().__init__(panel, area_id, unique_id, True, False, True)
         self._attr_unique_id = self._area_unique_id
+
+        # Enable ARM_NIGHT for B/G Panels
+        if self.panel.model in BG_PANELS:
+            self._attr_supported_features |= AlarmControlPanelEntityFeature.ARM_NIGHT
 
     @property
     def alarm_state(self) -> AlarmControlPanelState | None:
@@ -65,9 +81,14 @@ class AreaAlarmControlPanel(BoschAlarmAreaEntity, AlarmControlPanelEntity):
         if self._area.is_pending():
             return AlarmControlPanelState.PENDING
         if self._area.is_part_armed():
+            # For B/G Panels, distinguish between Arm Home (Part On Delay)
+            # and Arm Night (Part On Instant)
+            if self._area.status == 0x02:  # Part On Instant
+                return AlarmControlPanelState.ARMED_NIGHT
             return AlarmControlPanelState.ARMED_HOME
         if self._area.is_all_armed():
             return AlarmControlPanelState.ARMED_AWAY
+
         return None
 
     async def async_alarm_disarm(self, code: str | None = None) -> None:
@@ -76,7 +97,11 @@ class AreaAlarmControlPanel(BoschAlarmAreaEntity, AlarmControlPanelEntity):
 
     async def async_alarm_arm_home(self, code: str | None = None) -> None:
         """Send arm home command."""
-        await self.panel.area_arm_part(self._area_id)
+        await self.panel.area_arm_part(self._area_id, delay=True)
+
+    async def async_alarm_arm_night(self, code: str | None = None) -> None:
+        """Send arm night command for B/G Panels."""
+        await self.panel.area_arm_part(self._area_id, delay=False)
 
     async def async_alarm_arm_away(self, code: str | None = None) -> None:
         """Send arm away command."""
