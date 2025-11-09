f53c581845cc7461cf8cc1f07b2144f0e5202868
Manu <4445816+tr4nt0r@users.noreply.github.com>
2025-11-09 19:28:11 +0100

    Inject httpx.AsyncClient session in Xbox integration (#156172)

--- tests/components/xbox/conftest.py
+++ tests/components/xbox/conftest.py
@@ -86,25 +86,8 @@ def mock_authentication_manager() -> Generator[AsyncMock]:
         yield client
 
 
-@pytest.fixture(name="signed_session")
-def mock_signed_session() -> Generator[AsyncMock]:
-    """Mock xbox-webapi SignedSession."""
-
-    with (
-        patch(
-            "homeassistant.components.xbox.coordinator.SignedSession", autospec=True
-        ) as mock_client,
-        patch(
-            "homeassistant.components.xbox.config_flow.SignedSession", new=mock_client
-        ),
-    ):
-        client = mock_client.return_value
-
-        yield client
-
-
 @pytest.fixture(name="xbox_live_client")
-def mock_xbox_live_client(signed_session) -> Generator[AsyncMock]:
+def mock_xbox_live_client() -> Generator[AsyncMock]:
     """Mock xbox-webapi XboxLiveClient."""
 
     with (
