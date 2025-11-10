7a2024dd856023f57cbc06cf37134cca615fa6f6
Matthew Gibson <matthew.gibzon@gmail.com>
2025-11-10 19:12:12 +0000

    Changed update time to 60 seconds

--- homeassistant/components/ptdevices/const.py
+++ homeassistant/components/ptdevices/const.py
@@ -4,4 +4,4 @@ from datetime import timedelta
 
 DOMAIN = "ptdevices"
 DEFAULT_URL = "https://www.ptdevices.com/token/v1/device/"
-UPDATE_INTERVAL = timedelta(seconds=30)
+UPDATE_INTERVAL = timedelta(seconds=60)
