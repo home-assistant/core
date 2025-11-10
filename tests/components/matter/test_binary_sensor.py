511e54efcb44c93e25c752a32c79a809b0aeb951
Ludovic BOUÉ <lboue@users.noreply.github.com>
2025-11-10 17:35:30 +0000

    Add tests for dishwasher alarm states in Matter binary sensor

--- tests/components/matter/test_binary_sensor.py
+++ tests/components/matter/test_binary_sensor.py
@@ -270,10 +270,27 @@ async def test_dishwasher_alarm(
     state = hass.states.get("binary_sensor.dishwasher_door_alarm")
     assert state
 
+    # set DoorAlarm alarm
     set_node_attribute(matter_node, 1, 93, 2, 4)
     await trigger_subscription_callback(hass, matter_client)
 
     state = hass.states.get("binary_sensor.dishwasher_door_alarm")
+    assert state
+    assert state.state == "on"
+
+    # clear DoorAlarm alarm
+    set_node_attribute(matter_node, 1, 93, 2, 0)
+    await trigger_subscription_callback(hass, matter_client)
+
+    state = hass.states.get("binary_sensor.dishwasher_inflow_alarm")
+    assert state
+    assert state.state == "off"
+
+    # set InflowError alarm
+    set_node_attribute(matter_node, 1, 93, 2, 1)
+    await trigger_subscription_callback(hass, matter_client)
+
+    state = hass.states.get("binary_sensor.dishwasher_inflow_alarm")
     assert state
     assert state.state == "on"
 
