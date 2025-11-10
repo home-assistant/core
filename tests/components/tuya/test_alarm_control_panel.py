d341b986ce5a9ef072cc08928ad845b117d06615
epenet <6771947+epenet@users.noreply.github.com>
2025-11-10 20:27:29 +0000

    Use dpcode_wrapper in tuya alarm control panel platform

--- tests/components/tuya/test_alarm_control_panel.py
+++ tests/components/tuya/test_alarm_control_panel.py
@@ -2,14 +2,22 @@
 
 from __future__ import annotations
 
+from typing import Any
 from unittest.mock import patch
 
 import pytest
 from syrupy.assertion import SnapshotAssertion
 from tuya_sharing import CustomerDevice, Manager
 
-from homeassistant.components.alarm_control_panel import AlarmControlPanelState
-from homeassistant.const import Platform
+from homeassistant.components.alarm_control_panel import (
+    DOMAIN as ALARM_DOMAIN,
+    SERVICE_ALARM_ARM_AWAY,
+    SERVICE_ALARM_ARM_HOME,
+    SERVICE_ALARM_DISARM,
+    SERVICE_ALARM_TRIGGER,
+    AlarmControlPanelState,
+)
+from homeassistant.const import ATTR_ENTITY_ID, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
@@ -31,6 +39,57 @@ async def test_platform_setup_and_discovery(
     await initialize_entry(hass, mock_manager, mock_config_entry, mock_devices)
 
     await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
+
+
+@patch("homeassistant.components.tuya.PLATFORMS", [Platform.ALARM_CONTROL_PANEL])
+@pytest.mark.parametrize(
+    ("mock_device_code", "service", "command"),
+    [
+        (
+            "mal_gyitctrjj1kefxp2",
+            SERVICE_ALARM_ARM_AWAY,
+            {"code": "master_mode", "value": "arm"},
+        ),
+        (
+            "mal_gyitctrjj1kefxp2",
+            SERVICE_ALARM_ARM_HOME,
+            {"code": "master_mode", "value": "home"},
+        ),
+        (
+            "mal_gyitctrjj1kefxp2",
+            SERVICE_ALARM_DISARM,
+            {"code": "master_mode", "value": "disarmed"},
+        ),
+        (
+            "mal_gyitctrjj1kefxp2",
+            SERVICE_ALARM_TRIGGER,
+            {"code": "master_mode", "value": "sos"},
+        ),
+    ],
+)
+async def test_service(
+    hass: HomeAssistant,
+    mock_manager: Manager,
+    mock_config_entry: MockConfigEntry,
+    mock_device: CustomerDevice,
+    service: str,
+    command: dict[str, Any],
+) -> None:
+    """Test service."""
+    entity_id = "alarm_control_panel.multifunction_alarm"
+    await initialize_entry(hass, mock_manager, mock_config_entry, mock_device)
+
+    state = hass.states.get(entity_id)
+    assert state is not None, f"{entity_id} does not exist"
+    await hass.services.async_call(
+        ALARM_DOMAIN,
+        service,
+        {
+            ATTR_ENTITY_ID: entity_id,
+        },
+        blocking=True,
+    )
+    mock_manager.send_commands.assert_called_once_with(mock_device.id, [command])
 
 
 @pytest.mark.parametrize(
