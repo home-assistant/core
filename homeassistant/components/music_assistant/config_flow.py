af9c18d72470598a57ffbd205aa8017e0e3c6f52
Artur Pragacz <49985303+arturpragacz@users.noreply.github.com>
2025-11-11 16:06:54 +0100

    Improve code quality of music assistant config flow (#156263)

--- homeassistant/components/music_assistant/config_flow.py
+++ homeassistant/components/music_assistant/config_flow.py
@@ -13,7 +13,7 @@ from music_assistant_client.exceptions import (
 from music_assistant_models.api import ServerInfoMessage
 import voluptuous as vol
 
-from homeassistant.config_entries import SOURCE_IGNORE, ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_URL
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import aiohttp_client
@@ -21,21 +21,14 @@ from homeassistant.helpers.service_info.zeroconf import ZeroconfServiceInfo
 
 from .const import DOMAIN, LOGGER
 
-DEFAULT_URL = "http://mass.local:8095"
 DEFAULT_TITLE = "Music Assistant"
+DEFAULT_URL = "http://mass.local:8095"
 
 
-def get_manual_schema(user_input: dict[str, Any]) -> vol.Schema:
-    """Return a schema for the manual step."""
-    default_url = user_input.get(CONF_URL, DEFAULT_URL)
-    return vol.Schema(
-        {
-            vol.Required(CONF_URL, default=default_url): str,
-        }
-    )
+STEP_USER_SCHEMA = vol.Schema({vol.Required(CONF_URL): str})
 
 
-async def get_server_info(hass: HomeAssistant, url: str) -> ServerInfoMessage:
+async def _get_server_info(hass: HomeAssistant, url: str) -> ServerInfoMessage:
     """Validate the user input allows us to connect."""
     async with MusicAssistantClient(
         url, aiohttp_client.async_get_clientsession(hass)
@@ -52,25 +45,17 @@ class MusicAssistantConfigFlow(ConfigFlow, domain=DOMAIN):
 
     def __init__(self) -> None:
         """Set up flow instance."""
-        self.server_info: ServerInfoMessage | None = None
+        self.url: str | None = None
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle a manual configuration."""
         errors: dict[str, str] = {}
+
         if user_input is not None:
             try:
-                self.server_info = await get_server_info(
-                    self.hass, user_input[CONF_URL]
-                )
-                await self.async_set_unique_id(
-                    self.server_info.server_id, raise_on_progress=False
-                )
-                self._abort_if_unique_id_configured(
-                    updates={CONF_URL: user_input[CONF_URL]},
-                    reload_on_update=True,
-                )
+                server_info = await _get_server_info(self.hass, user_input[CONF_URL])
             except CannotConnect:
                 errors["base"] = "cannot_connect"
             except InvalidServerVersion:
@@ -79,68 +64,49 @@ class MusicAssistantConfigFlow(ConfigFlow, domain=DOMAIN):
                 LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
             else:
-                return self.async_create_entry(
-                    title=DEFAULT_TITLE,
-                    data={
-                        CONF_URL: user_input[CONF_URL],
-                    },
+                await self.async_set_unique_id(
+                    server_info.server_id, raise_on_progress=False
+                )
+                self._abort_if_unique_id_configured(
+                    updates={CONF_URL: user_input[CONF_URL]}
                 )
 
-            return self.async_show_form(
-                step_id="user", data_schema=get_manual_schema(user_input), errors=errors
-            )
+                return self.async_create_entry(
+                    title=DEFAULT_TITLE,
+                    data={CONF_URL: user_input[CONF_URL]},
+                )
 
-        return self.async_show_form(step_id="user", data_schema=get_manual_schema({}))
+        suggested_values = user_input
+        if suggested_values is None:
+            suggested_values = {CONF_URL: DEFAULT_URL}
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=self.add_suggested_values_to_schema(
+                STEP_USER_SCHEMA, suggested_values
+            ),
+            errors=errors,
+        )
 
     async def async_step_zeroconf(
         self, discovery_info: ZeroconfServiceInfo
     ) -> ConfigFlowResult:
-        """Handle a discovered Mass server.
-
-        This flow is triggered by the Zeroconf component. It will check if the
-        host is already configured and delegate to the import step if not.
-        """
-        # abort if discovery info is not what we expect
-        if "server_id" not in discovery_info.properties:
-            return self.async_abort(reason="missing_server_id")
-
-        self.server_info = ServerInfoMessage.from_dict(discovery_info.properties)
-        await self.async_set_unique_id(self.server_info.server_id)
-
-        # Check if we already have a config entry for this server_id
-        existing_entry = self.hass.config_entries.async_entry_for_domain_unique_id(
-            DOMAIN, self.server_info.server_id
-        )
-
-        if existing_entry:
-            # If the entry was ignored or disabled, don't make any changes
-            if existing_entry.source == SOURCE_IGNORE or existing_entry.disabled_by:
-                return self.async_abort(reason="already_configured")
-
-            # Test connectivity to the current URL first
-            current_url = existing_entry.data[CONF_URL]
-            try:
-                await get_server_info(self.hass, current_url)
-                # Current URL is working, no need to update
-                return self.async_abort(reason="already_configured")
-            except CannotConnect:
-                # Current URL is not working, update to the discovered URL
-                # and continue to discovery confirm
-                self.hass.config_entries.async_update_entry(
-                    existing_entry,
-                    data={**existing_entry.data, CONF_URL: self.server_info.base_url},
-                )
-                # Schedule reload since URL changed
-                self.hass.config_entries.async_schedule_reload(existing_entry.entry_id)
-        else:
-            # No existing entry, proceed with normal flow
-            self._abort_if_unique_id_configured()
-
-        # Test connectivity to the discovered URL
+        """Handle a zeroconf discovery for a Music Assistant server."""
         try:
-            await get_server_info(self.hass, self.server_info.base_url)
+            server_info = ServerInfoMessage.from_dict(discovery_info.properties)
+        except LookupError:
+            return self.async_abort(reason="invalid_discovery_info")
+
+        self.url = server_info.base_url
+
+        await self.async_set_unique_id(server_info.server_id)
+        self._abort_if_unique_id_configured(updates={CONF_URL: self.url})
+
+        try:
+            await _get_server_info(self.hass, self.url)
         except CannotConnect:
             return self.async_abort(reason="cannot_connect")
+
         return await self.async_step_discovery_confirm()
 
     async def async_step_discovery_confirm(
@@ -148,16 +114,16 @@ class MusicAssistantConfigFlow(ConfigFlow, domain=DOMAIN):
     ) -> ConfigFlowResult:
         """Handle user-confirmation of discovered server."""
         if TYPE_CHECKING:
-            assert self.server_info is not None
+            assert self.url is not None
+
         if user_input is not None:
             return self.async_create_entry(
                 title=DEFAULT_TITLE,
-                data={
-                    CONF_URL: self.server_info.base_url,
-                },
+                data={CONF_URL: self.url},
             )
+
         self._set_confirm_only()
         return self.async_show_form(
             step_id="discovery_confirm",
-            description_placeholders={"url": self.server_info.base_url},
+            description_placeholders={"url": self.url},
         )
