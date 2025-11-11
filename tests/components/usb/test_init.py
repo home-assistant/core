a5f193e2460a17a31c4611d8bb410a1eae0a6afe
puddly <32534428+puddly@users.noreply.github.com>
2025-11-11 13:51:50 -0500

    Remove invalid `test_resolve_serial_by_id` test `/dev/serial/by-id/` lookups are done internally by `scan_serial_ports`, there should be no opportunity for non-unique serial port paths to make it through and thus no need for the USB integration to do it a second time.

--- tests/components/usb/test_init.py
+++ tests/components/usb/test_init.py
@@ -978,48 +978,6 @@ async def test_cancel_initial_scan_callback(
 
 
 @pytest.mark.usefixtures("force_usb_polling_watcher")
-async def test_resolve_serial_by_id(
-    hass: HomeAssistant, hass_ws_client: WebSocketGenerator
-) -> None:
-    """Test the discovery data resolves to serial/by-id."""
-    new_usb = [{"domain": "test1", "vid": "3039", "pid": "3039"}]
-
-    mock_ports = [
-        USBDevice(
-            device=slae_sh_device.device,
-            vid="3039",
-            pid="3039",
-            serial_number=slae_sh_device.serial_number,
-            manufacturer=slae_sh_device.manufacturer,
-            description=slae_sh_device.description,
-        )
-    ]
-
-    with (
-        patch("homeassistant.components.usb.async_get_usb", return_value=new_usb),
-        patch_scanned_serial_ports(return_value=mock_ports),
-        patch(
-            "homeassistant.components.usb.get_serial_by_id",
-            return_value="/dev/serial/by-id/bla",
-        ),
-        patch.object(hass.config_entries.flow, "async_init") as mock_config_flow,
-    ):
-        assert await async_setup_component(hass, "usb", {"usb": {}})
-        await hass.async_block_till_done()
-        hass.bus.async_fire(EVENT_HOMEASSISTANT_STARTED)
-        await hass.async_block_till_done()
-        ws_client = await hass_ws_client(hass)
-        await ws_client.send_json({"id": 1, "type": "usb/scan"})
-        response = await ws_client.receive_json()
-        assert response["success"]
-        await hass.async_block_till_done()
-
-    assert len(mock_config_flow.mock_calls) == 1
-    assert mock_config_flow.mock_calls[0][1][0] == "test1"
-    assert mock_config_flow.mock_calls[0][2]["data"].device == "/dev/serial/by-id/bla"
-
-
-@pytest.mark.usefixtures("force_usb_polling_watcher")
 @pytest.mark.parametrize(
     "ports",
     [
