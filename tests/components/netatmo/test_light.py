50faf039695db990b274720f86a47c31ffcde761
Will Moss <wbmoss@gmail.com>
2025-11-11 13:37:56 -0800

    Improved error handling for oauth2 configuration in netatmo integration (#156207)

--- tests/components/netatmo/test_light.py
+++ tests/components/netatmo/test_light.py
@@ -129,7 +129,7 @@ async def test_setup_component_no_devices(hass: HomeAssistant, config_entry) -> 
         ) as mock_auth,
         patch("homeassistant.components.netatmo.data_handler.PLATFORMS", ["light"]),
         patch(
-            "homeassistant.helpers.config_entry_oauth2_flow.async_get_config_entry_implementation",
+            "homeassistant.components.netatmo.async_get_config_entry_implementation",
         ),
         patch(
             "homeassistant.components.netatmo.webhook_generate_url",
