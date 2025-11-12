a1a98dd495af0c14664fc8e18f32e950070db7b9
Tom Matheussen <tmatheussen@avia-gis.com>
2025-11-12 14:45:32 +0000

    Add initial common entity

---
+++ homeassistant/components/satel_integra/entity.py
@@ -0,0 +1,19 @@
+"""Satel Integra base entity."""
+
+from __future__ import annotations
+
+from satel_integra.satel_integra import AsyncSatel
+
+from homeassistant.helpers.entity import Entity
+
+
+class SatelIntegraEntity(Entity):
+    """Defines a base Satel Integra entity."""
+
+    _attr_should_poll = False
+    _attr_has_entity_name = True
+    _attr_name = None
+
+    def __init__(self, controller: AsyncSatel) -> None:
+        """Initialize the Satel Integra entity."""
+        self._satel = controller
