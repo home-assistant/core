85161c208ef0930d3a7caf4367b6f9066ab5817c
jlanchares <87146197+jlanchares@users.noreply.github.com>
2025-11-13 21:12:47 +0100

    Goodwe port502ftp support with PORT stored on config data. (#148628)
    Co-authored-by: starkillerOG <starkiller.og@gmail.com>

--- tests/components/goodwe/test_config_flow.py
+++ tests/components/goodwe/test_config_flow.py
@@ -1,8 +1,9 @@
 """Test the Goodwe config flow."""
 
-from unittest.mock import AsyncMock, patch
+from unittest.mock import MagicMock, patch
 
 from goodwe import InverterError
+from goodwe.const import GOODWE_UDP_PORT
 
 from homeassistant.components.goodwe.const import (
     CONF_MODEL_FAMILY,
@@ -10,24 +11,19 @@ from homeassistant.components.goodwe.const import (
     DOMAIN,
 )
 from homeassistant.config_entries import SOURCE_USER
-from homeassistant.const import CONF_HOST
+from homeassistant.const import CONF_HOST, CONF_PORT
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
+
+from .conftest import TEST_SERIAL
 
 from tests.common import MockConfigEntry
 
 TEST_HOST = "1.2.3.4"
-TEST_SERIAL = "123456789"
+TEST_PORT = GOODWE_UDP_PORT
 
 
-def mock_inverter():
-    """Get a mock object of the inverter."""
-    goodwe_inverter = AsyncMock()
-    goodwe_inverter.serial_number = TEST_SERIAL
-    return goodwe_inverter
-
-
-async def test_manual_setup(hass: HomeAssistant) -> None:
+async def test_manual_setup(hass: HomeAssistant, mock_inverter: MagicMock) -> None:
     """Test manually setting up."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": SOURCE_USER}
@@ -37,10 +33,6 @@ async def test_manual_setup(hass: HomeAssistant) -> None:
     assert not result["errors"]
 
     with (
-        patch(
-            "homeassistant.components.goodwe.config_flow.connect",
-            return_value=mock_inverter(),
-        ),
         patch(
             "homeassistant.components.goodwe.async_setup_entry", return_value=True
         ) as mock_setup_entry,
@@ -54,15 +46,20 @@ async def test_manual_setup(hass: HomeAssistant) -> None:
     assert result["title"] == DEFAULT_NAME
     assert result["data"] == {
         CONF_HOST: TEST_HOST,
-        CONF_MODEL_FAMILY: "AsyncMock",
+        CONF_PORT: TEST_PORT,
+        CONF_MODEL_FAMILY: "MagicMock",
     }
     assert len(mock_setup_entry.mock_calls) == 1
 
 
-async def test_manual_setup_already_exists(hass: HomeAssistant) -> None:
+async def test_manual_setup_already_exists(
+    hass: HomeAssistant, mock_inverter: MagicMock
+) -> None:
     """Test manually setting up and the device already exists."""
     entry = MockConfigEntry(
-        domain=DOMAIN, data={CONF_HOST: TEST_HOST}, unique_id=TEST_SERIAL
+        domain=DOMAIN,
+        data={CONF_HOST: TEST_HOST},
+        unique_id=TEST_SERIAL,
     )
     entry.add_to_hass(hass)
     result = await hass.config_entries.flow.async_init(
@@ -72,13 +69,7 @@ async def test_manual_setup_already_exists(hass: HomeAssistant) -> None:
     assert result["step_id"] == "user"
     assert not result["errors"]
 
-    with (
-        patch(
-            "homeassistant.components.goodwe.config_flow.connect",
-            return_value=mock_inverter(),
-        ),
-        patch("homeassistant.components.goodwe.async_setup_entry", return_value=True),
-    ):
+    with patch("homeassistant.components.goodwe.async_setup_entry", return_value=True):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"], {CONF_HOST: TEST_HOST}
         )
--- tests/components/goodwe/test_init.py
+++ tests/components/goodwe/test_init.py
@@ -1,9 +1,6 @@
-"""Test the GoodWe diagnostics."""
+"""Test the GoodWe initialization."""
 
 from unittest.mock import MagicMock
-
-from syrupy.assertion import SnapshotAssertion
-from syrupy.filters import props
 
 from homeassistant.components.goodwe import CONF_MODEL_FAMILY, DOMAIN
 from homeassistant.const import CONF_HOST, CONF_PORT
@@ -13,30 +10,27 @@ from homeassistant.setup import async_setup_component
 from .conftest import TEST_HOST, TEST_PORT
 
 from tests.common import MockConfigEntry
-from tests.components.diagnostics import get_diagnostics_for_config_entry
-from tests.typing import ClientSessionGenerator
 
 
-async def test_entry_diagnostics(
+async def test_migration(
     hass: HomeAssistant,
-    hass_client: ClientSessionGenerator,
-    snapshot: SnapshotAssertion,
     mock_inverter: MagicMock,
 ) -> None:
-    """Test config entry diagnostics."""
+    """Test config entry migration."""
 
     config_entry = MockConfigEntry(
-        version=2,
+        version=1,
         domain=DOMAIN,
         data={
             CONF_HOST: TEST_HOST,
-            CONF_PORT: TEST_PORT,
-            CONF_MODEL_FAMILY: "ET",
+            CONF_MODEL_FAMILY: "MagicMock",
         },
         entry_id="3bd2acb0e4f0476d40865546d0d91921",
     )
     config_entry.add_to_hass(hass)
     assert await async_setup_component(hass, DOMAIN, {})
 
-    result = await get_diagnostics_for_config_entry(hass, hass_client, config_entry)
-    assert result == snapshot(exclude=props("created_at", "modified_at"))
+    assert config_entry.version == 2
+    assert config_entry.data[CONF_HOST] == TEST_HOST
+    assert config_entry.data[CONF_MODEL_FAMILY] == "MagicMock"
+    assert config_entry.data[CONF_PORT] == TEST_PORT
