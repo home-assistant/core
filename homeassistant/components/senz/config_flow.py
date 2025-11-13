35dc5b4368ba3f7904159143be3a0dfdd034639f
Åke Strandberg <ake@strandberg.eu>
2025-11-13 15:28:45 +0100

    Add reauthentication flow and tests to senz (#156534)

--- homeassistant/components/senz/config_flow.py
+++ homeassistant/components/senz/config_flow.py
@@ -1,10 +1,12 @@
 """Config flow for nVent RAYCHEM SENZ."""
 
+from collections.abc import Mapping
 import logging
+from typing import Any
 
 import jwt
 
-from homeassistant.config_entries import ConfigFlowResult
+from homeassistant.config_entries import SOURCE_REAUTH, ConfigFlowResult
 from homeassistant.helpers import config_entry_oauth2_flow
 
 from .const import DOMAIN
@@ -29,6 +31,22 @@ class OAuth2FlowHandler(
         """Extra data that needs to be appended to the authorize url."""
         return {"scope": "restapi offline_access"}
 
+    async def async_step_reauth(
+        self, entry_data: Mapping[str, Any]
+    ) -> ConfigFlowResult:
+        """Perform reauth upon an API authentication error."""
+
+        return await self.async_step_reauth_confirm()
+
+    async def async_step_reauth_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Dialog that informs the user that reauth is required."""
+        if user_input is None:
+            return self.async_show_form(step_id="reauth_confirm")
+
+        return await self.async_step_user()
+
     async def async_oauth_create_entry(self, data: dict) -> ConfigFlowResult:
         """Create or update the config entry."""
 
@@ -37,6 +55,12 @@ class OAuth2FlowHandler(
         )
         uid = token["sub"]
         await self.async_set_unique_id(uid)
+
+        if self.source == SOURCE_REAUTH:
+            self._abort_if_unique_id_mismatch(reason="account_mismatch")
+            return self.async_update_reload_and_abort(
+                self._get_reauth_entry(), data=data
+            )
 
         self._abort_if_unique_id_configured()
         return await super().async_oauth_create_entry(data)
