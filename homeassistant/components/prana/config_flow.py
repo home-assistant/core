8bf4d104df1bab43cd65f717d20a714a47c555bf
Prana Developer <devprana18@gmail.com>
2025-11-14 17:34:54 +0200

    remove others platfrom and stay switch platform

--- homeassistant/components/prana/config_flow.py
+++ homeassistant/components/prana/config_flow.py
@@ -12,7 +12,6 @@ from homeassistant.const import CONF_HOST, CONF_NAME
 from homeassistant.helpers.service_info.zeroconf import ZeroconfServiceInfo
 
 from .const import CONF_CONFIG, CONF_MDNS, DOMAIN
-from .tools import Tools
 
 SERVICE_TYPE = "_prana._tcp.local."
 
@@ -115,13 +114,3 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         # Empty form -> shows only a Submit button
         return self.async_show_form(step_id="confirm")
-
-    def _get_name_from_config(self, config: str) -> str:
-        bytes_ = Tools.hex_string_to_int_list(config)
-        _LOGGER.debug("Byte array from config: %s", bytes_)
-        data = bytearray(bytes_[8:32])
-        _LOGGER.debug("Name bytes from config: %s", data)
-        name = data.decode("ascii").rstrip("\x00")
-        if name.startswith("PRN"):
-            name = name[7:]
-        return name
