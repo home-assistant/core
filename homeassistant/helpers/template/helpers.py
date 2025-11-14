4fee70fe4807253f619ca7b69e3da936a9bc0174
Franck Nijhof <git@frenck.dev>
2025-11-14 15:35:09 +0000

    Process review comments

--- homeassistant/helpers/template/helpers.py
+++ homeassistant/helpers/template/helpers.py
@@ -34,6 +34,8 @@ def resolve_area_id(hass: HomeAssistant, lookup_value: Any) -> str | None:
     Returns the area ID or None if not found.
     """
     area_reg = ar.async_get(hass)
+    dev_reg = dr.async_get(hass)
+    ent_reg = er.async_get(hass)
     lookup_str = str(lookup_value)
 
     # Check if it's an area name
@@ -54,19 +56,15 @@ def resolve_area_id(hass: HomeAssistant, lookup_value: Any) -> str | None:
     except vol.Invalid:
         pass
     else:
-        ent_reg = er.async_get(hass)
         if entity := ent_reg.async_get(lookup_value):
             # If entity has an area ID, return that
             if entity.area_id:
                 return entity.area_id
             # If entity has a device ID, return the area ID for the device
-            if entity.device_id:
-                dev_reg = dr.async_get(hass)
-                if device := dev_reg.async_get(entity.device_id):
-                    return device.area_id
+            if entity.device_id and (device := dev_reg.async_get(entity.device_id)):
+                return device.area_id
 
     # Check if it's a device ID
-    dev_reg = dr.async_get(hass)
     if device := dev_reg.async_get(lookup_value):
         return device.area_id
 
