7067913fb718f95214552801b00d0925b9a7ab36
Paul <git@cavill.co.nz>
2025-11-17 10:47:51 +0000

    Add Test

--- tests/components/icloud/const.py
+++ tests/components/icloud/const.py
@@ -10,6 +10,8 @@ from homeassistant.components.icloud.const import (
 )
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 
+FIRST_NAME = "user"
+LAST_NAME = "name"
 USERNAME = "username@me.com"
 USERNAME_2 = "second_username@icloud.com"
 PASSWORD = "password"
@@ -17,6 +19,30 @@ PASSWORD_2 = "second_password"
 WITH_FAMILY = True
 MAX_INTERVAL = 15
 GPS_ACCURACY_THRESHOLD = 250
+
+MEMBER_1_FIRST_NAME = "John"
+MEMBER_1_LAST_NAME = "TRAVOLTA"
+MEMBER_1_FULL_NAME = MEMBER_1_FIRST_NAME + " " + MEMBER_1_LAST_NAME
+MEMBER_1_PERSON_ID = (MEMBER_1_FIRST_NAME + MEMBER_1_LAST_NAME).lower()
+MEMBER_1_APPLE_ID = MEMBER_1_PERSON_ID + "@icloud.com"
+
+USER_INFO = {
+    "accountFormatter": 0,
+    "firstName": FIRST_NAME,
+    "lastName": LAST_NAME,
+    "membersInfo": {
+        MEMBER_1_PERSON_ID: {
+            "accountFormatter": 0,
+            "firstName": MEMBER_1_FIRST_NAME,
+            "lastName": MEMBER_1_LAST_NAME,
+            "deviceFetchStatus": "DONE",
+            "useAuthWidget": True,
+            "isHSA": True,
+            "appleId": MEMBER_1_APPLE_ID,
+        }
+    },
+    "hasMembers": True,
+}
 
 MOCK_CONFIG = {
     CONF_USERNAME: USERNAME,
