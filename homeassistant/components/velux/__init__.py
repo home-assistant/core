9c14f37b1cb36a71b1d82e760912dc96647cb7c0
Wolfgang Miller-Reichling <wolfgang@miller-reichling.de>
2025-11-18 07:47:46 +0000

    raise when setup fails, fulfill test-before-setup

--- homeassistant/components/velux/__init__.py
+++ homeassistant/components/velux/__init__.py
@@ -12,6 +12,7 @@ from homeassistant.const import (
     EVENT_HOMEASSISTANT_STOP,
 )
 from homeassistant.core import HomeAssistant, ServiceCall
+from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import device_registry as dr, issue_registry as ir
 
 from .const import DOMAIN, LOGGER, PLATFORMS
@@ -25,14 +26,19 @@ async def async_setup_entry(hass: HomeAssistant, entry: VeluxConfigEntry) -> boo
     password = entry.data[CONF_PASSWORD]
     pyvlx = PyVLX(host=host, password=password)
 
-    LOGGER.debug("Velux interface started")
+    LOGGER.debug(f"Setting up Velux gateway {host}")
     try:
+        LOGGER.debug(f"Retrieving scenes from {host}")
         await pyvlx.load_scenes()
+        LOGGER.debug(f"Retrieving nodes from {host}")
         await pyvlx.load_nodes()
-    except PyVLXException as ex:
-        LOGGER.exception("Can't connect to velux interface: %s", ex)
-        return False
+    except (OSError, PyVLXException) as ex:
+        # Defer setup and retry later as the bridge is not ready/available
+        raise ConfigEntryNotReady(
+            f"Error connecting to Velux gateway at {host}"
+        ) from ex
 
+    LOGGER.debug(f"Velux connection to {host} successful")
     entry.runtime_data = pyvlx
 
     connections = None
---
+++ tests/components/velux/test_init.py
@@ -0,0 +1,35 @@
+"""Tests for Velux integration initialization and retry behavior.
+
+These tests verify that setup retries (ConfigEntryNotReady) are triggered
+when scene or node loading fails.
+"""
+
+from __future__ import annotations
+
+from pyvlx.exception import PyVLXException
+
+from homeassistant.config_entries import ConfigEntryState
+from homeassistant.core import HomeAssistant
+
+from tests.common import AsyncMock, ConfigEntry
+
+
+async def test_setup_retry_on_nodes_failure(
+    mock_config_entry: ConfigEntry, hass: HomeAssistant, mock_pyvlx: AsyncMock
+) -> None:
+    """Test that a failure loading nodes triggers setup retry.
+
+    The integration loads scenes first, then nodes. If loading raises PyVLXException,
+    (which could have a multitude of reasons, unfortunately there are no specialized
+    exceptions that give a reason), the ConfigEntry should enter SETUP_RETRY.
+    """
+
+    mock_pyvlx.load_nodes.side_effect = PyVLXException("nodes boom")
+    mock_config_entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert mock_config_entry.state is ConfigEntryState.SETUP_RETRY
+    mock_pyvlx.load_scenes.assert_awaited_once()
+    mock_pyvlx.load_nodes.assert_awaited_once()
