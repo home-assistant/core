f3270c56b8ba8d9fef4c0e63666b29d6b6e4f7bb
Erik <erik@montnemery.com>
2025-11-19 09:27:18 +0100

    Refactor parametrize_trigger_states test helper

--- tests/components/assist_satellite/test_trigger.py
+++ tests/components/assist_satellite/test_trigger.py
@@ -8,6 +8,7 @@ from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.setup import async_setup_component
 
 from tests.components import (
+    StateDescription,
     arm_trigger,
     parametrize_target_entities,
     parametrize_trigger_states,
@@ -66,7 +67,7 @@ async def test_assist_satellite_state_trigger_behavior_any(
     entity_id: str,
     entities_in_target: int,
     trigger: str,
-    states: list[tuple[str, int]],
+    states: list[StateDescription],
 ) -> None:
     """Test that the assist satellite state trigger fires when any assist satellite state changes to a specific state."""
     await async_setup_component(hass, "assist_satellite", {})
@@ -75,24 +76,24 @@ async def test_assist_satellite_state_trigger_behavior_any(
 
     # Set all assist satellites, including the tested one, to the initial state
     for eid in target_assist_satellites:
-        set_or_remove_state(hass, eid, states[0][0])
+        set_or_remove_state(hass, eid, states[0]["state"])
         await hass.async_block_till_done()
 
     await arm_trigger(hass, trigger, {}, trigger_target_config)
 
-    for state, expected_calls in states[1:]:
-        set_or_remove_state(hass, entity_id, state)
+    for state in states[1:]:
+        set_or_remove_state(hass, entity_id, state["state"])
         await hass.async_block_till_done()
-        assert len(service_calls) == expected_calls
+        assert len(service_calls) == state["count"]
         for service_call in service_calls:
             assert service_call.data[CONF_ENTITY_ID] == entity_id
         service_calls.clear()
 
         # Check if changing other assist satellites also triggers
         for other_entity_id in other_entity_ids:
-            set_or_remove_state(hass, other_entity_id, state)
+            set_or_remove_state(hass, other_entity_id, state["state"])
             await hass.async_block_till_done()
-        assert len(service_calls) == (entities_in_target - 1) * expected_calls
+        assert len(service_calls) == (entities_in_target - 1) * state["count"]
         service_calls.clear()
 
 
@@ -133,7 +134,7 @@ async def test_assist_satellite_state_trigger_behavior_first(
     entity_id: str,
     entities_in_target: int,
     trigger: str,
-    states: list[tuple[str, int, list[str]]],
+    states: list[StateDescription],
 ) -> None:
     """Test that the assist satellite state trigger fires when the first assist satellite changes to a specific state."""
     await async_setup_component(hass, "assist_satellite", {})
@@ -142,22 +143,22 @@ async def test_assist_satellite_state_trigger_behavior_first(
 
     # Set all assist satellites, including the tested one, to the initial state
     for eid in target_assist_satellites:
-        set_or_remove_state(hass, eid, states[0][0])
+        set_or_remove_state(hass, eid, states[0]["state"])
         await hass.async_block_till_done()
 
     await arm_trigger(hass, trigger, {"behavior": "first"}, trigger_target_config)
 
-    for state, expected_calls in states[1:]:
-        set_or_remove_state(hass, entity_id, state)
+    for state in states[1:]:
+        set_or_remove_state(hass, entity_id, state["state"])
         await hass.async_block_till_done()
-        assert len(service_calls) == expected_calls
+        assert len(service_calls) == state["count"]
         for service_call in service_calls:
             assert service_call.data[CONF_ENTITY_ID] == entity_id
         service_calls.clear()
 
         # Triggering other assist satellites should not cause the trigger to fire again
         for other_entity_id in other_entity_ids:
-            set_or_remove_state(hass, other_entity_id, state)
+            set_or_remove_state(hass, other_entity_id, state["state"])
             await hass.async_block_till_done()
         assert len(service_calls) == 0
 
@@ -199,7 +200,7 @@ async def test_assist_satellite_state_trigger_behavior_last(
     entity_id: str,
     entities_in_target: int,
     trigger: str,
-    states: list[tuple[str, int]],
+    states: list[StateDescription],
 ) -> None:
     """Test that the assist_satellite state trigger fires when the last assist_satellite changes to a specific state."""
     await async_setup_component(hass, "assist_satellite", {})
@@ -208,20 +209,20 @@ async def test_assist_satellite_state_trigger_behavior_last(
 
     # Set all assist satellites, including the tested one, to the initial state
     for eid in target_assist_satellites:
-        set_or_remove_state(hass, eid, states[0][0])
+        set_or_remove_state(hass, eid, states[0]["state"])
         await hass.async_block_till_done()
 
     await arm_trigger(hass, trigger, {"behavior": "last"}, trigger_target_config)
 
-    for state, expected_calls in states[1:]:
+    for state in states[1:]:
         for other_entity_id in other_entity_ids:
-            set_or_remove_state(hass, other_entity_id, state)
+            set_or_remove_state(hass, other_entity_id, state["state"])
             await hass.async_block_till_done()
         assert len(service_calls) == 0
 
-        set_or_remove_state(hass, entity_id, state)
+        set_or_remove_state(hass, entity_id, state["state"])
         await hass.async_block_till_done()
-        assert len(service_calls) == expected_calls
+        assert len(service_calls) == state["count"]
         for service_call in service_calls:
             assert service_call.data[CONF_ENTITY_ID] == entity_id
         service_calls.clear()
--- tests/components/climate/test_trigger.py
+++ tests/components/climate/test_trigger.py
@@ -12,6 +12,7 @@ from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.setup import async_setup_component
 
 from tests.components import (
+    StateDescription,
     arm_trigger,
     parametrize_attribute_trigger_states,
     parametrize_target_entities,
@@ -52,7 +53,7 @@ async def test_climate_state_trigger_behavior_any(
     entity_id: str,
     entities_in_target: int,
     trigger: str,
-    states: list[tuple[str, int]],
+    states: list[StateDescription],
 ) -> None:
     """Test that the climate state trigger fires when any climate state changes to a specific state."""
     await async_setup_component(hass, "climate", {})
@@ -61,24 +62,24 @@ async def test_climate_state_trigger_behavior_any(
 
     # Set all climates, including the tested climate, to the initial state
     for eid in target_climates:
-        set_or_remove_state(hass, eid, states[0][0])
+        set_or_remove_state(hass, eid, states[0]["state"])
         await hass.async_block_till_done()
 
     await arm_trigger(hass, trigger, {}, trigger_target_config)
 
-    for state, expected_calls in states[1:]:
-        set_or_remove_state(hass, entity_id, state)
+    for state in states[1:]:
+        set_or_remove_state(hass, entity_id, state["state"])
         await hass.async_block_till_done()
-        assert len(service_calls) == expected_calls
+        assert len(service_calls) == state["count"]
         for service_call in service_calls:
             assert service_call.data[CONF_ENTITY_ID] == entity_id
         service_calls.clear()
 
         # Check if changing other climates also triggers
         for other_entity_id in other_entity_ids:
-            set_or_remove_state(hass, other_entity_id, state)
+            set_or_remove_state(hass, other_entity_id, state["state"])
             await hass.async_block_till_done()
-        assert len(service_calls) == (entities_in_target - 1) * expected_calls
+        assert len(service_calls) == (entities_in_target - 1) * state["count"]
         service_calls.clear()
 
 
@@ -157,7 +158,7 @@ async def test_climate_state_trigger_behavior_first(
     entities_in_target: int,
     entity_id: str,
     trigger: str,
-    states: list[tuple[str, int, list[str]]],
+    states: list[StateDescription],
 ) -> None:
     """Test that the climate state trigger fires when the first climate changes to a specific state."""
     await async_setup_component(hass, "climate", {})
@@ -166,22 +167,22 @@ async def test_climate_state_trigger_behavior_first(
 
     # Set all climates, including the tested climate, to the initial state
     for eid in target_climates:
-        set_or_remove_state(hass, eid, states[0][0])
+        set_or_remove_state(hass, eid, states[0]["state"])
         await hass.async_block_till_done()
 
     await arm_trigger(hass, trigger, {"behavior": "first"}, trigger_target_config)
 
-    for state, expected_calls in states[1:]:
-        set_or_remove_state(hass, entity_id, state)
+    for state in states[1:]:
+        set_or_remove_state(hass, entity_id, state["state"])
         await hass.async_block_till_done()
-        assert len(service_calls) == expected_calls
+        assert len(service_calls) == state["count"]
         for service_call in service_calls:
             assert service_call.data[CONF_ENTITY_ID] == entity_id
         service_calls.clear()
 
         # Triggering other climates should not cause the trigger to fire again
         for other_entity_id in other_entity_ids:
-            set_or_remove_state(hass, other_entity_id, state)
+            set_or_remove_state(hass, other_entity_id, state["state"])
             await hass.async_block_till_done()
         assert len(service_calls) == 0
 
@@ -260,7 +261,7 @@ async def test_climate_state_trigger_behavior_last(
     entities_in_target: int,
     entity_id: str,
     trigger: str,
-    states: list[tuple[str, int]],
+    states: list[StateDescription],
 ) -> None:
     """Test that the climate state trigger fires when the last climate changes to a specific state."""
     await async_setup_component(hass, "climate", {})
@@ -269,20 +270,20 @@ async def test_climate_state_trigger_behavior_last(
 
     # Set all climates, including the tested climate, to the initial state
     for eid in target_climates:
-        set_or_remove_state(hass, eid, states[0][0])
+        set_or_remove_state(hass, eid, states[0]["state"])
         await hass.async_block_till_done()
 
     await arm_trigger(hass, trigger, {"behavior": "last"}, trigger_target_config)
 
-    for state, expected_calls in states[1:]:
+    for state in states[1:]:
         for other_entity_id in other_entity_ids:
-            set_or_remove_state(hass, other_entity_id, state)
+            set_or_remove_state(hass, other_entity_id, state["state"])
             await hass.async_block_till_done()
         assert len(service_calls) == 0
 
-        set_or_remove_state(hass, entity_id, state)
+        set_or_remove_state(hass, entity_id, state["state"])
         await hass.async_block_till_done()
-        assert len(service_calls) == expected_calls
+        assert len(service_calls) == state["count"]
         for service_call in service_calls:
             assert service_call.data[CONF_ENTITY_ID] == entity_id
         service_calls.clear()
