b144eae69b6fa7cc834acacd116ccbc7a1d5bee3
Erik Montnemery <erik@montnemery.com>
2025-11-20 11:09:34 +0100

    Add trigger alarm_control_panel.armed (#156913)

--- homeassistant/components/media_player/trigger.py
+++ homeassistant/components/media_player/trigger.py
@@ -1,38 +1,25 @@
 """Provides triggers for media players."""
 
-from homeassistant.core import HomeAssistant, State
-from homeassistant.helpers.trigger import EntityTriggerBase, Trigger
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.trigger import Trigger, make_conditional_entity_state_trigger
 
 from . import MediaPlayerState
 from .const import DOMAIN
 
-
-class MediaPlayerStoppedPlayingTrigger(EntityTriggerBase):
-    """Class for media player stopped playing trigger."""
-
-    _domain: str = DOMAIN
-    _from_states = {
-        MediaPlayerState.BUFFERING,
-        MediaPlayerState.PAUSED,
-        MediaPlayerState.PLAYING,
-    }
-    _to_states = {
-        MediaPlayerState.IDLE,
-        MediaPlayerState.OFF,
-        MediaPlayerState.ON,
-    }
-
-    def is_from_state(self, state: State) -> bool:
-        """Check if the state matches the origin state."""
-        return state.state in self._from_states
-
-    def is_to_state(self, state: State) -> bool:
-        """Check if the state matches the target state."""
-        return state.state in self._to_states
-
-
 TRIGGERS: dict[str, type[Trigger]] = {
-    "stopped_playing": MediaPlayerStoppedPlayingTrigger,
+    "stopped_playing": make_conditional_entity_state_trigger(
+        DOMAIN,
+        from_states={
+            MediaPlayerState.BUFFERING,
+            MediaPlayerState.PAUSED,
+            MediaPlayerState.PLAYING,
+        },
+        to_states={
+            MediaPlayerState.IDLE,
+            MediaPlayerState.OFF,
+            MediaPlayerState.ON,
+        },
+    ),
 }
 
 
