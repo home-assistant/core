cfd1b522daf760b9fba9f4f50ce69c10afcbef67
Jaap Pieroen <github@pieroen.nl>
2025-11-21 23:44:59 +0100

    Remove unused Essent setup scaffolding

--- homeassistant/components/essent/__init__.py
+++ homeassistant/components/essent/__init__.py
@@ -4,19 +4,10 @@ from __future__ import annotations
 
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN
 from .coordinator import EssentConfigEntry, EssentDataUpdateCoordinator
 
 PLATFORMS: list[Platform] = [Platform.SENSOR]
-CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
-
-
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the Essent integration."""
-    return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: EssentConfigEntry) -> bool:
