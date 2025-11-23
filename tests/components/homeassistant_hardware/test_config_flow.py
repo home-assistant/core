4d66c0648077f66857e950313c25716109164749
puddly <32534428+puddly@users.noreply.github.com>
2025-11-22 22:37:17 -0500

    Fix unit tests

--- tests/components/homeassistant_hardware/test_config_flow.py
+++ tests/components/homeassistant_hardware/test_config_flow.py
@@ -23,9 +23,6 @@ from homeassistant.components.homeassistant_hardware.firmware_config_flow import
     BaseFirmwareConfigFlow,
     BaseFirmwareOptionsFlow,
 )
-from homeassistant.components.homeassistant_hardware.helpers import (
-    async_firmware_update_context,
-)
 from homeassistant.components.homeassistant_hardware.util import (
     ApplicationType,
     FirmwareInfo,
@@ -307,21 +304,18 @@ def mock_firmware_info(
         bootloader_reset_methods: Sequence[ResetTarget] = (),
         application_probe_methods: Sequence[tuple[ApplicationType, int]] = (),
         progress_callback: Callable[[int, int], None] | None = None,
-        *,
-        domain: str = "homeassistant_hardware",
     ) -> FirmwareInfo:
-        async with async_firmware_update_context(hass, device, domain):
-            await asyncio.sleep(0)
-            progress_callback(0, 100)
-            await asyncio.sleep(0)
-            progress_callback(50, 100)
-            await asyncio.sleep(0)
-            progress_callback(100, 100)
+        await asyncio.sleep(0)
+        progress_callback(0, 100)
+        await asyncio.sleep(0)
+        progress_callback(50, 100)
+        await asyncio.sleep(0)
+        progress_callback(100, 100)
 
-            if flashed_firmware_info is None:
-                raise HomeAssistantError("Failed to probe the firmware after flashing")
+        if flashed_firmware_info is None:
+            raise HomeAssistantError("Failed to probe the firmware after flashing")
 
-            return flashed_firmware_info
+        return flashed_firmware_info
 
     with (
         patch(
