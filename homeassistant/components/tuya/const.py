baebe160bdc0e1dcb2652cc0a4947e54c6b71e42
epenet <6771947+epenet@users.noreply.github.com>
2025-11-25 13:34:01 +0100

    Remove old migration from Tuya (#157237)

--- homeassistant/components/tuya/const.py
+++ homeassistant/components/tuya/const.py
@@ -30,7 +30,6 @@ from homeassistant.const import (
 DOMAIN = "tuya"
 LOGGER = logging.getLogger(__package__)
 
-CONF_APP_TYPE = "tuya_app_type"
 CONF_ENDPOINT = "endpoint"
 CONF_TERMINAL_ID = "terminal_id"
 CONF_TOKEN_INFO = "token_info"
