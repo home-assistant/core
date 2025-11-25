4e4882527bc711f76ca5addee63e22e5cc728f81
scheric <38077357+scheric@users.noreply.github.com>
2025-11-25 21:22:17 +0100

    auto ruff

--- tests/components/generic_thermostat/test_config_flow.py
+++ tests/components/generic_thermostat/test_config_flow.py
@@ -221,9 +221,9 @@ async def test_config_flow_with_keep_alive(hass: HomeAssistant) -> None:
 
         val = result["options"].get(CONF_KEEP_ALIVE)
         assert val is not None
+
         assert isinstance(val, timedelta)
         assert val.total_seconds() == 60
 
         await hass.async_block_till_done()
         assert len(mock_setup_entry.mock_calls) == 1
-
