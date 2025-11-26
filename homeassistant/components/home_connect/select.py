22c4978874fa5db8e583a879299a85d728e6e00b
boergegrunicke <boerge@grunicke.de>
2025-11-26 11:20:07 +0000

    add raw_value to select

--- homeassistant/components/home_connect/select.py
+++ homeassistant/components/home_connect/select.py
@@ -411,11 +411,17 @@ class HomeConnectProgramSelectEntity(HomeConnectEntity, SelectEntity):
     def update_native_value(self) -> None:
         """Set the program value."""
         event = self.appliance.events.get(cast(EventKey, self.bsh_key))
-        self._attr_current_option = (
-            PROGRAMS_TRANSLATION_KEYS_MAP.get(ProgramKey(event_value))
-            if event and isinstance(event_value := event.value, str)
-            else None
-        )
+        if event and isinstance(event_value := event.value, str):
+            self._attr_extra_state_attributes = {"raw_value": event_value}
+            try:
+                self._attr_current_option = PROGRAMS_TRANSLATION_KEYS_MAP.get(
+                    ProgramKey(event_value)
+                )
+            except ValueError:
+                self._attr_current_option = None
+        else:
+            self._attr_current_option = None
+            self._attr_extra_state_attributes = {"raw_value": None}
 
     async def async_select_option(self, option: str) -> None:
         """Select new program."""
