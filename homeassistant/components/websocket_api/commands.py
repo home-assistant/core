b9664610b9c9770c67c3495d97a922817a56d6e5
Abílio Costa <abmantis@users.noreply.github.com>
2025-11-26 11:05:03 +0100

    Add get_triggers_for_target websocket command (#156778)

--- homeassistant/components/websocket_api/commands.py
+++ homeassistant/components/websocket_api/commands.py
@@ -87,6 +87,7 @@ from homeassistant.setup import (
 from homeassistant.util.json import format_unserializable_data
 
 from . import const, decorators, messages
+from .automation import async_get_triggers_for_target
 from .connection import ActiveConnection
 from .messages import construct_event_message, construct_result_message
 
@@ -107,6 +108,7 @@ def async_register_commands(
     async_reg(hass, handle_entity_source)
     async_reg(hass, handle_execute_script)
     async_reg(hass, handle_extract_from_target)
+    async_reg(hass, handle_get_triggers_for_target)
     async_reg(hass, handle_fire_event)
     async_reg(hass, handle_get_config)
     async_reg(hass, handle_get_services)
@@ -875,6 +877,29 @@ def handle_extract_from_target(
     }
 
     connection.send_result(msg["id"], extracted_dict)
+
+
+@decorators.websocket_command(
+    {
+        vol.Required("type"): "get_triggers_for_target",
+        vol.Required("target"): cv.TARGET_FIELDS,
+        vol.Optional("expand_group", default=True): bool,
+    }
+)
+@decorators.async_response
+async def handle_get_triggers_for_target(
+    hass: HomeAssistant, connection: ActiveConnection, msg: dict[str, Any]
+) -> None:
+    """Handle get triggers for target command.
+
+    This command returns all triggers that can be used with any entities that are currently
+    part of a target.
+    """
+    triggers = await async_get_triggers_for_target(
+        hass, msg["target"], msg["expand_group"]
+    )
+
+    connection.send_result(msg["id"], triggers)
 
 
 @decorators.websocket_command(
