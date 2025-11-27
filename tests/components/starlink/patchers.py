678c4dd7fe5b80717b1c7da7ccb6a0e747a84e97
David Rapan <david@rapan.cz>
2025-11-27 14:05:18 +0100

    Refactor test
    Signed-off-by: David Rapan <david@rapan.cz>

--- tests/components/starlink/patchers.py
+++ tests/components/starlink/patchers.py
@@ -19,9 +19,10 @@ SLEEP_DATA_SUCCESS_PATCHER = patch(
     return_value=json.loads(load_fixture("sleep_data_success.json", "starlink")),
 )
 
+STATUS_DATA_TARGET = "homeassistant.components.starlink.coordinator.status_data"
+STATUS_DATA_FIXTURE = json.loads(load_fixture("status_data_success.json", "starlink"))
 STATUS_DATA_SUCCESS_PATCHER = patch(
-    "homeassistant.components.starlink.coordinator.status_data",
-    return_value=json.loads(load_fixture("status_data_success.json", "starlink")),
+    STATUS_DATA_TARGET, return_value=STATUS_DATA_FIXTURE
 )
 
 HISTORY_STATS_SUCCESS_PATCHER = patch(
--- tests/components/starlink/test_init.py
+++ tests/components/starlink/test_init.py
@@ -1,24 +1,31 @@
 """Tests Starlink integration init/unload."""
 
+from copy import deepcopy
 from datetime import datetime, timedelta
 from unittest.mock import patch
 
-from freezegun.api import FrozenDateTimeFactory
+from freezegun import freeze_time
 
 from homeassistant.components.starlink.const import DOMAIN
-from homeassistant.components.starlink.sensor import uptime_to_stable_datetime
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import CONF_IP_ADDRESS
 from homeassistant.core import HomeAssistant, State
+from homeassistant.util import dt as dt_util
 
 from .patchers import (
     HISTORY_STATS_SUCCESS_PATCHER,
     LOCATION_DATA_SUCCESS_PATCHER,
     SLEEP_DATA_SUCCESS_PATCHER,
+    STATUS_DATA_FIXTURE,
     STATUS_DATA_SUCCESS_PATCHER,
+    STATUS_DATA_TARGET,
 )
 
-from tests.common import MockConfigEntry, mock_restore_cache_with_extra_data
+from tests.common import (
+    MockConfigEntry,
+    async_fire_time_changed,
+    mock_restore_cache_with_extra_data,
+)
 
 
 async def test_successful_entry(hass: HomeAssistant) -> None:
@@ -118,21 +125,60 @@ async def test_restore_cache_with_accumulation(hass: HomeAssistant) -> None:
         assert hass.states.get(entity_id).state == str(1 + 0.01572462736977)
 
 
-async def test_uptime_to_stable_datetime(freezer: FrozenDateTimeFactory) -> None:
-    """Test uptime to stable datetime."""
+async def test_last_restart_state(hass: HomeAssistant) -> None:
+    """Test Starlink last restart state."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={CONF_IP_ADDRESS: "1.2.3.4:0000"},
+    )
+    entity_id = "sensor.starlink_last_restart"
     utc_now = datetime.fromisoformat("2025-10-22T13:31:29+00:00")
-    freezer.move_to(utc_now)
 
-    assert uptime_to_stable_datetime(804138).isoformat() == "2025-10-13T06:09:11+00:00"
+    with (
+        LOCATION_DATA_SUCCESS_PATCHER,
+        SLEEP_DATA_SUCCESS_PATCHER,
+        STATUS_DATA_SUCCESS_PATCHER,
+        HISTORY_STATS_SUCCESS_PATCHER,
+    ):
+        with freeze_time(utc_now):
+            entry.add_to_hass(hass)
 
-    freezer.move_to(utc_now + timedelta(seconds=5))
+            await hass.config_entries.async_setup(entry.entry_id)
+            await hass.async_block_till_done()
 
-    assert uptime_to_stable_datetime(804144).isoformat() == "2025-10-13T06:09:11+00:00"
+        assert hass.states.get(entity_id).state == "2025-10-13T06:09:11+00:00"
 
-    freezer.move_to(utc_now + timedelta(seconds=10))
+        with patch.object(entry.runtime_data, "always_update", return_value=True):
+            status_data = deepcopy(STATUS_DATA_FIXTURE)
+            status_data[0]["uptime"] = 804144
 
-    assert uptime_to_stable_datetime(804134).isoformat() == "2025-10-13T06:09:11+00:00"
+            with (
+                freeze_time(utc_now + timedelta(seconds=5)),
+                patch(STATUS_DATA_TARGET, return_value=status_data),
+            ):
+                async_fire_time_changed(hass, dt_util.utcnow() + timedelta(seconds=5))
+                await hass.async_block_till_done(wait_background_tasks=True)
 
-    freezer.move_to(utc_now + timedelta(seconds=15))
+            assert hass.states.get(entity_id).state == "2025-10-13T06:09:11+00:00"
 
-    assert uptime_to_stable_datetime(100).isoformat() == "2025-10-22T13:30:04+00:00"
+            status_data[0]["uptime"] = 804134
+
+            with (
+                freeze_time(utc_now + timedelta(seconds=10)),
+                patch(STATUS_DATA_TARGET, return_value=status_data),
+            ):
+                async_fire_time_changed(hass, dt_util.utcnow() + timedelta(seconds=10))
+                await hass.async_block_till_done(wait_background_tasks=True)
+
+            assert hass.states.get(entity_id).state == "2025-10-13T06:09:11+00:00"
+
+            status_data[0]["uptime"] = 100
+
+            with (
+                freeze_time(utc_now + timedelta(seconds=15)),
+                patch(STATUS_DATA_TARGET, return_value=status_data),
+            ):
+                async_fire_time_changed(hass, dt_util.utcnow() + timedelta(seconds=15))
+                await hass.async_block_till_done(wait_background_tasks=True)
+
+            assert hass.states.get(entity_id).state == "2025-10-22T13:30:04+00:00"
