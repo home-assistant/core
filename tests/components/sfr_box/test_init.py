3b52c5df79a5ceefef81a04311ef5868bd4cde46
epenet <6771947+epenet@users.noreply.github.com>
2025-11-28 10:44:39 +0100

    Use snapshot_platform helper in SFR Box tests (#157481)

--- tests/components/sfr_box/test_init.py
+++ tests/components/sfr_box/test_init.py
@@ -5,10 +5,12 @@ from unittest.mock import patch
 
 import pytest
 from sfrbox_api.exceptions import SFRBoxAuthenticationError, SFRBoxError
+from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.components.sfr_box.const import DOMAIN
 from homeassistant.config_entries import ConfigEntry, ConfigEntryState
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
 
 
 @pytest.fixture(autouse=True)
@@ -78,3 +80,21 @@ async def test_setup_entry_invalid_auth(
 
     assert len(hass.config_entries.async_entries(DOMAIN)) == 1
     assert config_entry_with_auth.state is ConfigEntryState.SETUP_ERROR
+
+
+@patch("homeassistant.components.sfr_box.PLATFORMS", [])
+async def test_device_registry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    device_registry: dr.DeviceRegistry,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Ensure devices are correctly registered."""
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    # Ensure devices are correctly registered
+    device_entries = dr.async_entries_for_config_entry(
+        device_registry, config_entry.entry_id
+    )
+    assert device_entries == snapshot
