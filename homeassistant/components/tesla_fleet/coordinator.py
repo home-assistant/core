eaf848b4b63e40e766757d655cb8388075fa58d2
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2025-11-28 19:25:48 -0800

    Fix docstrings

--- homeassistant/components/tesla_fleet/coordinator.py
+++ homeassistant/components/tesla_fleet/coordinator.py
@@ -73,7 +73,7 @@ def flatten(data: dict[str, Any], parent: str | None = None) -> dict[str, Any]:
 
 
 class TeslaFleetVehicleDataCoordinator(DataUpdateCoordinator[dict[str, Any]]):
-    """Class to manage fetching energy site history import and export from the Tesla Fleet API."""
+    """Class to manage fetching data from the TeslaFleet API."""
 
     config_entry: TeslaFleetConfigEntry
     updated_once: bool
@@ -215,7 +215,7 @@ class TeslaFleetEnergySiteLiveCoordinator(DataUpdateCoordinator[dict[str, Any]])
 
 
 class TeslaFleetEnergySiteHistoryCoordinator(DataUpdateCoordinator[dict[str, Any]]):
-    """Class to manage fetching energy site history from the TeslaFleet API."""
+    """Class to manage fetching energy site history import and export from the Tesla Fleet API."""
 
     config_entry: TeslaFleetConfigEntry
 
