27215d15e06c52ac85095dad4a267be4eff6f300
RaHehl <7577984+RaHehl@users.noreply.github.com>
2025-11-29 19:57:03 +0000

    Fix UFPConfigEntry type consistency in unifiprotect

--- homeassistant/components/unifiprotect/config_flow.py
+++ homeassistant/components/unifiprotect/config_flow.py
@@ -55,7 +55,7 @@ from .const import (
     MIN_REQUIRED_PROTECT_V,
     OUTDATED_LOG_MESSAGE,
 )
-from .data import async_last_update_was_successful
+from .data import UFPConfigEntry, async_last_update_was_successful
 from .discovery import async_start_discovery
 from .utils import _async_resolve, _async_short_mac, _async_unifi_mac_from_hass
 
@@ -80,7 +80,7 @@ def _host_is_direct_connect(host: str) -> bool:
 
 async def _async_console_is_offline(
     hass: HomeAssistant,
-    entry: ConfigEntry,
+    entry: UFPConfigEntry,
 ) -> bool:
     """Check if a console is offline.
 
