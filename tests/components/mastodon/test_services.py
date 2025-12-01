5c388a21ff9e4587cb3d2e07b379e3b6451d6071
Andrew Jackson <andrew@codechimp.org>
2025-12-01 16:49:24 +0000

    Add test for multiline

--- tests/components/mastodon/test_services.py
+++ tests/components/mastodon/test_services.py
@@ -118,9 +118,12 @@ from tests.common import MockConfigEntry
             },
         ),
         (
-            {ATTR_STATUS: "test toot", ATTR_IDEMPOTENCY_KEY: "post_once_only"},
             {
-                "status": "test toot",
+                ATTR_STATUS: "test toot\nwith idemmpotency",
+                ATTR_IDEMPOTENCY_KEY: "post_once_only",
+            },
+            {
+                "status": "test toot\nwith idemmpotency",
                 "idempotency_key": "post_once_only",
                 "language": None,
                 "spoiler_text": None,
