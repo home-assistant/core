1a9d0a7098fd8638966a2a585de275d40f0b2376
David Rapan <david@rapan.cz>
2025-12-02 16:12:55 +0100

    Refactor to use freezer
    Signed-off-by: David Rapan <david@rapan.cz>

--- tests/components/cloudflare/test_init.py
+++ tests/components/cloudflare/test_init.py
@@ -3,6 +3,7 @@
 from datetime import timedelta
 from unittest.mock import MagicMock, patch
 
+from freezegun.api import FrozenDateTimeFactory
 import pycfdns
 import pytest
 
@@ -14,7 +15,6 @@ from homeassistant.components.cloudflare.const import (
 )
 from homeassistant.config_entries import SOURCE_REAUTH, ConfigEntryState
 from homeassistant.core import HomeAssistant
-from homeassistant.util import dt as dt_util
 
 from . import ENTRY_CONFIG, init_integration
 from .conftest import LOCATION_PATCH_TARGET
@@ -80,7 +80,6 @@ async def test_unload_entry(hass: HomeAssistant, cfupdate: MagicMock) -> None:
     await hass.async_block_till_done()
 
     assert entry.state is ConfigEntryState.NOT_LOADED
-    assert not hass.data.get(DOMAIN)
 
 
 @pytest.mark.usefixtures("location_info")
@@ -153,7 +152,10 @@ async def test_integration_services_with_nonexisting_record(
 
 @pytest.mark.usefixtures("location_info")
 async def test_integration_update_interval(
-    hass: HomeAssistant, cfupdate: MagicMock, caplog: pytest.LogCaptureFixture
+    hass: HomeAssistant,
+    cfupdate: MagicMock,
+    caplog: pytest.LogCaptureFixture,
+    freezer: FrozenDateTimeFactory,
 ) -> None:
     """Test integration update interval."""
     instance = cfupdate.return_value
@@ -161,26 +163,23 @@ async def test_integration_update_interval(
     entry = await init_integration(hass)
     assert entry.state is ConfigEntryState.LOADED
 
-    async_fire_time_changed(
-        hass, dt_util.utcnow() + timedelta(minutes=DEFAULT_UPDATE_INTERVAL)
-    )
+    freezer.tick(timedelta(minutes=DEFAULT_UPDATE_INTERVAL))
+    async_fire_time_changed(hass)
     await hass.async_block_till_done(wait_background_tasks=True)
     assert len(instance.list_dns_records.mock_calls) == 2
     assert len(instance.update_dns_record.mock_calls) == 4
     assert "All target records are up to date" not in caplog.text
 
     instance.list_dns_records.side_effect = pycfdns.AuthenticationException()
-    async_fire_time_changed(
-        hass, dt_util.utcnow() + timedelta(minutes=DEFAULT_UPDATE_INTERVAL)
-    )
+    freezer.tick(timedelta(minutes=DEFAULT_UPDATE_INTERVAL))
+    async_fire_time_changed(hass)
     await hass.async_block_till_done(wait_background_tasks=True)
     assert len(instance.list_dns_records.mock_calls) == 3
     assert len(instance.update_dns_record.mock_calls) == 4
 
     instance.list_dns_records.side_effect = pycfdns.ComunicationException()
-    async_fire_time_changed(
-        hass, dt_util.utcnow() + timedelta(minutes=DEFAULT_UPDATE_INTERVAL)
-    )
+    freezer.tick(timedelta(minutes=DEFAULT_UPDATE_INTERVAL))
+    async_fire_time_changed(hass)
     await hass.async_block_till_done(wait_background_tasks=True)
     assert len(instance.list_dns_records.mock_calls) == 4
     assert len(instance.update_dns_record.mock_calls) == 4
