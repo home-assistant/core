20e70edaa2b5f12783825354d3070668abefcc34
Markus Jacobsen <markusjacobsen@hotmail.com>
2025-12-02 17:21:00 +0100

    Add Beoremote One battery support

--- homeassistant/components/bang_olufsen/sensor.py
+++ homeassistant/components/bang_olufsen/sensor.py
@@ -2,9 +2,12 @@
 
 from __future__ import annotations
 
+import contextlib
 from datetime import timedelta
 
-from mozart_api.models import BatteryState
+from aiohttp import ClientConnectorError
+from mozart_api.exceptions import ApiException
+from mozart_api.models import BatteryState, PairedRemote
 
 from homeassistant.components.sensor import (
     SensorDeviceClass,
@@ -12,13 +15,14 @@ from homeassistant.components.sensor import (
     SensorStateClass,
 )
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from . import BeoConfigEntry
 from .const import CONNECTION_STATUS, DOMAIN, WebsocketNotification
 from .entity import BeoEntity
-from .util import supports_battery
+from .util import get_remotes, supports_battery
 
 SCAN_INTERVAL = timedelta(minutes=15)
 
@@ -36,6 +40,14 @@ async def async_setup_entry(
     # Check for Mozart device with battery
     if await supports_battery(config_entry.runtime_data.client):
         entities.append(BeoSensorBatteryLevel(config_entry))
+
+    # Add any Beoremote One remotes
+    entities.extend(
+        [
+            BeoSensorRemoteBatteryLevel(config_entry, remote)
+            for remote in (await get_remotes(config_entry.runtime_data.client))
+        ]
+    )
 
     async_add_entities(entities, update_before_add=True)
 
@@ -83,3 +95,46 @@ class BeoSensorBatteryLevel(BeoSensor):
         """Update sensor value."""
         self._attr_native_value = data.battery_level
         self.async_write_ha_state()
+
+
+class BeoSensorRemoteBatteryLevel(BeoSensor):
+    """Battery level Sensor for the Beoremote One."""
+
+    _attr_native_unit_of_measurement = "%"
+    _attr_translation_key = "remote_battery_level"
+    _attr_should_poll = True
+    _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def __init__(self, config_entry: BeoConfigEntry, remote: PairedRemote) -> None:
+        """Init the battery level Sensor."""
+        super().__init__(config_entry)
+        # Serial number is not None, as the remote object is provided by get_remotes
+        assert remote.serial_number
+
+        self._attr_device_class = SensorDeviceClass.BATTERY
+        self._attr_unique_id = (
+            f"{remote.serial_number}_{self._unique_id}_remote_battery_level"
+        )
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, f"{remote.serial_number}_{self._unique_id}")}
+        )
+        self._attr_native_value = remote.battery_level
+        self._remote = remote
+
+    async def async_added_to_hass(self) -> None:
+        """Turn on the dispatchers."""
+        self.async_on_remove(
+            async_dispatcher_connect(
+                self.hass,
+                f"{DOMAIN}_{self._unique_id}_{CONNECTION_STATUS}",
+                self._async_update_connection_state,
+            )
+        )
+
+    async def async_update(self) -> None:
+        """Poll battery status."""
+        with contextlib.suppress(ApiException, ClientConnectorError, TimeoutError):
+            for remote in await get_remotes(self._client):
+                if remote.serial_number == self._remote.serial_number:
+                    self._attr_native_value = remote.battery_level
+                    break
