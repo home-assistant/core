d388948c49c6bbe7d941242eca4a88d2182fb728
hanwg <han.wuguang@gmail.com>
2025-12-02 01:24:52 +0000

    update tests

--- tests/components/group/test_notify.py
+++ tests/components/group/test_notify.py
@@ -333,11 +333,21 @@ async def help_async_unload_entry(
 @pytest.fixture
 async def mock_notifiers(
     hass: HomeAssistant, config_flow_fixture: None
-) -> list[NotifyEntity]:
+) -> list[MockNotifyEntity]:
     """Set up the notify entities."""
-    entity = MockNotifyEntity(is_title_supported=True, name="test", entity_id="notify.test")
-    entity2 = MockNotifyEntity(is_title_supported=False, name="test2", entity_id="notify.test2")
-    entities = [entity, entity2]
+    entity_title_1 = MockNotifyEntity(
+        is_title_supported=True, name="has_title_1", entity_id="notify.has_title_1"
+    )
+    entity_title_2 = MockNotifyEntity(
+        is_title_supported=True, name="has_title_2", entity_id="notify.has_title_2"
+    )
+    entity_no_title_1 = MockNotifyEntity(
+        is_title_supported=False, name="no_title_1", entity_id="notify.no_title_1"
+    )
+    entity_no_title_2 = MockNotifyEntity(
+        is_title_supported=False, name="no_title_2", entity_id="notify.no_title_2"
+    )
+    entities = [entity_title_1, entity_title_2, entity_no_title_1, entity_no_title_2]
     test_entry = MockConfigEntry(domain="test")
     test_entry.add_to_hass(hass)
     mock_integration(
@@ -355,21 +365,23 @@ async def mock_notifiers(
 
 
 async def test_notify_entity_group(
-    hass: HomeAssistant, mock_notifiers: list[NotifyEntity]
+    hass: HomeAssistant, mock_notifiers: list[MockNotifyEntity]
 ) -> None:
     """Test sending a message to a notify group."""
-    entity, entity2 = mock_notifiers
-    assert entity.send_message_mock_calls.call_count == 0
-    assert entity2.send_message_mock_calls.call_count == 0
+    entity_title_1, entity_title_2, entity_no_title_1, entity_no_title_2 = (
+        mock_notifiers
+    )
+    for mock_notifier in mock_notifiers:
+        assert mock_notifier.send_message_mock_calls.call_count == 0
 
-    # test with title supported
+    # test group containing 1 member with title supported
 
     config_entry = MockConfigEntry(
         domain=DOMAIN,
         options={
             "group_type": "notify",
             "name": "Test Group",
-            "entities": ["notify.test"],
+            "entities": ["notify.has_title_1"],
             "hide_members": True,
         },
         title="Test Group",
@@ -389,22 +401,43 @@ async def test_notify_entity_group(
         blocking=True,
     )
 
-    assert entity.send_message_mock_calls.call_count == 1
-    assert entity.send_message_mock_calls.call_args == call(
+    assert entity_title_1.send_message_mock_calls.call_count == 1
+    assert entity_title_1.send_message_mock_calls.call_args == call(
         "Hello", title="Test notification"
     )
 
-    # test without title supported
+    for mock_notifier in mock_notifiers:
+        mock_notifier.send_message_mock_calls.reset_mock()
+
+    # test group containing 1 member with title supported but no title provided
+
+    await hass.services.async_call(
+        NOTIFY_DOMAIN,
+        SERVICE_SEND_MESSAGE,
+        {
+            ATTR_MESSAGE: "Hello",
+            ATTR_ENTITY_ID: "notify.test_group",
+        },
+        blocking=True,
+    )
+
+    assert entity_title_1.send_message_mock_calls.call_count == 1
+    assert entity_title_1.send_message_mock_calls.call_args == call("Hello", title=None)
+
+    for mock_notifier in mock_notifiers:
+        mock_notifier.send_message_mock_calls.reset_mock()
+
+    # test group containing 2 members with title supported
 
     config_entry = MockConfigEntry(
         domain=DOMAIN,
         options={
             "group_type": "notify",
-            "name": "Test Group",
-            "entities": ["notify.test2"],
+            "name": "Test Group 2",
+            "entities": ["notify.has_title_1", "notify.has_title_2"],
             "hide_members": True,
         },
-        title="Test Group2",
+        title="Test Group 2",
     )
     config_entry.add_to_hass(hass)
     await hass.config_entries.async_setup(config_entry.entry_id)
@@ -416,13 +449,96 @@ async def test_notify_entity_group(
         {
             ATTR_MESSAGE: "Hello",
             ATTR_TITLE: "Test notification",
-            ATTR_ENTITY_ID: "notify.test_group2",
+            ATTR_ENTITY_ID: "notify.test_group_2",
         },
         blocking=True,
     )
 
-    assert entity2.send_message_mock_calls.call_count == 1
-    assert entity2.send_message_mock_calls.call_args == call("Hello", title=None)
+    assert entity_title_1.send_message_mock_calls.call_count == 1
+    assert entity_title_1.send_message_mock_calls.call_args == call(
+        "Hello", title="Test notification"
+    )
+    assert entity_title_2.send_message_mock_calls.call_count == 1
+    assert entity_title_2.send_message_mock_calls.call_args == call(
+        "Hello", title="Test notification"
+    )
+
+    for mock_notifier in mock_notifiers:
+        mock_notifier.send_message_mock_calls.reset_mock()
+
+    # test group containing 2 members: 1 title supported and 1 not supported
+    # title is not supported since not all members support it
+
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        options={
+            "group_type": "notify",
+            "name": "Test Group",
+            "entities": ["notify.has_title_1", "notify.no_title_1"],
+            "hide_members": True,
+        },
+        title="Test Group 3",
+    )
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    await hass.services.async_call(
+        NOTIFY_DOMAIN,
+        SERVICE_SEND_MESSAGE,
+        {
+            ATTR_MESSAGE: "Hello",
+            ATTR_TITLE: "Test notification",
+            ATTR_ENTITY_ID: "notify.test_group_3",
+        },
+        blocking=True,
+    )
+
+    assert entity_title_1.send_message_mock_calls.call_count == 1
+    assert entity_title_1.send_message_mock_calls.call_args == call("Hello", title=None)
+    assert entity_no_title_1.send_message_mock_calls.call_count == 1
+    assert entity_no_title_1.send_message_mock_calls.call_args == call(
+        "Hello", title=None
+    )
+
+    for mock_notifier in mock_notifiers:
+        mock_notifier.send_message_mock_calls.reset_mock()
+
+    # test group containing 2 members: both not supporting title
+
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        options={
+            "group_type": "notify",
+            "name": "Test Group",
+            "entities": ["notify.no_title_1", "notify.no_title_2"],
+            "hide_members": True,
+        },
+        title="Test Group 4",
+    )
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    await hass.services.async_call(
+        NOTIFY_DOMAIN,
+        SERVICE_SEND_MESSAGE,
+        {
+            ATTR_MESSAGE: "Hello",
+            ATTR_TITLE: "Test notification",
+            ATTR_ENTITY_ID: "notify.test_group_4",
+        },
+        blocking=True,
+    )
+
+    assert entity_no_title_1.send_message_mock_calls.call_count == 1
+    assert entity_no_title_1.send_message_mock_calls.call_args == call(
+        "Hello", title=None
+    )
+    assert entity_no_title_2.send_message_mock_calls.call_count == 1
+    assert entity_no_title_2.send_message_mock_calls.call_args == call(
+        "Hello", title=None
+    )
 
 
 async def test_state_reporting(hass: HomeAssistant) -> None:
