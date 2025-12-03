d1691e5a32c46b0ed79e8fe9dc9a7b77eec67703
Franck Nijhof <git@frenck.dev>
2025-12-03 13:14:37 +0100

    Add final learn more and feedback links for purpose-specific triggers and conditions preview feature (#157830)

--- homeassistant/generated/labs.py
+++ homeassistant/generated/labs.py
@@ -6,8 +6,8 @@ To update, run python3 -m script.hassfest
 LABS_PREVIEW_FEATURES = {
     "automation": {
         "new_triggers_conditions": {
-            "feedback_url": "",
-            "learn_more_url": "",
+            "feedback_url": "https://forms.gle/fWFZqf5MzuwWTsCH8",
+            "learn_more_url": "https://www.home-assistant.io/blog/2025/12/03/release-202512/#purpose-specific-triggers-and-conditions",
             "report_issue_url": "https://github.com/home-assistant/core/issues/new?template=bug_report.yml&integration_link=https://www.home-assistant.io/integrations/automation&integration_name=Automation",
         },
     },
