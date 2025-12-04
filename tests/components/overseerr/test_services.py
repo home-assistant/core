28e847639a4eb34afcacc83da1cc355afb5c5a14
Felix Schneider <mail@fschneider.me>
2025-12-04 22:04:42 +0100

    test: add test for request media service

--- tests/components/overseerr/test_services.py
+++ tests/components/overseerr/test_services.py
@@ -1,20 +1,28 @@
 """Tests for the Overseerr services."""
 
+import dataclasses
 from unittest.mock import AsyncMock
 
 import pytest
 from python_overseerr import OverseerrConnectionError
+from python_overseerr.models import MediaType
 from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.components.overseerr.const import (
+    ATTR_LIMIT,
+    ATTR_MEDIA_TYPE,
     ATTR_REQUESTED_BY,
+    ATTR_SEASONS,
     ATTR_SORT_ORDER,
     ATTR_STATUS,
+    ATTR_TMDB_ID,
     DOMAIN,
 )
 from homeassistant.components.overseerr.services import (
     ATTR_QUERY,
     SERVICE_GET_REQUESTS,
+    SERVICE_REQUEST_MEDIA,
+    SERVICE_SEARCH_AND_REQUEST,
     SERVICE_SEARCH_MEDIA,
     parse_seasons_input,
 )
@@ -92,22 +100,6 @@ async def test_service_search_media(
 
     await setup_integration(hass, mock_config_entry)
 
-    response = await hass.services.async_call(
-        DOMAIN,
-        SERVICE_SEARCH_MEDIA,
-        {
-            ATTR_CONFIG_ENTRY_ID: mock_config_entry.entry_id,
-            ATTR_QUERY: "test",
-        },
-        blocking=True,
-        return_response=True,
-    )
-    assert response == {"results": []}
-    mock_overseerr_client.search.assert_called_once_with("test")
-
-    # Reset mock
-    mock_overseerr_client.search.reset_mock()
-
     # Test with a query containing spaces
     response = await hass.services.async_call(
         DOMAIN,
@@ -121,6 +113,40 @@ async def test_service_search_media(
     )
     assert response == {"results": []}
     mock_overseerr_client.search.assert_called_once_with("test query with spaces")
+
+
+async def test_service_request_media(
+    hass: HomeAssistant,
+    mock_overseerr_client: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test the request_media service."""
+
+    # Mock the create request method
+    @dataclasses.dataclass
+    class RequestWithMediaMock:
+        tmdb_id: str = "123456789"
+        media_type: MediaType = MediaType.TV
+
+    mock_overseerr_client.create_request.return_value = RequestWithMediaMock()
+
+    await setup_integration(hass, mock_config_entry)
+
+    response = await hass.services.async_call(
+        DOMAIN,
+        SERVICE_REQUEST_MEDIA,
+        {
+            ATTR_CONFIG_ENTRY_ID: mock_config_entry.entry_id,
+            ATTR_MEDIA_TYPE: "tv",
+            ATTR_TMDB_ID: "123456789",
+            ATTR_SEASONS: "1",
+        },
+        blocking=True,
+        return_response=True,
+    )
+
+    assert response == {"request": {"media_type": MediaType.TV, "tmdb_id": 123456789}}
 
 
 @pytest.mark.parametrize(
@@ -175,7 +201,12 @@ async def test_services_connection_error(
     ("service", "payload"),
     [
         (SERVICE_GET_REQUESTS, {}),
-        (SERVICE_SEARCH_MEDIA, {ATTR_QUERY: "test"}),
+        (SERVICE_SEARCH_MEDIA, {ATTR_QUERY: "test", ATTR_LIMIT: 3}),
+        (
+            SERVICE_REQUEST_MEDIA,
+            {ATTR_MEDIA_TYPE: "tv", ATTR_TMDB_ID: 123456789, ATTR_SEASONS: "1"},
+        ),
+        (SERVICE_SEARCH_AND_REQUEST, {ATTR_QUERY: "test", ATTR_SEASONS: "1"}),
     ],
 )
 async def test_service_entry_availability(
@@ -217,6 +248,7 @@ async def test_service_entry_availability(
     ("seasons_input", "expected_seasons"),
     [
         ("1", [1]),
+        ("1,", [1]),
         ("1,2,3", [1, 2, 3]),
         ("1, 2, 3", [1, 2, 3]),
         (" 1 ,     2,  3    ", [1, 2, 3]),
