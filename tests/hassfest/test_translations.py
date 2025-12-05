503178a544618aef675723eaf79dc58db1e37b75
jbouwh <jan@jbsoft.nl>
2025-12-05 18:55:50 +0000

    Add test

--- tests/hassfest/test_translations.py
+++ tests/hassfest/test_translations.py
@@ -19,3 +19,19 @@ def test_string_with_no_placeholders_in_single_quotes() -> None:
         "No placeholder",
     ):
         schema(value)
+
+
+@pytest.mark.parametrize(
+    "translation_string",
+    [
+        "An example is: https://example.com.",
+        "An image path like /config/www/image.jpg",
+        "www.example.com",
+    ],
+)
+def test_no_placeholders_used_for_urls(translation_string: str) -> None:
+    """Test string with no placeholders in single quotes."""
+    schema = vol.Schema(translations.translation_value_validator)
+
+    with pytest.raises(vol.Invalid):
+        schema(translation_string)
