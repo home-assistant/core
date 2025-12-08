82a4144d27a79e3ba6accee1e4183a9ac0844939
Michel D'Astous <213245943+mikedast@users.noreply.github.com>
2025-12-08 10:54:02 -0500

    fix formatting error in test

--- tests/components/webhook/test_trigger.py
+++ tests/components/webhook/test_trigger.py
@@ -379,6 +379,7 @@ async def test_webhook_template(
     assert len(events) == 1
     assert events[0].data["hello"] == "yo world"
 
+
 async def test_webhook_query_json_header_no_payload(
     hass: HomeAssistant, hass_client_no_auth: ClientSessionGenerator
 ) -> None:
