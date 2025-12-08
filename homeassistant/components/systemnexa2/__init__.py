0dda35638364754fa7a4371796db71fc64b2f127
konsulten <nordmarkclaes@gmail.com>
2025-12-08 19:44:32 +0100

    systemnexa

---
+++ homeassistant/components/systemnexa2/__init__.py
@@ -0,0 +1,158 @@
+"""The System Nexa 2 integration."""
+
+from functools import partial
+import logging
+from typing import Final
+
+from sn2 import DeviceInitializationError
+from sn2.device import (
+    ConnectionStatus,
+    Device,
+    InformationData,
+    InformationUpdate,
+    OnOffSetting,
+    Setting,
+    SettingsUpdate,
+    StateChange,
+    UpdateEvent,
+)
+import voluptuous as vol
+
+from homeassistant.const import CONF_HOST, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.typing import ConfigType
+
+from .helpers import NexaSystem2RuntimeData, SystemNexa2ConfigEntry
+from .light import SN2Light
+from .sensor import SensorValue
+from .switch import ConfigurationSwitch, SN2SwitchPlug
+
+_LOGGER = logging.getLogger(__name__)
+
+# Define constants for the component
+DOMAIN = "systemnexa2"
+SWITCH_MODELS = ["WBR-01"]
+PLUG_MODELS = ["WPR-01", "WPO-01"]
+LIGHT_MODELS = ["WBD-01", "WPD-01"]
+
+# Configuration schema
+CONFIG_SCHEMA = vol.Schema(
+    {DOMAIN: vol.Schema({})},
+    extra=vol.ALLOW_EXTRA,
+)
+PLATFORMS: Final = [Platform.LIGHT, Platform.SENSOR, Platform.SWITCH]
+
+
+async def async_setup(hass: HomeAssistant, _config: ConfigType) -> bool:
+    """Set up the component from configuration.yaml."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: SystemNexa2ConfigEntry) -> bool:
+    """Set up from a config entry."""
+    entry_process_update = partial(_process_update, entry)
+    device = Device(host=entry.data[CONF_HOST], on_update=entry_process_update)
+    try:
+        await device.initialize()
+    except DeviceInitializationError as e:
+        raise ConfigEntryNotReady from e
+    if device.info_data is None:
+        return False
+
+    # Store device info
+    device_registry = dr.async_get(hass)
+    device_registry.async_get_or_create(
+        config_entry_id=entry.entry_id,
+        identifiers={(DOMAIN, entry.entry_id)},
+        manufacturer="NEXA",
+        name=device.info_data.name,
+        model=device.info_data.model,
+        sw_version=device.info_data.sw_version,
+        hw_version=str(device.info_data.hw_version),
+    )
+    device_info = DeviceInfo(
+        identifiers={(DOMAIN, entry.entry_id)},
+        manufacturer="NEXA",
+        name=device.info_data.name,
+        model=device.info_data.model,
+        sw_version=device.info_data.sw_version,
+        hw_version=str(device.info_data.hw_version),
+    )
+    entry.runtime_data = NexaSystem2RuntimeData(device=device, device_info=device_info)
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await device.connect()
+
+    return True
+
+
+def _handle_connection_status(
+    entry: SystemNexa2ConfigEntry, *, connected: bool
+) -> None:
+    """Handle connection status updates."""
+    for entity in entry.runtime_data.config_entries:
+        if entity.available != connected:
+            entity.available = connected
+            entity.async_write_ha_state()
+
+
+def _handle_state_change(entry: SystemNexa2ConfigEntry, state: float) -> None:
+    """Handle state change updates."""
+    main_entry = entry.runtime_data.main_entry
+    if isinstance(main_entry, SN2Light):
+        main_entry.handle_state_update(state)
+    elif isinstance(main_entry, SN2SwitchPlug):
+        main_entry.handle_state_update(state=bool(state))
+
+
+def _handle_settings_update(
+    entry: SystemNexa2ConfigEntry, settings: list[Setting]
+) -> None:
+    """Handle settings updates."""
+    for entity in entry.runtime_data.config_entries:
+        if isinstance(entity, ConfigurationSwitch):
+            for setting in settings:
+                if isinstance(setting, OnOffSetting) and entity.name == setting.name:
+                    entity.handle_state_update(is_on=setting.is_enabled())
+
+
+def _handle_information_update(
+    entry: SystemNexa2ConfigEntry, information: InformationData
+) -> None:
+    """Handle information updates."""
+    for entity in entry.runtime_data.config_entries:
+        if isinstance(entity, SensorValue):
+            if entity.name == "Wifi":
+                entity.handle_state_update(information.wifi_dbm)
+            elif entity.name == "Wifi SSID":
+                entity.handle_state_update(information.wifi_ssid)
+
+
+async def _process_update(
+    entry: SystemNexa2ConfigEntry, update_event: UpdateEvent
+) -> None:
+    match update_event:
+        case ConnectionStatus(connected):
+            _handle_connection_status(entry, connected=connected)
+        case StateChange(state):
+            _handle_state_change(entry, state)
+        case SettingsUpdate(settings):
+            _handle_settings_update(entry, settings)
+        case InformationUpdate(information):
+            _handle_information_update(entry, information)
+
+
+async def async_unload_entry(
+    hass: HomeAssistant, entry: SystemNexa2ConfigEntry
+) -> bool:
+    """Unload a config entry."""
+    if entry.runtime_data.device:
+        _LOGGER.info("Unload")
+        await entry.runtime_data.device.disconnect()
+
+    # Unload the platforms
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
---
+++ homeassistant/components/systemnexa2/entity.py
@@ -0,0 +1,33 @@
+"""Base entity for SystemNexa2 integration."""
+
+from sn2.device import Device
+
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity import Entity
+
+
+class SystemNexa2Entity(Entity):
+    """Base entity class for SystemNexa2 devices."""
+
+    _attr_should_poll = False
+    _attr_has_entity_name = True
+
+    def __init__(
+        self,
+        device: Device,
+        entry_id: str,
+        unique_entity_id: str,
+        name: str,
+        device_info: DeviceInfo,
+    ) -> None:
+        """Initialize the SystemNexa2 entity."""
+        self._device = device
+        self._attr_name = name
+        self._attr_unique_id = f"{entry_id}-{unique_entity_id}"
+        self._attr_device_info = device_info
+
+    async def async_added_to_hass(self) -> None:
+        """Add data updated listener after this object has been initialized."""
+
+    async def async_will_remove_from_hass(self) -> None:
+        """Remove data updated listener after this object has been initialized."""
