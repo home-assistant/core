4f4a59dcd3aa30d572bcf936b714540a615e8d8a
epenet <6771947+epenet@users.noreply.github.com>
2025-12-09 17:29:11 +0100

    Add Tuya local_strategy to Tuya diagnostic (#158450)

--- tests/components/tuya/conftest.py
+++ tests/components/tuya/conftest.py
@@ -176,6 +176,7 @@ async def _create_device(hass: HomeAssistant, mock_device_code: str) -> Customer
     if device.update_time:
         device.update_time = int(dt_util.as_timestamp(device.update_time))
     device.support_local = details.get("support_local")
+    device.local_strategy = details.get("local_strategy")
     device.mqtt_connected = details.get("mqtt_connected")
 
     device.function = {
