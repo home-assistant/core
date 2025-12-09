c0bff5c1340dab4214880caffab8d4ac3e3ad6e1
Joostlek <joostlek@outlook.com>
2025-12-09 15:11:24 +0100

    Add temperature entities to SmartThings One Door fridge

--- homeassistant/components/smartthings/sensor.py
+++ homeassistant/components/smartthings/sensor.py
@@ -912,8 +912,9 @@ CAPABILITY_TO_SENSORS: dict[
                     if Capability.CUSTOM_OUTING_MODE in status
                     else None
                 ),
-                component_fn=lambda component: component
-                in {"freezer", "cooler", "onedoor"},
+                component_fn=(
+                    lambda component: component in {"freezer", "cooler", "onedoor"}
+                ),
                 component_translation_key={
                     "freezer": "freezer_temperature",
                     "cooler": "cooler_temperature",
