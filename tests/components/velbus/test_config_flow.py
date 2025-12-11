9a0fbf9d156206065953e21f99ba7fd241528160
Maikel Punie <maikel.punie@gmail.com>
2025-12-11 13:02:11 +0100

    make sure we have 100% coverage again

--- tests/components/velbus/test_config_flow.py
+++ tests/components/velbus/test_config_flow.py
@@ -156,6 +156,64 @@ async def test_user_network_succes(
     assert data[CONF_PORT] == expected
 
 
+@pytest.mark.usefixtures("controller_connection_failed")
+async def test_user_network_connect_failure(
+    hass: HomeAssistant,
+) -> None:
+    """Test user network config."""
+    # inttial menu show
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+    assert result
+    assert result.get("flow_id")
+    assert result.get("type") is FlowResultType.MENU
+    assert result.get("step_id") == "user"
+    assert result.get("menu_options") == ["network", "usbselect"]
+    # select the network option
+    result = await hass.config_entries.flow.async_configure(
+        result.get("flow_id"),
+        {"next_step_id": "network"},
+    )
+    assert result["type"] is FlowResultType.FORM
+    # fill in the network form
+    result = await hass.config_entries.flow.async_configure(
+        result.get("flow_id"),
+        {
+            CONF_HOST: "velbus",
+            CONF_PORT: 6000,
+            CONF_TLS: True,
+            CONF_PASSWORD: "password",
+        },
+    )
+    assert result
+    assert result.get("type") is FlowResultType.FORM
+    assert result.get("errors") == {"host": "cannot_connect"}
+
+
+@pytest.mark.usefixtures("controller_connection_failed")
+@patch("serial.tools.list_ports.comports", MagicMock(return_value=[com_port()]))
+async def test_user_usb_connect_failure(hass: HomeAssistant) -> None:
+    """Test user usb step."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+    result = await hass.config_entries.flow.async_configure(
+        result.get("flow_id"),
+        {"next_step_id": "usbselect"},
+    )
+    assert result["type"] is FlowResultType.FORM
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_PORT: USB_DEV,
+        },
+    )
+    assert result
+    assert result.get("type") is FlowResultType.FORM
+    assert result.get("errors") == {"port": "cannot_connect"}
+
+
 @pytest.mark.usefixtures("controller")
 @patch("serial.tools.list_ports.comports", MagicMock(return_value=[com_port()]))
 async def test_user_usb_succes(hass: HomeAssistant) -> None:
