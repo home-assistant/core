c25b6cef98d6555af1734dc590c4c22722828ac4
Muhammad Ihsan <113167213+ihsan-cielo@users.noreply.github.com>
2025-12-11 03:53:45 +0100

    Minor Fix: Removing setting of coordinator in async_setup_entry
    Co-authored-by: Robert Resch <robert@resch.dev>

--- homeassistant/components/cielo_home/__init__.py
+++ homeassistant/components/cielo_home/__init__.py
@@ -17,7 +17,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: CieloHomeConfigEntry) ->
     coordinator = CieloDataUpdateCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
     entry.runtime_data = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
