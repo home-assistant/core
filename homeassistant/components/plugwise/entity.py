dbf902aa7c332137fc60fcf5556d928b93749380
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-12-13 11:29:03 +0100

    Add back plugwise import

--- homeassistant/components/plugwise/entity.py
+++ homeassistant/components/plugwise/entity.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+from plugwise import GwEntityData
+
 from homeassistant.const import ATTR_NAME, ATTR_VIA_DEVICE, CONF_HOST
 from homeassistant.helpers.device_registry import (
     CONNECTION_NETWORK_MAC,
