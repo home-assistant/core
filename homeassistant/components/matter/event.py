f2eeaee9f1e8d56f6b1344ae2fd85ec17f189cbe
Pierre Hoffmeister <pierre.git@posteo.de>
2025-12-14 22:31:48 +0100

    pass information for ongoing press events

--- homeassistant/components/matter/event.py
+++ homeassistant/components/matter/event.py
@@ -68,14 +68,11 @@ class MatterEventEntity(MatterEntity, EventEntity):
             event_types.append("switch_latched")
         elif feature_map & SwitchFeature.kMomentarySwitchMultiPress:
             # Momentary switch with multi press support
-            # NOTE: We ignore 'multi press ongoing' as it doesn't make a lot
-            # of sense and many devices do not support it.
-            # Instead we report on the 'multi press complete' event with the number
-            # of presses.
             max_presses_supported = self.get_matter_attribute_value(
                 clusters.Switch.Attributes.MultiPressMax
             )
-            max_presses_supported = min(max_presses_supported or 2, 8)
+            max_presses_supported = min(max_presses_supported or 2, 18)
+            event_types.append("multi_press_ongoing")
             for i in range(max_presses_supported):
                 event_types.append(f"multi_press_{i + 1}")  # noqa: PERF401
         elif feature_map & SwitchFeature.kMomentarySwitch:
