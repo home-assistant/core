d3af0acb93d96cb69922fffb4ca23d3221a07744
Ryan Jones <ryan.jones@gentex.com>
2025-12-15 14:33:15 +0000

    Add test for unique configurations

--- tests/components/gentex_homelink/test_config_flow.py
+++ tests/components/gentex_homelink/test_config_flow.py
@@ -1,15 +1,18 @@
 """Test the homelink config flow."""
 
+import time
 from unittest.mock import AsyncMock
 
 import botocore.exceptions
 import pytest
 
 from homeassistant.components.gentex_homelink.const import DOMAIN
-from homeassistant.config_entries import SOURCE_USER
+from homeassistant.config_entries import SOURCE_USER, ConfigEntryState
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
+
+from tests.common import MockConfigEntry
 
 
 async def test_full_flow(
@@ -40,6 +43,43 @@ async def test_full_flow(
         },
     }
     assert result["title"] == "SRPAuth"
+
+
+async def test_unique_configurations(
+    hass: HomeAssistant, mock_srp_auth: AsyncMock, mock_setup_entry: AsyncMock
+) -> None:
+    """Check full flow."""
+    user_credentials = {CONF_EMAIL: "test@test.com", CONF_PASSWORD: "SomePassword"}
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id=user_credentials[CONF_EMAIL],
+        version=1,
+        data={
+            "auth_implementation": "gentex_homelink",
+            "token": {
+                "expires_at": time.time() + 10000,
+                "access_token": "",
+                "refresh_token": "",
+            },
+            "last_update_id": None,
+        },
+        state=ConfigEntryState.LOADED,
+    )
+    config_entry.add_to_hass(hass)
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "user"
+    assert not result["errors"]
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input=user_credentials,
+    )
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "already_configured"
 
 
 @pytest.mark.parametrize(
