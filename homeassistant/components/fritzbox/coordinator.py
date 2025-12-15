d1dc1936104b4f429909e7759026fb1805aff380
mib1185 <mail@mib85.de>
2025-12-15 23:35:07 +0000

    make generator better readable

--- homeassistant/components/fritzbox/coordinator.py
+++ homeassistant/components/fritzbox/coordinator.py
@@ -100,9 +100,7 @@ class FritzboxDataUpdateCoordinator(DataUpdateCoordinator[FritzboxCoordinatorDat
 
         available_main_ains = [
             ain
-            for ain, dev in data.devices.items()
-            | data.templates.items()
-            | data.triggers.items()
+            for ain, dev in (data.devices | data.templates | data.triggers).items()
             if dev.device_and_unit_id[1] is None
         ]
         device_reg = dr.async_get(self.hass)
