ed04dd87fce4598a34157043e458bda291e98ad2
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-12-16 19:13:09 +0100

    Try 3

--- homeassistant/components/plugwise/coordinator.py
+++ homeassistant/components/plugwise/coordinator.py
@@ -90,41 +90,39 @@ class PlugwiseDataUpdateCoordinator(DataUpdateCoordinator[dict[str, GwEntityData
 
     async def _async_update_data(self) -> dict[str, GwEntityData]:
         """Fetch data from Plugwise."""
-        if not self._connected:
-            try:
-                await self._connect()
-            except ConnectionFailedError as err:
-                raise UpdateFailed(
-                    translation_domain=DOMAIN,
-                    translation_key="failed_to_connect",
-                ) from err
-            except InvalidAuthentication as err:
-                raise ConfigEntryError(
-                    translation_domain=DOMAIN,
-                    translation_key="authentication_failed",
-                ) from err
-            except InvalidSetupError as err:
-                raise ConfigEntryError(
-                    translation_domain=DOMAIN,
-                    translation_key="invalid_setup",
-                ) from err
-            except (InvalidXMLError, ResponseError) as err:
-                raise UpdateFailed(
-                    translation_domain=DOMAIN,
-                    translation_key="response_error",
-                ) from err
-            except UnsupportedDeviceError as err:
-                raise ConfigEntryError(
-                    translation_domain=DOMAIN,
-                    translation_key="unsupported_firmware",
-                ) from err
-
         try:
+            if not self._connected:
+                await self._connect()
             data = await self.api.async_update()
+        except ConnectionFailedError as err:
+            raise UpdateFailed(
+                translation_domain=DOMAIN,
+                translation_key="failed_to_connect",
+            ) from err
+        except InvalidAuthentication as err:
+            raise ConfigEntryError(
+                translation_domain=DOMAIN,
+                translation_key="authentication_failed",
+            ) from err
+        except InvalidSetupError as err:
+            raise ConfigEntryError(
+                translation_domain=DOMAIN,
+                translation_key="invalid_setup",
+            ) from err
+        except (InvalidXMLError, ResponseError) as err:
+            raise UpdateFailed(
+                translation_domain=DOMAIN,
+                translation_key="response_error",
+            ) from err
         except PlugwiseError as err:
             raise UpdateFailed(
                 translation_domain=DOMAIN,
                 translation_key="data_incomplete_or_missing",
+            ) from err
+        except UnsupportedDeviceError as err:
+            raise ConfigEntryError(
+                translation_domain=DOMAIN,
+                translation_key="unsupported_firmware",
             ) from err
 
         self._async_add_remove_devices(data)
