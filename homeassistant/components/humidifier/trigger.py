91bdd8a0dd29725bbcc5a5bbc215d0de191929be
Erik <erik@montnemery.com>
2025-12-19 14:25:49 +0100

    Adapt to upstream changes

--- homeassistant/components/humidifier/trigger.py
+++ homeassistant/components/humidifier/trigger.py
@@ -4,21 +4,21 @@ from homeassistant.const import STATE_OFF, STATE_ON
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.trigger import (
     Trigger,
-    make_entity_state_attribute_trigger,
-    make_entity_state_trigger,
+    make_entity_target_state_attribute_trigger,
+    make_entity_target_state_trigger,
 )
 
 from .const import ATTR_ACTION, DOMAIN, HumidifierAction
 
 TRIGGERS: dict[str, type[Trigger]] = {
-    "started_drying": make_entity_state_attribute_trigger(
+    "started_drying": make_entity_target_state_attribute_trigger(
         DOMAIN, ATTR_ACTION, HumidifierAction.DRYING
     ),
-    "started_humidifying": make_entity_state_attribute_trigger(
+    "started_humidifying": make_entity_target_state_attribute_trigger(
         DOMAIN, ATTR_ACTION, HumidifierAction.HUMIDIFYING
     ),
-    "turned_off": make_entity_state_trigger(DOMAIN, STATE_OFF),
-    "turned_on": make_entity_state_trigger(DOMAIN, STATE_ON),
+    "turned_off": make_entity_target_state_trigger(DOMAIN, STATE_OFF),
+    "turned_on": make_entity_target_state_trigger(DOMAIN, STATE_ON),
 }
 
 
