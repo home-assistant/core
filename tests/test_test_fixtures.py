1d4e7ca94ecc0b912a7d970c1cdd31fde6e67530
Erik <erik@montnemery.com>
2025-12-19 10:12:59 +0100

    Fix evict_faked_translations fixture

--- tests/test_test_fixtures.py
+++ tests/test_test_fixtures.py
@@ -91,16 +91,22 @@ async def test_evict_faked_translations_assumptions(hass: HomeAssistant) -> None
 
 
 async def test_evict_faked_translations(hass: HomeAssistant, translations_once) -> None:
-    """Test the evict_faked_translations fixture."""
+    """Test the evict_faked_translations fixture.
+
+    In this test, we load translations for a fake integration backed by a module
+    and a real integration, then ensure that after the fixture is torn down, only
+    the real integration remains in the translations cache.
+    """
     cache: translation._TranslationsCacheData = translations_once.kwargs["return_value"]
     fake_domain = "test"
     real_domain = "homeassistant"
 
-    # Evict the real domain from the cache in case it's been loaded before
-    cache.loaded["en"].discard(real_domain)
+    if "en" in cache.loaded:
+        # Evict the real domain from the cache in case it's been loaded before
+        cache.loaded["en"].discard(real_domain)
 
-    assert fake_domain not in cache.loaded["en"]
-    assert real_domain not in cache.loaded["en"]
+        assert fake_domain not in cache.loaded["en"]
+        assert real_domain not in cache.loaded["en"]
 
     # The evict_faked_translations fixture has module scope, so we set it up and
     # tear it down manually
@@ -110,8 +116,52 @@ async def test_evict_faked_translations(hass: HomeAssistant, translations_once) 
     # Set up the evict_faked_translations fixture
     next(gen)
 
+    # Try loading translations for mock integration
     mock_integration(hass, MockModule(fake_domain), built_in=True)
     await translation.async_load_integrations(hass, {fake_domain, real_domain})
+    assert fake_domain in cache.loaded["en"]
+    assert real_domain in cache.loaded["en"]
+
+    # Tear down the evict_faked_translations fixture
+    with pytest.raises(StopIteration):
+        next(gen)
+
+    # The mock integration should be removed from the cache, the real domain should still be there
+    assert fake_domain not in cache.loaded["en"]
+    assert real_domain in cache.loaded["en"]
+
+
+async def test_evict_faked_translations_2(
+    hass: HomeAssistant, translations_once
+) -> None:
+    """Test the evict_faked_translations fixture.
+
+    In this test, we load translations for a fake integration not backed by a module
+    and a real integration, then ensure that after the fixture is torn down, only
+    the real integration remains in the translations cache.
+    """
+    cache: translation._TranslationsCacheData = translations_once.kwargs["return_value"]
+    fake_domain = "test"
+    real_domain = "homeassistant"
+
+    if "en" in cache.loaded:
+        # Evict the real domain from the cache in case it's been loaded before
+        cache.loaded["en"].discard(real_domain)
+
+        assert fake_domain not in cache.loaded["en"]
+        assert real_domain not in cache.loaded["en"]
+
+    # The evict_faked_translations fixture has module scope, so we set it up and
+    # tear it down manually
+    real_func = get_real_func(evict_faked_translations)
+    gen: Generator = real_func(translations_once)
+
+    # Set up the evict_faked_translations fixture
+    next(gen)
+
+    # Try loading translations for non existing integration
+    await translation.async_load_integrations(hass, {fake_domain, real_domain})
+
     assert fake_domain in cache.loaded["en"]
     assert real_domain in cache.loaded["en"]
 
