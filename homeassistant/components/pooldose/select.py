ebc8138f05ed515b4b0095590c2c18213b2243e6
Lukas <12813107+lmaertin@users.noreply.github.com>
2025-12-20 18:23:25 +0100

    Pooldose: Add parallel updates (Silver Qly Scale) (#159479)

--- homeassistant/components/pooldose/select.py
+++ homeassistant/components/pooldose/select.py
@@ -20,6 +20,8 @@ if TYPE_CHECKING:
 
 _LOGGER = logging.getLogger(__name__)
 
+PARALLEL_UPDATES = 1
+
 
 @dataclass(frozen=True, kw_only=True)
 class PooldoseSelectEntityDescription(SelectEntityDescription):
