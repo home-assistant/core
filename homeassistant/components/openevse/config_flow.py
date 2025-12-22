bf9b2c2deff4917a994ced68b3e913cb325c1e01
Colin L Rice <colin@daedrum.net>
2025-12-22 00:08:18 +0000

    openevse: config_flow.py cleanups

--- homeassistant/components/openevse/config_flow.py
+++ homeassistant/components/openevse/config_flow.py
@@ -1,15 +1,17 @@
 """Config flow for OpenEVSE integration."""
 
+from typing import Any
+
 import openevsewifi
 import voluptuous as vol
 
-from homeassistant import config_entries
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_HOST
 
 from .const import DOMAIN
 
 
-class OpenEVSEConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class OpenEVSEConfigFlow(ConfigFlow, domain=DOMAIN):
     """OpenEVSE config flow."""
 
     VERSION = 1
@@ -18,40 +20,43 @@ class OpenEVSEConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def check_status(self, host: str) -> bool:
         """Check if we can connect to the OpenEVSE charger."""
 
+        charger = openevsewifi.Charger(host)
         try:
-            charger = openevsewifi.Charger(host)
             result = await self.hass.async_add_executor_job(charger.getStatus)
         except AttributeError:
             return False
         else:
             return result is not None
 
-    async def async_step_user(self, user_input=None) -> config_entries.ConfigFlowResult:
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
         """Handle the initial step."""
 
+        errors = None
         if user_input is not None:
-            await self.async_set_unique_id(user_input[CONF_HOST])
-            self._abort_if_unique_id_configured()
+            self._async_abort_entries_match({CONF_HOST: user_input[CONF_HOST]})
 
-            if not await self.check_status(user_input[CONF_HOST]):
-                return self.async_show_form(
-                    step_id="user",
-                    data_schema=vol.Schema({vol.Required(CONF_HOST): str}),
-                    errors={CONF_HOST: "cannot_connect"},
+            if await self.check_status(user_input[CONF_HOST]):
+                return self.async_create_entry(
+                    title=f"OpenEVSE {user_input[CONF_HOST]}",
+                    data={
+                        CONF_HOST: user_input[CONF_HOST],
+                    },
                 )
+            errors = {CONF_HOST: "cannot_connect"}
 
-            return self.async_create_entry(
-                title=f"OpenEVSE {user_input[CONF_HOST]}",
-                data={
-                    CONF_HOST: user_input[CONF_HOST],
-                },
-            )
         return self.async_show_form(
-            step_id="user", data_schema=vol.Schema({vol.Required(CONF_HOST): str})
+            step_id="user",
+            data_schema=vol.Schema({vol.Required(CONF_HOST): str}),
+            errors=errors,
         )
 
-    async def async_step_import(self, data) -> config_entries.ConfigFlowResult:
+    async def async_step_import(self, data) -> ConfigFlowResult:
         """Handle the initial step."""
+
+        if not await self.check_status(data[CONF_HOST]):
+            self.async_abort(reason="Host is not available")
 
         return self.async_create_entry(
             title=f"OpenEVSE {data[CONF_HOST]}",
