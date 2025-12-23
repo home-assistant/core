802f41728b10a61562d2e98d9d6a71c2f671a35a
Paul Tarjan <github@paulisageek.com>
2025-12-23 23:13:27 +0000

    Hide update available when Tesla update is scheduled for later
    When an update is scheduled far in the future (more than 2 minutes),
    hide the update entirely by setting latest_version = installed_version.
    This makes the state "off" so no update notification appears.
    
    The update only shows as available when:
    - Status is "available" (not yet scheduled)
    - Status is "downloading" or "downloading_wifi_wait"
    - Status is "installing"
    - Status is "scheduled" AND within 2 minutes of starting

--- homeassistant/components/tesla_fleet/update.py
+++ homeassistant/components/tesla_fleet/update.py
@@ -88,13 +88,9 @@ class TeslaFleetUpdateEntity(TeslaFleetVehicleEntity, UpdateEntity):
             # Remove build from version
             self._attr_installed_version = self._attr_installed_version.split(" ")[0]
 
-        # Latest Version
-        if self._value in (
-            AVAILABLE,
-            SCHEDULED,
-            INSTALLING,
-            DOWNLOADING,
-            WIFI_WAIT,
+        # Latest Version - hide update if scheduled far in the future
+        if self._value in (AVAILABLE, INSTALLING, DOWNLOADING, WIFI_WAIT) or (
+            self._value == SCHEDULED and self._is_scheduled_soon()
         ):
             self._attr_latest_version = self.coordinator.data[
                 "vehicle_state_software_update_version"
