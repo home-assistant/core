9b28437815bf23c60365b9770edec186490d73e2
Kamil Breguła <mik-laj@users.noreply.github.com>
2025-12-25 06:24:36 +0100

    Add sensors to Google Drive (#156167)
    Co-authored-by: mik-laj <12058428+mik-laj@users.noreply.github.com>

--- homeassistant/components/google_drive/const.py
+++ homeassistant/components/google_drive/const.py
@@ -2,4 +2,9 @@
 
 from __future__ import annotations
 
+from datetime import timedelta
+
 DOMAIN = "google_drive"
+
+SCAN_INTERVAL = timedelta(hours=6)
+DRIVE_FOLDER_URL_PREFIX = "https://drive.google.com/drive/folders/"
