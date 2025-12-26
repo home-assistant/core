46b4d59751492ed83f50f5964972567663f229e8
dvdinth <dorus_van_dinther@hotmail.com>
2025-12-26 10:58:08 +0000

    removed sensor and binary_sensor platforms from intelliclima for initial pr

--- homeassistant/components/intelliclima/__init__.py
+++ homeassistant/components/intelliclima/__init__.py
@@ -9,7 +9,7 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from .const import LOGGER
 from .coordinator import IntelliClimaConfigEntry, IntelliClimaCoordinator
 
-PLATFORMS = [Platform.BINARY_SENSOR, Platform.FAN, Platform.SENSOR]
+PLATFORMS = [Platform.FAN]
 
 
 async def async_setup_entry(
