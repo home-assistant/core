d9e4d1a84989ec77fb8442d4a6eaaea7d7632eb3
dvdinth <dorus_van_dinther@hotmail.com>
2025-12-26 13:57:46 +0000

    changed discovery info statement to debug level

--- homeassistant/components/intelliclima/__init__.py
+++ homeassistant/components/intelliclima/__init__.py
@@ -30,7 +30,7 @@ async def async_setup_entry(
     # Fetch initial data
     await coordinator.async_config_entry_first_refresh()
 
-    LOGGER.info(
+    LOGGER.debug(
         "Discovered %d IntelliClima VMC device(s)",
         len(coordinator.data.ecocomfort2_devices),
     )
