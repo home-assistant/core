10425b5f95be3a55aeaab42074890ffa51881209
Patrick <14628713+patman15@users.noreply.github.com>
2025-12-27 13:47:20 +0000

    remove unnecessary hass.data init

--- homeassistant/components/bms_ble/__init__.py
+++ homeassistant/components/bms_ble/__init__.py
@@ -54,9 +54,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: BTBmsConfigEntry) -> boo
 
     # Query the device the first time, initialise coordinator.data
     await coordinator.async_config_entry_first_refresh()
-
-    # Insert the coordinator in the global registry
-    hass.data.setdefault(DOMAIN, {})
     entry.runtime_data = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
