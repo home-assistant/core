3015b81a338875fb651f33bf30fbb69cab716350
Lukas <12813107+lmaertin@users.noreply.github.com>
2025-12-29 21:41:24 +0000

    remove irrelevant diagnositcs meta data of config entry and coordinator

--- homeassistant/components/pooldose/diagnostics.py
+++ homeassistant/components/pooldose/diagnostics.py
@@ -29,22 +29,6 @@ async def async_get_config_entry_diagnostics(
     coordinator = entry.runtime_data
 
     return {
-        "entry": {
-            "title": entry.title,
-            "unique_id": entry.unique_id,
-            "version": entry.version,
-        },
         "device_info": async_redact_data(coordinator.device_info, TO_REDACT),
-        "coordinator": {
-            "update_interval": (
-                coordinator.update_interval.total_seconds()
-                if coordinator.update_interval
-                else None
-            ),
-            "last_update_success": coordinator.last_update_success,
-            "last_exception": str(coordinator.last_exception)
-            if coordinator.last_exception
-            else None,
-        },
         "data": coordinator.data,
     }
