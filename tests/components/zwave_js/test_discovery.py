39f7fb961870a5952825ed4cc0bf80a7cae133a3
Mark Ruys <mark.ruys@peercode.nl>
2025-12-30 20:41:15 +0000

    Add tests

--- tests/components/zwave_js/test_discovery.py
+++ tests/components/zwave_js/test_discovery.py
@@ -240,6 +240,37 @@ async def test_shelly_001p10_disabled_entities(
     assert state
 
 
+async def test_qubino_flush_shutter_disabled_entities(
+    hass: HomeAssistant,
+    entity_registry: er.EntityRegistry,
+    client,
+    qubino_flush_shutter,
+    integration,
+) -> None:
+    """Test that Qubino Flush Shutter entity created by endpoint 2 is disabled."""
+    entity_ids = [
+        "cover.flush_shutter_2",
+    ]
+    for entity_id in entity_ids:
+        state = hass.states.get(entity_id)
+        assert state is None
+        entry = entity_registry.async_get(entity_id)
+        assert entry
+        assert entry.disabled
+        assert entry.disabled_by is er.RegistryEntryDisabler.INTEGRATION
+
+        # Test enabling entity
+        updated_entry = entity_registry.async_update_entity(
+            entry.entity_id, disabled_by=None
+        )
+        assert updated_entry != entry
+        assert updated_entry.disabled is False
+
+    # Test if the main entity from endpoint 1 was created.
+    state = hass.states.get("cover.flush_shutter")
+    assert state
+
+
 async def test_merten_507801_disabled_enitites(
     hass: HomeAssistant,
     entity_registry: er.EntityRegistry,
