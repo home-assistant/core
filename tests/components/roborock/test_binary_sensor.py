cdb663edd64e75106dce95dd450f10bd95c04760
Allen Porter <allen.porter@gmail.com>
2025-12-30 05:13:52 +0000

    Update roborock binary sensor tests with snapshots
    Add a snapshot of the currently supported devices before we change behaior about which entities are supported by which devices.

--- tests/components/roborock/test_binary_sensor.py
+++ tests/components/roborock/test_binary_sensor.py
@@ -1,6 +1,7 @@
 """Test Roborock Binary Sensor."""
 
 import pytest
+from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
@@ -15,17 +16,9 @@ def platforms() -> list[Platform]:
 
 
 async def test_binary_sensors(
-    hass: HomeAssistant, setup_entry: MockConfigEntry
+    hass: HomeAssistant,
+    setup_entry: MockConfigEntry,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test binary sensors and check test values are correctly set."""
-    assert len(hass.states.async_all("binary_sensor")) == 10
-    assert hass.states.get("binary_sensor.roborock_s7_maxv_mop_attached").state == "on"
-    assert (
-        hass.states.get("binary_sensor.roborock_s7_maxv_water_box_attached").state
-        == "on"
-    )
-    assert (
-        hass.states.get("binary_sensor.roborock_s7_maxv_water_shortage").state == "off"
-    )
-    assert hass.states.get("binary_sensor.roborock_s7_maxv_cleaning").state == "off"
-    assert hass.states.get("binary_sensor.roborock_s7_maxv_charging").state == "on"
+    assert snapshot == hass.states.async_all("binary_sensor")
