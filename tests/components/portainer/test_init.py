f431c4239c09fdcd3e3390bcd4a0268ae56d2200
Erwin Douna <e.douna@gmail.com>
2025-12-30 20:18:53 +0000

    Portainer add config entry removal

--- tests/components/portainer/test_init.py
+++ tests/components/portainer/test_init.py
@@ -19,10 +19,13 @@ from homeassistant.const import (
     CONF_VERIFY_SSL,
 )
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
+from homeassistant.setup import async_setup_component
 
 from . import setup_integration
 
 from tests.common import MockConfigEntry
+from tests.typing import WebSocketGenerator
 
 
 @pytest.mark.parametrize(
@@ -69,3 +72,35 @@ async def test_migrations(hass: HomeAssistant) -> None:
     assert entry.data[CONF_URL] == "http://test_host"
     assert entry.data[CONF_API_TOKEN] == "test_key"
     assert entry.data[CONF_VERIFY_SSL] is True
+
+
+@pytest.mark.parametrize(
+    ("container_id", "expected_result"),
+    [("1", False), ("5", True)],
+    ids=("Present container", "Stale container"),
+)
+async def test_remove_config_entry_device(
+    hass: HomeAssistant,
+    mock_portainer_client: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+    device_registry: dr.DeviceRegistry,
+    hass_ws_client: WebSocketGenerator,
+    container_id: str,
+    expected_result: bool,
+) -> None:
+    """Test manually removing an stale device."""
+    assert await async_setup_component(hass, "config", {})
+    mock_config_entry.add_to_hass(hass)
+
+    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
+
+    device_entry = device_registry.async_get_or_create(
+        config_entry_id=mock_config_entry.entry_id,
+        identifiers={(DOMAIN, f"{mock_config_entry.entry_id}_{container_id}")},
+    )
+
+    ws_client = await hass_ws_client(hass)
+    response = await ws_client.remove_device(
+        device_entry.id, mock_config_entry.entry_id
+    )
+    assert response["success"] == expected_result
