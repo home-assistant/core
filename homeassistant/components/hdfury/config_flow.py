298beac09170630d5d11a75fec8c4c758bb435ac
Glenn de Haan <glenn@dehaan.cloud>
2026-01-01 13:17:10 +0000

    Removed options flow

--- homeassistant/components/hdfury/config_flow.py
+++ homeassistant/components/hdfury/config_flow.py
@@ -7,12 +7,11 @@ from aiohttp import ClientError
 from hdfury import HDFuryAPI, HDFuryError
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigEntry, ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_HOST
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN
-from .options_flow import HDFuryOptionsFlow
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -59,9 +58,3 @@ class HDFuryConfigFlow(ConfigFlow, domain=DOMAIN):
             return False
 
         return True
-
-    @staticmethod
-    def async_get_options_flow(config_entry: ConfigEntry) -> HDFuryOptionsFlow:
-        """Register Options Flow for HDFury."""
-
-        return HDFuryOptionsFlow()
