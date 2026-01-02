6aaafaf51d230fc161207d05609182113d7b50e2
Glenn de Haan <glenn@dehaan.cloud>
2026-01-02 08:53:42 +0000

    Removed old variable from const.py

--- homeassistant/components/hdfury/const.py
+++ homeassistant/components/hdfury/const.py
@@ -1,10 +1,3 @@
 """Constants for HDFury Integration."""
 
 DOMAIN = "hdfury"
-
-OPTION_INPUT_LABELS = {
-    "Input 0": "Input 0",
-    "Input 1": "Input 1",
-    "Input 2": "Input 2",
-    "Input 3": "Input 3",
-}
