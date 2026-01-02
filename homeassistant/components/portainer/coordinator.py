8bf7e178d5514c4a3aa9fcd6bbd21b30633d5c88
Erwin Douna <e.douna@gmail.com>
2026-01-02 13:33:47 +0000

    Add endpoint system df information

--- homeassistant/components/portainer/coordinator.py
+++ homeassistant/components/portainer/coordinator.py
@@ -14,7 +14,11 @@ from pyportainer import (
     PortainerConnectionError,
     PortainerTimeoutError,
 )
-from pyportainer.models.docker import DockerContainer, DockerContainerStats
+from pyportainer.models.docker import (
+    DockerContainer,
+    DockerContainerStats,
+    DockerSystemDF,
+)
 from pyportainer.models.docker_inspect import DockerInfo, DockerVersion
 from pyportainer.models.portainer import Endpoint
 
@@ -43,6 +47,7 @@ class PortainerCoordinatorData:
     containers: dict[str, PortainerContainerData]
     docker_version: DockerVersion
     docker_info: DockerInfo
+    docker_system_df: DockerSystemDF
 
 
 @dataclass(slots=True)
@@ -146,6 +151,7 @@ class PortainerCoordinator(DataUpdateCoordinator[dict[int, PortainerCoordinatorD
                 containers = await self.portainer.get_containers(endpoint.id)
                 docker_version = await self.portainer.docker_version(endpoint.id)
                 docker_info = await self.portainer.docker_info(endpoint.id)
+                docker_system_df = await self.portainer.docker_system_df(endpoint.id)
 
                 container_map: dict[str, PortainerContainerData] = {}
 
@@ -210,6 +216,7 @@ class PortainerCoordinator(DataUpdateCoordinator[dict[int, PortainerCoordinatorD
                 containers=container_map,
                 docker_version=docker_version,
                 docker_info=docker_info,
+                docker_system_df=docker_system_df,
             )
 
         self._async_add_remove_endpoints(mapped_endpoints)
