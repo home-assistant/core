302f14af2917f4e82e00b342135690269585ecd5
Dos Moonen <darsstar@gmail.com>
2026-01-05 00:39:25 +0000

    Solax: implement reconfiguration

--- homeassistant/components/solax/config_flow.py
+++ homeassistant/components/solax/config_flow.py
@@ -9,7 +9,12 @@ from solax import real_time_api
 from solax.discovery import DiscoveryError
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import (
+    SOURCE_RECONFIGURE,
+    SOURCE_USER,
+    ConfigFlow,
+    ConfigFlowResult,
+)
 from homeassistant.const import CONF_IP_ADDRESS, CONF_PASSWORD, CONF_PORT
 from homeassistant.helpers import config_validation as cv
 
@@ -46,10 +51,14 @@ class SolaxConfigFlow(ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle the initial step."""
-        errors: dict[str, Any] = {}
+        errors: dict[str, str] = {}
         if user_input is None:
             return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+                step_id=SOURCE_USER,
+                data_schema=self.add_suggested_values_to_schema(
+                    STEP_USER_DATA_SCHEMA, user_input
+                ),
+                errors=errors,
             )
 
         try:
@@ -65,5 +74,48 @@ class SolaxConfigFlow(ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title=serial_number, data=user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id=SOURCE_USER,
+            data_schema=self.add_suggested_values_to_schema(
+                STEP_USER_DATA_SCHEMA, user_input
+            ),
+            errors=errors,
+        )
+
+    async def async_step_reconfigure(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle reconfiguration."""
+        errors: dict[str, str] = {}
+        if user_input is None:
+            return self.async_show_form(
+                step_id=SOURCE_RECONFIGURE,
+                data_schema=self.add_suggested_values_to_schema(
+                    STEP_USER_DATA_SCHEMA,
+                    self._get_reconfigure_entry().data,
+                ),
+                errors=errors,
+            )
+        try:
+            serial_number = await validate_api(user_input)
+        except (ConnectionError, DiscoveryError):
+            errors["base"] = "cannot_connect"
+        except Exception:
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+        else:
+            await self.async_set_unique_id(serial_number)
+            self._abort_if_unique_id_mismatch()
+
+            return self.async_update_reload_and_abort(
+                self._get_reconfigure_entry(),
+                data=user_input,
+                reload_even_if_entry_is_unchanged=False,
+            )
+
+        return self.async_show_form(
+            step_id=SOURCE_RECONFIGURE,
+            data_schema=self.add_suggested_values_to_schema(
+                STEP_USER_DATA_SCHEMA, user_input
+            ),
+            errors=errors,
         )
