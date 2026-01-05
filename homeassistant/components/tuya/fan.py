7fcd01f508471795eaf9d4adcca141bde745f18b
epenet <6771947+epenet@users.noreply.github.com>
2026-01-05 16:30:49 +0000

    Add comment

--- homeassistant/components/tuya/fan.py
+++ homeassistant/components/tuya/fan.py
@@ -198,6 +198,8 @@ class TuyaFanEntity(TuyaEntity, FanEntity):
 
         if speed_wrapper:
             self._attr_supported_features |= FanEntityFeature.SET_SPEED
+            # if speed is from an enum, set speed count from options
+            # else keep entity default 100
             if hasattr(speed_wrapper, "options"):
                 self._attr_speed_count = len(speed_wrapper.options)
 
