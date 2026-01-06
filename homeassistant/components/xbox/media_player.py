63bc82f9a585d0517b644c694d0a99f0983c55ce
Manu <4445816+tr4nt0r@users.noreply.github.com>
2026-01-06 08:27:42 +0100

    Remove stale devices in Xbox integration (#160337)

--- homeassistant/components/xbox/media_player.py
+++ homeassistant/components/xbox/media_player.py
@@ -76,6 +76,7 @@ async def async_setup_entry(
                 ]
             )
             devices_added |= new_devices
+        devices_added &= set(consoles.data)
 
     entry.async_on_unload(consoles.async_add_listener(add_entities))
     add_entities()
