1d11ad1d7916f2a310ff4cea36833c59568489f2
epenet <6771947+epenet@users.noreply.github.com>
2026-01-08 09:23:55 +0000

    Reorder test

--- tests/components/tuya/test_binary_sensor.py
+++ tests/components/tuya/test_binary_sensor.py
@@ -37,89 +37,6 @@ async def test_platform_setup_and_discovery(
 
 @pytest.mark.parametrize(
     "mock_device_code",
-    ["mcs_8yhypbo7"],
-)
-@pytest.mark.parametrize(
-    ("updates", "expected_state", "last_reported"),
-    [
-        ({"battery_percentage": 50}, "off", "2024-01-01T00:00:00+00:00"),
-        ({"doorcontact_state": True}, "on", "2024-01-01T00:01:00+00:00"),
-    ],
-)
-@patch("homeassistant.components.tuya.PLATFORMS", [Platform.BINARY_SENSOR])
-@pytest.mark.freeze_time("2024-01-01")
-async def test_last_reported(
-    hass: HomeAssistant,
-    mock_manager: Manager,
-    mock_config_entry: MockConfigEntry,
-    mock_device: CustomerDevice,
-    mock_listener: MockDeviceListener,
-    freezer: FrozenDateTimeFactory,
-    updates: dict[str, Any],
-    expected_state: str,
-    last_reported: str,
-) -> None:
-    """Test skip_update/last_reported."""
-    entity_id = "binary_sensor.boite_aux_lettres_arriere_door"
-    await initialize_entry(hass, mock_manager, mock_config_entry, mock_device)
-
-    assert hass.states.get(entity_id).state == "off"
-    assert (
-        hass.states.get(entity_id).last_reported.isoformat()
-        == "2024-01-01T00:00:00+00:00"
-    )
-
-    freezer.tick(60)
-    await mock_listener.async_send_device_update(hass, mock_device, updates)
-
-    assert hass.states.get(entity_id).state == expected_state
-    assert hass.states.get(entity_id).last_reported.isoformat() == last_reported
-
-
-@pytest.mark.parametrize(
-    "mock_device_code",
-    ["cs_zibqa9dutqyaxym2"],
-)
-@pytest.mark.parametrize(
-    ("fault_value", "tankfull", "defrost", "wet"),
-    [
-        (0, "off", "off", "off"),
-        (0x1, "on", "off", "off"),
-        (0x2, "off", "on", "off"),
-        (0x80, "off", "off", "on"),
-        (0x83, "on", "on", "on"),
-    ],
-)
-@patch("homeassistant.components.tuya.PLATFORMS", [Platform.BINARY_SENSOR])
-async def test_bitmap(
-    hass: HomeAssistant,
-    mock_manager: Manager,
-    mock_config_entry: MockConfigEntry,
-    mock_device: CustomerDevice,
-    mock_listener: MockDeviceListener,
-    fault_value: int,
-    tankfull: str,
-    defrost: str,
-    wet: str,
-) -> None:
-    """Test BITMAP fault sensor on cs_zibqa9dutqyaxym2."""
-    await initialize_entry(hass, mock_manager, mock_config_entry, mock_device)
-
-    assert hass.states.get("binary_sensor.dehumidifier_tank_full").state == "off"
-    assert hass.states.get("binary_sensor.dehumidifier_defrost").state == "off"
-    assert hass.states.get("binary_sensor.dehumidifier_wet").state == "off"
-
-    await mock_listener.async_send_device_update(
-        hass, mock_device, {"fault": fault_value}
-    )
-
-    assert hass.states.get("binary_sensor.dehumidifier_tank_full").state == tankfull
-    assert hass.states.get("binary_sensor.dehumidifier_defrost").state == defrost
-    assert hass.states.get("binary_sensor.dehumidifier_wet").state == wet
-
-
-@pytest.mark.parametrize(
-    "mock_device_code",
     ["mcs_oxslv1c9"],
 )
 @pytest.mark.parametrize(
@@ -172,3 +89,45 @@ async def test_selective_state_update(
 
     assert hass.states.get(entity_id).state == expected_state
     assert hass.states.get(entity_id).last_reported.isoformat() == last_reported
+
+
+@pytest.mark.parametrize(
+    "mock_device_code",
+    ["cs_zibqa9dutqyaxym2"],
+)
+@pytest.mark.parametrize(
+    ("fault_value", "tankfull", "defrost", "wet"),
+    [
+        (0, "off", "off", "off"),
+        (0x1, "on", "off", "off"),
+        (0x2, "off", "on", "off"),
+        (0x80, "off", "off", "on"),
+        (0x83, "on", "on", "on"),
+    ],
+)
+@patch("homeassistant.components.tuya.PLATFORMS", [Platform.BINARY_SENSOR])
+async def test_bitmap(
+    hass: HomeAssistant,
+    mock_manager: Manager,
+    mock_config_entry: MockConfigEntry,
+    mock_device: CustomerDevice,
+    mock_listener: MockDeviceListener,
+    fault_value: int,
+    tankfull: str,
+    defrost: str,
+    wet: str,
+) -> None:
+    """Test BITMAP fault sensor on cs_zibqa9dutqyaxym2."""
+    await initialize_entry(hass, mock_manager, mock_config_entry, mock_device)
+
+    assert hass.states.get("binary_sensor.dehumidifier_tank_full").state == "off"
+    assert hass.states.get("binary_sensor.dehumidifier_defrost").state == "off"
+    assert hass.states.get("binary_sensor.dehumidifier_wet").state == "off"
+
+    await mock_listener.async_send_device_update(
+        hass, mock_device, {"fault": fault_value}
+    )
+
+    assert hass.states.get("binary_sensor.dehumidifier_tank_full").state == tankfull
+    assert hass.states.get("binary_sensor.dehumidifier_defrost").state == defrost
+    assert hass.states.get("binary_sensor.dehumidifier_wet").state == wet
