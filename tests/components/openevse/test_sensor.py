db20159c275167fe98b141ceaccad2730eb095e6
firstof9@gmail.com <firstof9@gmail.com>
2026-01-09 17:53:07 +0000

    fix tests

--- tests/components/openevse/test_sensor.py
+++ tests/components/openevse/test_sensor.py
@@ -36,18 +36,18 @@ async def test_disabled_by_default_entities(
     mock_config_entry.add_to_hass(hass)
     await hass.config_entries.async_setup(mock_config_entry.entry_id)
 
-    state = hass.states.get("sensor.openevse_mock_config_temperature_2")
+    state = hass.states.get("sensor.openevse_mock_config_ir_temperature")
     assert state is None
 
-    entry = entity_registry.async_get("sensor.openevse_mock_config_temperature_2")
+    entry = entity_registry.async_get("sensor.openevse_mock_config_ir_temperature")
     assert entry
     assert entry.disabled
     assert entry.disabled_by is er.RegistryEntryDisabler.INTEGRATION
 
-    state = hass.states.get("sensor.openevse_mock_config_temperature_3")
+    state = hass.states.get("sensor.openevse_mock_config_rtc_temperature")
     assert state is None
 
-    entry = entity_registry.async_get("sensor.openevse_mock_config_temperature_3")
+    entry = entity_registry.async_get("sensor.openevse_mock_config_rtc_temperature")
     assert entry
     assert entry.disabled
     assert entry.disabled_by is er.RegistryEntryDisabler.INTEGRATION
