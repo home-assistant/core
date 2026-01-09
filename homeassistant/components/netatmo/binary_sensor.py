79d4958bb42309e90af8d97d74601c70a43f4d51
farkasdi <93778865+farkasdi@users.noreply.github.com>
2026-01-09 13:53:32 +0000

    netatmo: binary_sensor add doortag reachable sensor

--- homeassistant/components/netatmo/binary_sensor.py
+++ homeassistant/components/netatmo/binary_sensor.py
@@ -4,6 +4,8 @@ from dataclasses import dataclass
 import logging
 from typing import Final, cast
 
+from pyatmo.modules.device_types import DeviceCategory as NetatmoDeviceCategory
+
 from homeassistant.components.binary_sensor import (
     BinarySensorDeviceClass,
     BinarySensorEntity,
@@ -15,9 +17,13 @@ from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from homeassistant.helpers.typing import StateType
 
-from .const import NETATMO_CREATE_WEATHER_BINARY_SENSOR
+from .const import (
+    CONF_URL_SECURITY,
+    NETATMO_CREATE_BINARY_SENSOR,
+    NETATMO_CREATE_WEATHER_BINARY_SENSOR,
+)
 from .data_handler import NetatmoDevice
-from .entity import NetatmoWeatherModuleEntity
+from .entity import NetatmoModuleEntity, NetatmoWeatherModuleEntity
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -40,6 +46,21 @@ NETATMO_WEATHER_BINARY_SENSOR_DESCRIPTIONS: Final[
         device_class=BinarySensorDeviceClass.CONNECTIVITY,
     ),
 ]
+
+NETATMO_BASE_BINARY_SENSOR_DESCRIPTIONS: Final[
+    list[NetatmoBinarySensorEntityDescription]
+] = [
+    NetatmoBinarySensorEntityDescription(
+        key="reachable",
+        name="Connectivity",
+        netatmo_name="reachable",
+        device_class=BinarySensorDeviceClass.CONNECTIVITY,
+    ),
+]
+
+DEVICE_CATEGORY_BINARY_URLS: Final[dict[NetatmoDeviceCategory, str]] = {
+    NetatmoDeviceCategory.opening: CONF_URL_SECURITY,
+}
 
 
 async def async_setup_entry(
@@ -85,6 +106,42 @@ async def async_setup_entry(
         )
     )
 
+    @callback
+    def _create_binary_sensor_entity(netatmo_device: NetatmoDevice) -> None:
+        """Create binary sensor entities for a Netatmo device."""
+
+        descriptions_to_add = NETATMO_BASE_BINARY_SENSOR_DESCRIPTIONS
+
+        entities: list[NetatmoBinarySensor] = []
+
+        # Create binary sensors for module
+        for description in descriptions_to_add:
+            # Actual check is simple for reachable
+            feature_check = description.key
+            if feature_check in netatmo_device.device.features:
+                _LOGGER.debug(
+                    'Adding "%s" binary sensor for device %s',
+                    feature_check,
+                    netatmo_device.device.name,
+                )
+                entities.append(
+                    NetatmoBinarySensor(
+                        netatmo_device,
+                        description,
+                    )
+                )
+
+        if entities:
+            async_add_entities(entities)
+
+    entry.async_on_unload(
+        async_dispatcher_connect(
+            hass,
+            NETATMO_CREATE_BINARY_SENSOR,
+            _create_binary_sensor_entity,
+        )
+    )
+
 
 class NetatmoWeatherBinarySensor(NetatmoWeatherModuleEntity, BinarySensorEntity):
     """Implementation of a Netatmo weather binary sensor."""
@@ -97,6 +154,51 @@ class NetatmoWeatherBinarySensor(NetatmoWeatherModuleEntity, BinarySensorEntity)
         description: NetatmoBinarySensorEntityDescription,
     ) -> None:
         """Initialize a Netatmo weather binary sensor."""
+
+        super().__init__(netatmo_device)
+
+        self.entity_description = description
+        self._attr_unique_id = f"{self.device.entity_id}-{description.key}"
+
+    @callback
+    def async_update_callback(self) -> None:
+        """Update the entity's state."""
+
+        value: StateType | None = None
+
+        value = getattr(self.device, self.entity_description.netatmo_name, None)
+
+        if value is None:
+            self._attr_available = False
+            self._attr_is_on = False
+        else:
+            self._attr_available = True
+            self._attr_is_on = cast(bool, value)
+
+        self.async_write_ha_state()
+
+
+class NetatmoBinarySensor(NetatmoModuleEntity, BinarySensorEntity):
+    """Implementation of a Netatmo binary sensor."""
+
+    entity_description: NetatmoBinarySensorEntityDescription
+
+    def __init__(
+        self,
+        netatmo_device: NetatmoDevice,
+        description: NetatmoBinarySensorEntityDescription,
+    ) -> None:
+        """Initialize a Netatmo binary sensor."""
+
+        # To prevent exception about missing URL we need to set it explicitly
+        if netatmo_device.device.device_category is not None:
+            if (
+                DEVICE_CATEGORY_BINARY_URLS.get(netatmo_device.device.device_category)
+                is not None
+            ):
+                self._attr_configuration_url = DEVICE_CATEGORY_BINARY_URLS[
+                    netatmo_device.device.device_category
+                ]
 
         super().__init__(netatmo_device)
 
