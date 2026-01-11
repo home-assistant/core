f7e5c6b9fe42dccfedbc1c55f7f10bc5e11a8a7e
Álvaro Fernández Rojas <noltari@gmail.com>
2026-01-11 20:20:45 +0100

    airzone: use extend for list comprehensions
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- homeassistant/components/airzone/select.py
+++ homeassistant/components/airzone/select.py
@@ -175,7 +175,7 @@ async def async_setup_entry(
         received_zones = set(zones_data)
         new_zones = received_zones - added_zones
         if new_zones:
-            entities += [
+            entities.extend(
                 AirzoneZoneSelect(
                     coordinator,
                     description,
@@ -187,8 +187,8 @@ async def async_setup_entry(
                 for description in MAIN_ZONE_SELECT_TYPES
                 if description.key in zones_data.get(system_zone_id)
                 and zones_data.get(system_zone_id).get(AZD_MASTER) is True
-            ]
-            entities += [
+            )
+            entities.extend(
                 AirzoneZoneSelect(
                     coordinator,
                     description,
@@ -199,7 +199,7 @@ async def async_setup_entry(
                 for system_zone_id in new_zones
                 for description in ZONE_SELECT_TYPES
                 if description.key in zones_data.get(system_zone_id)
-            ]
+            )
             added_zones.update(new_zones)
 
         async_add_entities(entities)
