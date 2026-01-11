8b05f623a501c8dbf3a1ddedb385beff6f305cd0
Carter <carter.green@icloud.com>
2026-01-11 21:28:26 +0000

    set yolink device temperature as a diagnostic sensor

--- homeassistant/components/yolink/sensor.py
+++ homeassistant/components/yolink/sensor.py
@@ -252,9 +252,11 @@ SENSOR_TYPES: tuple[YoLinkSensorEntityDescription, ...] = (
     # mcu temperature
     YoLinkSensorEntityDescription(
         key="devTemperature",
+        translation_key="device_temperature",
         device_class=SensorDeviceClass.TEMPERATURE,
         native_unit_of_measurement=UnitOfTemperature.CELSIUS,
         state_class=SensorStateClass.MEASUREMENT,
+        entity_category=EntityCategory.DIAGNOSTIC,
         exists_fn=lambda device: device.device_type in MCU_DEV_TEMPERATURE_SENSOR,
         should_update_entity=lambda value: value is not None,
         value=lambda device, data: data.get("devTemperature"),
