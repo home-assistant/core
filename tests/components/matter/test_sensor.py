4ed6dd3af6d7c755403e7c0da0898116c661fcda
Ludovic BOUÉ <lboue@users.noreply.github.com>
2026-01-12 20:16:13 +0100

    Rename Matter Eve Thermostat Fixture to eve_thermo_v4 (#160796)

--- tests/components/matter/test_sensor.py
+++ tests/components/matter/test_sensor.py
@@ -201,7 +201,7 @@ async def test_battery_sensor_description(
     state = hass.states.get("sensor.smoke_sensor_battery_type") is None
 
 
-@pytest.mark.parametrize("node_fixture", ["eve_thermo"])
+@pytest.mark.parametrize("node_fixture", ["eve_thermo_v4"])
 async def test_eve_thermo_sensor(
     hass: HomeAssistant,
     matter_client: MagicMock,
@@ -209,26 +209,26 @@ async def test_eve_thermo_sensor(
 ) -> None:
     """Test Eve Thermo."""
     # Valve position
-    state = hass.states.get("sensor.eve_thermo_valve_position")
+    state = hass.states.get("sensor.eve_thermo_20ebp1701_valve_position")
     assert state
     assert state.state == "10"
 
     set_node_attribute(matter_node, 1, 319486977, 319422488, 0)
     await trigger_subscription_callback(hass, matter_client)
 
-    state = hass.states.get("sensor.eve_thermo_valve_position")
+    state = hass.states.get("sensor.eve_thermo_20ebp1701_valve_position")
     assert state
     assert state.state == "0"
 
     # LocalTemperature
-    state = hass.states.get("sensor.eve_thermo_temperature")
+    state = hass.states.get("sensor.eve_thermo_20ebp1701_temperature")
     assert state
     assert state.state == "21.0"
 
     set_node_attribute(matter_node, 1, 513, 0, 1800)
     await trigger_subscription_callback(hass, matter_client)
 
-    state = hass.states.get("sensor.eve_thermo_temperature")
+    state = hass.states.get("sensor.eve_thermo_20ebp1701_temperature")
     assert state
     assert state.state == "18.0"
 
