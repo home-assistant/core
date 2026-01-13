fecc395f43ce1a0d97f4ea0cc5f24f7e5915f130
Simone Chemelli <simone.chemelli@gmail.com>
2026-01-13 13:57:59 +0100

    Add guest Wi-Fi QR code for Vodafone Station (#160307)
    Co-authored-by: Joostlek <joostlek@outlook.com>

--- tests/components/vodafone_station/const.py
+++ tests/components/vodafone_station/const.py
@@ -1,5 +1,7 @@
 """Common stuff for Vodafone Station tests."""
 
+from io import BytesIO
+
 from aiovodafone.models import DeviceType
 
 DEVICE_1_HOST = "WifiDevice0"
@@ -13,3 +15,16 @@ TEST_TYPE = DeviceType.SERCOMM
 TEST_URL = f"https://{TEST_HOST}"
 TEST_USERNAME = "fake_username"
 TEST_SERIAL_NUMBER = "m123456789"
+
+TEST_WIFI_DATA: dict = {
+    "guest": {
+        "on": 1,
+        "ssid": "Wifi-Guest",
+        "qr_code": BytesIO(b"fake-qr-code-guest"),
+    },
+    "guest_5g": {
+        "on": 0,
+        "ssid": "Wifi-Guest-5Ghz",
+        "qr_code": BytesIO(b"fake-qr-code-guest-5ghz"),
+    },
+}
