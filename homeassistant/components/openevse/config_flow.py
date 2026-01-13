9f074f62efcea4b6bbf4573bf654e53ec5f631c3
Joostlek <joostlek@outlook.com>
2026-01-13 15:54:35 +0100

    Fix

--- homeassistant/components/openevse/config_flow.py
+++ homeassistant/components/openevse/config_flow.py
@@ -13,6 +13,12 @@ from homeassistant.helpers.service_info import zeroconf
 
 from .const import CONF_ID, CONF_SERIAL, DOMAIN
 
+USER_SCHEMA = vol.Schema({vol.Required(CONF_HOST): cv.string})
+
+AUTH_SCHEMA = vol.Schema(
+    {vol.Required(CONF_USERNAME): cv.string, vol.Required(CONF_PASSWORD): cv.string}
+)
+
 
 class OpenEVSEConfigFlow(ConfigFlow, domain=DOMAIN):
     """OpenEVSE config flow."""
@@ -23,23 +29,23 @@ class OpenEVSEConfigFlow(ConfigFlow, domain=DOMAIN):
     def __init__(self) -> None:
         """Set up the instance."""
         self.discovery_info: dict[str, Any] = {}
-        self._user_input: dict[str, Any] = {}
+        self._host: str | None = None
 
     async def check_status(
-        self, host: str, user: str = "", password: str = ""
-    ) -> dict[str, Any]:
+        self, host: str, user: str | None = None, password: str | None = None
+    ) -> tuple[dict[str, str], str | None]:
         """Check if we can connect to the OpenEVSE charger."""
 
         charger = OpenEVSE(host, user, password)
         try:
             result = await charger.test_and_get()
         except TimeoutError:
-            return {CONF_HOST: "cannot_connect"}
+            return {"base": "cannot_connect"}, None
         except AuthenticationError:
-            return {"base": "invalid_auth"}
+            return {"base": "invalid_auth"}, None
         except MissingSerial:
-            return {CONF_SERIAL: None}
-        return {CONF_SERIAL: result.get(CONF_SERIAL)}
+            return {}, None
+        return {}, result.get(CONF_SERIAL)
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -49,24 +55,23 @@ class OpenEVSEConfigFlow(ConfigFlow, domain=DOMAIN):
         errors: dict[str, str] = {}
         if user_input is not None:
             self._async_abort_entries_match({CONF_HOST: user_input[CONF_HOST]})
-            result = await self.check_status(user_input[CONF_HOST])
+            errors, serial = await self.check_status(user_input[CONF_HOST])
 
-            if CONF_SERIAL in result:
-                if (serial := result[CONF_SERIAL]) is not None:
+            if not errors:
+                if serial is not None:
                     await self.async_set_unique_id(serial, raise_on_progress=False)
                     self._abort_if_unique_id_configured()
                 return self.async_create_entry(
                     title=f"OpenEVSE {user_input[CONF_HOST]}",
                     data=user_input,
                 )
-            if "base" in result:
-                self._user_input.update(user_input)
+            if errors["base"] == "invalid_auth":
+                self._host = user_input[CONF_HOST]
                 return await self.async_step_auth()
-            errors = result
 
         return self.async_show_form(
             step_id="user",
-            data_schema=_get_schema_user(user_input, {}),
+            data_schema=self.add_suggested_values_to_schema(USER_SCHEMA, user_input),
             errors=errors,
         )
 
@@ -74,10 +79,10 @@ class OpenEVSEConfigFlow(ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
 
         self._async_abort_entries_match({CONF_HOST: data[CONF_HOST]})
-        result = await self.check_status(data[CONF_HOST])
+        errors, serial = await self.check_status(data[CONF_HOST])
 
-        if CONF_SERIAL in result:
-            if (serial := result[CONF_SERIAL]) is not None:
+        if not errors:
+            if serial is not None:
                 await self.async_set_unique_id(serial)
                 self._abort_if_unique_id_configured()
         else:
@@ -112,19 +117,16 @@ class OpenEVSEConfigFlow(ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Confirm discovery."""
-        data_schema = vol.Schema({})
-        errors = await self.check_status(self.discovery_info[CONF_HOST])
-
-        if CONF_SERIAL not in errors:
-            data_schema = _get_schema_auth(user_input, self.discovery_info)
-        else:
-            errors.pop(CONF_SERIAL)
+        errors, _ = await self.check_status(self.discovery_info[CONF_HOST])
+        if errors:
+            if errors["base"] == "invalid_auth":
+                return await self.async_step_auth()
+            return self.async_abort(reason="unavailable_host")
 
         if user_input is None:
+            self._set_confirm_only()
             return self.async_show_form(
                 step_id="discovery_confirm",
-                data_schema=data_schema,
-                errors=errors,
                 description_placeholders={"name": self.discovery_info[CONF_NAME]},
             )
 
@@ -140,61 +142,28 @@ class OpenEVSEConfigFlow(ConfigFlow, domain=DOMAIN):
 
         errors: dict[str, str] = {}
         if user_input is not None:
-            result = await self.check_status(
-                self._user_input[CONF_HOST],
+            host = self._host or self.discovery_info[CONF_HOST]
+            errors, serial = await self.check_status(
+                host,
                 user_input[CONF_USERNAME],
                 user_input[CONF_PASSWORD],
             )
 
-            if "base" not in result:
-                self._user_input.update(user_input)
+            if not errors:
+                if self.unique_id is None and serial is not None:
+                    await self.async_set_unique_id(serial)
+                    self._abort_if_unique_id_configured()
                 return self.async_create_entry(
-                    title=f"OpenEVSE {self._user_input[CONF_HOST]}",
-                    data=self._user_input,
+                    title=f"OpenEVSE {host}",
+                    data={
+                        CONF_HOST: host,
+                        CONF_USERNAME: user_input[CONF_USERNAME],
+                        CONF_PASSWORD: user_input[CONF_PASSWORD],
+                    },
                 )
-            errors = result
 
         return self.async_show_form(
             step_id="auth",
-            data_schema=_get_schema_auth(user_input, {}),
+            data_schema=self.add_suggested_values_to_schema(AUTH_SCHEMA, user_input),
             errors=errors,
         )
-
-
-def _get_schema_user(
-    user_input: dict[str, Any] | None,
-    default_dict: dict[str, Any],
-) -> vol.Schema:
-    """Get a schema using the default_dict as a backup."""
-    if user_input is None:
-        user_input = {}
-
-    def _get_default(key: str, fallback_default: Any = None) -> Any | None:
-        """Get default value for key."""
-        return user_input.get(key, default_dict.get(key, fallback_default))
-
-    return vol.Schema(
-        {
-            vol.Required(CONF_HOST, default=_get_default(CONF_HOST)): cv.string,
-        },
-    )
-
-
-def _get_schema_auth(
-    user_input: dict[str, Any] | None,
-    default_dict: dict[str, Any],
-) -> vol.Schema:
-    """Get a schema using the default_dict as a backup."""
-    if user_input is None:
-        user_input = {}
-
-    def _get_default(key: str, fallback_default: Any = None) -> Any | None:
-        """Get default value for key."""
-        return user_input.get(key, default_dict.get(key, fallback_default))
-
-    return vol.Schema(
-        {
-            vol.Required(CONF_USERNAME, default=_get_default(CONF_USERNAME)): cv.string,
-            vol.Required(CONF_PASSWORD, default=_get_default(CONF_PASSWORD)): cv.string,
-        },
-    )
