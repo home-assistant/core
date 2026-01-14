3eef05b1c8d1e13a7c24ead8163f8f78cb8e8e09
Tucker Kern <mill1000@users.noreply.github.com>
2026-01-14 17:45:38 +0000

    Remove deprecated Snapcast group entities and custom grouping services

--- tests/components/snapcast/test_media_player.py
+++ tests/components/snapcast/test_media_player.py
@@ -7,18 +7,15 @@ from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.components.media_player import (
     ATTR_GROUP_MEMBERS,
-    ATTR_MEDIA_VOLUME_LEVEL,
     DOMAIN as MEDIA_PLAYER_DOMAIN,
     SERVICE_JOIN,
     SERVICE_UNJOIN,
-    SERVICE_VOLUME_SET,
 )
-from homeassistant.components.snapcast.const import ATTR_MASTER, DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ServiceValidationError
-from homeassistant.helpers import entity_registry as er, issue_registry as ir
+from homeassistant.helpers import entity_registry as er
 
 from . import setup_integration
 
@@ -140,90 +137,3 @@ async def test_join_exception(
 
     # Ensure that the group did not attempt to add a non-Snapcast client
     mock_group_1.add_client.assert_not_awaited()
-
-
-async def test_legacy_join_issue(
-    hass: HomeAssistant,
-    issue_registry: ir.IssueRegistry,
-    mock_config_entry: MockConfigEntry,
-    mock_create_server: AsyncMock,
-) -> None:
-    """Test the legacy grouping services create issues when used."""
-
-    # Setup and verify the integration is loaded
-    with patch("secrets.token_hex", return_value="mock_token"):
-        await setup_integration(hass, mock_config_entry)
-        assert mock_config_entry.state is ConfigEntryState.LOADED
-
-    # Call the legacy join service
-    await hass.services.async_call(
-        DOMAIN,
-        SERVICE_JOIN,
-        {
-            ATTR_ENTITY_ID: "media_player.test_client_2_snapcast_client",
-            ATTR_MASTER: "media_player.test_client_1_snapcast_client",
-        },
-        blocking=True,
-    )
-
-    # Verify the issue is created
-    issue = issue_registry.async_get_issue(
-        domain=DOMAIN,
-        issue_id="deprecated_grouping_actions",
-    )
-    assert issue is not None
-
-    # Clear existing issue
-    issue_registry.async_delete(
-        domain=DOMAIN,
-        issue_id="deprecated_grouping_actions",
-    )
-
-    # Call legacy unjoin service
-    await hass.services.async_call(
-        DOMAIN,
-        SERVICE_UNJOIN,
-        {
-            ATTR_ENTITY_ID: "media_player.test_client_2_snapcast_client",
-        },
-        blocking=True,
-    )
-
-    # Verify the issue is created again
-    issue = issue_registry.async_get_issue(
-        domain=DOMAIN,
-        issue_id="deprecated_grouping_actions",
-    )
-    assert issue is not None
-
-
-async def test_deprecated_group_entity_issue(
-    hass: HomeAssistant,
-    issue_registry: ir.IssueRegistry,
-    mock_config_entry: MockConfigEntry,
-    mock_create_server: AsyncMock,
-) -> None:
-    """Test the legacy group entities create issues when used."""
-
-    # Setup and verify the integration is loaded
-    with patch("secrets.token_hex", return_value="mock_token"):
-        await setup_integration(hass, mock_config_entry)
-        assert mock_config_entry.state is ConfigEntryState.LOADED
-
-    # Call a servuce that uses a group entity
-    await hass.services.async_call(
-        MEDIA_PLAYER_DOMAIN,
-        SERVICE_VOLUME_SET,
-        service_data={
-            ATTR_ENTITY_ID: "media_player.test_group_1_snapcast_group",
-            ATTR_MEDIA_VOLUME_LEVEL: 0.5,
-        },
-        blocking=True,
-    )
-
-    # Verify the issue is created
-    issue = issue_registry.async_get_issue(
-        domain=DOMAIN,
-        issue_id="deprecated_group_entities",
-    )
-    assert issue is not None
