3a955c8b3fe07a952a29b5cf98afdd97f81dbd99
Erwin Douna <e.douna@gmail.com>
2026-01-14 12:38:15 +0000

    Initial concept

--- homeassistant/components/incomfort/__init__.py
+++ homeassistant/components/incomfort/__init__.py
@@ -2,22 +2,15 @@
 
 from __future__ import annotations
 
-from aiohttp import ClientResponseError
-from incomfortclient import InvalidGateway, InvalidHeaterList
+from incomfortclient import Gateway as InComfortGateway
 
-from homeassistant.const import Platform
+from homeassistant.const import CONF_HOST, Platform
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.exceptions import ConfigEntryAuthFailed
 from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN
-from .coordinator import (
-    InComfortConfigEntry,
-    InComfortData,
-    InComfortDataCoordinator,
-    async_connect_gateway,
-)
-from .errors import InComfortTimeout, InComfortUnknownError, NoHeaters, NotFound
+from .coordinator import InComfortConfigEntry, InComfortData, InComfortDataCoordinator
 
 PLATFORMS = (
     Platform.WATER_HEATER,
@@ -68,36 +61,18 @@ def async_cleanup_stale_devices(
 
 async def async_setup_entry(hass: HomeAssistant, entry: InComfortConfigEntry) -> bool:
     """Set up a config entry."""
-    try:
-        data = await async_connect_gateway(hass, dict(entry.data))
-        for heater in data.heaters:
-            await heater.update()
-    except InvalidHeaterList as exc:
-        raise NoHeaters from exc
-    except InvalidGateway as exc:
-        raise ConfigEntryAuthFailed("Incorrect credentials") from exc
-    except ClientResponseError as exc:
-        if exc.status == 404:
-            raise NotFound from exc
-        raise InComfortUnknownError from exc
-    except TimeoutError as exc:
-        raise InComfortTimeout from exc
 
-    # Register discovered gateway device
-    device_registry = dr.async_get(hass)
-    gateway_device = device_registry.async_get_or_create(
-        config_entry_id=entry.entry_id,
-        identifiers={(DOMAIN, entry.entry_id)},
-        connections={(dr.CONNECTION_NETWORK_MAC, entry.unique_id)}
-        if entry.unique_id is not None
-        else set(),
-        manufacturer="Intergas",
-        name="RFGateway",
+    credentials = dict(entry.data)
+    hostname = credentials.pop(CONF_HOST)
+    client = InComfortGateway(
+        hostname, **credentials, session=async_get_clientsession(hass)
     )
-    async_cleanup_stale_devices(hass, entry, data, gateway_device)
-    coordinator = InComfortDataCoordinator(hass, entry, data)
+
+    coordinator = InComfortDataCoordinator(hass, entry, client)
     entry.runtime_data = coordinator
     await coordinator.async_config_entry_first_refresh()
+
+    entry.runtime_data = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
