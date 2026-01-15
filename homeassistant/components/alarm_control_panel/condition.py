a88f414a8f8e24fa85e7dfe89002f3d777695480
Erik <erik@montnemery.com>
2026-01-15 08:10:32 +0100

    Add alarm_control_panel conditions

---
+++ homeassistant/components/alarm_control_panel/condition.py
@@ -0,0 +1,93 @@
+"""Provides conditions for alarm control panels."""
+
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers.condition import (
+    Condition,
+    EntityStateConditionBase,
+    make_entity_state_condition,
+)
+from homeassistant.helpers.entity import get_supported_features
+
+from .const import DOMAIN, AlarmControlPanelEntityFeature, AlarmControlPanelState
+
+
+def supports_feature(hass: HomeAssistant, entity_id: str, features: int) -> bool:
+    """Get the device class of an entity or UNDEFINED if not found."""
+    try:
+        return bool(get_supported_features(hass, entity_id) & features)
+    except HomeAssistantError:
+        return False
+
+
+class EntityStateConditionRequiredFeatures(EntityStateConditionBase):
+    """State condition."""
+
+    _required_features: int
+
+    def entity_filter(self, entities: set[str]) -> set[str]:
+        """Filter entities of this domain."""
+        entities = super().entity_filter(entities)
+        return {
+            entity_id
+            for entity_id in entities
+            if supports_feature(self._hass, entity_id, self._required_features)
+        }
+
+
+def make_entity_state_condition_required_features(
+    domain: str, to_state: str, required_features: int
+) -> type[EntityStateConditionRequiredFeatures]:
+    """Create an entity state trigger class."""
+
+    class CustomCondition(EntityStateConditionRequiredFeatures):
+        """Trigger for entity state changes."""
+
+        _domain = domain
+        _states = {to_state}
+        _required_features = required_features
+
+    return CustomCondition
+
+
+CONDITIONS: dict[str, type[Condition]] = {
+    "is_armed": make_entity_state_condition(
+        DOMAIN,
+        {
+            AlarmControlPanelState.ARMED_AWAY,
+            AlarmControlPanelState.ARMED_CUSTOM_BYPASS,
+            AlarmControlPanelState.ARMED_HOME,
+            AlarmControlPanelState.ARMED_NIGHT,
+            AlarmControlPanelState.ARMED_VACATION,
+        },
+    ),
+    "is_armed_away": make_entity_state_condition_required_features(
+        DOMAIN,
+        AlarmControlPanelState.ARMED_AWAY,
+        AlarmControlPanelEntityFeature.ARM_AWAY,
+    ),
+    "is_armed_home": make_entity_state_condition_required_features(
+        DOMAIN,
+        AlarmControlPanelState.ARMED_HOME,
+        AlarmControlPanelEntityFeature.ARM_HOME,
+    ),
+    "is_armed_night": make_entity_state_condition_required_features(
+        DOMAIN,
+        AlarmControlPanelState.ARMED_NIGHT,
+        AlarmControlPanelEntityFeature.ARM_NIGHT,
+    ),
+    "is_armed_vacation": make_entity_state_condition_required_features(
+        DOMAIN,
+        AlarmControlPanelState.ARMED_VACATION,
+        AlarmControlPanelEntityFeature.ARM_VACATION,
+    ),
+    "is_disarmed": make_entity_state_condition(DOMAIN, AlarmControlPanelState.DISARMED),
+    "is_triggered": make_entity_state_condition(
+        DOMAIN, AlarmControlPanelState.TRIGGERED
+    ),
+}
+
+
+async def async_get_conditions(hass: HomeAssistant) -> dict[str, type[Condition]]:
+    """Return the alarm control panel conditions."""
+    return CONDITIONS
--- homeassistant/components/automation/__init__.py
+++ homeassistant/components/automation/__init__.py
@@ -123,6 +123,7 @@ SERVICE_TRIGGER = "trigger"
 NEW_TRIGGERS_CONDITIONS_FEATURE_FLAG = "new_triggers_conditions"
 
 _EXPERIMENTAL_CONDITION_PLATFORMS = {
+    "alarm_control_panel",
     "fan",
     "light",
 }
