2cde6f5d76a6dfff6b695c4ce5d38809b5d367fe
Erik <erik@montnemery.com>
2026-01-15 20:41:46 +0100

    Add humidifier conditions

--- homeassistant/components/automation/__init__.py
+++ homeassistant/components/automation/__init__.py
@@ -125,6 +125,7 @@ NEW_TRIGGERS_CONDITIONS_FEATURE_FLAG = "new_triggers_conditions"
 _EXPERIMENTAL_CONDITION_PLATFORMS = {
     "alarm_control_panel",
     "fan",
+    "humidifier",
     "light",
 }
 
