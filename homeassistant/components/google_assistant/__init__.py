fb239baebb7446bfaa904553331293f9108d86f7
Daniel Fairhurst <daniel.fairhurst.82@gmail.com>
2026-01-15 19:46:06 -0500

    Add ability to expose devices by label in google_assistant integration

--- homeassistant/components/google_assistant/__init__.py
+++ homeassistant/components/google_assistant/__init__.py
@@ -19,6 +19,7 @@ from .const import (  # noqa: F401
     CONF_EXPOSE,
     CONF_EXPOSE_BY_DEFAULT,
     CONF_EXPOSED_DOMAINS,
+    CONF_EXPOSED_LABELS,
     CONF_PRIVATE_KEY,
     CONF_PROJECT_ID,
     CONF_REPORT_STATE,
@@ -77,6 +78,7 @@ GOOGLE_ASSISTANT_SCHEMA = vol.All(
             vol.Optional(
                 CONF_EXPOSED_DOMAINS, default=DEFAULT_EXPOSED_DOMAINS
             ): cv.ensure_list,
+            vol.Optional(CONF_EXPOSED_LABELS): cv.ensure_list,
             vol.Optional(CONF_ENTITY_CONFIG): {cv.entity_id: ENTITY_SCHEMA},
             # str on purpose, makes sure it is configured correctly.
             vol.Optional(CONF_SECURE_DEVICES_PIN): str,
