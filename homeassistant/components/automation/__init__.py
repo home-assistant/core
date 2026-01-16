b856e04825096721059581c30fd4b71ae32d822b
Erik Montnemery <erik@montnemery.com>
2026-01-16 07:39:59 +0100

    Add assist_satellite conditions (#161019)

--- homeassistant/components/automation/__init__.py
+++ homeassistant/components/automation/__init__.py
@@ -124,6 +124,7 @@ NEW_TRIGGERS_CONDITIONS_FEATURE_FLAG = "new_triggers_conditions"
 
 _EXPERIMENTAL_CONDITION_PLATFORMS = {
     "alarm_control_panel",
+    "assist_satellite",
     "fan",
     "light",
 }
