e1cb64f60eae73e95c56fe747a5e9906d1ecf6b3
Joost Lekkerkerker <joostlek@outlook.com>
2026-01-19 10:33:13 +0100

    Update tests/components/github/const.py

--- tests/components/github/const.py
+++ tests/components/github/const.py
@@ -1,4 +1,4 @@
-"""Constants for GitHub component tests."""
+"""Constants for GitHub integration tests."""
 
 MOCK_ACCESS_TOKEN = "gho_16C7e42F292c6912E7710c838347Ae178B4a"
 TEST_REPOSITORY = "octocat/Hello-World"
