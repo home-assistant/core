ab7ce949a32d0f86138829b616f90e927dcddaaf
Artur Pragacz <49985303+arturpragacz@users.noreply.github.com>
2026-01-23 15:37:13 +0100

    Add pre announce URL to Music Assistant (#161448)

--- tests/components/music_assistant/test_media_player.py
+++ tests/components/music_assistant/test_media_player.py
@@ -37,6 +37,7 @@ from homeassistant.components.music_assistant.const import (
     ATTR_AUTO_PLAY,
     ATTR_MEDIA_ID,
     ATTR_MEDIA_TYPE,
+    ATTR_PRE_ANNOUNCE_URL,
     ATTR_RADIO_MODE,
     ATTR_SOURCE_PLAYER,
     ATTR_URL,
@@ -529,6 +530,7 @@ async def test_media_player_play_announcement_action(
             ATTR_ENTITY_ID: entity_id,
             ATTR_URL: "http://blah.com/announcement.mp3",
             ATTR_USE_PRE_ANNOUNCE: True,
+            ATTR_PRE_ANNOUNCE_URL: "http://blah.com/chime.mp3",
             ATTR_ANNOUNCE_VOLUME: 50,
         },
         blocking=True,
@@ -540,7 +542,7 @@ async def test_media_player_play_announcement_action(
         url="http://blah.com/announcement.mp3",
         pre_announce=True,
         volume_level=50,
-        pre_announce_url=None,
+        pre_announce_url="http://blah.com/chime.mp3",
     )
 
 
