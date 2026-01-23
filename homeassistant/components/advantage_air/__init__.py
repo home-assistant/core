ae34543c064de55d707891a9c6c223e81d66256b
epenet <6771947+epenet@users.noreply.github.com>
2026-01-23 15:22:40 +0100

    Move advantage_air service registration (#161487)

--- homeassistant/components/advantage_air/__init__.py
+++ homeassistant/components/advantage_air/__init__.py
@@ -8,12 +8,15 @@ from advantage_air import ApiError, advantage_air
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_IP_ADDRESS, CONF_PORT, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.debounce import Debouncer
+from homeassistant.helpers.typing import ConfigType
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from .const import ADVANTAGE_AIR_RETRY
+from .const import ADVANTAGE_AIR_RETRY, DOMAIN
 from .models import AdvantageAirData
+from .services import async_setup_services
 
 type AdvantageAirDataConfigEntry = ConfigEntry[AdvantageAirData]
 
@@ -31,6 +34,14 @@ PLATFORMS = [
 
 _LOGGER = logging.getLogger(__name__)
 REQUEST_REFRESH_DELAY = 0.5
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the component."""
+    async_setup_services(hass)
+    return True
 
 
 async def async_setup_entry(
