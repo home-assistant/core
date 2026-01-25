33c090d1d4a145245ca827dac38d9467ec9f17f3
Allen Porter <allen.porter@gmail.com>
2026-01-25 04:26:41 +0000

    Simplify config flow assertions

--- tests/components/open_router/test_config_flow.py
+++ tests/components/open_router/test_config_flow.py
@@ -406,8 +406,7 @@ async def test_reconfigure_conversation_subentry_llm_api_schema(
     schema = result["data_schema"].schema
     key = next(k for k in schema if k == CONF_LLM_HASS_API)
 
-    current_default = key.default()
-    assert current_default == suggested_llm_apis
+    assert key.default() == suggested_llm_apis
 
     field_schema = schema[key]
     assert field_schema.config
