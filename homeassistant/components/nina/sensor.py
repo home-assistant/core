8bde126a9fda39cc5c30acddcfbb61c71a284b21
DeerMaximum <git983456@parabelmail.de>
2026-01-26 19:59:21 +0100

    Add basic sensors for attributes

--- homeassistant/components/nina/sensor.py
+++ homeassistant/components/nina/sensor.py
@@ -1,5 +1,9 @@
 """NINA sensor platform."""
 
+from collections.abc import Sequence
+from dataclasses import dataclass
+from enum import StrEnum
+
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
@@ -9,6 +13,53 @@ from .coordinator import NinaConfigEntry, NINADataUpdateCoordinator
 from .entity import NinaEntity
 
 PARALLEL_UPDATES = 0
+
+
+# pylint: disable=fixme
+class SensorDataPoints(StrEnum):
+    """Enum of data point."""
+
+    HEADLINE = "headline"
+    SENDER = "sender"
+    SEVERITY = "severity"  # TODO set possible options in entity
+    AFFECTED_AREAS = "affected_areas"
+    MORE_INFORMATION_URL = "more_info_url"
+
+
+@dataclass
+class SensorData:
+    """Representation of a sensor configuration."""
+
+    type: SensorDataPoints
+    friendly_type: str
+
+
+FRIENDLY_TYPE_MAPPING = {
+    SensorDataPoints.HEADLINE: "Headline",
+    SensorDataPoints.SENDER: "Sender",
+    SensorDataPoints.SEVERITY: "Severity",
+    SensorDataPoints.AFFECTED_AREAS: "Affected Areas",
+    SensorDataPoints.MORE_INFORMATION_URL: "More Information",
+}
+
+
+def create_sensors_for_warning(
+    coordinator: NINADataUpdateCoordinator, region: str, region_name: str, slot_id: int
+) -> Sequence["NinaSensor"]:
+    """Create sensors for a warning."""
+    return [
+        NinaSensor(
+            coordinator,
+            region,
+            region_name,
+            slot_id,
+            SensorData(
+                sensor_type,
+                FRIENDLY_TYPE_MAPPING.get(sensor_type, sensor_type.value),
+            ),
+        )
+        for sensor_type in SensorDataPoints
+    ]
 
 
 async def async_setup_entry(
@@ -23,14 +74,18 @@ async def async_setup_entry(
     regions: dict[str, str] = config_entry.data[CONF_REGIONS]
     message_slots: int = config_entry.data[CONF_MESSAGE_SLOTS]
 
-    async_add_entities(
-        NinaHeadline(coordinator, ent, regions[ent], i + 1)
+    entities = [
+        create_sensors_for_warning(coordinator, ent, regions[ent], i + 1)
         for ent in coordinator.data
         for i in range(message_slots)
+    ]
+
+    async_add_entities(
+        [entity for slot_entities in entities for entity in slot_entities]
     )
 
 
-class NinaHeadline(NinaEntity, SensorEntity):
+class NinaSensor(NinaEntity, SensorEntity):
     """Representation of a NINA headline."""
 
     _attr_has_entity_name = True
@@ -41,12 +96,15 @@ class NinaHeadline(NinaEntity, SensorEntity):
         region: str,
         region_name: str,
         slot_id: int,
+        sensor_data: SensorData,
     ) -> None:
         """Initialize."""
         super().__init__(coordinator, region, slot_id)
 
-        self._attr_name = f"Headline: {region_name} {slot_id}"
-        self._attr_unique_id = f"{region_name}_{slot_id}-headline"
+        self._sensor_data: SensorData = sensor_data
+
+        self._attr_name = f"{sensor_data.friendly_type}: {region_name} {slot_id}"
+        self._attr_unique_id = f"{region_name}_{slot_id}-{sensor_data.type.value}"
         self._attr_device_info = coordinator.device_info
 
     @property
@@ -57,4 +115,4 @@ class NinaHeadline(NinaEntity, SensorEntity):
     @property
     def native_value(self) -> str | None:
         """Return the state of the sensor."""
-        return self._get_warning_data().headline
+        return getattr(self._get_warning_data(), self._sensor_data.type.value, None)
