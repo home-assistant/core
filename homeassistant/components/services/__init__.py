fe2bc93652aa07db9e55e0922c919f9bca6bf72d
epenet <6771947+epenet@users.noreply.github.com>
2026-01-27 09:39:58 +0000

    Tweak

--- homeassistant/components/services/__init__.py
+++ homeassistant/components/services/__init__.py
@@ -19,21 +19,21 @@ CONFIG_SCHEMA = cv.empty_config_schema(DOMAIN)
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up Diagnostics from a config entry."""
     await integration_platform.async_process_integration_platforms(
-        hass, DOMAIN, _register_actions_platform
+        hass, DOMAIN, _register_service_actions_platform
     )
 
     return True
 
 
 class ServiceActionsProtocol(Protocol):
-    """Define the format that actions platforms can have."""
+    """Define the format that service actions platforms can have."""
 
     async def async_setup_service_actions(self, hass: HomeAssistant) -> None:
         """Set up service actions for an integration."""
 
 
-async def _register_actions_platform(
+async def _register_service_actions_platform(
     hass: HomeAssistant, integration_domain: str, platform: ServiceActionsProtocol
 ) -> None:
-    """Register an actions platform."""
+    """Register a service actions platform."""
     await platform.async_setup_service_actions(hass)
