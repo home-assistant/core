7f5474fffc5d4bd8694447db5e565e429d82e10d
Andrew Jackson <andrew@codechimp.org>
2026-01-27 09:16:14 +0000

    Change field attributes to single named attribute

--- homeassistant/components/mastodon/sensor.py
+++ homeassistant/components/mastodon/sensor.py
@@ -44,7 +44,7 @@ def account_meta(data: Account) -> Mapping[str, Any]:
         "display_name": data.display_name,
         "bio": data.note,
         "created": dt_util.as_local(data.created_at).date(),
-        **{f.name: f.value for f in data.fields},
+        "extra_fields": {f.name: f.value for f in data.fields},
     }
 
 
