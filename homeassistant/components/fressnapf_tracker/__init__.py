3913d4f867954240d1e3b3d6eafcec5cb20bee9f
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2026-01-27 12:30:03 +0100

    filter out invalid trackers in fressnapf_tracker

--- homeassistant/components/fressnapf_tracker/__init__.py
+++ homeassistant/components/fressnapf_tracker/__init__.py
@@ -1,11 +1,21 @@
 """The Fressnapf Tracker integration."""
 
-from fressnapftracker import AuthClient, FressnapfTrackerAuthenticationError
+import logging
+
+from fressnapftracker import (
+    ApiClient,
+    AuthClient,
+    Device,
+    FressnapfTrackerAuthenticationError,
+    FressnapfTrackerError,
+    FressnapfTrackerInvalidTrackerResponseError,
+)
 
 from homeassistant.const import CONF_ACCESS_TOKEN, Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryAuthFailed
+from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryNotReady
 from homeassistant.helpers.httpx_client import get_async_client
+from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
 
 from .const import CONF_USER_ID, DOMAIN
 from .coordinator import (
@@ -20,6 +30,43 @@ PLATFORMS: list[Platform] = [
     Platform.SENSOR,
     Platform.SWITCH,
 ]
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def _tracker_is_valid(hass: HomeAssistant, device: Device) -> bool:
+    """Test if the tracker returns valid data.
+
+    Malformed data might indicate the tracker is broken or hasn't been properly registered with the app.
+    """
+    client = ApiClient(
+        serial_number=device.serialnumber,
+        device_token=device.token,
+        client=get_async_client(hass),
+    )
+    try:
+        await client.get_tracker()
+    except FressnapfTrackerInvalidTrackerResponseError:
+        _LOGGER.warning(
+            "Tracker with serialnumber %s is invalid. Consider removing it via the App",
+            device.serialnumber,
+        )
+        async_create_issue(
+            hass,
+            DOMAIN,
+            f"invalid_fressnapf_tracker_{device.serialnumber}",
+            learn_more_url="https://www.home-assistant.io/integrations/fressnapf_tracker/",
+            is_fixable=False,
+            severity=IssueSeverity.WARNING,
+            translation_key="invalid_fressnapf_tracker",
+            translation_placeholders={
+                "tracker_id": device.serialnumber,
+            },
+        )
+        return False
+    except FressnapfTrackerError as err:
+        raise ConfigEntryNotReady(err) from err
+    return True
 
 
 async def async_setup_entry(
@@ -40,6 +87,8 @@ async def async_setup_entry(
 
     coordinators: list[FressnapfTrackerDataUpdateCoordinator] = []
     for device in devices:
+        if not await _tracker_is_valid(hass, device):
+            continue
         coordinator = FressnapfTrackerDataUpdateCoordinator(
             hass,
             entry,
