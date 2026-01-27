f90c89c3ff5fd4d4cc83f213f4726d8c761206ed
cdnninja <jaydenaphillips@gmail.com>
2026-01-27 08:18:37 -0700

    Bump pyvesync to 3.4.1 (#160573)
    Co-authored-by: Joe Trabulsy <jtrabulsy@gmail.com>

--- homeassistant/components/vesync/switch.py
+++ homeassistant/components/vesync/switch.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 import logging
 from typing import Any, Final
 
-from pyvesync.base_devices.vesyncbasedevice import VeSyncBaseDevice
+from pyvesync.base_devices import VeSyncBaseDevice, VeSyncHumidifier
 from pyvesync.device_container import DeviceContainer
 
 from homeassistant.components.switch import (
@@ -28,6 +28,38 @@ _LOGGER = logging.getLogger(__name__)
 PARALLEL_UPDATES = 1
 
 
+def _toggle_switch(device: VeSyncBaseDevice, *args) -> Awaitable[bool]:
+    """Toggle power on."""
+    if args and args[0] is True and hasattr(device, "turn_on"):
+        return device.turn_on()
+    if args and args[0] is False and hasattr(device, "turn_off"):
+        return device.turn_off()
+    raise HomeAssistantError("Device does not support toggling power.")
+
+
+def _toggle_display(device: VeSyncBaseDevice, *args) -> Awaitable[bool]:
+    """Toggle display on."""
+    if hasattr(device, "toggle_display"):
+        return device.toggle_display(*args)
+    raise HomeAssistantError("Device does not support toggling display.")
+
+
+def _toggle_child_lock(device: VeSyncBaseDevice, *args) -> Awaitable[bool]:
+    """Toggle child lock on."""
+    if hasattr(device, "toggle_child_lock"):
+        return device.toggle_child_lock(*args)
+    raise HomeAssistantError("Device does not support toggling child lock.")
+
+
+def _toggle_auto_stop(device: VeSyncBaseDevice, *args) -> Awaitable[bool]:
+    """Toggle automatic stop on."""
+    match device:
+        case VeSyncHumidifier() as sw if hasattr(sw, "toggle_automatic_stop"):
+            return sw.toggle_automatic_stop(*args)
+        case _:
+            raise HomeAssistantError("Device does not support toggling automatic stop.")
+
+
 @dataclass(frozen=True, kw_only=True)
 class VeSyncSwitchEntityDescription(SwitchEntityDescription):
     """A class that describes custom switch entities."""
@@ -45,8 +77,8 @@ SENSOR_DESCRIPTIONS: Final[tuple[VeSyncSwitchEntityDescription, ...]] = (
         # Other types of wall switches support dimming.  Those use light.py platform.
         exists_fn=lambda device: is_wall_switch(device) or is_outlet(device),
         name=None,
-        on_fn=lambda device: device.turn_on(),
-        off_fn=lambda device: device.turn_off(),
+        on_fn=lambda device: _toggle_switch(device, True),
+        off_fn=lambda device: _toggle_switch(device, False),
     ),
     VeSyncSwitchEntityDescription(
         key="display",
@@ -55,16 +87,16 @@ SENSOR_DESCRIPTIONS: Final[tuple[VeSyncSwitchEntityDescription, ...]] = (
             lambda device: rgetattr(device, "state.display_set_status") is not None
         ),
         translation_key="display",
-        on_fn=lambda device: device.toggle_display(True),
-        off_fn=lambda device: device.toggle_display(False),
+        on_fn=lambda device: _toggle_display(device, True),
+        off_fn=lambda device: _toggle_display(device, False),
     ),
     VeSyncSwitchEntityDescription(
         key="child_lock",
         is_on=lambda device: device.state.child_lock,
         exists_fn=(lambda device: rgetattr(device, "state.child_lock") is not None),
         translation_key="child_lock",
-        on_fn=lambda device: device.toggle_child_lock(True),
-        off_fn=lambda device: device.toggle_child_lock(False),
+        on_fn=lambda device: _toggle_child_lock(device, True),
+        off_fn=lambda device: _toggle_child_lock(device, False),
     ),
     VeSyncSwitchEntityDescription(
         key="auto_off_config",
@@ -73,8 +105,8 @@ SENSOR_DESCRIPTIONS: Final[tuple[VeSyncSwitchEntityDescription, ...]] = (
             lambda device: rgetattr(device, "state.automatic_stop_config") is not None
         ),
         translation_key="auto_off_config",
-        on_fn=lambda device: device.toggle_automatic_stop(True),
-        off_fn=lambda device: device.toggle_automatic_stop(False),
+        on_fn=lambda device: _toggle_auto_stop(device, True),
+        off_fn=lambda device: _toggle_auto_stop(device, False),
     ),
 )
 
@@ -89,7 +121,7 @@ async def async_setup_entry(
     coordinator = config_entry.runtime_data
 
     @callback
-    def discover(devices: list[VeSyncBaseDevice]) -> None:
+    def discover(devices: DeviceContainer) -> None:
         """Add new devices to platform."""
         _setup_entities(devices, async_add_entities, coordinator)
 
@@ -98,13 +130,15 @@ async def async_setup_entry(
     )
 
     _setup_entities(
-        config_entry.runtime_data.manager.devices, async_add_entities, coordinator
+        config_entry.runtime_data.manager.devices,
+        async_add_entities,
+        coordinator,
     )
 
 
 @callback
 def _setup_entities(
-    devices: DeviceContainer | list[VeSyncBaseDevice],
+    devices: DeviceContainer,
     async_add_entities: AddConfigEntryEntitiesCallback,
     coordinator: VeSyncDataCoordinator,
 ) -> None:
@@ -117,7 +151,7 @@ def _setup_entities(
     )
 
 
-class VeSyncSwitchEntity(SwitchEntity, VeSyncBaseEntity):
+class VeSyncSwitchEntity(SwitchEntity, VeSyncBaseEntity[VeSyncBaseDevice]):
     """VeSync switch entity class."""
 
     entity_description: VeSyncSwitchEntityDescription
@@ -145,13 +179,17 @@ class VeSyncSwitchEntity(SwitchEntity, VeSyncBaseEntity):
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the entity off."""
         if not await self.entity_description.off_fn(self.device):
-            raise HomeAssistantError(self.device.last_response.message)
+            if self.device.last_response:
+                raise HomeAssistantError(self.device.last_response.message)
+            raise HomeAssistantError("Unknown error turning off device, no response.")
 
         self.async_write_ha_state()
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the entity on."""
         if not await self.entity_description.on_fn(self.device):
-            raise HomeAssistantError(self.device.last_response.message)
+            if self.device.last_response:
+                raise HomeAssistantError(self.device.last_response.message)
+            raise HomeAssistantError("Unknown error turning on device, no response.")
 
         self.async_write_ha_state()
