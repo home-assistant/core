378d7b7519ca0d2b37758cbe8206e550469e93df
DeerMaximum <git983456@parabelmail.de>
2026-01-28 14:50:20 +0100

    Translate severity states

--- homeassistant/components/nina/const.py
+++ homeassistant/components/nina/const.py
@@ -15,7 +15,7 @@ DOMAIN: str = "nina"
 NO_MATCH_REGEX: str = "/(?!)/"
 ALL_MATCH_REGEX: str = ".*"
 
-SEVERITY_VALUES: list[str] = ["Extreme", "Severe", "Moderate", "Minor", "Unknown"]
+SEVERITY_VALUES: list[str] = ["extreme", "severe", "moderate", "minor", "unknown"]
 
 CONF_REGIONS: str = "regions"
 CONF_MESSAGE_SLOTS: str = "slots"
