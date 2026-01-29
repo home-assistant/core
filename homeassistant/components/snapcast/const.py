9cce8d0535dfce23ff83a33b42445fca56f073f6
Tucker Kern <mill1000@users.noreply.github.com>
2026-01-29 08:44:50 -0700

    Remove deprecated Snapcast group entities and custom grouping services (#160945)

--- homeassistant/components/snapcast/const.py
+++ homeassistant/components/snapcast/const.py
@@ -4,18 +4,13 @@ from homeassistant.const import Platform
 
 PLATFORMS: list[Platform] = [Platform.MEDIA_PLAYER]
 
-GROUP_PREFIX = "snapcast_group_"
-GROUP_SUFFIX = "Snapcast Group"
 CLIENT_PREFIX = "snapcast_client_"
 CLIENT_SUFFIX = "Snapcast Client"
 
 SERVICE_SNAPSHOT = "snapshot"
 SERVICE_RESTORE = "restore"
-SERVICE_JOIN = "join"
-SERVICE_UNJOIN = "unjoin"
 SERVICE_SET_LATENCY = "set_latency"
 
-ATTR_MASTER = "master"
 ATTR_LATENCY = "latency"
 
 DOMAIN = "snapcast"
