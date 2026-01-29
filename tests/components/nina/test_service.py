47a30a977e3f1fe43e33f4c0893b225ad9fb66b4
DeerMaximum <git983456@parabelmail.de>
2026-01-29 17:41:52 +0100

    Add action to get the recommended actions

--- tests/components/nina/test_service.py
+++ tests/components/nina/test_service.py
@@ -6,6 +6,7 @@ from homeassistant.components.nina.const import (
     DOMAIN,
     SERVICE_GET_AFFECTED_AREAS,
     SERVICE_GET_DESCRIPTION,
+    SERVICE_GET_RECOMMENDED_ACTIONS,
 )
 from homeassistant.const import ATTR_ENTITY_ID, MAX_LENGTH_STATE_STATE
 from homeassistant.core import HomeAssistant
@@ -23,9 +24,10 @@ async def test_service_registration(
 
     services = hass.services.async_services_for_domain(DOMAIN)
 
-    assert len(services) == 2
+    assert len(services) == 3
     assert SERVICE_GET_DESCRIPTION in services
     assert SERVICE_GET_AFFECTED_AREAS in services
+    assert SERVICE_GET_RECOMMENDED_ACTIONS in services
 
 
 async def test_description_service(
@@ -114,6 +116,52 @@ async def test_affected_area_service_no_warning(
     result = await hass.services.async_call(
         DOMAIN,
         SERVICE_GET_AFFECTED_AREAS,
+        {ATTR_ENTITY_ID: target_entity_id},
+        blocking=True,
+        return_response=True,
+    )
+
+    assert result[target_entity_id] is None
+
+
+async def test_recommended_actions_service(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_nina_class: AsyncMock,
+    nina_warnings: list[Warning],
+) -> None:
+    """Test that the get recommended actions service return the full area."""
+    await setup_platform(hass, mock_config_entry, mock_nina_class, nina_warnings)
+
+    target_entity_id = "binary_sensor.nina_warning_aach_1"
+
+    result = await hass.services.async_call(
+        DOMAIN,
+        SERVICE_GET_RECOMMENDED_ACTIONS,
+        {ATTR_ENTITY_ID: target_entity_id},
+        blocking=True,
+        return_response=True,
+    )
+
+    assert (
+        result[target_entity_id]
+        == "ACHTUNG! Hinweis auf mögliche Gefahren: Es können zum Beispiel einzelne Äste herabstürzen. Achte besonders auf herabfallende Gegenstände."
+    )
+
+
+async def test_recommended_actions_service_no_warning(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_nina_class: AsyncMock,
+) -> None:
+    """Test that the get recommended actions service return None if no warning is present."""
+    await setup_platform(hass, mock_config_entry, mock_nina_class, [])
+
+    target_entity_id = "binary_sensor.nina_warning_aach_1"
+
+    result = await hass.services.async_call(
+        DOMAIN,
+        SERVICE_GET_RECOMMENDED_ACTIONS,
         {ATTR_ENTITY_ID: target_entity_id},
         blocking=True,
         return_response=True,
