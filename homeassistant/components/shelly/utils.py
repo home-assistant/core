600475abf42f29da4a3a6e380335ac975a5c21f2
Simone Chemelli <simone.chemelli@gmail.com>
2026-01-30 12:26:48 +0100

    Handle hostname resolution for Shelly repair issue (#161914)

--- homeassistant/components/shelly/utils.py
+++ homeassistant/components/shelly/utils.py
@@ -29,6 +29,7 @@ from yarl import URL
 
 from homeassistant.components import network
 from homeassistant.components.http import HomeAssistantView
+from homeassistant.components.network import async_get_source_ip
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     CONF_HOST,
@@ -732,12 +733,12 @@ def _get_homeassistant_url(hass: HomeAssistant) -> URL | None:
     return URL(raw_url)
 
 
-def get_coiot_address(hass: HomeAssistant) -> str | None:
+async def get_coiot_address(hass: HomeAssistant) -> str | None:
     """Return the CoIoT ip address."""
     url = _get_homeassistant_url(hass)
-    if url is None:
+    if url is None or url.host is None:
         return None
-    return str(url.host)
+    return await async_get_source_ip(hass, url.host)
 
 
 def get_rpc_ws_url(hass: HomeAssistant) -> str | None:
