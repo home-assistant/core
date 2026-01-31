8d3a531f7c83638f733dd45a786a3875bea653a6
Alex Fishlock <alex.fishlock@racingjag.com>
2026-01-31 18:06:33 +0000

    Fix type annotation for async_add_entities parameter
    Change from AddEntitiesCallback to AddConfigEntryEntitiesCallback to match the expected type for config entry entity callbacks. Fixes pylint W7431 warning.

--- homeassistant/components/lyngdorf/media_player.py
+++ homeassistant/components/lyngdorf/media_player.py
@@ -17,7 +17,7 @@ from homeassistant.components.media_player import (
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from .const import FEATURES_MP60, FEATURES_MP60_ZONE_B, NAME_MAIN_ZONE, NAME_ZONE_B
 from .models import LyngdorfConfigEntry
@@ -342,7 +342,7 @@ class MP60MainDevice(MP60Device):
 async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: LyngdorfConfigEntry,
-    async_add_entities: AddEntitiesCallback,
+    async_add_entities: AddConfigEntryEntitiesCallback,
 ) -> None:
     """Set up the receiver from a config entry."""
     client = config_entry.runtime_data.receiver
