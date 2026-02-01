4f57beeeb821f379cd8053263820e5952f7b5cf3
Fabian Munkes <105975993+fmunkes@users.noreply.github.com>
2026-02-01 13:30:46 +0100

    add sensor

--- homeassistant/components/music_assistant/sensor.py
+++ homeassistant/components/music_assistant/sensor.py
@@ -1,10 +1,11 @@
-"""Music Assistant Text platform."""
+"""Music Assistant Sensor platform."""
 
 from __future__ import annotations
 
-from music_assistant_models.player import PlayerOption, PlayerOptionType
+from music_assistant_client.client import MusicAssistantClient
+from music_assistant_models.player import PlayerOption
 
-from homeassistant.components.text import TextEntity, TextEntityDescription
+from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
@@ -26,15 +27,12 @@ async def async_setup_entry(
         player = mass.players.get(player_id)
         if player is None:
             return
-        entities: list[MusicAssistantPlayerConfigText] = []
+        entities: list[MusicAssistantPlayerConfigSensor] = []
         for player_option in player.player_options:
-            if (
-                not player_option.read_only
-                and player_option.type == PlayerOptionType.TEXT
-            ):
+            if player_option.read_only:
                 entities.extend(
                     [
-                        MusicAssistantPlayerConfigText(
+                        MusicAssistantPlayerConfigSensor(
                             mass, player_id, player_option=player_option
                         )
                     ]
@@ -42,29 +40,36 @@ async def async_setup_entry(
         async_add_entities(entities)
 
     # register callback to add players when they are discovered
-    entry.runtime_data.platform_handlers.setdefault(Platform.TEXT, add_player)
+    entry.runtime_data.platform_handlers.setdefault(Platform.SENSOR, add_player)
 
 
-class MusicAssistantPlayerConfigText(MusicAssistantPlayerOptionEntity, TextEntity):
+class MusicAssistantPlayerConfigSensor(MusicAssistantPlayerOptionEntity, SensorEntity):
     """Representation of a Number entity to control player provider dependent settings."""
+
+    def __init__(
+        self, mass: MusicAssistantClient, player_id: str, player_option: PlayerOption
+    ) -> None:
+        """Initialize MusicAssistantPlayerConfigSensor."""
+        self.player_config_type = player_option.type
+        super().__init__(mass, player_id, player_option)
 
     def on_player_option_update(self, player_option: PlayerOption) -> None:
         """Update on player option update."""
         super().on_player_option_update(player_option)
 
-        self.entity_description = TextEntityDescription(
+        self.entity_description = SensorEntityDescription(
             name=player_option.name,
             key=player_option.translation_key or "",
             translation_key=player_option.translation_key or "",
         )
 
     @property
-    def native_value(self) -> str | None:
-        """Return the value reported by the text."""
-        return str(self.mass_value)
-
-    async def async_set_value(self, value: str) -> None:
-        """Set text value."""
-        await self.mass.players.set_player_option(
-            self.player_id, self.mass_option_id, value
-        )
+    def native_value(self) -> int | str:
+        """Return native value."""
+        # MA's PlayerOptionValueType = bool | int | str.
+        # but native_value only allows str | int | float | date | datetime | Decimal | None
+        # convert bool to int.
+        _value = self.mass_value
+        if isinstance(_value, bool):
+            _value = int(_value)
+        return _value
