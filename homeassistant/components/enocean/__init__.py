894d88f6c3be168c7c12327ca13ec141db2fa4e7
Henning Kerstan <mail@henningkerstan.de>
2026-02-01 20:46:01 +0100

    store EnOcean yaml-based platform config in hass.data

--- homeassistant/components/enocean/__init__.py
+++ homeassistant/components/enocean/__init__.py
@@ -5,26 +5,103 @@ from homeassistant_enocean.gateway import EnOceanHomeAssistantGateway
 from homeassistant_enocean.legacy import combine_hex
 import voluptuous as vol
 
-from homeassistant.config_entries import SOURCE_IMPORT
+from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
 from homeassistant.const import CONF_DEVICE, Platform
 from homeassistant.core import _LOGGER, HomeAssistant
 from homeassistant.exceptions import ConfigEntryError
 from homeassistant.helpers import config_validation as cv, device_registry as dr
 from homeassistant.helpers.typing import ConfigType
 
-from .config_entry import EnOceanConfigEntry, EnOceanConfigRuntimeData
 from .const import DOMAIN, PLATFORMS
 
 CONFIG_SCHEMA = vol.Schema(
     {DOMAIN: vol.Schema({vol.Required(CONF_DEVICE): cv.string})}, extra=vol.ALLOW_EXTRA
 )
 
+type EnOceanConfigEntry = ConfigEntry[EnOceanHomeAssistantGateway]
+
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the EnOcean component."""
+    store_enocean_yaml_platform_config_in_hass_data(hass, config)
 
-    # gateway = EnOceanHomeAssistantGateway()
+    # support for text-based configuration (legacy)
+    if DOMAIN not in config:
+        return True
 
+    if hass.config_entries.async_entries(DOMAIN):
+        # We can only have one dongle. If there is already one in the config,
+        # there is no need to import the yaml based config.
+        return True
+
+    hass.async_create_task(
+        hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": SOURCE_IMPORT}, data=config[DOMAIN]
+        )
+    )
+
+    return True
+
+
+def store_enocean_yaml_platform_config_in_hass_data(
+    hass: HomeAssistant, config: ConfigType
+) -> None:
+    """Store yaml configuration to hass.data for later retrieval during config entry setup."""
+    hass.data.setdefault(DOMAIN, {})
+
+    enocean_devices: dict[str, list[dict]] = hass.data[DOMAIN]
+
+    for platform in PLATFORMS:
+        if platform.value not in config:
+            continue
+
+        platform_config = config[platform.value]
+
+        for entry in platform_config:
+            if "platform" not in entry or entry["platform"] != DOMAIN:
+                continue
+
+            try:
+                dev_id = entry["id"]
+                eurid = EnOceanDeviceAddress.from_number(combine_hex(dev_id))
+                device_name = (
+                    "EnOcean " + platform.value.capitalize() + " " + eurid.to_string()
+                )
+                if "name" in entry:
+                    device_name = entry["name"]
+
+                if enocean_devices.get(eurid.to_string()) is None:
+                    enocean_devices[eurid.to_string()] = []
+
+                device_data = {
+                    "platform": platform.value,
+                    "name": device_name,
+                }
+
+                for key in (
+                    "channel",
+                    "sender_id",
+                    "device_class",
+                    "range_from",
+                    "range_to",
+                ):
+                    if key in entry:
+                        device_data[key] = entry[key]
+
+                enocean_devices[eurid.to_string()].append(device_data)
+
+            except ValueError:
+                continue
+
+    _LOGGER.warning(
+        "Completed storing EnOcean yaml configuration to hass.data: %s", enocean_devices
+    )
+
+
+async def add_devices_from_config(
+    gateway: EnOceanHomeAssistantGateway, config: ConfigType
+) -> None:
+    """Add devices from configuration."""
     if Platform.BINARY_SENSOR in config:
         for entry in config[Platform.BINARY_SENSOR]:
             if "platform" not in entry or entry["platform"] != DOMAIN:
@@ -49,23 +126,6 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
     # _LOGGER.warning("EnOcean platform %s found in config entries", config[platform.value])
 
-    # support for text-based configuration (legacy)
-    if DOMAIN not in config:
-        return True
-
-    if hass.config_entries.async_entries(DOMAIN):
-        # We can only have one dongle. If there is already one in the config,
-        # there is no need to import the yaml based config.
-        return True
-
-    hass.async_create_task(
-        hass.config_entries.flow.async_init(
-            DOMAIN, context={"source": SOURCE_IMPORT}, data=config[DOMAIN]
-        )
-    )
-
-    return True
-
 
 async def async_setup_entry(
     hass: HomeAssistant, config_entry: EnOceanConfigEntry
@@ -80,12 +140,12 @@ async def async_setup_entry(
         )
         _LOGGER.info("Starting EnOcean gateway")
         await gateway.start()
-        gateway.legacy_callback()
+        # gateway.legacy_callback()
         _LOGGER.info("EnOcean gateway started")
     except Exception as ex:
         raise ConfigEntryError from ex
 
-    config_entry.runtime_data = EnOceanConfigRuntimeData(gateway=gateway)
+    config_entry.runtime_data = gateway
 
     config_entry.async_on_unload(config_entry.add_update_listener(async_reload_entry))
 
@@ -117,6 +177,6 @@ async def async_unload_entry(
     if unload_platforms := await hass.config_entries.async_unload_platforms(
         config_entry, PLATFORMS
     ):
-        config_entry.runtime_data.gateway.stop()
+        config_entry.runtime_data.stop()
 
     return unload_platforms
