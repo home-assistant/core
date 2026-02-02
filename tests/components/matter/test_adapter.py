405f2b490cf1aa90f3b7924ba35114d09d808c78
Ludovic BOUÉ <lboue@users.noreply.github.com>
2026-02-02 12:29:41 +0000

    Update Inovelli VTM31 snapshots

--- tests/components/matter/test_adapter.py
+++ tests/components/matter/test_adapter.py
@@ -156,7 +156,7 @@ async def test_device_registry_single_node_composed_device(
 
 
 @pytest.mark.usefixtures("matter_node")
-@pytest.mark.parametrize("node_fixture", ["multi_endpoint_light"])
+@pytest.mark.parametrize("node_fixture", ["inovelli_vtm31"])
 async def test_multi_endpoint_name(hass: HomeAssistant) -> None:
     """Test that the entity name gets postfixed if the device has multiple primary endpoints."""
     entity_state = hass.states.get("light.inovelli_light_1")
