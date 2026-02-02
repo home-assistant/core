cabce83d9a0946a4637678746041ca3c06a16294
Brett Adams <Bre77@users.noreply.github.com>
2026-02-02 19:29:04 +1000

    Apply suggestion from @Bre77

--- homeassistant/components/teslemetry/__init__.py
+++ homeassistant/components/teslemetry/__init__.py
@@ -325,7 +325,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: TeslemetryConfigEntry) -
 async def async_unload_entry(hass: HomeAssistant, entry: TeslemetryConfigEntry) -> bool:
     """Unload Teslemetry Config."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        # Remove stream listeners for each vehicle
         for vehicle in entry.runtime_data.vehicles:
             vehicle.remove_listener()
 
