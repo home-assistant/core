ce2faff1d0df22347e7c34532758bdebf16849ac
Brett Adams <Bre77@users.noreply.github.com>
2026-02-03 21:31:14 +1000

    Update tests/components/splunk/conftest.py
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- tests/components/splunk/conftest.py
+++ tests/components/splunk/conftest.py
@@ -44,14 +44,11 @@ def mock_hass_splunk() -> Generator[MagicMock]:
             "homeassistant.components.splunk.hass_splunk", autospec=True
         ) as mock_client_class,
         patch(
-            "homeassistant.components.splunk.config_flow.hass_splunk", autospec=True
-        ) as mock_config_flow_client_class,
+            "homeassistant.components.splunk.config_flow.hass_splunk", new=mock_client_class
+        ),
     ):
-        mock_client = MagicMock()
-        mock_client.check = AsyncMock(return_value=True)
+        mock_client = mock_client_class.return_value
+        mock_client.check.return_value = True
         mock_client.queue = AsyncMock()
-
-        mock_client_class.return_value = mock_client
-        mock_config_flow_client_class.return_value = mock_client
 
         yield mock_client
