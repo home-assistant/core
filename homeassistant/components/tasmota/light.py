f13bbc44ddee2c3a1740e61f388659263a685ba8
epenet <6771947+epenet@users.noreply.github.com>
2026-02-05 12:28:09 +0000

    Tweak

--- homeassistant/components/tasmota/light.py
+++ homeassistant/components/tasmota/light.py
@@ -212,15 +212,15 @@ class TasmotaLight(
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the entity on."""
-        supported_color_modes: set[ColorMode] = self._attr_supported_color_modes  # type: ignore[assignment]
+        supported_color_modes = self._attr_supported_color_modes
 
         attributes: dict[str, Any] = {}
 
-        if ATTR_HS_COLOR in kwargs and ColorMode.HS in supported_color_modes:
+        if ATTR_HS_COLOR in kwargs and ColorMode.HS in supported_color_modes:  # type: ignore[operator]
             hs_color = kwargs[ATTR_HS_COLOR]
             attributes["color_hs"] = [hs_color[0], hs_color[1]]
 
-        if ATTR_WHITE in kwargs and ColorMode.WHITE in supported_color_modes:
+        if ATTR_WHITE in kwargs and ColorMode.WHITE in supported_color_modes:  # type: ignore[operator]
             attributes["white_value"] = scale_brightness(kwargs[ATTR_WHITE])
 
         if ATTR_TRANSITION in kwargs:
@@ -231,7 +231,7 @@ class TasmotaLight(
 
         if (
             ATTR_COLOR_TEMP_KELVIN in kwargs
-            and ColorMode.COLOR_TEMP in supported_color_modes
+            and ColorMode.COLOR_TEMP in supported_color_modes  # type: ignore[operator]
         ):
             attributes["color_temp"] = color_util.color_temperature_kelvin_to_mired(
                 kwargs[ATTR_COLOR_TEMP_KELVIN]
