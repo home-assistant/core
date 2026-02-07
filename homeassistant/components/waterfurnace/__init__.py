d1c41a33b39327e2795070aabbe595f25566c096
Andres Jose Ruiz Torres <andresruiz2010@gmail.com>
2026-02-07 21:40:13 +0000

    Ensure coordinator is able to fetch data on setup

--- homeassistant/components/waterfurnace/__init__.py
+++ homeassistant/components/waterfurnace/__init__.py
@@ -111,6 +111,7 @@ async def async_setup_entry(
 
     coordinator = WaterFurnaceCoordinator(hass, client, entry)
     entry.runtime_data = coordinator
+    await coordinator.async_config_entry_first_refresh()
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
