770df8aa5f396c451d2a73935aad293c89434f7f
Brett Adams <brett.whynot@gmail.com>
2026-02-08 14:48:15 +1000

    Fix PARALLEL_UPDATES

--- homeassistant/components/teslemetry/calendar.py
+++ homeassistant/components/teslemetry/calendar.py
@@ -15,6 +15,8 @@ from homeassistant.util import dt as dt_util
 from . import TeslemetryConfigEntry
 from .entity import TeslemetryEnergyInfoEntity
 
+PARALLEL_UPDATES = 0
+
 
 async def async_setup_entry(
     hass: HomeAssistant,
