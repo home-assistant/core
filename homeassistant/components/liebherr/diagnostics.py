1e7ce68e2dfda0952b24343b9b0cbcd1edb61ca1
Mario Mett <1007649+mettolen@users.noreply.github.com>
2026-02-08 07:51:47 +0000

    Code review fixes

--- homeassistant/components/liebherr/diagnostics.py
+++ homeassistant/components/liebherr/diagnostics.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 from dataclasses import asdict
 from typing import Any
 
-from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.const import CONF_API_KEY
 from homeassistant.core import HomeAssistant
 
@@ -19,13 +18,13 @@ async def async_get_config_entry_diagnostics(
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
     return {
-        "entry_data": async_redact_data(entry.data, TO_REDACT),
         "devices": {
             device_id: {
                 "coordinator": {
                     "last_update_success": coordinator.last_update_success,
-                    "update_interval": coordinator.update_interval.total_seconds()
-                    if coordinator.update_interval
+                    "update_interval": str(coordinator.update_interval),
+                    "last_exception": str(coordinator.last_exception)
+                    if coordinator.last_exception
                     else None,
                 },
                 "data": asdict(coordinator.data),
