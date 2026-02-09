feb5db189880ea82b25a6882afd89f28a99c6916
epenet <6771947+epenet@users.noreply.github.com>
2026-02-09 08:31:04 +0000

    Cleanup mired light test

--- tests/components/light/test_init.py
+++ tests/components/light/test_init.py
@@ -1952,49 +1952,6 @@ async def test_light_service_call_color_temp_conversion(hass: HomeAssistant) -> 
     assert data == {"brightness": 255, "rgbww_color": (0, 0, 0, 66, 189)}
 
 
-async def test_light_mired_color_temp_conversion(hass: HomeAssistant) -> None:
-    """Test color temp conversion from K to legacy mired."""
-    entities = [
-        MockLight("Test_rgbww_ct", STATE_ON),
-        MockLight("Test_rgbww", STATE_ON),
-    ]
-    setup_test_component_platform(hass, light.DOMAIN, entities)
-
-    entity0 = entities[0]
-    entity0.supported_color_modes = {
-        light.ColorMode.COLOR_TEMP,
-    }
-    entity0._attr_min_color_temp_kelvin = 1800
-    entity0._attr_max_color_temp_kelvin = 6700
-
-    assert await async_setup_component(hass, "light", {"light": {"platform": "test"}})
-    await hass.async_block_till_done()
-
-    state = hass.states.get(entity0.entity_id)
-    assert state.attributes["supported_color_modes"] == [light.ColorMode.COLOR_TEMP]
-    assert state.attributes["min_color_temp_kelvin"] == 1800
-    assert state.attributes["max_color_temp_kelvin"] == 6700
-
-    await hass.services.async_call(
-        "light",
-        "turn_on",
-        {
-            "entity_id": [
-                entity0.entity_id,
-            ],
-            "brightness_pct": 100,
-            "color_temp_kelvin": 3500,
-        },
-        blocking=True,
-    )
-    _, data = entity0.last_call("turn_on")
-    assert data == {"brightness": 255, "color_temp_kelvin": 3500}
-
-    state = hass.states.get(entity0.entity_id)
-    assert state.attributes["color_mode"] == light.ColorMode.COLOR_TEMP
-    assert state.attributes["color_temp_kelvin"] == 3500
-
-
 async def test_light_service_call_white_mode(hass: HomeAssistant) -> None:
     """Test color_mode white in service calls."""
     entity0 = MockLight("Test_white", STATE_ON)
