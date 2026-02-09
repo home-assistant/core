c98cae61a47d9d63b13aa9d02e3a1240448ca399
epenet <6771947+epenet@users.noreply.github.com>
2026-02-09 12:21:59 +0000

    revert Ignore matter failure

--- tests/components/matter/test_light.py
+++ tests/components/matter/test_light.py
@@ -119,20 +119,8 @@ async def test_light_turn_on_off(
     [
         ("extended_color_light", "light.mock_extended_color_light"),
         ("color_temperature_light", "light.mock_color_temperature_light"),
-        pytest.param(
-            "mock_dimmable_light",
-            "light.mock_dimmable_light",
-            marks=pytest.mark.xfail(
-                reason="not a real device - invalid color mode reported"
-            ),
-        ),
-        pytest.param(
-            "mock_dimmable_plugin_unit",
-            "light.dimmable_plugin_unit",
-            marks=pytest.mark.xfail(
-                reason="not a real device - invalid color mode reported"
-            ),
-        ),
+        ("mock_dimmable_light", "light.mock_dimmable_light"),
+        ("mock_dimmable_plugin_unit", "light.dimmable_plugin_unit"),
     ],
 )
 async def test_dimmable_light(
