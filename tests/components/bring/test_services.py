d3b6ea85aa06064ae7e589475365634ec289f1d4
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2026-02-12 09:34:53 +0000

    Use async_get_config_entry helper in Bring! integration

--- tests/components/bring/test_services.py
+++ tests/components/bring/test_services.py
@@ -112,10 +112,7 @@ async def test_send_reaction_config_entry_not_loaded(
 
     assert bring_config_entry.state is ConfigEntryState.NOT_LOADED
 
-    with pytest.raises(
-        ServiceValidationError,
-        match="The account associated with this Bring! list is either not loaded or disabled in Home Assistant",
-    ):
+    with pytest.raises(ServiceValidationError) as e:
         await hass.services.async_call(
             DOMAIN,
             SERVICE_ACTIVITY_STREAM_REACTION,
@@ -125,6 +122,7 @@ async def test_send_reaction_config_entry_not_loaded(
             },
             blocking=True,
         )
+    assert e.value.translation_key == "service_config_entry_not_loaded"
 
 
 @pytest.mark.usefixtures("mock_bring_client")
