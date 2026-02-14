017f189b3b01b68fa515257b48e7f73f957dd5da
Glenn de Haan <glenn@dehaan.cloud>
2026-02-14 11:01:45 +0000

    Add HDFury CEC and 5v switches

--- tests/components/hdfury/test_switch.py
+++ tests/components/hdfury/test_switch.py
@@ -25,6 +25,7 @@ from . import setup_integration
 from tests.common import MockConfigEntry, async_fire_time_changed, snapshot_platform
 
 
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
 async def test_switch_entities(
     hass: HomeAssistant,
     snapshot: SnapshotAssertion,
