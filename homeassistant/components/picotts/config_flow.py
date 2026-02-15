94eff97d42e8c982c253755748c040323f1d4dca
Rogier van Staveren <rogier@batoid.com>
2026-02-15 22:03:52 +0100

    Add config flow to Pico TTS

---
+++ homeassistant/components/picotts/config_flow.py
@@ -0,0 +1,42 @@
+"""Config flow for Pico TTS integration."""
+
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant.components.tts import CONF_LANG
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+
+from .const import DEFAULT_LANG, DOMAIN, SUPPORT_LANGUAGES
+
+_LOGGER = logging.getLogger(__name__)
+
+STEP_USER_DATA_SCHEMA = vol.Schema(
+    {vol.Optional(CONF_LANG, default=DEFAULT_LANG): vol.In(SUPPORT_LANGUAGES)}
+)
+
+
+class PicoTTSConfigFlow(ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Pico TTS."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle the initial step."""
+        if user_input is None:
+            return self.async_show_form(
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+            )
+
+        language = user_input[CONF_LANG]
+        title = f"Pico TTS {language}"
+        data = {
+            CONF_LANG: language,
+        }
+
+        return self.async_create_entry(title=title, data=data)
