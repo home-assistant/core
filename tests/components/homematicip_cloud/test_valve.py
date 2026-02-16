bcb7b2004285308d22fcdef7373730844a4598be
epenet <6771947+epenet@users.noreply.github.com>
2026-02-16 14:11:44 +0100

    Fix incorrect use of Platform enum in homematicip_cloud tests (#163149)

--- tests/components/homematicip_cloud/test_valve.py
+++ tests/components/homematicip_cloud/test_valve.py
@@ -1,7 +1,10 @@
 """Test HomematicIP Cloud valve entities."""
 
-from homeassistant.components.valve import SERVICE_OPEN_VALVE, ValveState
-from homeassistant.const import Platform
+from homeassistant.components.valve import (
+    DOMAIN as VALVE_DOMAIN,
+    SERVICE_OPEN_VALVE,
+    ValveState,
+)
 from homeassistant.core import HomeAssistant
 
 from .helper import HomeFactory, async_manipulate_test_data, get_and_check_entity_basics
@@ -25,7 +28,7 @@ async def test_watering_valve(
     assert ha_state.state == ValveState.CLOSED
 
     await hass.services.async_call(
-        Platform.VALVE, SERVICE_OPEN_VALVE, {"entity_id": entity_id}, blocking=True
+        VALVE_DOMAIN, SERVICE_OPEN_VALVE, {"entity_id": entity_id}, blocking=True
     )
 
     await async_manipulate_test_data(
