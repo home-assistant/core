dc38023896ab81ae728ba093abaa913cb2f9444f
epenet <6771947+epenet@users.noreply.github.com>
2026-02-16 16:53:22 +0100

    Rename devolo test variables and aliases (#163175)

--- tests/components/devolo_home_network/test_button.py
+++ tests/components/devolo_home_network/test_button.py
@@ -6,7 +6,7 @@ from devolo_plc_api.exceptions.device import DevicePasswordProtected, DeviceUnav
 import pytest
 from syrupy.assertion import SnapshotAssertion
 
-from homeassistant.components.button import DOMAIN as PLATFORM, SERVICE_PRESS
+from homeassistant.components.button import DOMAIN as BUTTON_DOMAIN, SERVICE_PRESS
 from homeassistant.components.devolo_home_network.const import DOMAIN
 from homeassistant.config_entries import SOURCE_REAUTH, ConfigEntryState
 from homeassistant.const import ATTR_ENTITY_ID
@@ -31,15 +31,17 @@ async def test_button_setup(
     assert entry.state is ConfigEntryState.LOADED
 
     assert not entity_registry.async_get(
-        f"{PLATFORM}.{device_name}_identify_device_with_a_blinking_led"
+        f"{BUTTON_DOMAIN}.{device_name}_identify_device_with_a_blinking_led"
     ).disabled
     assert not entity_registry.async_get(
-        f"{PLATFORM}.{device_name}_start_plc_pairing"
+        f"{BUTTON_DOMAIN}.{device_name}_start_plc_pairing"
     ).disabled
     assert not entity_registry.async_get(
-        f"{PLATFORM}.{device_name}_restart_device"
+        f"{BUTTON_DOMAIN}.{device_name}_restart_device"
     ).disabled
-    assert not entity_registry.async_get(f"{PLATFORM}.{device_name}_start_wps").disabled
+    assert not entity_registry.async_get(
+        f"{BUTTON_DOMAIN}.{device_name}_start_wps"
+    ).disabled
 
 
 @pytest.mark.parametrize(
@@ -80,23 +82,23 @@ async def test_button(
     """Test a button."""
     entry = configure_integration(hass)
     device_name = entry.title.replace(" ", "_").lower()
-    state_key = f"{PLATFORM}.{device_name}_{name}"
+    entity_id = f"{BUTTON_DOMAIN}.{device_name}_{name}"
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
 
-    assert hass.states.get(state_key) == snapshot
-    assert entity_registry.async_get(state_key) == snapshot
+    assert hass.states.get(entity_id) == snapshot
+    assert entity_registry.async_get(entity_id) == snapshot
 
     # Emulate button press
     await hass.services.async_call(
-        PLATFORM,
+        BUTTON_DOMAIN,
         SERVICE_PRESS,
-        {ATTR_ENTITY_ID: state_key},
+        {ATTR_ENTITY_ID: entity_id},
         blocking=True,
     )
     await hass.async_block_till_done()
 
-    state = hass.states.get(state_key)
+    state = hass.states.get(entity_id)
     assert state.state == "2023-01-13T12:00:00+00:00"
     api = getattr(mock_device, api_name)
     assert getattr(api, trigger_method).call_count == 1
@@ -106,9 +108,9 @@ async def test_button(
     getattr(api, trigger_method).side_effect = DeviceUnavailable
     with pytest.raises(HomeAssistantError):
         await hass.services.async_call(
-            PLATFORM,
+            BUTTON_DOMAIN,
             SERVICE_PRESS,
-            {ATTR_ENTITY_ID: state_key},
+            {ATTR_ENTITY_ID: entity_id},
             blocking=True,
         )
 
@@ -117,7 +119,7 @@ async def test_auth_failed(hass: HomeAssistant, mock_device: MockDevice) -> None
     """Test setting unautherized triggers the reauth flow."""
     entry = configure_integration(hass)
     device_name = entry.title.replace(" ", "_").lower()
-    state_key = f"{PLATFORM}.{device_name}_start_wps"
+    entity_id = f"{BUTTON_DOMAIN}.{device_name}_start_wps"
 
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
@@ -126,9 +128,9 @@ async def test_auth_failed(hass: HomeAssistant, mock_device: MockDevice) -> None
 
     with pytest.raises(HomeAssistantError):
         await hass.services.async_call(
-            PLATFORM,
+            BUTTON_DOMAIN,
             SERVICE_PRESS,
-            {ATTR_ENTITY_ID: state_key},
+            {ATTR_ENTITY_ID: entity_id},
             blocking=True,
         )
 
