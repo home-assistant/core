7cf4ef65d5ad37ea1d3cf76aa245f02b8d0c6513
iometer <torben@iometer.de>
2026-02-17 09:25:37 +0100

    add snapshot tests for sensor entities

--- tests/components/iometer/__init__.py
+++ tests/components/iometer/__init__.py
@@ -17,3 +17,10 @@ async def setup_platform(
     with patch("homeassistant.components.iometer.PLATFORMS", platforms):
         await hass.config_entries.async_setup(config_entry.entry_id)
         await hass.async_block_till_done()
+
+
+async def setup_integration(hass: HomeAssistant, config_entry: MockConfigEntry) -> None:
+    """Fixture for setting up the integration."""
+    config_entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(config_entry.entry_id)
---
+++ tests/components/iometer/test_sensor.py
@@ -0,0 +1,31 @@
+"""Test the sensors provided by the Powerfox integration."""
+
+from __future__ import annotations
+
+from unittest.mock import AsyncMock, patch
+
+import pytest
+from syrupy.assertion import SnapshotAssertion
+
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
+
+from . import setup_integration
+
+from tests.common import MockConfigEntry, snapshot_platform
+
+
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_all_sensors(
+    hass: HomeAssistant,
+    mock_iometer_client: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+    entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test the Iometer sensors."""
+    with patch("homeassistant.components.iometer.PLATFORMS", [Platform.SENSOR]):
+        await setup_integration(hass, mock_config_entry)
+
+    await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
