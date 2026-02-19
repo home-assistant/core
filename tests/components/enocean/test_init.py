0fdb05e49ba2585d8f74d0643d424b0fd84f306f
Joostlek <joostlek@outlook.com>
2026-02-19 18:18:06 +0100

    Fix

--- tests/components/enocean/test_init.py
+++ tests/components/enocean/test_init.py
@@ -1,4 +1,4 @@
-"""Test ViCare migration."""
+"""Test the EnOcean integration."""
 
 from unittest.mock import patch
 
@@ -10,18 +10,14 @@ from homeassistant.core import HomeAssistant
 from tests.common import MockConfigEntry
 
 
-# Device migration test can be removed in 2025.4.0
 async def test_device_not_connected(
-    hass: HomeAssistant,
-    # device_registry: dr.DeviceRegistry,
-    # entity_registry: er.EntityRegistry,
-    mock_config_entry: MockConfigEntry,
+    hass: HomeAssistant, mock_config_entry: MockConfigEntry
 ) -> None:
     """Test that a config entry is not ready if the device is not connected."""
     mock_config_entry.add_to_hass(hass)
 
     with patch(
-        "enocean.communicators.serialcommunicator.SerialCommunicator.__init__",
+        "homeassistant.components.enocean.dongle.SerialCommunicator",
         side_effect=SerialException("Device not found"),
     ):
         await hass.config_entries.async_setup(mock_config_entry.entry_id)
