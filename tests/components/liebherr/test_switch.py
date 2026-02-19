650de2e7097cbf9cc6e99a3d7ae8807ddd17678a
Mario Mett <1007649+mettolen@users.noreply.github.com>
2026-02-19 11:03:10 +0000

    Pump pyliebherrhomeapi to 0.3.0

--- tests/components/liebherr/test_switch.py
+++ tests/components/liebherr/test_switch.py
@@ -65,19 +65,19 @@ async def test_switches(
         (
             "switch.test_fridge_top_zone_supercool",
             SERVICE_TURN_ON,
-            "set_supercool",
+            "set_super_cool",
             {"device_id": "test_device_id", "zone_id": 1, "value": True},
         ),
         (
             "switch.test_fridge_top_zone_supercool",
             SERVICE_TURN_OFF,
-            "set_supercool",
+            "set_super_cool",
             {"device_id": "test_device_id", "zone_id": 1, "value": False},
         ),
         (
             "switch.test_fridge_bottom_zone_superfrost",
             SERVICE_TURN_ON,
-            "set_superfrost",
+            "set_super_frost",
             {"device_id": "test_device_id", "zone_id": 2, "value": True},
         ),
         (
@@ -122,7 +122,7 @@ async def test_switch_service_calls(
 @pytest.mark.parametrize(
     ("entity_id", "method"),
     [
-        ("switch.test_fridge_top_zone_supercool", "set_supercool"),
+        ("switch.test_fridge_top_zone_supercool", "set_super_cool"),
         ("switch.test_fridge_party_mode", "set_party_mode"),
     ],
 )
