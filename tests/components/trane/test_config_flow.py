2d504142644934ac50dbd71d9360e0302a047f9c
J. Nick Koston <nick@home-assistant.io>
2026-02-19 11:39:02 -0600

    fix mock

--- tests/components/trane/test_config_flow.py
+++ tests/components/trane/test_config_flow.py
@@ -13,6 +13,8 @@ from homeassistant.data_entry_flow import FlowResultType
 
 from .conftest import MOCK_HOST, MOCK_SECRET_KEY
 
+from tests.common import MockConfigEntry
+
 
 @pytest.mark.usefixtures("mock_setup_entry")
 async def test_full_user_flow(
@@ -89,7 +91,7 @@ async def test_form_errors_can_recover(
 async def test_already_configured(
     hass: HomeAssistant,
     mock_connection: MagicMock,
-    mock_config_entry: MagicMock,
+    mock_config_entry: MockConfigEntry,
 ) -> None:
     """Test config flow aborts when already configured."""
     mock_config_entry.add_to_hass(hass)
