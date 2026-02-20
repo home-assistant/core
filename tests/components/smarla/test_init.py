358c05a10eec8f69c480d4547923fe6f73749b87
Robin Lintermann <robin.lintermann@explicatis.com>
2026-02-20 15:34:15 +0000

    Refactor auth failure during runtime test

--- tests/components/smarla/test_init.py
+++ tests/components/smarla/test_init.py
@@ -1,5 +1,6 @@
 """Test switch platform for Swing2Sleep Smarla integration."""
 
+from collections.abc import Awaitable, Callable
 from unittest.mock import MagicMock
 
 from pysmarlaapi.connection.exceptions import (
@@ -39,18 +40,32 @@ async def test_init_exception(
     assert mock_config_entry.state is expected_state
 
 
-async def test_init_auth_failure(
+async def test_init_auth_failure_during_runtime(
     hass: HomeAssistant,
     mock_config_entry: MockConfigEntry,
     mock_federwiege_cls: MagicMock,
 ) -> None:
     """Test behavior on invalid authentication during runtime."""
-    assert await setup_integration(hass, mock_config_entry)
+    invalid_auth_callback: Callable[[], Awaitable[None]] | None = None
 
+    def mocked_federwiege(_1, _2, callback):
+        nonlocal invalid_auth_callback
+        invalid_auth_callback = callback
+        return mock_federwiege_cls.return_value
+
+    # Mock Federwiege class to gather authentication failure callback
+    mock_federwiege_cls.side_effect = mocked_federwiege
+
+    assert await setup_integration(hass, mock_config_entry)
     assert mock_config_entry.state is ConfigEntryState.LOADED
+
+    # Check that config entry has no active reauth flows
     assert not any(mock_config_entry.async_get_active_flows(hass, {"reauth"}))
 
-    auth_failed_cb = mock_federwiege_cls.call_args.args[2]
-    await auth_failed_cb()
+    # Simulate authentication failure during runtime
+    assert invalid_auth_callback is not None
+    await invalid_auth_callback()
     await hass.async_block_till_done()
+
+    # Check that a reauth flow has been started
     assert any(mock_config_entry.async_get_active_flows(hass, {"reauth"}))
