1727c20e7c3da8e5c2b997a07d1e6e40a7fb68dc
dvdinth <dorus_van_dinther@hotmail.com>
2026-02-20 13:34:17 +0000

    updated sensor and binary_sensor tests to be in line with fan tests

--- tests/components/intelliclima/test_binary_sensor.py
+++ tests/components/intelliclima/test_binary_sensor.py
@@ -1,16 +1,36 @@
 """Test IntelliClima Binary Sensors."""
 
+from collections.abc import AsyncGenerator
 from unittest.mock import AsyncMock, patch
 
+import pytest
 from syrupy.assertion import SnapshotAssertion
 
+from homeassistant.components.binary_sensor import DOMAIN as BINARY_SENSOR_DOMAIN
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import entity_registry as er
+from homeassistant.helpers import device_registry as dr, entity_registry as er
 
 from . import setup_integration
 
 from tests.common import MockConfigEntry, snapshot_platform
+
+
+@pytest.fixture(autouse=True)
+async def setup_intelliclima_binary_sensor_only(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_cloud_interface: AsyncMock,
+) -> AsyncGenerator[None]:
+    """Set up IntelliClima integration with only the binary sensor platform."""
+    with (
+        patch(
+            "homeassistant.components.intelliclima.PLATFORMS", [Platform.BINARY_SENSOR]
+        ),
+    ):
+        await setup_integration(hass, mock_config_entry)
+        # Let tests run against this initialized state
+        yield
 
 
 async def test_all_binary_sensor_entities(
@@ -18,16 +38,23 @@ async def test_all_binary_sensor_entities(
     snapshot: SnapshotAssertion,
     mock_config_entry: MockConfigEntry,
     entity_registry: er.EntityRegistry,
+    device_registry: dr.DeviceRegistry,
     mock_cloud_interface: AsyncMock,
 ) -> None:
     """Test all entities."""
 
-    with (
-        patch(
-            "homeassistant.components.intelliclima.PLATFORMS", [Platform.BINARY_SENSOR]
-        ),
-    ):
-        await setup_integration(hass, mock_config_entry)
-        await snapshot_platform(
-            hass, entity_registry, snapshot, mock_config_entry.entry_id
-        )
+    await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
+
+    # There should be exactly one binary_sensor entity
+    binary_sensor_entries = [
+        entry
+        for entry in entity_registry.entities.values()
+        if entry.platform == "intelliclima" and entry.domain == BINARY_SENSOR_DOMAIN
+    ]
+    assert len(binary_sensor_entries) == 3
+
+    for entity_entry in binary_sensor_entries:
+        # Device should exist and match snapshot
+        assert entity_entry.device_id
+        assert (device_entry := device_registry.async_get(entity_entry.device_id))
+        assert device_entry == snapshot
