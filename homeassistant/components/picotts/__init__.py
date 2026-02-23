c72e254e148e4932b4682d8dfbe9ef6740ca1147
Rogier van Staveren <rogier@batoid.com>
2026-02-23 12:16:27 +0100

    raise ConfigEntryError when pico2wave could not be found

--- homeassistant/components/picotts/__init__.py
+++ homeassistant/components/picotts/__init__.py
@@ -8,6 +8,9 @@ import shutil
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryError
+
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -17,8 +20,9 @@ PLATFORMS: list[Platform] = [Platform.TTS]
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Pico TTS from a config entry."""
     if await hass.async_add_executor_job(shutil.which, "pico2wave") is None:
-        _LOGGER.error("'pico2wave' was not found")
-        return False
+        raise ConfigEntryError(
+            translation_domain=DOMAIN, translation_key="binary_not_found"
+        )
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
