e4289f8f2411d6f07991aa5953eb665276d83c56
mib1185 <mail@mib85.de>
2026-02-23 22:28:52 +0000

    make turn on trigger capable for back-to-back blocks

--- homeassistant/components/schedule/trigger.py
+++ homeassistant/components/schedule/trigger.py
@@ -1,13 +1,39 @@
 """Provides triggers for schedulers."""
 
-from homeassistant.const import STATE_OFF, STATE_ON
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.trigger import Trigger, make_entity_target_state_trigger
+from homeassistant.const import STATE_OFF, STATE_ON, STATE_UNAVAILABLE, STATE_UNKNOWN
+from homeassistant.core import HomeAssistant, State
+from homeassistant.helpers.trigger import (
+    EntityTransitionTriggerBase,
+    Trigger,
+    make_entity_target_state_trigger,
+)
 
 from . import DOMAIN
+from .const import ATTR_NEXT_EVENT
+
+
+class ScheduleBackToBackTrigger(EntityTransitionTriggerBase):
+    """Trigger for back-to-back schedule blocks."""
+
+    _domain = DOMAIN
+    _from_states = {STATE_OFF, STATE_ON}
+    _to_states = {STATE_ON}
+
+    def is_valid_transition(self, from_state: State, to_state: State) -> bool:
+        """Check if the origin state matches the expected ones."""
+        if from_state.state in (STATE_UNAVAILABLE, STATE_UNKNOWN):
+            return False
+
+        from_next_event = from_state.attributes.get(ATTR_NEXT_EVENT)
+        to_next_event = to_state.attributes.get(ATTR_NEXT_EVENT)
+
+        return (
+            from_state.state in self._from_states and from_next_event != to_next_event
+        )
+
 
 TRIGGERS: dict[str, type[Trigger]] = {
-    "turned_on": make_entity_target_state_trigger(DOMAIN, STATE_ON),
+    "turned_on": ScheduleBackToBackTrigger,
     "turned_off": make_entity_target_state_trigger(DOMAIN, STATE_OFF),
 }
 
--- tests/components/schedule/test_trigger.py
+++ tests/components/schedule/test_trigger.py
@@ -1,13 +1,29 @@
 """Test schedule triggers."""
 
+from collections.abc import Callable, Coroutine
 from typing import Any
 
+from freezegun.api import FrozenDateTimeFactory
 import pytest
 
-from homeassistant.components.schedule.const import DOMAIN
-from homeassistant.const import ATTR_LABEL_ID, CONF_ENTITY_ID, STATE_OFF, STATE_ON
+from homeassistant.components.schedule.const import (
+    ATTR_NEXT_EVENT,
+    CONF_FROM,
+    CONF_SUNDAY,
+    CONF_TO,
+    DOMAIN,
+)
+from homeassistant.const import (
+    ATTR_LABEL_ID,
+    CONF_ENTITY_ID,
+    CONF_ICON,
+    CONF_NAME,
+    STATE_OFF,
+    STATE_ON,
+)
 from homeassistant.core import HomeAssistant, ServiceCall
 
+from tests.common import async_fire_time_changed
 from tests.components import (
     TriggerStateDescription,
     arm_trigger,
@@ -54,13 +70,13 @@ async def test_schedule_triggers_gated_by_labs_flag(
     [
         *parametrize_trigger_states(
             trigger="schedule.turned_off",
-            target_states=[STATE_OFF],
-            other_states=[STATE_ON],
+            target_states=[(STATE_OFF, {ATTR_NEXT_EVENT: "2022-08-30T13:20:00-07:00"})],
+            other_states=[(STATE_ON, {ATTR_NEXT_EVENT: "2022-08-30T13:30:00-07:00"})],
         ),
         *parametrize_trigger_states(
             trigger="schedule.turned_on",
-            target_states=[STATE_ON],
-            other_states=[STATE_OFF],
+            target_states=[(STATE_ON, {ATTR_NEXT_EVENT: "2022-08-30T13:20:00-07:00"})],
+            other_states=[(STATE_OFF, {ATTR_NEXT_EVENT: "2022-08-30T13:30:00-07:00"})],
         ),
     ],
 )
@@ -112,13 +128,13 @@ async def test_schedule_state_trigger_behavior_any(
     [
         *parametrize_trigger_states(
             trigger="schedule.turned_off",
-            target_states=[STATE_OFF],
-            other_states=[STATE_ON],
+            target_states=[(STATE_OFF, {ATTR_NEXT_EVENT: "2022-08-30T13:20:00-07:00"})],
+            other_states=[(STATE_ON, {ATTR_NEXT_EVENT: "2022-08-30T13:30:00-07:00"})],
         ),
         *parametrize_trigger_states(
             trigger="schedule.turned_on",
-            target_states=[STATE_ON],
-            other_states=[STATE_OFF],
+            target_states=[(STATE_ON, {ATTR_NEXT_EVENT: "2022-08-30T13:20:00-07:00"})],
+            other_states=[(STATE_OFF, {ATTR_NEXT_EVENT: "2022-08-30T13:30:00-07:00"})],
         ),
     ],
 )
@@ -169,13 +185,13 @@ async def test_schedule_state_trigger_behavior_first(
     [
         *parametrize_trigger_states(
             trigger="schedule.turned_off",
-            target_states=[STATE_OFF],
-            other_states=[STATE_ON],
+            target_states=[(STATE_OFF, {ATTR_NEXT_EVENT: "2022-08-30T13:20:00-07:00"})],
+            other_states=[(STATE_ON, {ATTR_NEXT_EVENT: "2022-08-30T13:30:00-07:00"})],
         ),
         *parametrize_trigger_states(
             trigger="schedule.turned_on",
-            target_states=[STATE_ON],
-            other_states=[STATE_OFF],
+            target_states=[(STATE_ON, {ATTR_NEXT_EVENT: "2022-08-30T13:20:00-07:00"})],
+            other_states=[(STATE_OFF, {ATTR_NEXT_EVENT: "2022-08-30T13:30:00-07:00"})],
         ),
     ],
 )
@@ -213,3 +229,84 @@ async def test_schedule_state_trigger_behavior_last(
         for service_call in service_calls:
             assert service_call.data[CONF_ENTITY_ID] == entity_id
         service_calls.clear()
+
+
+@pytest.mark.usefixtures("enable_labs_preview_features")
+@pytest.mark.parametrize(
+    ("trigger", "trigger_options", "states"),
+    [
+        *parametrize_trigger_states(
+            trigger="schedule.turned_on",
+            target_states=[STATE_ON],
+            other_states=[STATE_OFF],
+        ),
+    ],
+)
+async def test_schedule_state_trigger_back_to_back(
+    hass: HomeAssistant,
+    service_calls: list[ServiceCall],
+    schedule_setup: Callable[..., Coroutine[Any, Any, bool]],
+    freezer: FrozenDateTimeFactory,
+    trigger: str,
+    trigger_options: dict[str, Any],
+    states: list[TriggerStateDescription],
+) -> None:
+    """Test that the schedule state trigger fires when transitioning between two back-to-back schedule blocks."""
+    freezer.move_to("2022-08-30 13:20:00-07:00")
+    entity_id = "schedule.from_yaml"
+
+    assert await schedule_setup(
+        config={
+            DOMAIN: {
+                "from_yaml": {
+                    CONF_NAME: "from yaml",
+                    CONF_ICON: "mdi:party-popper",
+                    CONF_SUNDAY: [
+                        {CONF_FROM: "22:00:00", CONF_TO: "22:30:00"},
+                        {CONF_FROM: "22:30:00", CONF_TO: "23:00:00"},
+                    ],
+                }
+            }
+        },
+        items=[],
+    )
+
+    await arm_trigger(
+        hass,
+        "schedule.turned_on",
+        {},
+        {"entity_id": [entity_id]},
+    )
+
+    # initial state
+    state = hass.states.get(entity_id)
+    assert state
+    assert state.state == STATE_OFF
+    assert state.attributes[ATTR_NEXT_EVENT].isoformat() == "2022-09-04T22:00:00-07:00"
+
+    # move time into first block
+    freezer.move_to(state.attributes[ATTR_NEXT_EVENT])
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+
+    state = hass.states.get(entity_id)
+    assert state
+    assert state.state == STATE_ON
+    assert state.attributes[ATTR_NEXT_EVENT].isoformat() == "2022-09-04T22:30:00-07:00"
+
+    assert len(service_calls) == 1
+    assert service_calls[0].data[CONF_ENTITY_ID] == entity_id
+    service_calls.clear()
+
+    # move time into second block (back-to-back)
+    freezer.move_to(state.attributes[ATTR_NEXT_EVENT])
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+
+    state = hass.states.get(entity_id)
+    assert state
+    assert state.state == STATE_ON
+    assert state.attributes[ATTR_NEXT_EVENT].isoformat() == "2022-09-04T23:00:00-07:00"
+
+    assert len(service_calls) == 1
+    assert service_calls[0].data[CONF_ENTITY_ID] == entity_id
