f2112401ab9f5b8cbc2db0c72140d73f5ea2359d
konsulten <nordmarkclaes@gmail.com>
2026-02-24 22:02:54 +0000

    Use premade fixture for entity enablement in test

--- tests/components/systemnexa2/test_sensor.py
+++ tests/components/systemnexa2/test_sensor.py
@@ -1,21 +1,19 @@
 """Test the System Nexa 2 sensor platform."""
 
-from datetime import timedelta
 from unittest.mock import patch
 
 import pytest
 from syrupy.assertion import SnapshotAssertion
 
-from homeassistant.config_entries import RELOAD_AFTER_UPDATE_DELAY
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 import homeassistant.helpers.entity_registry as er
-from homeassistant.util import dt as dt_util
 
-from tests.common import MockConfigEntry, async_fire_time_changed, snapshot_platform
+from tests.common import MockConfigEntry, snapshot_platform
 
 
 @pytest.mark.usefixtures("mock_system_nexa_2_device")
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
 async def test_sensor_entities(
     hass: HomeAssistant,
     snapshot: SnapshotAssertion,
@@ -31,17 +29,6 @@ async def test_sensor_entities(
         [Platform.SENSOR],
     ):
         await hass.config_entries.async_setup(mock_config_entry.entry_id)
-        entity = entity_registry.entities.get(
-            "sensor.outdoor_smart_plug_signal_strength"
-        )
-        assert entity is not None
-        entity_registry.async_update_entity(entity.entity_id, disabled_by=None)
-        await hass.async_block_till_done()
-        async_fire_time_changed(
-            hass,
-            dt_util.utcnow() + timedelta(seconds=RELOAD_AFTER_UPDATE_DELAY + 1),
-        )
-
         await snapshot_platform(
             hass, entity_registry, snapshot, mock_config_entry.entry_id
         )
