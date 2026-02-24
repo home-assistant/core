49284289686c10ca8e6e71ab8b6f47c9bae97951
Sab44 <64696149+Sab44@users.noreply.github.com>
2026-02-24 08:18:57 +0100

    Add sensor device classes, map from lhm sensor types

--- homeassistant/components/libre_hardware_monitor/sensor.py
+++ homeassistant/components/libre_hardware_monitor/sensor.py
@@ -6,7 +6,12 @@ from typing import Any, override
 from librehardwaremonitor_api.model import DeviceId, LibreHardwareMonitorSensorData
 from librehardwaremonitor_api.sensor_type import SensorType
 
-from homeassistant.components.sensor import SensorEntity, SensorStateClass
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import UnitOfDataRate
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
@@ -73,8 +78,9 @@ class LibreHardwareMonitorSensor(
         super().__init__(coordinator)
 
         self._attr_name: str = sensor_data.name
+        self._attr_device_class = self._map_device_class(sensor_data)
 
-        self._set_state(coordinator.data.is_deprecated_version, sensor_data)
+        self._set_state(sensor_data)
         self._attr_unique_id: str = f"{entry_id}_{sensor_data.sensor_id}"
 
         self._sensor_id: str = sensor_data.sensor_id
@@ -86,37 +92,66 @@ class LibreHardwareMonitorSensor(
             model=sensor_data.device_type,
         )
 
-    def _set_state(
-        self,
-        is_deprecated_lhm_version: bool,
-        sensor_data: LibreHardwareMonitorSensorData,
-    ) -> None:
-        value = sensor_data.value
-        min_value = sensor_data.min
-        max_value = sensor_data.max
-        unit = sensor_data.unit
-
-        if not is_deprecated_lhm_version and sensor_data.type == SensorType.THROUGHPUT:
-            # Temporary fix: convert the B/s value to KB/s to not break existing entries
-            # This will be migrated properly once SensorDeviceClass is introduced
-            value = f"{(float(value) / 1024):.1f}" if value else None
-            min_value = f"{(float(min_value) / 1024):.1f}" if min_value else None
-            max_value = f"{(float(max_value) / 1024):.1f}" if max_value else None
-            unit = "KB/s"
-
-        self._attr_native_value: str | None = value
+    def _set_state(self, sensor_data: LibreHardwareMonitorSensorData) -> None:
+        self._attr_native_value: str | None = sensor_data.value
         self._attr_extra_state_attributes: dict[str, Any] = {
-            STATE_MIN_VALUE: min_value,
-            STATE_MAX_VALUE: max_value,
+            STATE_MIN_VALUE: sensor_data.min,
+            STATE_MAX_VALUE: sensor_data.max,
         }
-        self._attr_native_unit_of_measurement = unit
+        self._attr_native_unit_of_measurement = sensor_data.unit
+
+        if sensor_data.type == SensorType.THROUGHPUT:
+            self._attr_suggested_unit_of_measurement = (
+                UnitOfDataRate.KILOBYTES_PER_SECOND
+            )
+
+    @staticmethod
+    def _map_device_class(
+        sensor_data: LibreHardwareMonitorSensorData,
+    ) -> SensorDeviceClass | None:
+        if sensor_data.type is None:
+            _LOGGER.warning("Missing type for sensor: %s", sensor_data.name)
+            return None
+
+        match sensor_data.type:
+            case SensorType.VOLTAGE:
+                return SensorDeviceClass.VOLTAGE
+            case SensorType.CURRENT:
+                return SensorDeviceClass.CURRENT
+            case SensorType.POWER:
+                return SensorDeviceClass.POWER
+            case SensorType.CLOCK | SensorType.FREQUENCY:
+                return SensorDeviceClass.FREQUENCY
+            case SensorType.TEMPERATURE:
+                return SensorDeviceClass.TEMPERATURE
+            case SensorType.FLOW:
+                return SensorDeviceClass.VOLUME_FLOW_RATE
+            case SensorType.FACTOR:
+                return SensorDeviceClass.POWER_FACTOR
+            case SensorType.DATA | SensorType.SMALL_DATA:
+                return SensorDeviceClass.DATA_SIZE
+            case SensorType.THROUGHPUT:
+                return SensorDeviceClass.DATA_RATE
+            case SensorType.TIMESPAN:
+                return SensorDeviceClass.DURATION
+            case SensorType.ENERGY:
+                return SensorDeviceClass.ENERGY
+            case SensorType.NOISE:
+                return SensorDeviceClass.SOUND_PRESSURE
+            case SensorType.CONDUCTIVITY:
+                return SensorDeviceClass.CONDUCTIVITY
+            case SensorType.HUMIDITY:
+                return SensorDeviceClass.HUMIDITY
+            # no matching HA sensor device classes for remaining types
+            case _:
+                return None
 
     @callback
     @override
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
         if sensor_data := self.coordinator.data.sensor_data.get(self._sensor_id):
-            self._set_state(self.coordinator.data.is_deprecated_version, sensor_data)
+            self._set_state(sensor_data)
         else:
             self._attr_native_value = None
 
