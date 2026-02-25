f036603547bfe3baf16e346912da86fa685e9de9
Marcel Winklmüller <marcel@qapture.at>
2026-02-25 16:19:09 +0000

    fix access to hass.data

--- tests/components/sonarr/test_init.py
+++ tests/components/sonarr/test_init.py
@@ -74,13 +74,12 @@ async def test_unload_config_entry(
     await hass.async_block_till_done()
 
     assert mock_config_entry.state is ConfigEntryState.LOADED
-    assert hass.data[DOMAIN][mock_config_entry.entry_id] is not None
+    assert mock_config_entry.runtime_data is not None
 
     await hass.config_entries.async_unload(mock_config_entry.entry_id)
     await hass.async_block_till_done()
 
     assert mock_config_entry.state is ConfigEntryState.NOT_LOADED
-    assert mock_config_entry.entry_id not in hass.data[DOMAIN]
 
 
 async def test_migrate_config_entry(hass: HomeAssistant) -> None:
