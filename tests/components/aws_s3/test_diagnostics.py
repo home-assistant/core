ff8e3c0462828d5c1ecc4c061637de7079075d5b
mik-laj <12058428+mik-laj@users.noreply.github.com>
2026-02-25 22:24:56 +0000

    Add diagnostics platform to AWS S3

---
+++ tests/components/aws_s3/test_diagnostics.py
@@ -0,0 +1,34 @@
+"""Tests for AWS S3 diagnostics."""
+
+import pytest
+from syrupy.assertion import SnapshotAssertion
+
+from homeassistant.components.backup import DOMAIN as BACKUP_DOMAIN
+from homeassistant.core import HomeAssistant
+from homeassistant.setup import async_setup_component
+
+from tests.common import MockConfigEntry
+from tests.components.diagnostics import get_diagnostics_for_config_entry
+from tests.typing import ClientSessionGenerator
+
+pytestmark = [
+    pytest.mark.freeze_time("2024-11-22 11:48:48+01:00"),
+]
+
+
+async def test_entry_diagnostics(
+    hass: HomeAssistant,
+    hass_client: ClientSessionGenerator,
+    mock_config_entry: MockConfigEntry,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test config entry diagnostics."""
+    mock_config_entry.add_to_hass(hass)
+    assert await async_setup_component(hass, BACKUP_DOMAIN, {BACKUP_DOMAIN: {}})
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert (
+        await get_diagnostics_for_config_entry(hass, hass_client, mock_config_entry)
+        == snapshot
+    )
