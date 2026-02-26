508554c00aabc329817dbb8fcd199dfee77debf9
mik-laj <12058428+mik-laj@users.noreply.github.com>
2026-02-26 22:26:49 +0000

    Prefix support

--- homeassistant/components/aws_s3/diagnostics.py
+++ homeassistant/components/aws_s3/diagnostics.py
@@ -26,8 +26,11 @@ async def async_get_config_entry_diagnostics(
     """Return diagnostics for a config entry."""
     coordinator = entry.runtime_data
     backup_manager: BackupManager = hass.data[BACKUP_DATA_MANAGER]
-    bucket = entry.data[CONF_BUCKET]
-    backups = await async_list_backups_from_s3(coordinator.client, entry.data[CONF_BUCKET])
+    backups = await async_list_backups_from_s3(
+        coordinator.client,
+        bucket=entry.data[CONF_BUCKET],
+        prefix=entry.data[CONF_BUCKET],
+    )
 
     data = {
         "coordinator_data": dataclasses.asdict(coordinator.data),
