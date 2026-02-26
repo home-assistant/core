b6220e07056ffcdcb718a786398e749e95eb320f
Erwin Douna <e.douna@gmail.com>
2026-02-26 08:46:46 +0000

    Portainer fix stack & container entity merger

--- homeassistant/components/portainer/entity.py
+++ homeassistant/components/portainer/entity.py
@@ -78,7 +78,7 @@ class PortainerContainerEntity(PortainerCoordinatorEntity):
             identifiers={
                 (
                     DOMAIN,
-                    f"{self.coordinator.config_entry.entry_id}_{self.endpoint_id}_{self.device_name}",
+                    f"{self.coordinator.config_entry.entry_id}_{self.endpoint_id}_container_{self.device_name}",
                 )
             },
             manufacturer=DEFAULT_NAME,
@@ -91,7 +91,7 @@ class PortainerContainerEntity(PortainerCoordinatorEntity):
             # else it's the endpoint
             via_device=(
                 DOMAIN,
-                f"{coordinator.config_entry.entry_id}_{self.endpoint_id}_{device_info.stack.name}"
+                f"{coordinator.config_entry.entry_id}_{self.endpoint_id}_stack_{device_info.stack.name}"
                 if device_info.stack
                 else f"{coordinator.config_entry.entry_id}_{self.endpoint_id}",
             ),
@@ -135,7 +135,7 @@ class PortainerStackEntity(PortainerCoordinatorEntity):
             identifiers={
                 (
                     DOMAIN,
-                    f"{coordinator.config_entry.entry_id}_{self.endpoint_id}_{self.device_name}",
+                    f"{coordinator.config_entry.entry_id}_{self.endpoint_id}_stack_{self.device_name}",
                 )
             },
             manufacturer=DEFAULT_NAME,
