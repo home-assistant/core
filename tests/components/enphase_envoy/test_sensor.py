c8419e3a5febcf6a512bb2a62e4c86f67399d6e6
Arie Catsman <120491684+catsmanac@users.noreply.github.com>
2026-02-27 17:43:46 +0100

    Add missing production ct data, total-consumption and new CT to enphase_envoy (#164270)

--- tests/components/enphase_envoy/test_sensor.py
+++ tests/components/enphase_envoy/test_sensor.py
@@ -680,6 +680,186 @@ async def test_sensor_storage_ct_phase_data(
         assert entity_state.state == target
 
 
+CT_NAMES_FLOAT = (
+    "<cttype>_ct_energy_delivered",
+    "<cttype>_ct_energy_received",
+    "<cttype>_ct_power",
+    "frequency_<cttype>_ct",
+    "voltage_<cttype>_ct",
+    "<cttype>_ct_current",
+    "power_factor_<cttype>_ct",
+    "meter_status_flags_active_<cttype>_ct",
+)
+CT_NAMES_STR = ("metering_status_<cttype>_ct",)
+
+
+@pytest.mark.parametrize(
+    ("cttype", "mock_envoy"),
+    [
+        (CtType.PRODUCTION, "envoy_metered_batt_relay"),
+        (CtType.TOTAL_CONSUMPTION, "envoy_tot_cons_metered"),
+        (CtType.BACKFEED, "envoy_metered_batt_relay"),
+        (CtType.LOAD, "envoy_metered_batt_relay"),
+        (CtType.EVSE, "envoy_metered_batt_relay"),
+        (CtType.PV3P, "envoy_metered_batt_relay"),
+    ],
+    indirect=["mock_envoy"],
+)
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_sensor_ct_data(
+    hass: HomeAssistant,
+    mock_envoy: AsyncMock,
+    config_entry: MockConfigEntry,
+    cttype: CtType,
+) -> None:
+    """Test ct entities values."""
+    with patch("homeassistant.components.enphase_envoy.PLATFORMS", [Platform.SENSOR]):
+        await setup_integration(hass, config_entry)
+
+    sn = mock_envoy.serial_number
+    ENTITY_BASE: str = f"{Platform.SENSOR}.envoy_{sn}"
+
+    data = mock_envoy.data.ctmeters[cttype]
+
+    CT_TARGETS_FLOAT = (
+        data.energy_delivered / 1000000.0,
+        data.energy_received / 1000000.0,
+        data.active_power / 1000.0,
+        data.frequency,
+        data.voltage,
+        data.current,
+        data.power_factor,
+        len(data.status_flags),
+    )
+    count_names: int = 0
+
+    for name, target in list(
+        zip(
+            [
+                entity.replace("<cttype>", cttype).replace("-", "_")
+                for entity in CT_NAMES_FLOAT
+            ],
+            CT_TARGETS_FLOAT,
+            strict=False,
+        )
+    ):
+        assert (entity_state := hass.states.get(f"{ENTITY_BASE}_{name}"))
+        assert float(entity_state.state) == target
+        count_names += 1
+
+    CT_TARGETS_STR = (data.metering_status,)
+    for name, target in list(
+        zip(
+            [
+                entity.replace("<cttype>", cttype).replace("-", "_")
+                for entity in CT_NAMES_STR
+            ],
+            CT_TARGETS_STR,
+            strict=False,
+        )
+    ):
+        assert (entity_state := hass.states.get(f"{ENTITY_BASE}_{name}"))
+        assert entity_state.state == target
+        count_names += 1
+
+    # verify we're testing them all
+    assert len(CT_NAMES_FLOAT) + len(CT_NAMES_STR) == count_names
+
+
+CT_NAMES_FLOAT_PHASE = [
+    f"{name}_{phase.lower()}" for phase in PHASENAMES for name in (CT_NAMES_FLOAT)
+]
+
+CT_NAMES_STR_PHASE = [
+    f"{name}_{phase.lower()}" for phase in PHASENAMES for name in (CT_NAMES_STR)
+]
+
+
+@pytest.mark.parametrize(
+    "cttype",
+    [
+        CtType.PRODUCTION,
+        CtType.BACKFEED,
+        CtType.LOAD,
+        CtType.EVSE,
+        CtType.PV3P,
+    ],
+)
+@pytest.mark.parametrize(
+    ("mock_envoy"),
+    [
+        "envoy_metered_batt_relay",
+    ],
+    indirect=["mock_envoy"],
+)
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_sensor_ct_phase_data(
+    hass: HomeAssistant,
+    mock_envoy: AsyncMock,
+    config_entry: MockConfigEntry,
+    cttype: CtType,
+) -> None:
+    """Test ct phase entities values."""
+    with patch("homeassistant.components.enphase_envoy.PLATFORMS", [Platform.SENSOR]):
+        await setup_integration(hass, config_entry)
+
+    sn = mock_envoy.serial_number
+    ENTITY_BASE: str = f"{Platform.SENSOR}.envoy_{sn}"
+
+    CT_NAMES_FLOAT_PHASE_TARGET = chain(
+        *[
+            (
+                phase_data.energy_delivered / 1000000.0,
+                phase_data.energy_received / 1000000.0,
+                phase_data.active_power / 1000.0,
+                phase_data.frequency,
+                phase_data.voltage,
+                phase_data.current,
+                phase_data.power_factor,
+                len(phase_data.status_flags),
+            )
+            for phase_data in mock_envoy.data.ctmeters_phases[cttype].values()
+        ]
+    )
+
+    count_names: int = 0
+    for name, target in list(
+        zip(
+            [
+                entity.replace("<cttype>", cttype).replace("-", "_")
+                for entity in CT_NAMES_FLOAT_PHASE
+            ],
+            CT_NAMES_FLOAT_PHASE_TARGET,
+            strict=False,
+        )
+    ):
+        assert (entity_state := hass.states.get(f"{ENTITY_BASE}_{name}"))
+        assert float(entity_state.state) == target
+        count_names += 1
+
+    CT_NAMES_STR_PHASE_TARGET = [
+        phase_data.metering_status
+        for phase_data in mock_envoy.data.ctmeters_phases[cttype].values()
+    ]
+
+    for name, target in list(
+        zip(
+            [
+                entity.replace("<cttype>", cttype).replace("-", "_")
+                for entity in CT_NAMES_STR_PHASE
+            ],
+            CT_NAMES_STR_PHASE_TARGET,
+            strict=False,
+        )
+    ):
+        assert (entity_state := hass.states.get(f"{ENTITY_BASE}_{name}"))
+        assert entity_state.state == target
+        count_names += 1
+
+    # verify we're testing them all
+    assert len(CT_NAMES_FLOAT_PHASE) + len(CT_NAMES_STR_PHASE) == count_names
+
+
 @pytest.mark.parametrize(
     ("mock_envoy"),
     [
