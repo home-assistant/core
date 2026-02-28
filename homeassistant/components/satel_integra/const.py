5c1f2f7121e46cf250683818c94881c5030845dd
Tom Matheussen <13683094+Tommatheussen@users.noreply.github.com>
2026-02-28 15:41:00 +0000

    Remove YAML import code

--- homeassistant/components/satel_integra/const.py
+++ homeassistant/components/satel_integra/const.py
@@ -1,9 +1,5 @@
 """Constants for the Satel Integra integration."""
 
-DEFAULT_CONF_ARM_HOME_MODE = 1
-DEFAULT_PORT = 7094
-DEFAULT_ZONE_TYPE = "motion"
-
 DOMAIN = "satel_integra"
 
 SUBENTRY_TYPE_PARTITION = "partition"
@@ -16,11 +12,7 @@ CONF_ZONE_NUMBER = "zone_number"
 CONF_OUTPUT_NUMBER = "output_number"
 CONF_SWITCHABLE_OUTPUT_NUMBER = "switchable_output_number"
 
-CONF_DEVICE_PARTITIONS = "partitions"
 CONF_ARM_HOME_MODE = "arm_home_mode"
 CONF_ZONE_TYPE = "type"
-CONF_ZONES = "zones"
-CONF_OUTPUTS = "outputs"
-CONF_SWITCHABLE_OUTPUTS = "switchable_outputs"
 
 ZONES = "zones"
