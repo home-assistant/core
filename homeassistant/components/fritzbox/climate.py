d858399b629d7738845252d4c1e6ee7df86b881c
chli1 <chli1@users.noreply.github.com>
2026-03-02 10:24:47 +0100

    fix #163316: FRITZ!SmartHome integration not showing boost status on climate entity for thermostat
    According diagnostics data there is a dedicated field (boost_active) for indicating if boost mode is active or not. So let's use this one.

--- homeassistant/components/fritzbox/climate.py
+++ homeassistant/components/fritzbox/climate.py
@@ -179,7 +179,7 @@ class FritzboxThermostat(FritzBoxDeviceEntity, ClimateEntity):
             return PRESET_HOLIDAY
         if self.data.summer_active:
             return PRESET_SUMMER
-        if self.data.target_temperature == ON_API_TEMPERATURE:
+        if self.data.target_temperature == ON_API_TEMPERATURE or self.data.boost_active:
             return PRESET_BOOST
         if self.data.target_temperature == self.data.comfort_temperature:
             return PRESET_COMFORT
