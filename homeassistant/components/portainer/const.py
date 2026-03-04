d8416d924f7715c281d04393b19ecf87842576b6
Erwin Douna <e.douna@gmail.com>
2026-03-04 18:53:59 +0000

    Add strictness

--- homeassistant/components/portainer/const.py
+++ homeassistant/components/portainer/const.py
@@ -1,30 +1,32 @@
 """Constants for the Portainer integration."""
 
+from enum import IntEnum, StrEnum
+
 DOMAIN = "portainer"
 DEFAULT_NAME = "Portainer"
 
 
-class EndpointStatus:
+class EndpointStatus(IntEnum):
     """Portainer endpoint status."""
 
     UP = 1
     DOWN = 2
 
 
-class ContainerState:
+class ContainerState(StrEnum):
     """Portainer container state."""
 
     RUNNING = "running"
 
 
-class StackStatus:
+class StackStatus(IntEnum):
     """Portainer stack status."""
 
     ACTIVE = 1
     INACTIVE = 2
 
 
-class StackType:
+class StackType(IntEnum):
     """Portainer stack type."""
 
     SWARM = 1
