0a52406fadb0723a22c871bcbe3a0a89e6b65566
Erik <erik@montnemery.com>
2026-03-04 14:00:55 +0100

    Deprecate unused helper function cover.is_closed

--- homeassistant/components/cover/__init__.py
+++ homeassistant/components/cover/__init__.py
@@ -25,6 +25,7 @@ from homeassistant.const import (
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ServiceValidationError
 from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.deprecation import deprecated_function
 from homeassistant.helpers.entity import Entity, EntityDescription
 from homeassistant.helpers.entity_component import EntityComponent
 from homeassistant.helpers.typing import ConfigType
@@ -91,6 +92,7 @@ __all__ = [
 ]
 
 
+@deprecated_function("code which checks the state", breaks_in_ha_version="2027.4")
 def is_closed(hass: HomeAssistant, entity_id: str) -> bool:
     """Return if the cover is closed based on the statemachine."""
     return hass.states.is_state(entity_id, CoverState.CLOSED)
