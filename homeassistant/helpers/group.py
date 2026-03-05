e0f7d5fed82ec63903daf9812b48efe72865355c
Artur Pragacz <artur@pragacz.com>
2026-03-05 20:40:52 +0100

    Better filter

--- homeassistant/helpers/group.py
+++ homeassistant/helpers/group.py
@@ -125,6 +125,8 @@ class IntegrationSpecificGroup(Group):
             if (
                 event.data["action"] in {"create", "update"}
                 and (entry := entity_registry.async_get(event.data["entity_id"]))
+                and entry.domain == entity.platform.domain
+                and entry.platform == entity.platform.platform_name
                 and entry.unique_id in self.member_unique_ids
             ) or (
                 event.data["action"] == "remove"
