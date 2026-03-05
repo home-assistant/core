ce926bbcca74efe4211ac46d3a0d6100b4c2f18d
J. Diego Rodríguez Royo <jdrr1998@hotmail.com>
2026-03-05 20:52:17 +0000

    Get program from base program option at Home Connect

--- homeassistant/components/home_connect/select.py
+++ homeassistant/components/home_connect/select.py
@@ -430,10 +430,23 @@ class HomeConnectProgramSelectEntity(HomeConnectEntity, SelectEntity):
     def update_native_value(self) -> None:
         """Set the program value."""
         event = self.appliance.events.get(cast(EventKey, self.bsh_key))
-        self._attr_current_option = (
-            PROGRAMS_TRANSLATION_KEYS_MAP.get(ProgramKey(event_value))
+        program_key = (
+            ProgramKey(event_value)
             if event and isinstance(event_value := event.value, str)
             else None
+        )
+        if (
+            program_key == ProgramKey.BSH_COMMON_FAVORITE_001
+            and (
+                base_program_event := self.appliance.events.get(
+                    EventKey.BSH_COMMON_OPTION_BASE_PROGRAM
+                )
+            )
+            and isinstance(base_program_event.value, str)
+        ):
+            program_key = ProgramKey(base_program_event.value)
+        self._attr_current_option = (
+            PROGRAMS_TRANSLATION_KEYS_MAP.get(program_key) if program_key else None
         )
 
     async def async_select_option(self, option: str) -> None:
