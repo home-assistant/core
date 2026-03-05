ea21d656abb61830845161f21a3bfa64802af306
jbouwh <jan@jbsoft.nl>
2026-03-05 19:01:34 +0000

    Follow up on code review

--- homeassistant/components/mqtt/entity.py
+++ homeassistant/components/mqtt/entity.py
@@ -471,7 +471,7 @@ class MqttAttributesMixin(Entity):
         [MessageCallbackType, set[str] | None, ReceiveMessage], None
     ]
     _process_update_extra_state_attributes: Callable[[dict[str, Any]], None]
-    group: IntegrationSpecificGroup
+    group: IntegrationSpecificGroup | None = None
 
     def __init__(self, config: ConfigType) -> None:
         """Initialize the JSON attributes and handle group entities."""
@@ -488,7 +488,7 @@ class MqttAttributesMixin(Entity):
 
     def attributes_prepare_discovery_update(self, config: DiscoveryInfoType) -> None:
         """Handle updated discovery message."""
-        if hasattr(self, "group") and CONF_GROUP in config:
+        if self.group is not None and CONF_GROUP in config:
             self.group.included_unique_ids = config[CONF_GROUP]
         self._attributes_config = config
         self._attributes_prepare_subscribe_topics()
