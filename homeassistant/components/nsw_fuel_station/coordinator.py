1063c50a58223cdfd361e7de41cceec2e1eca1ea
epenet <6771947+epenet@users.noreply.github.com>
2026-03-06 09:56:01 +0000

    Reduce

--- homeassistant/components/nsw_fuel_station/coordinator.py
+++ homeassistant/components/nsw_fuel_station/coordinator.py
@@ -27,6 +27,8 @@ class StationPriceData:
 class NSWFuelStationCoordinator(DataUpdateCoordinator[StationPriceData | None]):
     """Class to manage fetching NSW fuel station data."""
 
+    config_entry: None
+
     def __init__(self, hass: HomeAssistant, client: FuelCheckClient) -> None:
         """Initialize the coordinator."""
         super().__init__(
@@ -45,20 +47,19 @@ class NSWFuelStationCoordinator(DataUpdateCoordinator[StationPriceData | None]):
         )
 
 
-def _fetch_station_price_data(
-    client: FuelCheckClient,
-) -> StationPriceData | None:
+def _fetch_station_price_data(client: FuelCheckClient) -> StationPriceData | None:
     """Fetch fuel price and station data."""
     try:
         raw_price_data = client.get_fuel_prices()
+        # Restructure prices and station details to be indexed by station code
+        # for O(1) lookup
+        return StationPriceData(
+            stations={s.code: s for s in raw_price_data.stations},
+            prices={
+                (p.station_code, p.fuel_type): p.price for p in raw_price_data.prices
+            },
+        )
     except FuelCheckError as exc:
         raise UpdateFailed(
             f"Failed to fetch NSW Fuel station price data: {exc}"
         ) from exc
-
-    # Restructure prices and station details to be indexed by station code
-    # for O(1) lookup
-    return StationPriceData(
-        stations={s.code: s for s in raw_price_data.stations},
-        prices={(p.station_code, p.fuel_type): p.price for p in raw_price_data.prices},
-    )
