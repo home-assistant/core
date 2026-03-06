02bce6d2ce8db11d245306f86689e3c90a4e3680
epenet <6771947+epenet@users.noreply.github.com>
2026-03-06 18:03:17 +0100

    Improve type hints in schluter climate (#164970)

--- homeassistant/components/schluter/climate.py
+++ homeassistant/components/schluter/climate.py
@@ -89,19 +89,15 @@ class SchluterThermostat(CoordinatorEntity, ClimateEntity):
         self._serial_number = serial_number
         self._api = api
         self._session_id = session_id
+        self._attr_unique_id = serial_number
 
     @property
-    def unique_id(self):
-        """Return unique ID for this device."""
-        return self._serial_number
-
-    @property
-    def name(self):
+    def name(self) -> str:
         """Return the name of the thermostat."""
         return self.coordinator.data[self._serial_number].name
 
     @property
-    def current_temperature(self):
+    def current_temperature(self) -> float:
         """Return the current temperature."""
         return self.coordinator.data[self._serial_number].temperature
 
@@ -113,7 +109,7 @@ class SchluterThermostat(CoordinatorEntity, ClimateEntity):
         return HVACAction.IDLE
 
     @property
-    def target_temperature(self):
+    def target_temperature(self) -> float:
         """Return the temperature we try to reach."""
         return self.coordinator.data[self._serial_number].set_point_temp
 
