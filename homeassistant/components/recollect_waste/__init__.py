84ca290c9590c5485146f01ea07f4606d2b7e0d8
epenet <6771947+epenet@users.noreply.github.com>
2026-03-06 07:21:00 +0000

    docstring

--- homeassistant/components/recollect_waste/__init__.py
+++ homeassistant/components/recollect_waste/__init__.py
@@ -16,7 +16,7 @@ PLATFORMS = [Platform.CALENDAR, Platform.SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up RainMachine as config entry."""
+    """Set up ReCollect Waste as config entry."""
     coordinator = ReCollectWasteDataUpdateCoordinator(hass, entry)
 
     await coordinator.async_config_entry_first_refresh()
@@ -36,7 +36,7 @@ async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload an RainMachine config entry."""
+    """Unload an ReCollect Waste config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
