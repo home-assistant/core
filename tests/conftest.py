b9737370446c4d00e2f72ebe1cc8f89cc314a44a
tronikos <tronikos@users.noreply.github.com>
2026-03-08 07:25:04 +0000

    Add diagnostics in Opower

--- tests/conftest.py
+++ tests/conftest.py
@@ -150,7 +150,7 @@ from .test_util.aiohttp import (  # noqa: E402, isort:skip
 _LOGGER = logging.getLogger(__name__)
 
 logging.basicConfig(level=logging.INFO)
-logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+# logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
 
 asyncio.set_event_loop_policy(runner.HassEventLoopPolicy(False))
 # Disable fixtures overriding our beautiful policy
