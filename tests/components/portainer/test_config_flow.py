c43300819a7a8ee2448e7b1af322c3d2b50404f9
Erwin Douna <e.douna@gmail.com>
2026-03-08 13:19:45 +0100

    Fix forced VERIFY_SSL in Portainer (#165079)

--- tests/components/portainer/test_config_flow.py
+++ tests/components/portainer/test_config_flow.py
@@ -263,20 +263,28 @@ async def test_full_flow_reconfigure_unique_id(
 ) -> None:
     """Test the full flow of the config flow, this time with a known unique ID."""
     mock_config_entry.add_to_hass(hass)
+
+    other_entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="Portainer other",
+        data=USER_INPUT_RECONFIGURE,
+        unique_id=USER_INPUT_RECONFIGURE[CONF_API_TOKEN],
+    )
+    other_entry.add_to_hass(hass)
+
     result = await mock_config_entry.start_reconfigure_flow(hass)
     assert result["type"] is FlowResultType.FORM
     assert result["step_id"] == "reconfigure"
 
     result = await hass.config_entries.flow.async_configure(
         result["flow_id"],
-        user_input=MOCK_USER_SETUP,
+        user_input=USER_INPUT_RECONFIGURE,
     )
 
     assert result["type"] is FlowResultType.ABORT
     assert result["reason"] == "already_configured"
     assert mock_config_entry.data[CONF_API_TOKEN] == "test_api_token"
     assert mock_config_entry.data[CONF_URL] == "https://127.0.0.1:9000/"
-    assert mock_config_entry.data[CONF_VERIFY_SSL] is True
     assert len(mock_setup_entry.mock_calls) == 0
 
 
