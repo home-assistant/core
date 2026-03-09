3da5896096ae78a965d74da4f29df95ecbcd2131
Alex Merkel <mail@alexmerkel.com>
2026-03-09 19:45:21 +0100

    Fix incorrect state using additional conditions

--- homeassistant/components/lg_soundbar/media_player.py
+++ homeassistant/components/lg_soundbar/media_player.py
@@ -161,17 +161,27 @@ class LGDevice(MediaPlayerEntity):
     def _update_playinfo(self, data: dict[str, Any]) -> None:
         """Update the player info."""
         if "i_play_ctrl" in data:
-            if self._attr_device_on:
+            if self._attr_device_on and self._attr_stream_type != 0:
                 if data["i_play_ctrl"] == 0:
                     self._attr_state = MediaPlayerState.PLAYING
                 else:
                     self._attr_state = MediaPlayerState.PAUSED
         if "i_stream_type" in data:
-            self._attr_stream_type = data["i_stream_type"]
+            if self._attr_stream_type != data["i_stream_type"]:
+                self._attr_stream_type = data["i_stream_type"]
+                # Ask device for current play info when stream type changed.
+                self._device.get_play()
             if data["i_stream_type"] == 0:
+                # If the stream type is 0 (aka the soundbar is used as an actual soundbar)
+                # the last track info should be cleared and the state should only be on or off,
+                # as all playing/paused are not applicable in this mode
                 self._attr_media_image_url = None
                 self._attr_media_artist = None
                 self._attr_media_title = None
+                if self._attr_device_on:
+                    self._attr_state = MediaPlayerState.ON
+                else:
+                    self._attr_state = MediaPlayerState.OFF
         if "s_albumart" in data:
             if data["s_albumart"].strip():
                 self._attr_media_image_url = data["s_albumart"]
