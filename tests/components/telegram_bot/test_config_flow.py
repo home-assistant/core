de15fe046c2715f9264c8fde25452ee2ca86244e
Erik <erik@montnemery.com>
2026-03-10 21:03:19 +0100

    Prevent network access in telegram_bot tests

--- tests/components/telegram_bot/test_config_flow.py
+++ tests/components/telegram_bot/test_config_flow.py
@@ -1,5 +1,6 @@
 """Config flow tests for the Telegram Bot integration."""
 
+from collections.abc import Generator
 from unittest.mock import AsyncMock, patch
 
 from telegram import User
@@ -27,6 +28,15 @@ from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
 from tests.common import MockConfigEntry, pytest
+
+
+@pytest.fixture
+def mock_setup_entry() -> Generator[AsyncMock]:
+    """Override async_setup_entry."""
+    with patch(
+        "homeassistant.components.telegram_bot.async_setup_entry", return_value=True
+    ) as mock_setup_entry:
+        yield mock_setup_entry
 
 
 async def test_options_flow(
@@ -393,6 +403,7 @@ async def test_create_entry(hass: HomeAssistant) -> None:
         ),
     ],
 )
+@pytest.mark.usefixtures("mock_setup_entry")
 async def test_create_webhook_entry(
     hass: HomeAssistant, api_endpoint: str, webhook_url: str
 ) -> None:
