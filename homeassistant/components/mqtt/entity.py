c18c7b9180e88aa4c3892987ff37e16bd434d6e0
jbouwh <jan@jbsoft.nl>
2026-03-10 17:50:38 +0000

    Proces upstream changes

--- homeassistant/components/mqtt/entity.py
+++ homeassistant/components/mqtt/entity.py
@@ -489,7 +489,7 @@ class MqttAttributesMixin(Entity):
     def attributes_prepare_discovery_update(self, config: DiscoveryInfoType) -> None:
         """Handle updated discovery message."""
         if self.group is not None and CONF_GROUP in config:
-            self.group.included_unique_ids = config[CONF_GROUP]
+            self.group.member_unique_ids = config[CONF_GROUP]
         self._attributes_config = config
         self._attributes_prepare_subscribe_topics()
 
