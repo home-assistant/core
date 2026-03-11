3da78d155b194714cf689e1f197d7329d1596d45
Sab44 <64696149+Sab44@users.noreply.github.com>
2026-03-11 13:19:36 +0100

    add test to assert custom equivalent units are joined across platforms

--- tests/components/recorder/test_statistics.py
+++ tests/components/recorder/test_statistics.py
@@ -3937,6 +3937,82 @@ async def test_recorder_platform_with_partial_statistics_support(
         getattr(recorder_platform, meth).assert_called_once()
 
 
+async def test_recorder_platforms_with_custom_equivalent_units(
+    hass: HomeAssistant,
+    setup_recorder: None,
+) -> None:
+    """Test recorder platforms providing custom equivalent units are joined when compiling statistics."""
+    recorder_data = hass.data["recorder"]
+    assert not recorder_data.recorder_platforms
+
+    def _mock_compile_statistics(*args: Any) -> PlatformCompiledStatistics:
+        return PlatformCompiledStatistics([], {})
+
+    custom_equivalent_units_recorder_platform_one = {
+        "sensor.test_sensor_1": {"custom_unitA": "unitA"}
+    }
+
+    def _mock_custom_equivalent_units_one(*args: Any) -> dict[str, dict[str, str]]:
+        return custom_equivalent_units_recorder_platform_one
+
+    recorder_platform_one = Mock(
+        compile_statistics=Mock(wraps=_mock_compile_statistics),
+        custom_equivalent_units=Mock(wraps=_mock_custom_equivalent_units_one),
+        list_statistic_ids=None,
+        update_statistics_issues=None,
+        validate_statistics=None,
+    )
+
+    mock_platform(hass, "some_domain_one.recorder", recorder_platform_one)
+    assert await async_setup_component(hass, "some_domain_one", {})
+
+    custom_equivalent_units_recorder_platform_two = {
+        "sensor.test_sensor_2": {"custom_unitB": "unitB"},
+        "sensor.test_sensor_3": {"custom_unitC": "unitC"},
+    }
+
+    def _mock_custom_equivalent_units_two(*args: Any) -> dict[str, dict[str, str]]:
+        return custom_equivalent_units_recorder_platform_two
+
+    recorder_platform_two = Mock(
+        compile_statistics=None,
+        custom_equivalent_units=Mock(wraps=_mock_custom_equivalent_units_two),
+        list_statistic_ids=None,
+        update_statistics_issues=None,
+        validate_statistics=None,
+    )
+
+    mock_platform(hass, "some_domain_two.recorder", recorder_platform_two)
+    assert await async_setup_component(hass, "some_domain_two", {})
+
+    # Wait for the recorder platforms to be added
+    await async_recorder_block_till_done(hass)
+    assert recorder_data.recorder_platforms == {
+        "some_domain_one": recorder_platform_one,
+        "some_domain_two": recorder_platform_two,
+    }
+
+    recorder_platform_one.compile_statistics.assert_not_called()
+    recorder_platform_one.custom_equivalent_units.assert_not_called()
+    recorder_platform_two.custom_equivalent_units.assert_not_called()
+
+    # Test compile statistics
+    zero = get_start_time(dt_util.utcnow()).replace(minute=50) + timedelta(hours=1)
+    do_adhoc_statistics(hass, start=zero)
+    await async_wait_recording_done(hass)
+
+    expected_custom_equivalent_units = {
+        **custom_equivalent_units_recorder_platform_one,
+        **custom_equivalent_units_recorder_platform_two,
+    }
+
+    recorder_platform_one.compile_statistics.assert_called_once_with(
+        hass, ANY, zero, zero + timedelta(minutes=5), expected_custom_equivalent_units
+    )
+    recorder_platform_one.custom_equivalent_units.assert_called_once()
+    recorder_platform_two.custom_equivalent_units.assert_called_once()
+
+
 @pytest.mark.parametrize(
     ("service_args", "expected_result"),
     [
