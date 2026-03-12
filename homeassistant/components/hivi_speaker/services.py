85a953bac9a829be479a883e35b610ce4599d53d
swansmart <swansmart@swanspeakers.com>
2026-03-12 10:16:53 +0800

    Refactor service registration for speaker devices

--- homeassistant/components/hivi_speaker/services.py
+++ homeassistant/components/hivi_speaker/services.py
@@ -45,32 +45,47 @@ async def async_setup_services(hass: HomeAssistant) -> None:
             return
 
         for domain_data in hass.data[DOMAIN].values():
-            if "device_manager" in domain_data:
-                success = await domain_data["device_manager"].remove_device(
-                    speaker_device_id
+            if "device_manager" not in domain_data:
+                continue
+            device_manager = domain_data["device_manager"]
+            ha_device_id = (
+                device_manager.device_data_registry
+                .get_ha_device_id_by_speaker_device_id(speaker_device_id)
+            )
+            if ha_device_id is None:
+                _LOGGER.warning(
+                    "Cannot find HA device for speaker_device_id %s",
+                    speaker_device_id,
                 )
-                if success:
-                    _LOGGER.debug("Remove device %s called", speaker_device_id)
-                    break
+                return
+            await device_manager.async_remove_device_with_entities(ha_device_id)
+            await device_manager.remove_control_entities_by_speaker_device_id(
+                speaker_device_id
+            )
+            _LOGGER.debug("Remove device %s called", speaker_device_id)
+            break
 
-    hass.services.async_register(
-        DOMAIN, "refresh_discovery", async_handle_refresh, schema=vol.Schema({})
-    )
+    if not hass.services.has_service(DOMAIN, "refresh_discovery"):
+        hass.services.async_register(
+            DOMAIN, "refresh_discovery", async_handle_refresh, schema=vol.Schema({})
+        )
 
-    hass.services.async_register(
-        DOMAIN,
-        "postpone_discovery",
-        async_handle_postpone_discovery,
-        schema=vol.Schema({}),
-    )
+    if not hass.services.has_service(DOMAIN, "postpone_discovery"):
+        hass.services.async_register(
+            DOMAIN,
+            "postpone_discovery",
+            async_handle_postpone_discovery,
+            schema=vol.Schema({}),
+        )
 
-    hass.services.async_register(
-        DOMAIN,
-        "remove_device",
-        async_handle_remove_device,
-        schema=vol.Schema(
-            {
-                vol.Required("speaker_device_id"): str,
-            }
-        ),
-    )
+    if not hass.services.has_service(DOMAIN, "remove_device"):
+        hass.services.async_register(
+            DOMAIN,
+            "remove_device",
+            async_handle_remove_device,
+            schema=vol.Schema(
+                {
+                    vol.Required("speaker_device_id"): str,
+                }
+            ),
+        )
