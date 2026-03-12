e5d896bf6c87d04889cd166cf999215c0d5a60e2
swansmart <swansmart@swanspeakers.com>
2026-03-12 18:03:51 +0800

    Add SIGNAL_DEVICE_STATUS_UPDATED constant

--- homeassistant/components/hivi_speaker/const.py
+++ homeassistant/components/hivi_speaker/const.py
@@ -1,8 +1,13 @@
+"""Constants for the HiVi Speaker integration."""
+
+from __future__ import annotations
+
 DOMAIN = "hivi_speaker"
 
 DISCOVERY_UPDATED = "hivi_speaker_discovery_updated"
 DEVICE_RELATION_CHANGED = "hivi_speaker_device_relation_changed"
 SIGNAL_DEVICE_DISCOVERED = "hivi_speaker_signal_device_discovered"
+SIGNAL_DEVICE_STATUS_UPDATED = "hivi_speaker_signal_device_status_updated"
 
 DISCOVERY_BASE_INTERVAL = 300  # seconds
 DEVICE_OFFLINE_THRESHOLD = 180  # seconds
