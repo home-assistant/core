80166706df3939eed7ea5109805a02ea61181fd1
justin <justin@boyd.net.au>
2026-03-13 02:36:57 +0000

    commiting the migration code.

--- homeassistant/components/airtouch5/__init__.py
+++ homeassistant/components/airtouch5/__init__.py
@@ -5,12 +5,15 @@ from __future__ import annotations
 import logging
 
 from airtouch5py.airtouch5_simple_client import Airtouch5SimpleClient
-from airtouch5py.discovery import AirtouchDevice
+from airtouch5py.discovery import AirtouchDiscovery
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers import device_registry as dr, entity_registry as er
+
+from .const import DOMAIN
 
 # device_registry as dr, Maybe needed in migration. If not can be removed later.
 
@@ -64,11 +67,72 @@ async def async_unload_entry(hass: HomeAssistant, entry: Airtouch5ConfigEntry) -
     return unload_ok
 
 
-async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+async def async_migrate_entry(hass: HomeAssistant, entry: Airtouch5ConfigEntry) -> bool:
     """Migrate old entry."""
-    _LOGGER.info(
-        "Migrating configuration from version %s.%s",
-        config_entry.version,
-        config_entry.minor_version,
-    )
+    if entry.minor_version == 1:
+        host = entry.data[CONF_HOST]
+        try:
+            identifier = entry.unique_id
+            assert identifier is not None
+            AirtouchDiscovery_instance = AirtouchDiscovery()
+            await AirtouchDiscovery_instance.establish_server()
+            airtouch_device = await AirtouchDiscovery_instance.discover_by_ip(host)
+            assert airtouch_device is not None, "Device not found during migration"
+            # If for some reason the device is not found during migration, it will fail and will retry next time. This could leave a persistent error if the device cannout route UDP.
+        except TimeoutError as exception:
+            _LOGGER.error("Error while migrating: %s", exception)
+            return False
+
+        # looking for climate entities
+        entity_registry = er.async_get(hass)
+        for entity in entity_registry.entities.values():
+            new_unique_id = None  # initialize
+            if entity.platform == DOMAIN:
+                if entity.domain == "climate":
+                    if entity.unique_id.startswith("zone_"):
+                        zone_number = entity.unique_id.split("_")[1]
+                        new_unique_id = f"{airtouch_device.system_id}_{zone_number}"
+                    elif entity.unique_id.startswith("ac_"):
+                        new_unique_id = f"{airtouch_device.system_id}"
+                    else:
+                        continue
+                elif entity.domain == "cover":
+                    zone_number = entity.unique_id.split("_")[1]
+                    new_unique_id = (
+                        f"{airtouch_device.system_id}_{zone_number}_open_percentage"
+                    )
+                else:
+                    continue
+                entity_id = entity_registry.async_get_entity_id(
+                    entity.domain, DOMAIN, entity.unique_id
+                )
+                assert entity_id is not None
+                entity_entry = entity_registry.async_get(entity_id)
+                assert entity_entry is not None
+                entity_registry.async_update_entity(
+                    entity_entry.entity_id, new_unique_id=new_unique_id
+                )
+                _LOGGER.debug(
+                    "Found entity: %s (unique_id=%s) new ID=%s",
+                    entity.entity_id,
+                    entity.unique_id,
+                    new_unique_id,
+                )
+        device_registry = dr.async_get(hass)
+        for device in device_registry.devices.values():
+            if any(d[0] == DOMAIN for d in device.identifiers):
+                _LOGGER.debug(
+                    "Found device: %s, identifiers=%s",
+                    airtouch_device.name,
+                    device.identifiers,
+                )
+                device_registry.async_update_device(
+                    device.id, new_identifiers={(DOMAIN, airtouch_device.system_id)}
+                )
+
+        hass.config_entries.async_update_entry(
+            entry,
+            unique_id=airtouch_device.system_id,
+            minor_version=2,
+        )
     return True
