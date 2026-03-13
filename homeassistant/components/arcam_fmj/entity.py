86b23db627046b2e1247a98284a869ab9768b42c
epenet <6771947+epenet@users.noreply.github.com>
2026-03-13 12:27:52 +0100

    Add base entity to arcam_fmj (#165447)

---
+++ homeassistant/components/arcam_fmj/entity.py
@@ -0,0 +1,20 @@
+"""Base entity for Arcam FMJ integration."""
+
+from __future__ import annotations
+
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .coordinator import ArcamFmjCoordinator
+
+
+class ArcamFmjEntity(CoordinatorEntity[ArcamFmjCoordinator]):
+    """Base entity for Arcam FMJ."""
+
+    _attr_has_entity_name = True
+
+    def __init__(self, coordinator: ArcamFmjCoordinator) -> None:
+        """Initialize the entity."""
+        super().__init__(coordinator)
+        self._attr_device_info = coordinator.device_info
+        self._attr_entity_registry_enabled_default = coordinator.state.zn == 1
+        self._attr_unique_id = coordinator.zone_unique_id
