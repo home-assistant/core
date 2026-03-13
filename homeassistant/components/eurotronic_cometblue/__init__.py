6b8c0821b18d84bd9dd7bac21ad14c0c3406b457
Richard <42204099+rikroe@users.noreply.github.com>
2026-03-14 00:15:54 +0100

    Fix async_unload_entry

--- homeassistant/components/eurotronic_cometblue/__init__.py
+++ homeassistant/components/eurotronic_cometblue/__init__.py
@@ -252,9 +252,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        coordinator: CometBlueDataUpdateCoordinator = hass.data[DOMAIN].pop(
-            entry.entry_id
-        )
+        coordinator: CometBlueDataUpdateCoordinator = entry.runtime_data
         await coordinator.async_shutdown()
 
     return unload_ok
