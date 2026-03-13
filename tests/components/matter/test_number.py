a1258b56ab82042f93f52760edc1a5717f1d49b4
jvmahon <jvm33@columbia.edu>
2026-03-13 12:04:12 -0400

    Add Matter start-up Power-on level entity (#164775)

--- tests/components/matter/test_number.py
+++ tests/components/matter/test_number.py
@@ -43,6 +43,10 @@ async def test_level_control_config_entities(
     assert state
     assert state.state == "255"
 
+    state = hass.states.get("number.mock_dimmable_light_power_on_level")
+    assert state
+    assert state.state == "255"
+
     state = hass.states.get("number.mock_dimmable_light_on_transition_time")
     assert state
     assert state.state == "0.0"
@@ -61,6 +65,64 @@ async def test_level_control_config_entities(
     state = hass.states.get("number.mock_dimmable_light_on_level")
     assert state
     assert state.state == "20"
+
+    set_node_attribute(matter_node, 1, 0x00000008, 0x4000, 128)
+    await trigger_subscription_callback(hass, matter_client)
+
+    state = hass.states.get("number.mock_dimmable_light_power_on_level")
+    assert state
+    assert state.state == "128"
+
+    set_node_attribute(matter_node, 1, 0x00000008, 0x4000, 255)
+    await trigger_subscription_callback(hass, matter_client)
+
+    state = hass.states.get("number.mock_dimmable_light_power_on_level")
+    assert state
+    assert state.state == "255"
+    # Set a concrete value (not null)
+    await hass.services.async_call(
+        "number",
+        "set_value",
+        {
+            "entity_id": "number.mock_dimmable_light_power_on_level",
+            "value": 128,
+        },
+        blocking=True,
+    )
+
+    # Verify write
+    assert matter_client.write_attribute.call_count == 1
+    assert matter_client.write_attribute.call_args_list[0] == call(
+        node_id=matter_node.node_id,
+        attribute_path=create_attribute_path_from_attribute(
+            endpoint_id=1,
+            attribute=clusters.LevelControl.Attributes.StartUpCurrentLevel,
+        ),
+        value=128,
+    )
+
+    matter_client.write_attribute.reset_mock()
+    # Set a null-equivalent value (255 should map to None on the wire)
+    await hass.services.async_call(
+        "number",
+        "set_value",
+        {
+            "entity_id": "number.mock_dimmable_light_power_on_level",
+            "value": 255,
+        },
+        blocking=True,
+    )
+
+    # Verify write
+    assert matter_client.write_attribute.call_count == 1
+    assert matter_client.write_attribute.call_args_list[0] == call(
+        node_id=matter_node.node_id,
+        attribute_path=create_attribute_path_from_attribute(
+            endpoint_id=1,
+            attribute=clusters.LevelControl.Attributes.StartUpCurrentLevel,
+        ),
+        value=None,
+    )
 
 
 @pytest.mark.parametrize("node_fixture", ["eve_weather_sensor"])
