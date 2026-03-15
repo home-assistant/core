588cf47d40812a22ff72d3b0a30eeb9e63f4716e
Joost Lekkerkerker <joostlek@outlook.com>
2026-03-15 11:39:57 +0100

    Remove stateclass from timestamp entity in Intellifire (#165403)

--- homeassistant/components/intellifire/sensor.py
+++ homeassistant/components/intellifire/sensor.py
@@ -97,7 +97,6 @@ INTELLIFIRE_SENSORS: tuple[IntellifireSensorEntityDescription, ...] = (
     IntellifireSensorEntityDescription(
         key="timer_end_timestamp",
         translation_key="timer_end_timestamp",
-        state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.TIMESTAMP,
         value_fn=_time_remaining_to_timestamp,
     ),
