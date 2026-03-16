16d016eb8ae04d8a0e8a138c0baad315e19f4a31
David Knowles <dknowles2@gmail.com>
2026-03-16 01:16:33 +0000

    Move the connected availability check to the base entity and clean up more tests

--- tests/components/schlage/test_binary_sensor.py
+++ tests/components/schlage/test_binary_sensor.py
@@ -1,65 +1,64 @@
 """Test Schlage binary_sensor."""
 
-from datetime import timedelta
-from unittest.mock import Mock
+from collections.abc import Awaitable, Callable
+from unittest.mock import Mock, patch
 
-from freezegun.api import FrozenDateTimeFactory
 from pyschlage.exceptions import UnknownError
+from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.components.binary_sensor import BinarySensorDeviceClass
-from homeassistant.const import STATE_ON
+from homeassistant.const import STATE_ON, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 
 from . import MockSchlageConfigEntry
 
-from tests.common import async_fire_time_changed
+from tests.common import snapshot_platform
+
+
+async def test_binary_sensor_attributes(
+    hass: HomeAssistant,
+    mock_add_config_entry: Callable[[], Awaitable[MockSchlageConfigEntry]],
+    entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test binary sensor attributes."""
+    with patch("homeassistant.components.schlage.PLATFORMS", [Platform.BINARY_SENSOR]):
+        config_entry = await mock_add_config_entry()
+        await snapshot_platform(hass, entity_registry, snapshot, config_entry.entry_id)
 
 
 async def test_keypad_disabled_binary_sensor(
     hass: HomeAssistant,
-    mock_schlage: Mock,
     mock_lock: Mock,
-    mock_added_config_entry: MockSchlageConfigEntry,
-    freezer: FrozenDateTimeFactory,
+    mock_add_config_entry: Callable[[], Awaitable[MockSchlageConfigEntry]],
 ) -> None:
     """Test the keypad_disabled binary_sensor."""
     mock_lock.keypad_disabled.reset_mock()
     mock_lock.keypad_disabled.return_value = True
-
-    # Make the coordinator refresh data.
-    freezer.tick(timedelta(seconds=30))
-    async_fire_time_changed(hass)
-    await hass.async_block_till_done(wait_background_tasks=True)
-
-    keypad = hass.states.get("binary_sensor.vault_door_keypad_disabled")
-    assert keypad is not None
-    assert keypad.state == STATE_ON
-    assert keypad.attributes["device_class"] == BinarySensorDeviceClass.PROBLEM
-
-    mock_lock.keypad_disabled.assert_called_once_with([])
+    with patch("homeassistant.components.schlage.PLATFORMS", [Platform.BINARY_SENSOR]):
+        await mock_add_config_entry()
+        keypad = hass.states.get("binary_sensor.vault_door_keypad_disabled")
+        assert keypad is not None
+        assert keypad.state == STATE_ON
+        assert keypad.attributes["device_class"] == BinarySensorDeviceClass.PROBLEM
+        mock_lock.keypad_disabled.assert_called_once_with([])
 
 
 async def test_keypad_disabled_binary_sensor_use_previous_logs_on_failure(
     hass: HomeAssistant,
-    mock_schlage: Mock,
     mock_lock: Mock,
-    mock_added_config_entry: MockSchlageConfigEntry,
-    freezer: FrozenDateTimeFactory,
+    mock_add_config_entry: Callable[[], Awaitable[MockSchlageConfigEntry]],
 ) -> None:
     """Test the keypad_disabled binary_sensor."""
     mock_lock.keypad_disabled.reset_mock()
     mock_lock.keypad_disabled.return_value = True
     mock_lock.logs.reset_mock()
     mock_lock.logs.side_effect = UnknownError("Cannot load logs")
-
-    # Make the coordinator refresh data.
-    freezer.tick(timedelta(seconds=30))
-    async_fire_time_changed(hass)
-    await hass.async_block_till_done(wait_background_tasks=True)
-
-    keypad = hass.states.get("binary_sensor.vault_door_keypad_disabled")
-    assert keypad is not None
-    assert keypad.state == STATE_ON
-    assert keypad.attributes["device_class"] == BinarySensorDeviceClass.PROBLEM
-
-    mock_lock.keypad_disabled.assert_called_once_with([])
+    with patch("homeassistant.components.schlage.PLATFORMS", [Platform.BINARY_SENSOR]):
+        await mock_add_config_entry()
+        keypad = hass.states.get("binary_sensor.vault_door_keypad_disabled")
+        assert keypad is not None
+        assert keypad.state == STATE_ON
+        assert keypad.attributes["device_class"] == BinarySensorDeviceClass.PROBLEM
+        mock_lock.keypad_disabled.assert_called_once_with([])
