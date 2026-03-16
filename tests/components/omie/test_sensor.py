78685b3c70c3ce6752d325e7799168294a1b741f
abmantis <amfcalt@gmail.com>
2026-03-16 16:51:59 +0000

    Comment cleanup

--- tests/components/omie/test_sensor.py
+++ tests/components/omie/test_sensor.py
@@ -28,7 +28,6 @@ async def test_sensor_setup(
     assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
     await hass.async_block_till_done()
 
-    # Check that entities were created
     entity_registry = er.async_get(hass)
     entities = er.async_entries_for_config_entry(
         entity_registry, mock_config_entry.entry_id
@@ -37,10 +36,9 @@ async def test_sensor_setup(
     # Should have 2 sensors (PT and ES)
     assert len(entities) == 2
 
-    # Check entity unique IDs
-    entity_ids = {entity.unique_id for entity in entities}
+    unique_ids = {entity.unique_id for entity in entities}
     expected_ids = {"pt_spot_price", "es_spot_price"}
-    assert entity_ids == expected_ids
+    assert unique_ids == expected_ids
 
 
 @pytest.mark.usefixtures("hass_lisbon")
@@ -66,8 +64,6 @@ async def test_sensor_state_lisbon_timezone(
     with freeze_time("2024-01-15T12:01:00Z"):  # 12:01 UTC = 12:01 Lisbon = 13:01 CET
         assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
         await hass.async_block_till_done()
-
-        # Only 1 API call for Jan 15 (Jan 16 not yet published at 13:30 CET)
         assert mock_pyomie.spot_price.call_count == 1
 
     # Step 2: 3 PM CET - verify listeners update with existing data, no new API calls
@@ -75,19 +71,12 @@ async def test_sensor_state_lisbon_timezone(
     with freeze_time("2024-01-15T14:01:00Z"):  # 14:01 UTC = 14:01 Lisbon = 15:01 CET
         async_fire_time_changed(hass, dt_util.utcnow())
         await hass.async_block_till_done()
-
-        # No additional API calls should be made
         assert mock_pyomie.spot_price.call_count == 0
 
-        # Check sensor states - values should be converted from €/MWh to €/kWh
         pt_state_14 = hass.states.get("sensor.omie_portugal_spot_price")
         es_state_14 = hass.states.get("sensor.omie_spain_spot_price")
-
-        # At 14:00 UTC (= 14:00 Lisbon = 3 PM CET)
-        assert pt_state_14.state == "351151500.0"  # (PT day 15, hour 15, minute 00)
-        assert es_state_14.state == "34151500.0"  # (ES day 15, hour 15, minute 00)
-
-        # Check units are correct
+        assert pt_state_14.state == "351151500.0"
+        assert es_state_14.state == "34151500.0"
         assert pt_state_14.attributes["unit_of_measurement"] == "€/kWh"
         assert es_state_14.attributes["unit_of_measurement"] == "€/kWh"
 
@@ -99,12 +88,10 @@ async def test_sensor_state_lisbon_timezone(
 
         # CET has rolled over to the next date, one additional call to OMIE must be made.
         assert mock_pyomie.spot_price.call_count == 1
-
         pt_state_23 = hass.states.get("sensor.omie_portugal_spot_price")
         es_state_23 = hass.states.get("sensor.omie_spain_spot_price")
-
-        assert pt_state_23.state == "351160000.0"  # (PT day 16, hour 00, minute 00)
-        assert es_state_23.state == "34160000.0"  # (ES day 16, hour 00, minute 00)
+        assert pt_state_23.state == "351160000.0"
+        assert es_state_23.state == "34160000.0"
 
     # 00 UTC (+1 day) = 00 Lisbon (+1 day) = 01 CET (+1 day)
     mock_pyomie.spot_price.reset_mock()
@@ -114,12 +101,10 @@ async def test_sensor_state_lisbon_timezone(
 
         # No additional API calls should be made, was already fetched at 00 PM CET
         assert mock_pyomie.spot_price.call_count == 0
-
         pt_state_00 = hass.states.get("sensor.omie_portugal_spot_price")
         es_state_00 = hass.states.get("sensor.omie_spain_spot_price")
-
-        assert pt_state_00.state == "351160130.0"  # (PT day 16, hour 01, minute 30)
-        assert es_state_00.state == "34160130.0"  # (ES day 16, hour 01, minute 00)
+        assert pt_state_00.state == "351160130.0"
+        assert es_state_00.state == "34160130.0"
 
 
 @pytest.mark.usefixtures("hass_madrid")
@@ -144,8 +129,6 @@ async def test_sensor_state_madrid_timezone(
     with freeze_time("2024-01-15T12:01:00Z"):  # 12:00 UTC = 1 PM CET (Madrid)
         assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
         await hass.async_block_till_done()
-
-        # Should only have 1 API call for Jan 15 (Jan 16 not yet published)
         assert mock_pyomie.spot_price.call_count == 1
 
     # Step 2: 2 PM CET (after 13:30 CET publication) - Jan 16 data now available
@@ -166,15 +149,10 @@ async def test_sensor_state_madrid_timezone(
         # Still no additional call - Madrid doesn't need Jan 16 for Jan 15 prices
         assert mock_pyomie.spot_price.call_count == 0
 
-        # Check sensor states - values should be converted from €/MWh to €/kWh
         pt_state = hass.states.get("sensor.omie_portugal_spot_price")
         es_state = hass.states.get("sensor.omie_spain_spot_price")
-
-        # At 14:00 UTC ( = 3 PM CET)
-        assert pt_state.state == "351151515.0"  # (PT day 15, hour 15, minute 15)
-        assert es_state.state == "34151515.0"  # (ES day 15, hour 15, minute 15)
-
-        # Check units are correct
+        assert pt_state.state == "351151515.0"
+        assert es_state.state == "34151515.0"
         assert pt_state.attributes["unit_of_measurement"] == "€/kWh"
         assert es_state.attributes["unit_of_measurement"] == "€/kWh"
 
@@ -207,10 +185,8 @@ async def test_sensor_unavailable_when_pyomie_throws(
         async_fire_time_changed(hass, dt_util.utcnow())
         await hass.async_block_till_done()
 
-    # Both sensors should be unavailable
     pt_state = hass.states.get("sensor.omie_portugal_spot_price")
     es_state = hass.states.get("sensor.omie_spain_spot_price")
-
     assert pt_state.state == STATE_UNAVAILABLE
     assert es_state.state == STATE_UNAVAILABLE
 
@@ -245,6 +221,5 @@ async def test_sensor_unavailable_when_pyomie_returns_incomplete_data(
     # Both sensors should be unavailable
     pt_state = hass.states.get("sensor.omie_portugal_spot_price")
     es_state = hass.states.get("sensor.omie_spain_spot_price")
-
     assert pt_state.state == STATE_UNAVAILABLE
     assert es_state.state == STATE_UNAVAILABLE
