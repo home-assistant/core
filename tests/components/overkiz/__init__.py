635c6508dc5b6a54f9218a2c9497a213a85332a5
Mick Vleeshouwer <mick@imick.nl>
2026-03-16 20:52:21 +0000

    Fix docstring for Overkiz component tests and remove unused logging import

--- tests/components/overkiz/__init__.py
+++ tests/components/overkiz/__init__.py
@@ -1,6 +1,4 @@
-"""Tests for the overkiz component."""
-
-import logging
+"""Tests for the Overkiz component."""
 
 import humps
 from pyoverkiz.models import Setup
@@ -10,7 +8,6 @@ from homeassistant.components.overkiz.const import DOMAIN
 from tests.common import load_json_object_fixture
 
 DEFAULT_SETUP_FIXTURE = "setup/setup_tahoma_switch.json"
-LOGGER = logging.getLogger(__name__)
 
 
 def load_setup_fixture(fixture: str = DEFAULT_SETUP_FIXTURE) -> Setup:
