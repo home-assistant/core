b606ccc931563415e5b645517558d4346fcd1c78
Fabian Munkes <105975993+fmunkes@users.noreply.github.com>
2026-03-17 21:24:52 +0100

    add tests for number

---
+++ tests/components/music_assistant/test_number.py
@@ -0,0 +1,119 @@
+"""Test Music Assistant number entities."""
+
+from unittest.mock import MagicMock, call
+
+from music_assistant_models.enums import EventType
+import pytest
+from syrupy.assertion import SnapshotAssertion
+
+from homeassistant.components.number import (
+    ATTR_VALUE,
+    DOMAIN as NUMBER_DOMAIN,
+    SERVICE_SET_VALUE,
+)
+from homeassistant.const import ATTR_ENTITY_ID, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ServiceValidationError
+from homeassistant.helpers import entity_registry as er
+
+from .common import (
+    setup_integration_from_fixtures,
+    snapshot_music_assistant_entities,
+    trigger_subscription_callback,
+)
+
+
+async def test_number_entities(
+    hass: HomeAssistant,
+    entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
+    music_assistant_client: MagicMock,
+) -> None:
+    """Test number entities."""
+    await setup_integration_from_fixtures(hass, music_assistant_client)
+    snapshot_music_assistant_entities(hass, entity_registry, snapshot, Platform.NUMBER)
+
+
+async def test_number_set_action(
+    hass: HomeAssistant,
+    music_assistant_client: MagicMock,
+) -> None:
+    """Test number set action."""
+    mass_player_id = "00:00:00:00:00:01"
+    mass_option_key = "treble"
+    entity_id = "number.test_player_1_treble"
+
+    option_value = 3
+
+    await setup_integration_from_fixtures(hass, music_assistant_client)
+    state = hass.states.get(entity_id)
+    assert state
+
+    # test within range
+    await hass.services.async_call(
+        NUMBER_DOMAIN,
+        SERVICE_SET_VALUE,
+        {
+            ATTR_ENTITY_ID: entity_id,
+            ATTR_VALUE: option_value,
+        },
+        blocking=True,
+    )
+
+    assert music_assistant_client.send_command.call_count == 1
+    assert music_assistant_client.send_command.call_args == call(
+        "players/cmd/set_option",
+        player_id=mass_player_id,
+        option_key=mass_option_key,
+        option_value=option_value,
+    )
+
+    # test out of range
+    with pytest.raises(ServiceValidationError):
+        await hass.services.async_call(
+            NUMBER_DOMAIN,
+            SERVICE_SET_VALUE,
+            {
+                ATTR_ENTITY_ID: entity_id,
+                ATTR_VALUE: 20,
+            },
+            blocking=True,
+        )
+
+
+async def test_external_update(
+    hass: HomeAssistant,
+    music_assistant_client: MagicMock,
+) -> None:
+    """Test external value update."""
+    mass_player_id = "00:00:00:00:00:01"
+    mass_option_key = "treble"
+    entity_id = "number.test_player_1_treble"
+
+    await setup_integration_from_fixtures(hass, music_assistant_client)
+
+    # get current option and remove it
+    number_option = next(
+        option
+        for option in music_assistant_client.players._players[mass_player_id].options
+        if option.key == mass_option_key
+    )
+    music_assistant_client.players._players[mass_player_id].options.remove(
+        number_option
+    )
+
+    # set new value different from previous one
+    previous_value = number_option.value
+    new_value = 5
+    number_option.value = new_value
+    assert previous_value != number_option.value
+    music_assistant_client.players._players[mass_player_id].options.append(
+        number_option
+    )
+
+    await trigger_subscription_callback(
+        hass, music_assistant_client, EventType.PLAYER_OPTIONS_UPDATED, mass_player_id
+    )
+    state = hass.states.get(entity_id)
+    assert state
+    assert int(state.state) == new_value
