e72ecc5a1ec9ef727613a9b6f9d48f5925d237e9
Joostlek <joostlek@outlook.com>
2026-03-17 17:39:44 +0100

    Add binary sensor for smartthings microfilter blockage

--- homeassistant/components/smartthings/binary_sensor.py
+++ homeassistant/components/smartthings/binary_sensor.py
@@ -208,6 +208,14 @@ CAPABILITY_TO_SENSORS: dict[
             supported_states_attributes=Attribute.SUPPORTED_COOKTOP_OPERATING_STATE,
         )
     },
+    Capability.SAMSUNG_CE_MICROFIBER_FILTER_STATUS: {
+        Attribute.SUPPORTED_STATUS: SmartThingsBinarySensorEntityDescription(
+            key=Attribute.SUPPORTED_STATUS,
+            translation_key="microfiber_filter_blockage",
+            is_on_key="blockage",
+            device_class=BinarySensorDeviceClass.PROBLEM,
+        )
+    },
 }
 
 
