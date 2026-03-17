6735e4e6d424977a249a07d4c4e1921c7dd3ed38
Gabriel Woloz <wolozgabriel@gmail.com>
2026-03-17 17:52:49 -0400

    Add interface option to wake on lan integration

--- homeassistant/components/wake_on_lan/__init__.py
+++ homeassistant/components/wake_on_lan/__init__.py
@@ -7,7 +7,7 @@ import voluptuous as vol
 import wakeonlan
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_BROADCAST_ADDRESS, CONF_BROADCAST_PORT, CONF_MAC
+from homeassistant.const import CONF_BROADCAST_ADDRESS, CONF_BROADCAST_PORT, CONF_MAC, CONF_BROADCAST_INTERFACE
 from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.typing import ConfigType
@@ -23,6 +23,7 @@ WAKE_ON_LAN_SEND_MAGIC_PACKET_SCHEMA = vol.Schema(
         vol.Required(CONF_MAC): cv.string,
         vol.Optional(CONF_BROADCAST_ADDRESS): cv.string,
         vol.Optional(CONF_BROADCAST_PORT): cv.port,
+        vol.Optional(CONF_BROADCAST_INTERFACE): cv.string,
     }
 )
 
@@ -37,18 +38,22 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
         mac_address = call.data.get(CONF_MAC)
         broadcast_address = call.data.get(CONF_BROADCAST_ADDRESS)
         broadcast_port = call.data.get(CONF_BROADCAST_PORT)
+        broadcast_interface = call.data.get(CONF_BROADCAST_INTERFACE)
 
         service_kwargs = {}
         if broadcast_address is not None:
             service_kwargs["ip_address"] = broadcast_address
         if broadcast_port is not None:
             service_kwargs["port"] = broadcast_port
+        if broadcast_interface is not None:
+            service_kwargs["interface"] = broadcast_interface
 
         _LOGGER.debug(
             "Send magic packet to mac %s (broadcast: %s, port: %s)",
             mac_address,
             broadcast_address,
             broadcast_port,
+            broadcast_interface,
         )
 
         await hass.async_add_executor_job(
