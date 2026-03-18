0f8df6018d0b723686a6fde1e28b897f10d6f564
Domodom30 <tellier.eddy@gmx.fr>
2026-03-18 08:32:54 +0000

    Handle hosts without protocol in configuration_url

--- homeassistant/components/mealie/entity.py
+++ homeassistant/components/mealie/entity.py
@@ -19,7 +19,12 @@ class MealieEntity(CoordinatorEntity[MealieDataUpdateCoordinator]):
         unique_id = coordinator.config_entry.unique_id
         assert unique_id is not None
         self._attr_unique_id = f"{unique_id}_{key}"
+        host = coordinator.config_entry.data[CONF_HOST]
+        if not host.startswith(("http://", "https://")):
+            configuration_url = f"https://{host}"
+        else:
+            configuration_url = host
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, unique_id)},
-            configuration_url=coordinator.config_entry.data[CONF_HOST],
+            configuration_url=configuration_url,
         )
