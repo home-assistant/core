1cb39938a98a018d311b3f6c972fc9f2c96042fe
Erik Montnemery <erik@montnemery.com>
2026-03-18 17:54:36 +0100

    Allow specifying attribute in state selector (#165928)

--- homeassistant/helpers/selector.py
+++ homeassistant/helpers/selector.py
@@ -1511,6 +1511,7 @@ class StateSelectorConfig(BaseSelectorConfig, total=False):
 
     entity_id: str
     hide_states: list[str]
+    attribute: str
     multiple: bool
 
 
@@ -1533,11 +1534,7 @@ class StateSelector(Selector[StateSelectorConfig]):
         {
             vol.Optional("entity_id"): cv.entity_id,
             vol.Optional("hide_states"): [str],
-            # The attribute to filter on, is currently deliberately not
-            # configurable/exposed. We are considering separating state
-            # selectors into two types: one for state and one for attribute.
-            # Limiting the public use, prevents breaking changes in the future.
-            # vol.Optional("attribute"): str,
+            vol.Optional("attribute"): str,
             vol.Optional("multiple", default=False): cv.boolean,
         }
     )
--- tests/helpers/test_selector.py
+++ tests/helpers/test_selector.py
@@ -883,6 +883,11 @@ def test_time_selector_schema(schema, valid_selections, invalid_selections) -> N
             (),
             (),
         ),
+        (
+            {"attribute": "best_attribute"},
+            (),
+            (),
+        ),
     ],
 )
 def test_state_selector_schema(schema, valid_selections, invalid_selections) -> None:
