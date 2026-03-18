676204551e70e9b066ef8e0df6c2a1dba73c9bb0
Domodom30 <tellier.eddy@gmx.fr>
2026-03-18 09:28:00 +0000

    Add configuration_url to Mealie device info

--- homeassistant/components/mealie/entity.py
+++ homeassistant/components/mealie/entity.py
@@ -1,5 +1,6 @@
 """Base class for Mealie entities."""
 
+from homeassistant.const import CONF_HOST
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
@@ -18,6 +19,12 @@ class MealieEntity(CoordinatorEntity[MealieDataUpdateCoordinator]):
         unique_id = coordinator.config_entry.unique_id
         assert unique_id is not None
         self._attr_unique_id = f"{unique_id}_{key}"
+        host = coordinator.config_entry.data[CONF_HOST]
+        if not host.startswith(("http://", "https://")):
+            configuration_url = f"https://{host}"
+        else:
+            configuration_url = host
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, unique_id)},
+            configuration_url=configuration_url,
         )
