c805669c3433a9e3a7819cce93a5d6a1d76b0e26
Michael Hansen <mike@rhasspy.org>
2026-03-19 08:57:14 -0500

    Move parsing to _metadata_from_header

--- homeassistant/components/stt/models.py
+++ homeassistant/components/stt/models.py
@@ -23,14 +23,6 @@ class SpeechMetadata:
     sample_rate: AudioSampleRates
     channel: AudioChannels
 
-    def __post_init__(self) -> None:
-        """Finish initializing the metadata."""
-        self.format = AudioFormats(self.format)
-        self.codec = AudioCodecs(self.codec)
-        self.bit_rate = AudioBitRates(int(self.bit_rate))
-        self.sample_rate = AudioSampleRates(int(self.sample_rate))
-        self.channel = AudioChannels(int(self.channel))
-
 
 @dataclass
 class SpeechResult:
