d62c4f9aa97a9c786700f64903397fe1cefffcc3
Jeef <jeffstein@gmail.com>
2026-03-19 13:37:12 -0600

    Add initial quality scale for Weatherflow local"

--- script/hassfest/quality_scale.py
+++ script/hassfest/quality_scale.py
@@ -1025,7 +1025,6 @@ INTEGRATIONS_WITHOUT_QUALITY_SCALE_FILE = [
     "watson_tts",
     "watttime",
     "waze_travel_time",
-    "weatherflow",
     "weatherflow_cloud",
     "weatherkit",
     "webmin",
