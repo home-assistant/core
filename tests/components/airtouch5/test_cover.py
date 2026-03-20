1e887bb844bfe912c254689d78c5d1627da20794
justin <justin@boyd.net.au>
2026-03-20 22:33:48 +0000

    fixing asserts

--- tests/components/airtouch5/test_cover.py
+++ tests/components/airtouch5/test_cover.py
@@ -121,14 +121,18 @@ async def test_cover_callbacks(
     # And call it to effectively launch the callback as the server would do
 
     # Partly open
-    await _call_zone_status_callback(0.5)
+    await _call_zone_status_callback(0.9)
     state = hass.states.get(COVER_ENTITY_ID)
     assert state
     assert state.state == CoverState.OPEN
+<<<<<<< HEAD
     assert state.attributes.get(ATTR_CURRENT_POSITION) == 70
     zone_2 = hass.states.get(COVER_ZONE_2_ENTITY_ID)
     assert zone_2 and zone_2.state == zone_2_initial_state
     assert zone_2.attributes.get(ATTR_CURRENT_POSITION) == zone_2_initial_position
+=======
+    assert state.attributes.get(ATTR_CURRENT_POSITION) == 90
+>>>>>>> 9c70246491c (fixing asserts)
 
     # Fully open
     await _call_zone_status_callback(1)
