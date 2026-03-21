34095152c45aeea5c8db639a5c6fd1379449133d
DeerMaximum <git983456@parabelmail.de>
2026-03-21 16:38:47 +0100

    Remove services

--- homeassistant/components/nina/__init__.py
+++ homeassistant/components/nina/__init__.py
@@ -7,7 +7,6 @@ from typing import Any
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers.typing import ConfigType
 
 from .const import (
     _LOGGER,
@@ -20,7 +19,6 @@ from .const import (
     NO_MATCH_REGEX,
 )
 from .coordinator import NinaConfigEntry, NINADataUpdateCoordinator
-from .services import async_setup_services
 
 PLATFORMS: list[str] = [Platform.BINARY_SENSOR, Platform.SENSOR]
 CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
@@ -36,12 +34,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: NinaConfigEntry) -> bool
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    return True
-
-
-async def async_setup(hass: HomeAssistant, _: ConfigType) -> bool:
-    """Set up services."""
-    async_setup_services(hass)
     return True
 
 
--- homeassistant/components/nina/binary_sensor.py
+++ homeassistant/components/nina/binary_sensor.py
@@ -100,24 +100,3 @@ class NINAMessage(NinaEntity, BinarySensorEntity):
             ATTR_START: data.start,
             ATTR_EXPIRES: data.expires,
         }
-
-    def get_description(self) -> str | None:
-        """Return the description."""
-        if not self.is_on:
-            return None
-
-        return self._get_warning_data().description
-
-    def get_full_affected_areas(self) -> str | None:
-        """Return full affected areas."""
-        if not self.is_on:
-            return None
-
-        return self._get_warning_data().affected_areas
-
-    def get_recommended_actions(self) -> str | None:
-        """Return the recommended actions."""
-        if not self.is_on:
-            return None
-
-        return self._get_warning_data().recommended_actions
