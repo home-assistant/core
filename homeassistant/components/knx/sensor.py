1fceef35a71eb93308575472be922dc7f66e3110
farmio <farmio@alphart.net>
2026-03-21 21:07:13 +0100

    Add default_entity_id option to yaml entities

--- homeassistant/components/knx/sensor.py
+++ homeassistant/components/knx/sensor.py
@@ -12,6 +12,7 @@ from xknx.devices import Device as XknxDevice, Sensor as XknxSensor
 from homeassistant import config_entries
 from homeassistant.components.sensor import (
     CONF_STATE_CLASS,
+    ENTITY_ID_FORMAT,
     RestoreSensor,
     SensorDeviceClass,
     SensorEntity,
@@ -20,7 +21,6 @@ from homeassistant.components.sensor import (
 )
 from homeassistant.const import (
     CONF_DEVICE_CLASS,
-    CONF_ENTITY_CATEGORY,
     CONF_NAME,
     CONF_TYPE,
     CONF_UNIT_OF_MEASUREMENT,
@@ -168,6 +168,7 @@ class _KnxSensor(RestoreSensor, _KnxEntityBase):
     """Representation of a KNX sensor."""
 
     _device: XknxSensor
+    _entity_id_format = ENTITY_ID_FORMAT
 
     async def async_added_to_hass(self) -> None:
         """Restore last state."""
@@ -211,8 +212,7 @@ class KnxYamlSensor(_KnxSensor, KnxYamlEntity):
         super().__init__(
             knx_module=knx_module,
             unique_id=str(self._device.sensor_value.group_address_state),
-            name=config[CONF_NAME],
-            entity_category=config.get(CONF_ENTITY_CATEGORY),
+            entity_config=config,
         )
         dpt_string = self._device.sensor_value.dpt_class.dpt_number_str()
         dpt_info = get_supported_dpts()[dpt_string]
