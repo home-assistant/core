16a19f6b3f9dad27a8055b5b6d271add8513ead2
Joost Lekkerkerker <joostlek@outlook.com>
2026-03-21 16:57:26 +0100

    Add stick cleaner fixture to SmartThings (#166121)

--- tests/components/smartthings/__init__.py
+++ tests/components/smartthings/__init__.py
@@ -74,6 +74,7 @@ DEVICE_FIXTURES = [
     "da_wm_dw_01011",
     "da_rvc_normal_000001",
     "da_rvc_map_01011",
+    "da_vc_stick_01001",
     "da_ks_microwave_0101x",
     "da_ks_cooktop_000001",
     "da_ks_cooktop_31001",
