6642d50f43712c61fb5ad15c1e548f53ba069838
Tom Scholten <git@scholten.nu>
2026-03-23 12:43:55 +0000

    Add missing coverage

--- tests/components/proxmoxve/test_config_flow.py
+++ tests/components/proxmoxve/test_config_flow.py
@@ -202,6 +202,61 @@ async def test_form_exceptions(
     assert result["type"] is FlowResultType.CREATE_ENTRY
 
 
+@pytest.mark.parametrize(
+    ("exception", "reason"),
+    [
+        (
+            ResourceException("404", "status_message", "content"),
+            "no_nodes_found",
+        ),
+        (
+            requests.exceptions.ConnectionError("Connection error"),
+            "cannot_connect",
+        ),
+    ],
+)
+async def test_form_exceptions_qemu(
+    hass: HomeAssistant,
+    mock_proxmox_client: MagicMock,
+    exception: Exception,
+    reason: str,
+) -> None:
+    """Test we handle all exceptions."""
+    mock_proxmox_client.nodes.get.return_value = [{"node": "pve1"}]
+    node_resource = mock_proxmox_client.nodes.return_value
+    node_resource.qemu.get.side_effect = exception
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "user"
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input=MOCK_USER_STEP,
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "user_auth"
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input=MOCK_USER_AUTH_STEP_PASSWORD,
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] == {"base": reason}
+
+    node_resource.qemu.get.side_effect = None
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input=MOCK_USER_AUTH_STEP_PASSWORD
+    )
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+
+
 async def test_form_no_nodes_exception(
     hass: HomeAssistant,
     mock_proxmox_client: MagicMock,
