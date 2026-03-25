4b65d54690322cc53961f3522d3f32c036d59fb0
Erik Montnemery <erik@montnemery.com>
2026-03-25 21:54:34 +0100

    Add trigger water_heater.operation_mode_changed (#166450)

--- tests/components/water_heater/test_trigger.py
+++ tests/components/water_heater/test_trigger.py
@@ -38,6 +38,8 @@ ALL_ON_STATES = [
     STATE_PERFORMANCE,
 ]
 
+ALL_STATES = [STATE_OFF, *ALL_ON_STATES]
+
 
 @pytest.fixture
 async def target_water_heaters(hass: HomeAssistant) -> list[str]:
@@ -48,6 +50,7 @@ async def target_water_heaters(hass: HomeAssistant) -> list[str]:
 @pytest.mark.parametrize(
     "trigger_key",
     [
+        "water_heater.operation_mode_changed",
         "water_heater.target_temperature_changed",
         "water_heater.target_temperature_crossed_threshold",
         "water_heater.turned_off",
@@ -69,6 +72,24 @@ async def test_water_heater_triggers_gated_by_labs_flag(
 @pytest.mark.parametrize(
     ("trigger", "trigger_options", "states"),
     [
+        *(
+            param
+            for mode in ALL_STATES
+            for param in parametrize_trigger_states(
+                trigger="water_heater.operation_mode_changed",
+                trigger_options={"operation_mode": [mode]},
+                target_states=[mode],
+                other_states=[s for s in ALL_STATES if s != mode],
+            )
+        ),
+        *parametrize_trigger_states(
+            trigger="water_heater.operation_mode_changed",
+            trigger_options={"operation_mode": [STATE_ECO, STATE_ELECTRIC]},
+            target_states=[STATE_ECO, STATE_ELECTRIC],
+            other_states=[
+                s for s in ALL_STATES if s not in (STATE_ECO, STATE_ELECTRIC)
+            ],
+        ),
         *parametrize_trigger_states(
             trigger="water_heater.turned_off",
             target_states=[STATE_OFF],
@@ -161,6 +182,24 @@ async def test_water_heater_state_attribute_trigger_behavior_any(
 @pytest.mark.parametrize(
     ("trigger", "trigger_options", "states"),
     [
+        *(
+            param
+            for mode in ALL_STATES
+            for param in parametrize_trigger_states(
+                trigger="water_heater.operation_mode_changed",
+                trigger_options={"operation_mode": [mode]},
+                target_states=[mode],
+                other_states=[s for s in ALL_STATES if s != mode],
+            )
+        ),
+        *parametrize_trigger_states(
+            trigger="water_heater.operation_mode_changed",
+            trigger_options={"operation_mode": [STATE_ECO, STATE_ELECTRIC]},
+            target_states=[STATE_ECO, STATE_ELECTRIC],
+            other_states=[
+                s for s in ALL_STATES if s not in (STATE_ECO, STATE_ELECTRIC)
+            ],
+        ),
         *parametrize_trigger_states(
             trigger="water_heater.turned_off",
             target_states=[STATE_OFF],
@@ -247,6 +286,24 @@ async def test_water_heater_state_attribute_trigger_behavior_first(
 @pytest.mark.parametrize(
     ("trigger", "trigger_options", "states"),
     [
+        *(
+            param
+            for mode in ALL_STATES
+            for param in parametrize_trigger_states(
+                trigger="water_heater.operation_mode_changed",
+                trigger_options={"operation_mode": [mode]},
+                target_states=[mode],
+                other_states=[s for s in ALL_STATES if s != mode],
+            )
+        ),
+        *parametrize_trigger_states(
+            trigger="water_heater.operation_mode_changed",
+            trigger_options={"operation_mode": [STATE_ECO, STATE_ELECTRIC]},
+            target_states=[STATE_ECO, STATE_ELECTRIC],
+            other_states=[
+                s for s in ALL_STATES if s not in (STATE_ECO, STATE_ELECTRIC)
+            ],
+        ),
         *parametrize_trigger_states(
             trigger="water_heater.turned_off",
             target_states=[STATE_OFF],
