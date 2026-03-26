e48125f62e28c1a012bb9eabece069cdc36ce90a
Erik Montnemery <erik@montnemery.com>
2026-03-26 20:29:49 +0100

    Fix override of state write in dlna_dmr (#166628)

--- homeassistant/components/dlna_dmr/media_player.py
+++ homeassistant/components/dlna_dmr/media_player.py
@@ -353,10 +353,10 @@ class DlnaDmrEntity(MediaPlayerEntity):
         # Device was de/re-connected, state might have changed
         self.async_write_ha_state()
 
-    def async_write_ha_state(self) -> None:
+    def _async_write_ha_state(self) -> None:
         """Write the state."""
         self._attr_supported_features = self._supported_features()
-        super().async_write_ha_state()
+        super()._async_write_ha_state()
 
     async def _device_connect(self, location: str) -> None:
         """Connect to the device now that it's available."""
