f8fd6e842951ea965dade9b25df3431d58417f69
Tom Carpenter <t.carpenter@leeds.ac.uk>
2026-03-27 12:27:16 +0000

    Move deprecation check APIs to end of file

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -876,13 +876,6 @@ _DEPRECATED_PERCENTAGE: Final = DeprecatedConstant(
     "%", "UnitOfRatio.PERCENTAGE", "2027.3"
 )
 
-# These can be removed if no deprecated constant are in this module anymore
-__getattr__ = partial(check_if_deprecated_constant, module_globals=globals())
-__dir__ = partial(
-    dir_with_deprecated_constants, module_globals_keys=[*globals().keys()]
-)
-__all__ = all_with_deprecated_constants(globals())
-
 # States
 COMPRESSED_STATE_STATE: Final = "s"
 COMPRESSED_STATE_ATTRIBUTES: Final = "a"
@@ -1045,3 +1038,10 @@ FORMAT_DATETIME: Final = f"{FORMAT_DATE} {FORMAT_TIME}"
 # This is not a hard limit, but caches and other
 # data structures will be pre-allocated to this size
 MAX_EXPECTED_ENTITY_IDS: Final = 16384
+
+# These can be removed if no deprecated constants are in this module anymore
+__getattr__ = partial(check_if_deprecated_constant, module_globals=globals())
+__dir__ = partial(
+    dir_with_deprecated_constants, module_globals_keys=[*globals().keys()]
+)
+__all__ = all_with_deprecated_constants(globals())
