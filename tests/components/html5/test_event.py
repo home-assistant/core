2388074b05adeb1ba54cb7ddb18f6a0940660e1d
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2026-03-28 16:30:12 +0000

    load callback view

--- tests/components/html5/test_event.py
+++ tests/components/html5/test_event.py
@@ -1,27 +1,29 @@
 """Tests for the HTML5 event platform."""
 
 from collections.abc import Generator
+from http import HTTPStatus
 from typing import Any
 from unittest.mock import MagicMock, patch
 
+from aiohttp.hdrs import AUTHORIZATION
 import pytest
 from syrupy.assertion import SnapshotAssertion
 
-from homeassistant.components.html5.const import DOMAIN
 from homeassistant.components.html5.notify import ATTR_ACTION, ATTR_TAG, ATTR_TYPE
 from homeassistant.components.notify import ATTR_DATA, ATTR_TARGET
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import STATE_UNKNOWN, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
-from homeassistant.helpers.dispatcher import async_dispatcher_send
+from homeassistant.setup import async_setup_component
 
 from .test_notify import SUBSCRIPTION_1
 
 from tests.common import MockConfigEntry, snapshot_platform
+from tests.typing import ClientSessionGenerator
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def event_only() -> Generator[None]:
     """Enable only the event platform."""
     with patch(
@@ -31,8 +33,7 @@ def event_only() -> Generator[None]:
         yield
 
 
-@pytest.mark.usefixtures("entity_registry_enabled_by_default")
-@pytest.mark.usefixtures("mock_jwt", "mock_vapid", "mock_uuid")
+@pytest.mark.usefixtures("mock_jwt", "mock_vapid", "mock_uuid", "event_only")
 @pytest.mark.freeze_time("1970-01-01T00:00:00.000Z")
 async def test_setup(
     hass: HomeAssistant,
@@ -77,7 +78,6 @@ async def test_setup(
         },
     ],
 )
-@pytest.mark.usefixtures("entity_registry_enabled_by_default")
 @pytest.mark.usefixtures("mock_jwt", "mock_vapid", "mock_uuid")
 @pytest.mark.freeze_time("1970-01-01T00:00:00.000Z")
 async def test_events(
@@ -85,9 +85,12 @@ async def test_events(
     config_entry: MockConfigEntry,
     load_config: MagicMock,
     event_payload: dict[str, Any],
+    hass_client: ClientSessionGenerator,
+    mock_jwt: MagicMock,
 ) -> None:
     """Test events."""
     load_config.return_value = {"my-desktop": SUBSCRIPTION_1}
+    await async_setup_component(hass, "http", {})
 
     config_entry.add_to_hass(hass)
     await hass.config_entries.async_setup(config_entry.entry_id)
@@ -98,13 +101,17 @@ async def test_events(
     assert (state := hass.states.get("event.my_desktop")) is not None
     assert state.state == STATE_UNKNOWN
 
-    async_dispatcher_send(
-        hass,
-        DOMAIN,
-        event_payload[ATTR_TARGET],
-        event_payload[ATTR_TYPE],
-        event_payload,
+    client = await hass_client()
+
+    mock_jwt.decode.return_value = {ATTR_TARGET: event_payload[ATTR_TARGET]}
+
+    resp = await client.post(
+        "/api/notify.html5/callback",
+        json=event_payload,
+        headers={AUTHORIZATION: "Bearer JWT"},
     )
+
+    assert resp.status == HTTPStatus.OK
 
     assert (state := hass.states.get("event.my_desktop"))
     assert state.state == "1970-01-01T00:00:00.000+00:00"
