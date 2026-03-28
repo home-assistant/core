48ff51dc9463abf4918dc0feaf2a2de4ba3a489d
TimL <tl@smlight.tech>
2026-03-28 17:50:36 +1100

    Add Remote platform to SMLIGHT Integration (#166728)

--- homeassistant/components/smlight/__init__.py
+++ homeassistant/components/smlight/__init__.py
@@ -19,6 +19,7 @@ PLATFORMS: list[Platform] = [
     Platform.BINARY_SENSOR,
     Platform.BUTTON,
     Platform.LIGHT,
+    Platform.REMOTE,
     Platform.SENSOR,
     Platform.SWITCH,
     Platform.UPDATE,
