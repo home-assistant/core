7092124d6c8be6a3f1d3d20fdb387c86428ca2bc
jbouwh <jan@jbsoft.nl>
2026-03-29 15:32:29 +0000

    Remove redundant assignment

--- homeassistant/components/mqtt/vacuum.py
+++ homeassistant/components/mqtt/vacuum.py
@@ -263,14 +263,6 @@ class MqttStateVacuum(MqttEntity, StateVacuumEntity):
             # Clear any previously configured clean-segments handling when the
             # option is absent in the new config after a discovery update.
             self._attr_supported_features &= ~VacuumEntityFeature.CLEAN_AREA
-        if CONF_CLEAN_SEGMENTS_COMMAND_TOPIC in config:
-            self._clean_segments_command_topic = config[
-                CONF_CLEAN_SEGMENTS_COMMAND_TOPIC
-            ]
-            self._clean_segments_command_template = MqttCommandTemplate(
-                config.get(CONF_CLEAN_SEGMENTS_COMMAND_TEMPLATE),
-                entity=self,
-            ).async_render
 
         self._attr_fan_speed_list = config[CONF_FAN_SPEED_LIST]
         self._command_topic = config.get(CONF_COMMAND_TOPIC)
