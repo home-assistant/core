8d2d8230f3097b7cf243f1cdeeea2c1463f73271
epenet <6771947+epenet@users.noreply.github.com>
2026-03-30 08:04:39 +0000

    Update coordinator.py

--- homeassistant/components/motioneye/coordinator.py
+++ homeassistant/components/motioneye/coordinator.py
@@ -16,11 +16,16 @@ from .const import DEFAULT_SCAN_INTERVAL, DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 
+type MotionEyeConfigEntry = ConfigEntry[MotionEyeUpdateCoordinator]
+
+
 class MotionEyeUpdateCoordinator(DataUpdateCoordinator[dict[str, Any] | None]):
     """Coordinator for motionEye data."""
 
+    config_entry: MotionEyeConfigEntry
+
     def __init__(
-        self, hass: HomeAssistant, entry: ConfigEntry, client: MotionEyeClient
+        self, hass: HomeAssistant, entry: MotionEyeConfigEntry, client: MotionEyeClient
     ) -> None:
         """Initialize the coordinator."""
         super().__init__(
@@ -37,6 +42,3 @@ class MotionEyeUpdateCoordinator(DataUpdateCoordinator[dict[str, Any] | None]):
             return await self.client.async_get_cameras()
         except MotionEyeClientError as exc:
             raise UpdateFailed("Error communicating with API") from exc
-
-
-type MotionEyeConfigEntry = ConfigEntry[MotionEyeUpdateCoordinator]
