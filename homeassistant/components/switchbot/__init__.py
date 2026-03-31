11f9c32341dc9eed63e0799afc49407de0a83621
zerzhang <916250497@qq.com>
2026-03-31 10:13:47 +0000

    Sync non-air-purifier reverts from fan branch (restore METER_PRO_C, NON_CONNECTABLE, WOMETERTHPC)

--- homeassistant/components/switchbot/__init__.py
+++ homeassistant/components/switchbot/__init__.py
@@ -35,6 +35,7 @@ from .const import (
     DOMAIN,
     ENCRYPTED_MODELS,
     HASS_SENSOR_TYPE_TO_SWITCHBOT_MODEL,
+    NON_CONNECTABLE_SUPPORTED_MODEL_TYPES,
     SupportedModels,
 )
 from .coordinator import SwitchbotConfigEntry, SwitchbotDataUpdateCoordinator
@@ -109,26 +110,10 @@ PLATFORMS_BY_TYPE = {
         Platform.LOCK,
         Platform.SENSOR,
     ],
-    SupportedModels.AIR_PURIFIER_JP.value: [
-        Platform.FAN,
-        Platform.SENSOR,
-        Platform.BUTTON,
-    ],
-    SupportedModels.AIR_PURIFIER_US.value: [
-        Platform.FAN,
-        Platform.SENSOR,
-        Platform.BUTTON,
-    ],
-    SupportedModels.AIR_PURIFIER_TABLE_JP.value: [
-        Platform.FAN,
-        Platform.SENSOR,
-        Platform.BUTTON,
-    ],
-    SupportedModels.AIR_PURIFIER_TABLE_US.value: [
-        Platform.FAN,
-        Platform.SENSOR,
-        Platform.BUTTON,
-    ],
+    SupportedModels.AIR_PURIFIER_JP.value: [Platform.FAN, Platform.SENSOR],
+    SupportedModels.AIR_PURIFIER_US.value: [Platform.FAN, Platform.SENSOR],
+    SupportedModels.AIR_PURIFIER_TABLE_JP.value: [Platform.FAN, Platform.SENSOR],
+    SupportedModels.AIR_PURIFIER_TABLE_US.value: [Platform.FAN, Platform.SENSOR],
     SupportedModels.EVAPORATIVE_HUMIDIFIER.value: [
         Platform.HUMIDIFIER,
         Platform.SENSOR,
@@ -280,7 +265,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: SwitchbotConfigEntry) ->
     sensor_type: str = entry.data[CONF_SENSOR_TYPE]
     switchbot_model = HASS_SENSOR_TYPE_TO_SWITCHBOT_MODEL[sensor_type]
     # connectable means we can make connections to the device
-    connectable = switchbot_model in CONNECTABLE_SUPPORTED_MODEL_TYPES
+    connectable = (
+        switchbot_model in CONNECTABLE_SUPPORTED_MODEL_TYPES
+        and switchbot_model not in NON_CONNECTABLE_SUPPORTED_MODEL_TYPES
+    )
     address: str = entry.data[CONF_ADDRESS]
 
     await switchbot.close_stale_connections_by_address(address)
