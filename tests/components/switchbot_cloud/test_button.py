0986181fdefc893150afa7da8bc8099ffa9f736f
XiaoLing-git <samuel.ling.xiao@qq.com>
2026-04-01 18:37:46 +0800

    fixed

--- tests/components/switchbot_cloud/test_button.py
+++ tests/components/switchbot_cloud/test_button.py
@@ -16,7 +16,11 @@ from . import configure_integration
 
 
 async def test_pressmode_bot(
-    hass: HomeAssistant, mock_list_devices, mock_get_status, mock_setup_webhook
+    hass: HomeAssistant,
+    mock_list_devices,
+    mock_get_status,
+    mock_setup_webhook,
+    mock_get_webook_configuration,
 ) -> None:
     """Test press."""
     mock_list_devices.return_value = [
@@ -32,6 +36,11 @@ async def test_pressmode_bot(
     mock_get_status.return_value = {"deviceMode": "pressMode"}
 
     mock_setup_webhook.return_value = {
+        "statusCode": 100,
+        "body": {},
+        "message": "success",
+    }
+    mock_get_webook_configuration.return_value = {
         "statusCode": 100,
         "body": {},
         "message": "success",
@@ -55,7 +64,11 @@ async def test_pressmode_bot(
 
 
 async def test_switchmode_bot_no_button_entity(
-    hass: HomeAssistant, mock_list_devices, mock_get_status, mock_setup_webhook
+    hass: HomeAssistant,
+    mock_list_devices,
+    mock_get_status,
+    mock_setup_webhook,
+    mock_get_webook_configuration,
 ) -> None:
     """Test a switchMode bot isn't added as a button."""
     mock_list_devices.return_value = [
@@ -70,6 +83,11 @@ async def test_switchmode_bot_no_button_entity(
 
     mock_get_status.return_value = {"deviceMode": "switchMode"}
     mock_setup_webhook.return_value = {
+        "statusCode": 100,
+        "body": {},
+        "message": "success",
+    }
+    mock_get_webook_configuration.return_value = {
         "statusCode": 100,
         "body": {},
         "message": "success",
