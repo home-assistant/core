00203750f1d98fc4c9bb9cde1aa4ece580391a11
Rogier van Staveren <rogier@batoid.com>
2026-04-02 01:49:09 +0200

    Fix unittest

--- tests/components/picotts/test_config_flow.py
+++ tests/components/picotts/test_config_flow.py
@@ -1,6 +1,6 @@
 """Test the Pico TTS config flow."""
 
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
 
@@ -21,51 +21,67 @@ pytestmark = pytest.mark.usefixtures("mock_setup_entry")
 
 async def test_user_step(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
     """Test user step create entry result."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-    assert result["type"] is FlowResultType.FORM
-    assert result["errors"] is None
+    mock_result = MagicMock()
+    mock_result.returncode = 0
+    with (
+        patch(
+            "homeassistant.components.picotts.tts.subprocess.run",
+            return_value=mock_result,
+        ),
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_USER}
+        )
+        assert result["type"] is FlowResultType.FORM
+        assert result["errors"] is None
 
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        {
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_LANG: "es-ES",
+            },
+        )
+        await hass.async_block_till_done()
+
+        assert result["type"] is FlowResultType.CREATE_ENTRY
+        assert result["title"] == "Pico TTS es-ES"
+        assert result["data"] == {
             CONF_LANG: "es-ES",
-        },
-    )
-    await hass.async_block_till_done()
-
-    assert result["type"] is FlowResultType.CREATE_ENTRY
-    assert result["title"] == "Pico TTS es-ES"
-    assert result["data"] == {
-        CONF_LANG: "es-ES",
-    }
-    assert len(mock_setup_entry.mock_calls) == 1
+        }
+        assert len(mock_setup_entry.mock_calls) == 1
 
 
 async def test_already_configured(
     hass: HomeAssistant, mock_setup_entry: AsyncMock
 ) -> None:
     """Test user step already configured entry."""
-    config_entry = MockConfigEntry(domain=DOMAIN, data={CONF_LANG: "es-ES"})
-    config_entry.add_to_hass(hass)
+    mock_result = MagicMock()
+    mock_result.returncode = 0
+    with (
+        patch(
+            "homeassistant.components.picotts.tts.subprocess.run",
+            return_value=mock_result,
+        ),
+    ):
+        config_entry = MockConfigEntry(domain=DOMAIN, data={CONF_LANG: "es-ES"})
+        config_entry.add_to_hass(hass)
 
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-    assert result["type"] is FlowResultType.FORM
-    assert result["errors"] is None
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_USER}
+        )
+        assert result["type"] is FlowResultType.FORM
+        assert result["errors"] is None
 
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        {
-            CONF_LANG: "es-ES",
-        },
-    )
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_LANG: "es-ES",
+            },
+        )
 
-    assert result["type"] is FlowResultType.ABORT
-    assert result["reason"] == "already_configured"
-    assert len(mock_setup_entry.mock_calls) == 0
+        assert result["type"] is FlowResultType.ABORT
+        assert result["reason"] == "already_configured"
+        assert len(mock_setup_entry.mock_calls) == 0
 
 
 async def test_import_flow(
