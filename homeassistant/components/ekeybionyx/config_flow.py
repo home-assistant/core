f6771d2d903c2986a4fa3413e3949962929b2969
Richard Polzer <richard@polzer-eb.de>
2026-04-02 13:23:23 +0200

    Update homeassistant/components/ekeybionyx/config_flow.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/ekeybionyx/config_flow.py
+++ homeassistant/components/ekeybionyx/config_flow.py
@@ -32,6 +32,11 @@ class ConfigFlowEkeyApi(ekey_bionyxpy.AbstractAuth):
     """ekey bionyx authentication before a ConfigEntry exists.
 
     This implementation directly provides the token without supporting refresh.
+    """Authentication implementation used during config flow, without refresh.
+
+    This exists to allow the config flow to use the API before it has fully
+    created a config entry required by OAuth2Session. This does not support
+    refreshing tokens, which is fine since it should have been just created.
     """
 
     def __init__(
