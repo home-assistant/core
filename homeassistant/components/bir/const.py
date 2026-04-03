af5f9fdcd8e55ab148a6bb53ae417ba866fcaf82
Sander Blomvågnes <sander@blomvag.no>
2026-04-03 10:07:31 +0200

    refactor: imported WASTE_TYPES from the package

--- homeassistant/components/bir/const.py
+++ homeassistant/components/bir/const.py
@@ -4,18 +4,15 @@ from datetime import timedelta
 import logging
 from typing import Final
 
+from pybirno.const import WASTE_TYPE_MAP
+
 DOMAIN: Final = "bir"
 
 LOGGER = logging.getLogger(__package__)
 SCAN_INTERVAL = timedelta(hours=1)
 
 # Known waste types returned by pybirno
-WASTE_TYPES: Final[set[str]] = {
-    "mixed_waste",
-    "paper_and_plastic",
-    "food_waste",
-    "glass_and_metal_packaging",
-}
+WASTE_TYPES: Final[set[str]] = set(WASTE_TYPE_MAP.values())
 
 # Config keys
 CONF_PROPERTY_ID: Final = "property_id"
