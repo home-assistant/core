5ba7f3db36c7c68f19a1c05e33bcbb48d8f67e2c
Sakshar Dhawan <sakshardhawanfzk@gmail.com>
2026-04-05 18:30:09 +0530

    Merge branch 'dev' into fix-roborock-spam
    Resolve merge conflicts:
    - Transmission: Add EVENT platform to PLATFORMS list
    - Transmission: Add event icons configuration
    - Transmission: Add event strings and state attributes
    - Version: Bump to 2026.5.0.dev0
    - PyProject: Update version to 2026.5.0.dev0
    - Tests: Fix Fritz test data to use GuestWifi

--- tests/components/fritz/test_image.py
+++ tests/components/fritz/test_image.py
@@ -244,7 +244,7 @@ async def test_migrate_to_new_unique_id(
     )
     entry.add_to_hass(hass)
 
-    old_unique_id = slugify(f"{MOCK_MESH_MASTER_MAC}-MyWifi-qr-code")
+    old_unique_id = slugify(f"{MOCK_MESH_MASTER_MAC}-GuestWifi-qr-code")
     new_unique_id = f"{MOCK_MESH_MASTER_MAC}-guest_wifi_qr_code"
 
     entity_registry.async_get_or_create(
