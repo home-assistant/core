a65afff233f2ca5b18d3fdf2506d8378f47f5dfe
Erwin Douna <e.douna@gmail.com>
2026-04-06 19:00:16 +0200

    Tado add rate limit indicator (#164132)
    Co-authored-by: abmantis <amfcalt@gmail.com>
    Co-authored-by: Abílio Costa <abmantis@users.noreply.github.com>

--- homeassistant/components/tado/config_flow.py
+++ homeassistant/components/tado/config_flow.py
@@ -81,6 +81,15 @@ class TadoConfigFlow(ConfigFlow, domain=DOMAIN):
             try:
                 await self.hass.async_add_executor_job(self.tado.device_activation)
             except Exception as ex:
+                ratelimit = await self.hass.async_add_executor_job(
+                    self.tado.rate_limit_info
+                )
+                if ratelimit.get("remaining") == "0":
+                    _LOGGER.error(
+                        "Tado API rate limit reached while waiting for device activation: %s",
+                        ex,
+                    )
+                    raise TadoRateLimitExceeded from ex
                 _LOGGER.exception("Error while waiting for device activation")
                 raise CannotConnect from ex
 
@@ -97,7 +106,10 @@ class TadoConfigFlow(ConfigFlow, domain=DOMAIN):
 
         if self.login_task.done():
             _LOGGER.debug("Login task is done, checking results")
-            if self.login_task.exception():
+            ex = self.login_task.exception()
+            if isinstance(ex, TadoRateLimitExceeded):
+                return self.async_abort(reason="api_rate_limit_reached")
+            if ex:
                 return self.async_show_progress_done(next_step_id="timeout")
             self.refresh_token = await self.hass.async_add_executor_job(
                 self.tado.get_refresh_token
@@ -209,3 +221,7 @@ class OptionsFlowHandler(OptionsFlow):
 
 class CannotConnect(HomeAssistantError):
     """Error to indicate we cannot connect."""
+
+
+class TadoRateLimitExceeded(HomeAssistantError):
+    """Error to indicate Tado API rate limit exceeded."""
