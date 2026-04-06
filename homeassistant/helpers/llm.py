ce2ce94b50c2ce07da011192c92d8c9193bba7e1
Denis Shulyaka <Shulyaka@gmail.com>
2026-04-06 03:53:54 +0300

    Fix for llm tool results mutation

--- homeassistant/helpers/llm.py
+++ homeassistant/helpers/llm.py
@@ -331,6 +331,7 @@ class IntentResponseDict(dict):
         result = intent_response.as_dict()
         del result["language"]
         del result["card"]
+        result["speech"] = result["speech"].copy()
         super().__init__(result)
         self.original = intent_response
 
