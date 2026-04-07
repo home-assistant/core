d2169c6568147b75f1f6c429ba56d1a9ec524d63
reneboer <github@boerhome.nl>
2026-04-07 12:35:50 +0200

    Refactor schedules assignment for better readability
    Update on epenet's suggestion

--- homeassistant/components/renault/sensor.py
+++ homeassistant/components/renault/sensor.py
@@ -161,9 +161,11 @@ def _get_charging_settings_attributes(
     """Return the charging_settings attributes."""
     data = entity.coordinator.data
     return {
-        "schedules": [schedule.for_json() for schedule in schedules]
-        if (schedules := data.schedules)
-        else [],
+        "schedules": (
+            [schedule.for_json() for schedule in schedules]
+            if (schedules := data.schedules)
+            else [],
+        ),
         "dateTime": data.dateTime,
         "startDateTime": data.startDateTime,
         "delay": data.delay,
