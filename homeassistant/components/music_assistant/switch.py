9f08062070e27b9c84dec33c8ba86f117b5eb926
Fabian Munkes <105975993+fmunkes@users.noreply.github.com>
2026-04-07 21:55:23 +0200

    add initial support for platform switch to player options

---
+++ homeassistant/components/music_assistant/switch.py
@@ -0,0 +1,115 @@
+"""Music Assistant Switch platform."""
+
+from __future__ import annotations
+
+from typing import Any, Final
+
+from music_assistant_client.client import MusicAssistantClient
+from music_assistant_models.player import PlayerOption, PlayerOptionType
+
+from homeassistant.components.switch import SwitchEntity, SwitchEntityDescription
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+
+from . import MusicAssistantConfigEntry
+from .const import PLAYER_OPTIONS_TRANSLATION_KEY_PREFIX
+from .entity import MusicAssistantPlayerOptionEntity
+from .helpers import catch_musicassistant_error
+
+PLAYER_OPTIONS_TRANSLATION_KEYS_SWITCH: Final[list[str]] = [
+    "adaptive_drc",
+    "bass_extension",
+    "clear_voice",
+    "enhancer",
+    "extra_bass",
+    "party_mode",
+    "pure_direct",
+    "speaker_a",
+    "speaker_b",
+    "surround_3d",
+]
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: MusicAssistantConfigEntry,
+    async_add_entities: AddConfigEntryEntitiesCallback,
+) -> None:
+    """Set up Music Assistant Switch Entities (Player Options) from Config Entry."""
+    mass = entry.runtime_data.mass
+
+    def add_player(player_id: str) -> None:
+        """Handle add player."""
+        player = mass.players.get(player_id)
+        if player is None:
+            return
+        entities: list[MusicAssistantPlayerConfigSwitch] = []
+        for player_option in player.options:
+            if (
+                not player_option.read_only
+                and player_option.type == PlayerOptionType.BOOLEAN
+                and not player_option.options  # these we map to select
+            ):
+                # the MA translation key must have the format player_options.<translation key>
+                # we ignore entities with unknown translation keys.
+                if (
+                    player_option.translation_key is None
+                    or not player_option.translation_key.startswith(
+                        PLAYER_OPTIONS_TRANSLATION_KEY_PREFIX
+                    )
+                ):
+                    continue
+                translation_key = player_option.translation_key[
+                    len(PLAYER_OPTIONS_TRANSLATION_KEY_PREFIX) :
+                ]
+                if translation_key not in PLAYER_OPTIONS_TRANSLATION_KEYS_SWITCH:
+                    continue
+
+                entities.append(
+                    MusicAssistantPlayerConfigSwitch(
+                        mass,
+                        player_id,
+                        player_option=player_option,
+                        entity_description=SwitchEntityDescription(
+                            key=player_option.key,
+                            translation_key=translation_key,
+                        ),
+                    )
+                )
+        async_add_entities(entities)
+
+    # register callback to add players when they are discovered
+    entry.runtime_data.platform_handlers.setdefault(Platform.SWITCH, add_player)
+
+
+class MusicAssistantPlayerConfigSwitch(MusicAssistantPlayerOptionEntity, SwitchEntity):
+    """Representation of a Switch entity to control player provider dependent settings."""
+
+    def __init__(
+        self,
+        mass: MusicAssistantClient,
+        player_id: str,
+        player_option: PlayerOption,
+        entity_description: SwitchEntityDescription,
+    ) -> None:
+        """Initialize MusicAssistantPlayerConfigSwitch."""
+        super().__init__(mass, player_id, player_option)
+
+        self.entity_description = entity_description
+
+    @catch_musicassistant_error
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Handle turn on command."""
+        await self.mass.players.set_option(self.player_id, self.mass_option_key, True)
+
+    @catch_musicassistant_error
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Handle turn on command."""
+        await self.mass.players.set_option(self.player_id, self.mass_option_key, False)
+
+    def on_player_option_update(self, player_option: PlayerOption) -> None:
+        """Update on player option update."""
+        self._attr_is_on = (
+            player_option.value if isinstance(player_option.value, bool) else None
+        )
