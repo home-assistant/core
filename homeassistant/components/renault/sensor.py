4b1a34fb8112c7dd7c3eb18f4cc1f14635fa9ec8
reneboer <github@boerhome.nl>
2026-04-08 14:08:26 +0000

    Changed schedules arrtibute to tuple

--- homeassistant/components/renault/sensor.py
+++ homeassistant/components/renault/sensor.py
@@ -158,7 +158,7 @@ def _get_charging_settings_attributes(
     """Return the charging_settings attributes."""
     data = entity.coordinator.data
     return {
-        "schedules": [schedule.for_json() for schedule in schedules]
+        "schedules": tuple(schedule.for_json() for schedule in schedules)
         if (schedules := data.schedules)
         else None,
         "dateTime": data.dateTime,
