a60d3148c8abcafe9838aaa0c1a3f604990e285f
Taraman17 <mozilla@adrario.de>
2026-04-09 18:44:21 +0000

    Initial support for Homeegrams

--- homeassistant/components/homee/button.py
+++ homeassistant/components/homee/button.py
@@ -2,6 +2,7 @@
 
 from pyHomee.const import AttributeType
 from pyHomee.model import HomeeAttribute, HomeeNode
+from pyHomee.model_homeegram import HomeeGram
 
 from homeassistant.components.button import (
     ButtonDeviceClass,
@@ -10,9 +11,10 @@ from homeassistant.components.button import (
 )
 from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
-from . import HomeeConfigEntry
+from . import DOMAIN, HomeeConfigEntry
 from .entity import HomeeEntity
 from .helpers import setup_homee_platform
 
@@ -54,6 +56,14 @@ async def add_button_entities(
     )
 
 
+def get_homeegram_buttons(config_entry: HomeeConfigEntry) -> list[HomeegramButton]:
+    """Get buttons for homeegrams."""
+    return [
+        HomeegramButton(homeegram, config_entry)
+        for homeegram in config_entry.runtime_data.homeegrams
+    ]
+
+
 async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: HomeeConfigEntry,
@@ -62,6 +72,7 @@ async def async_setup_entry(
     """Add the homee platform for the button component."""
 
     await setup_homee_platform(add_button_entities, async_add_entities, config_entry)
+    async_add_entities(get_homeegram_buttons(config_entry))
 
 
 class HomeeButton(HomeeEntity, ButtonEntity):
@@ -88,3 +99,51 @@ class HomeeButton(HomeeEntity, ButtonEntity):
     async def async_press(self) -> None:
         """Handle the button press."""
         await self.async_set_homee_value(1)
+
+
+class HomeegramButton(ButtonEntity):
+    """Representation of a Homeegram as button."""
+
+    _attr_has_entity_name = True
+    _attr_should_poll = False
+
+    def __init__(self, homeegram: HomeeGram, entry: HomeeConfigEntry) -> None:
+        """Initialize a homee Homeegram button entity."""
+        self._homeegram = homeegram
+        self._entry = entry
+        self._attr_unique_id = f"{entry.unique_id}-hg-{homeegram.id}"
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, f"{entry.unique_id}-homeegrams")},
+            name="Homeegrams",
+            model="Homeegram Buttons",
+            via_device=(DOMAIN, entry.runtime_data.settings.uid),
+        )
+        self._host_connected = entry.runtime_data.connected
+        self._attr_name = homeegram.name
+
+    async def async_added_to_hass(self) -> None:
+        """Add the Homeegram entity to home assistant."""
+        self.async_on_remove(
+            self._homeegram.add_on_changed_listener(self._on_homeegram_updated)
+        )
+        self.async_on_remove(
+            self._entry.runtime_data.add_connection_listener(
+                self._on_connection_changed
+            )
+        )
+
+    @property
+    def available(self) -> bool:
+        """Return the availability of the homeegram based on host availability."""
+        return self._host_connected
+
+    async def async_press(self) -> None:
+        """Trigger Homeegram on push."""
+        await self._entry.runtime_data.play_homeegram(self._homeegram.id)
+
+    def _on_homeegram_updated(self, homeegram: HomeeGram) -> None:
+        self.schedule_update_ha_state()
+
+    async def _on_connection_changed(self, connected: bool) -> None:
+        self._host_connected = connected
+        self.schedule_update_ha_state()
