27a40e037c2c688aa351b08cca0ef231a77f0f5d
Andres Jose Ruiz Torres <andresruiz2010@gmail.com>
2026-04-11 02:29:43 +0000

    Make arg a callback
    This enables the SubaruButton to be more generic, while still
    preserving the ability to dynamically fetch the selected climate
    preset in the future.

--- homeassistant/components/subaru/button.py
+++ homeassistant/components/subaru/button.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+from collections.abc import Callable
+from dataclasses import dataclass
 from typing import Any
 
 from subarulink import Controller as SubaruAPI
@@ -25,12 +27,21 @@ from .const import (
 from .coordinator import SubaruDataUpdateCoordinator
 from .remote_service import async_call_remote_service
 
+
+@dataclass(frozen=True, kw_only=True)
+class SubaruButtonEntityDescription(ButtonEntityDescription):
+    """Describes a Subaru button entity."""
+
+    arg: Callable[[dict[str, Any]], str | None] | None = None
+
+
 REMOTE_BUTTONS = [
-    ButtonEntityDescription(
+    SubaruButtonEntityDescription(
         key=SERVICE_REMOTE_START,
         translation_key="remote_start",
+        arg=lambda _: "Auto",
     ),
-    ButtonEntityDescription(
+    SubaruButtonEntityDescription(
         key=SERVICE_REMOTE_STOP,
         translation_key="remote_stop",
     ),
@@ -59,13 +70,14 @@ class SubaruButton(ButtonEntity):
     """Class for a Subaru button."""
 
     _attr_has_entity_name = True
+    entity_description: SubaruButtonEntityDescription
 
     def __init__(
         self,
         vehicle_info: dict[str, Any],
         controller: SubaruAPI,
         coordinator: SubaruDataUpdateCoordinator,
-        description: ButtonEntityDescription,
+        description: SubaruButtonEntityDescription,
     ) -> None:
         """Initialize the button for the vehicle."""
         self.controller = controller
@@ -78,8 +90,11 @@ class SubaruButton(ButtonEntity):
 
     async def async_press(self) -> None:
         """Press the button."""
-        # Default to "Auto" until a climate preset selector entity is added
-        arg = "Auto" if self.entity_description.key == SERVICE_REMOTE_START else None
+        arg = (
+            self.entity_description.arg(self.vehicle_info)
+            if self.entity_description.arg
+            else None
+        )
         await async_call_remote_service(
             self.controller,
             self.entity_description.key,
