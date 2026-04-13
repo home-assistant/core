377b4766dc34a1a4741113e3bd66c747ab26d8dd
orandasoft <orandasoft@users.noreply.github.com>
2026-04-13 16:47:22 +0900

    Fix docstring indentation in async_setup_entry

--- homeassistant/components/lg_infrared/__init__.py
+++ homeassistant/components/lg_infrared/__init__.py
@@ -6,15 +6,26 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-PLATFORMS = [Platform.BUTTON, Platform.MEDIA_PLAYER]
+from .const import DOMAIN
+from .models import LGIRRemoteData
+
+PLATFORMS = [Platform.BUTTON, Platform.MEDIA_PLAYER, Platform.SELECT]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up LG IR from a config entry."""
+    data = LGIRRemoteData()
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = data
+    
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a LG IR config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+        
+    return unload_ok
