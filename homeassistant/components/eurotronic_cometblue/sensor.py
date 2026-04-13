a4509d0ac7fb61f4909cfa1145832fcb70ca13ae
Richard <42204099+rikroe@users.noreply.github.com>
2026-04-13 21:21:30 +0200

    Change sensor to custom entity without list of entity descriptions

--- homeassistant/components/eurotronic_cometblue/sensor.py
+++ homeassistant/components/eurotronic_cometblue/sensor.py
@@ -16,14 +16,6 @@ from .entity import CometBlueBluetoothEntity
 
 PARALLEL_UPDATES = 0
 
-DESCRIPTIONS = [
-    SensorEntityDescription(
-        key="battery",
-        device_class=SensorDeviceClass.BATTERY,
-        native_unit_of_measurement=PERCENTAGE,
-    ),
-]
-
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -33,28 +25,29 @@ async def async_setup_entry(
     """Set up the client entities."""
 
     coordinator = entry.runtime_data
-    entities: list[CometBlueSensorEntity] = [
-        CometBlueSensorEntity(coordinator, description) for description in DESCRIPTIONS
-    ]
+    entities = [CometBlueBatterySensorEntity(coordinator)]
 
     async_add_entities(entities)
 
 
-class CometBlueSensorEntity(CometBlueBluetoothEntity, SensorEntity):
+class CometBlueBatterySensorEntity(CometBlueBluetoothEntity, SensorEntity):
     """Representation of a sensor."""
 
     def __init__(
         self,
         coordinator: CometBlueDataUpdateCoordinator,
-        description: SensorEntityDescription,
     ) -> None:
         """Initialize CometBlueSensorEntity."""
 
         super().__init__(coordinator)
-        self.entity_description = description
-        self._attr_unique_id = f"{coordinator.address}-{description.key}"
+        self.entity_description = SensorEntityDescription(
+            key="battery",
+            device_class=SensorDeviceClass.BATTERY,
+            native_unit_of_measurement=PERCENTAGE,
+        )
+        self._attr_unique_id = f"{coordinator.address}-{self.entity_description.key}"
 
     @property
     def native_value(self) -> float | None:
         """Return the entity value to represent the entity state."""
-        return getattr(self.coordinator.data, self.entity_description.key, None)
+        return self.coordinator.data.battery
