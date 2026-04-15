0d042e90ebe2a0919a6e383e3136275b951ee337
Ronald van der Meer <ronald@vandermeer.frl>
2026-04-15 22:34:21 +0200

    Include node_id in DucoBoxSensorEntity unique_id

--- homeassistant/components/duco/sensor.py
+++ homeassistant/components/duco/sensor.py
@@ -175,7 +175,9 @@ class DucoBoxSensorEntity(DucoEntity, SensorEntity):
         """Initialize the box sensor entity."""
         super().__init__(coordinator, node)
         self.entity_description = description
-        self._attr_unique_id = f"{coordinator.config_entry.unique_id}_{description.key}"
+        self._attr_unique_id = (
+            f"{coordinator.config_entry.unique_id}_{node.node_id}_{description.key}"
+        )
 
     @property
     def native_value(self) -> int | float | None:
