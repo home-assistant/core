67e37c29b1865b5a759367bdf3956fa2d1538211
Florent Thoumie <florent@thoumie.net>
2026-04-17 16:09:37 +0000

    Complete dhcp unit test

--- tests/components/iaqualink/test_config_flow.py
+++ tests/components/iaqualink/test_config_flow.py
@@ -52,7 +52,9 @@ async def test_without_config(hass: HomeAssistant) -> None:
     assert result["errors"] == {}
 
 
-async def test_dhcp_discovery_starts_user_flow(hass: HomeAssistant) -> None:
+async def test_dhcp_discovery_starts_user_flow(
+    hass: HomeAssistant, config_data: dict[str, str]
+) -> None:
     """Test DHCP discovery starts the user flow."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
@@ -64,6 +66,26 @@ async def test_dhcp_discovery_starts_user_flow(hass: HomeAssistant) -> None:
     assert result["step_id"] == "user"
     assert result["errors"] == {}
     assert result["description_placeholders"] is None
+
+    with (
+        patch(
+            "homeassistant.components.iaqualink.config_flow.AqualinkClient.login",
+            return_value=None,
+        ),
+        patch(
+            "homeassistant.components.iaqualink.async_setup_entry",
+            return_value=True,
+        ),
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            config_data,
+        )
+        await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["title"] == config_data[CONF_USERNAME]
+    assert result["data"] == config_data
 
 
 async def test_with_invalid_credentials(
