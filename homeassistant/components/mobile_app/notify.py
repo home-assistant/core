b755174fce29b25a027908b20b3b008f094ba482
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2026-04-18 16:47:52 +0000

    Add notify entities to Mobile app integration

--- homeassistant/components/mobile_app/notify.py
+++ homeassistant/components/mobile_app/notify.py
@@ -17,10 +17,15 @@ from homeassistant.components.notify import (
     ATTR_TITLE,
     ATTR_TITLE_DEFAULT,
     BaseNotificationService,
+    NotifyEntity,
+    NotifyEntityFeature,
 )
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import ATTR_DEVICE_ID
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from homeassistant.util import dt as dt_util
 
@@ -43,9 +48,153 @@ from .const import (
     DATA_PUSH_CHANNEL,
     DOMAIN,
 )
+from .helpers import device_info
+from .push_notification import PushChannel
 from .util import supports_push
 
 _LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddConfigEntryEntitiesCallback,
+) -> None:
+    """Set up the Mobile app notify platform."""
+    if supports_push(hass, entry.data[ATTR_WEBHOOK_ID]):
+        async_add_entities([MobileAppNotifyEntity(entry)])
+
+
+class MobileAppNotifyEntity(NotifyEntity):
+    """Representation of a Mobile app notify entity."""
+
+    _attr_has_entity_name = True
+    _attr_translation_key = "notify"
+    _attr_name = None
+    _attr_supported_features = NotifyEntityFeature.TITLE
+
+    def __init__(self, entry: ConfigEntry) -> None:
+        """Initialize the notify entity."""
+
+        self._attr_unique_id = entry.data[ATTR_DEVICE_ID]
+        self._attr_device_info = device_info(entry.data)
+        self.entry = entry
+
+    async def async_send_message(self, message: str, title: str | None = None) -> None:
+        """Send a message via notify.send_message action."""
+        await self._async_send_remote_message(message=message, title=title)
+
+    async def _async_send_remote_message(
+        self,
+        message: str | None = None,
+        title: str | None = None,
+        **kwargs: Any,
+    ) -> None:
+        """Send the push notification."""
+
+        async def _send_message(data: dict[str, Any]):
+            reg_info = {
+                ATTR_APP_ID: self.entry.data[ATTR_APP_ID],
+                ATTR_APP_VERSION: self.entry.data[ATTR_APP_VERSION],
+                ATTR_WEBHOOK_ID: self.entry.data[ATTR_WEBHOOK_ID],
+            }
+            if ATTR_OS_VERSION in self.entry.data:
+                reg_info[ATTR_OS_VERSION] = self.entry.data[ATTR_OS_VERSION]
+
+            placeholders = {"device_name": self.entry.title}
+            try:
+                async with asyncio.timeout(10):
+                    response = await async_get_clientsession(self.hass).post(
+                        self.entry.data[ATTR_APP_DATA][ATTR_PUSH_URL],
+                        json={
+                            **data,
+                            ATTR_PUSH_TOKEN: self.entry.data[ATTR_APP_DATA][
+                                ATTR_PUSH_TOKEN
+                            ],
+                            "registration_info": reg_info,
+                        },
+                    )
+                    result: dict[str, Any] = await response.json()
+
+                log_rate_limits(self.hass, self.entry.title, result, logging.DEBUG)
+
+                if response.status in (
+                    HTTPStatus.OK,
+                    HTTPStatus.CREATED,
+                    HTTPStatus.ACCEPTED,
+                ):
+                    return
+
+                fallback_error = result.get("errorMessage", "Unknown error")
+                fallback_message = (
+                    f"Internal server error, please try again later: {fallback_error}"
+                )
+                message = result.get("message", fallback_message)
+
+                if "message" in result:
+                    if message[-1] not in [".", "?", "!"]:
+                        message += "."
+                    message += (
+                        " This message is generated externally to Home Assistant."
+                    )
+                _LOGGER.debug(
+                    "Error sending notification to %s: %s", self.entry.title, message
+                )
+
+                if response.status == HTTPStatus.TOO_MANY_REQUESTS:
+                    raise HomeAssistantError(
+                        translation_domain=DOMAIN,
+                        translation_key="rate_limit_exceeded_sending_notification",
+                        translation_placeholders=placeholders,
+                    )
+                raise HomeAssistantError(
+                    translation_domain=DOMAIN,
+                    translation_key="error_sending_notification",
+                    translation_placeholders=placeholders,
+                )
+            except TimeoutError as e:
+                raise HomeAssistantError(
+                    translation_domain=DOMAIN,
+                    translation_key="timeout_sending_notification",
+                    translation_placeholders=placeholders,
+                ) from e
+            except aiohttp.ClientError as e:
+                _LOGGER.debug(
+                    "Error sending notification to %s:", self.entry.title, exc_info=True
+                )
+                raise HomeAssistantError(
+                    translation_domain=DOMAIN,
+                    translation_key="error_sending_notification",
+                    translation_placeholders=placeholders,
+                ) from e
+
+        data: dict[str, Any] = {}
+        if message is not None:
+            data[ATTR_MESSAGE] = message
+        if title is not None:
+            data[ATTR_TITLE] = title
+        if kwargs:
+            data[ATTR_DATA] = kwargs
+
+        if (
+            self.entry.data[ATTR_WEBHOOK_ID]
+            in self.hass.data[DOMAIN][DATA_PUSH_CHANNEL]
+        ):
+            push_channel: PushChannel = self.hass.data[DOMAIN][DATA_PUSH_CHANNEL][
+                self.entry.data[ATTR_WEBHOOK_ID]
+            ]
+            push_channel.async_send_notification(data, _send_message)
+        elif ATTR_PUSH_URL in self.entry.data[ATTR_APP_DATA]:
+            await _send_message(data)
+        else:
+            raise HomeAssistantError(
+                f"Device {self.entry.title} not connected to local push notifications",
+                translation_domain=DOMAIN,
+                translation_key="device_not_connected_for_local_push_notifications",
+                translation_placeholders={
+                    "device_name": self.entry.data[ATTR_DEVICE_NAME]
+                },
+            )
 
 
 def push_registrations(hass: HomeAssistant) -> dict[str, str]:
