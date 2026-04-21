7f0a6b79f8a44c8371d63edd3ac08a24a866da1a
epenet <6771947+epenet@users.noreply.github.com>
2026-04-21 15:05:49 +0200

    Use runtime_data in abode integration (#168709)

--- homeassistant/components/abode/const.py
+++ homeassistant/components/abode/const.py
@@ -3,17 +3,10 @@
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING
-
-from homeassistant.util.hass_dict import HassKey
-
-if TYPE_CHECKING:
-    from . import AbodeSystem
 
 LOGGER = logging.getLogger(__package__)
 
 DOMAIN = "abode"
-DOMAIN_DATA: HassKey[AbodeSystem] = HassKey(DOMAIN)
 ATTRIBUTION = "Data provided by goabode.com"
 
 CONF_POLLING = "polling"
