159f86a5eddbf590eea41ac67fd78b11161f0db6
epenet <6771947+epenet@users.noreply.github.com>
2026-04-21 13:04:49 +0200

    Use runtime_data in youtube (#168696)

--- homeassistant/components/youtube/diagnostics.py
+++ homeassistant/components/youtube/diagnostics.py
@@ -4,20 +4,17 @@ from __future__ import annotations
 
 from typing import Any
 
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import ATTR_DESCRIPTION, ATTR_LATEST_VIDEO, COORDINATOR, DOMAIN
-from .coordinator import YouTubeDataUpdateCoordinator
+from .const import ATTR_DESCRIPTION, ATTR_LATEST_VIDEO
+from .coordinator import YouTubeConfigEntry
 
 
 async def async_get_config_entry_diagnostics(
-    hass: HomeAssistant, entry: ConfigEntry
+    hass: HomeAssistant, entry: YouTubeConfigEntry
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
-    coordinator: YouTubeDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id][
-        COORDINATOR
-    ]
+    coordinator = entry.runtime_data
     sensor_data: dict[str, Any] = {}
     for channel_id, channel_data in coordinator.data.items():
         channel_data.get(ATTR_LATEST_VIDEO, {}).pop(ATTR_DESCRIPTION)
