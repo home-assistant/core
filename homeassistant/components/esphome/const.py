d530d0bd3dbcdb1876921e996855adc90cbb493d
Johan Henkens <jhenkens@users.noreply.github.com>
2026-04-21 22:45:21 -0700

    Remove None from const

--- homeassistant/components/esphome/const.py
+++ homeassistant/components/esphome/const.py
@@ -35,7 +35,6 @@ WAKE_WORDS_DIR_NAME = "custom_wake_words"
 WAKE_WORDS_API_PATH = "/api/esphome/wake_words"
 
 TEMPERATURE_UNIT_MAP: dict[TemperatureUnit | None, UnitOfTemperature] = {
-    None: UnitOfTemperature.CELSIUS,
     TemperatureUnit.CELSIUS: UnitOfTemperature.CELSIUS,
     TemperatureUnit.FAHRENHEIT: UnitOfTemperature.FAHRENHEIT,
     TemperatureUnit.KELVIN: UnitOfTemperature.KELVIN,
