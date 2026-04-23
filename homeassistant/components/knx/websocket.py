597b639299b543653d88ad0747c3b54bdaa251d0
Matthias Alphart <farmio@alphart.net>
2026-04-23 14:44:24 +0200

    Update knx-frontend to 2026.4.22.141111 (#168837)

--- homeassistant/components/knx/websocket.py
+++ homeassistant/components/knx/websocket.py
@@ -65,7 +65,7 @@ async def register_panel(hass: HomeAssistant) -> None:
     websocket_api.async_register_command(hass, ws_update_entity)
     websocket_api.async_register_command(hass, ws_delete_entity)
     websocket_api.async_register_command(hass, ws_get_entity_config)
-    websocket_api.async_register_command(hass, ws_get_entity_entries)
+    websocket_api.async_register_command(hass, ws_get_entities_by_group)
     websocket_api.async_register_command(hass, ws_create_device)
     websocket_api.async_register_command(hass, ws_get_schema)
     websocket_api.async_register_command(hass, ws_get_time_server_config)
@@ -518,22 +518,22 @@ async def ws_delete_entity(
 @websocket_api.require_admin
 @websocket_api.websocket_command(
     {
-        vol.Required("type"): "knx/get_entity_entries",
+        vol.Required("type"): "knx/get_entities_by_group",
     }
 )
 @provide_knx
 @callback
-def ws_get_entity_entries(
+def ws_get_entities_by_group(
     hass: HomeAssistant,
     knx: KNXModule,
     connection: websocket_api.ActiveConnection,
     msg: dict,
 ) -> None:
-    """Get entities configured from entity store."""
-    entity_entries = [
-        entry.extended_dict for entry in knx.config_store.get_entity_entries()
-    ]
-    connection.send_result(msg["id"], entity_entries)
+    """Get entities by group address."""
+    data = {
+        str(ga): identifiers for ga, identifiers in knx.group_address_entities.items()
+    }
+    connection.send_result(msg["id"], data)
 
 
 @websocket_api.require_admin
