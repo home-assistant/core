92639a7e30904b5874ba7b946eed31af06c2c594
J. Nick Koston <nick@home-assistant.io>
2026-04-24 09:15:49 -0500

    snapshot

--- tests/components/flume/test_sensor.py
+++ tests/components/flume/test_sensor.py
@@ -3,18 +3,14 @@
 from unittest.mock import patch
 
 import pytest
-from requests_mock.mocker import Mocker
+from syrupy.assertion import SnapshotAssertion
 
-from homeassistant.components.flume.const import DOMAIN
-from homeassistant.components.sensor import SensorDeviceClass
-from homeassistant.const import Platform, UnitOfVolume, UnitOfVolumeFlowRate
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 from homeassistant.util.unit_system import US_CUSTOMARY_SYSTEM
 
-from .conftest import DEVICE_LIST, DEVICE_LIST_URL
-
-from tests.common import MockConfigEntry
+from tests.common import MockConfigEntry, snapshot_platform
 
 
 @pytest.fixture(autouse=True)
@@ -24,32 +20,16 @@ def platforms_fixture():
         yield
 
 
-@pytest.mark.usefixtures("access_token")
+@pytest.mark.usefixtures("access_token", "device_list")
 async def test_sensors(
     hass: HomeAssistant,
-    requests_mock: Mocker,
     config_entry: MockConfigEntry,
+    entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test sensors."""
     hass.config.units = US_CUSTOMARY_SYSTEM
 
-    # Add TZ to the device list from conftest
-    devices = [dict(device) for device in DEVICE_LIST]
-    for device in devices:
-        if "location" in device:
-            device["location"] = dict(device["location"])
-            device["location"]["tz"] = "America/New_York"
-
-    requests_mock.register_uri(
-        "GET",
-        DEVICE_LIST_URL,
-        status_code=200,
-        json={
-            "data": devices,
-        },
-    )
-
-    # Mock the data returned by PyFlume
     flume_values = {
         "current_interval": 1.23,
         "month_to_date": 100.1,
@@ -61,70 +41,9 @@ async def test_sensors(
     }
 
     with patch("homeassistant.components.flume.sensor.FlumeData") as mock_flume_data:
-        instance = mock_flume_data.return_value
-        instance.values = flume_values
-        instance.update_force.return_value = None
-        instance.query_payload = {}
+        mock_flume_data.return_value.values = flume_values
 
         assert await hass.config_entries.async_setup(config_entry.entry_id)
         await hass.async_block_till_done()
 
-    entity_registry = er.async_get(hass)
-
-    # current_interval
-    entry = entity_registry.async_get_entity_id(
-        "sensor", DOMAIN, "current_interval_1234"
-    )
-    state = hass.states.get(entry)
-    assert state.state == "1.23"
-    assert (
-        state.attributes["unit_of_measurement"]
-        == UnitOfVolumeFlowRate.GALLONS_PER_MINUTE
-    )
-    assert state.attributes["device_class"] == SensorDeviceClass.VOLUME_FLOW_RATE
-
-    # month_to_date
-    entry = entity_registry.async_get_entity_id("sensor", DOMAIN, "month_to_date_1234")
-    state = hass.states.get(entry)
-    assert state.state == "100.1"
-    assert state.attributes["unit_of_measurement"] == UnitOfVolume.GALLONS
-    assert state.attributes["device_class"] == SensorDeviceClass.WATER
-
-    # week_to_date
-    entry = entity_registry.async_get_entity_id("sensor", DOMAIN, "week_to_date_1234")
-    state = hass.states.get(entry)
-    assert state.state == "50.5"
-    assert state.attributes["unit_of_measurement"] == UnitOfVolume.GALLONS
-    assert state.attributes["device_class"] == SensorDeviceClass.WATER
-
-    # today
-    entry = entity_registry.async_get_entity_id("sensor", DOMAIN, "today_1234")
-    state = hass.states.get(entry)
-    assert state.state == "10.2"
-    assert state.attributes["unit_of_measurement"] == UnitOfVolume.GALLONS
-    assert state.attributes["device_class"] == SensorDeviceClass.WATER
-
-    # last_60_min
-    entry = entity_registry.async_get_entity_id("sensor", DOMAIN, "last_60_min_1234")
-    state = hass.states.get(entry)
-    assert state.state == "5.5"
-    assert (
-        state.attributes["unit_of_measurement"] == UnitOfVolumeFlowRate.GALLONS_PER_HOUR
-    )
-    assert state.attributes["device_class"] == SensorDeviceClass.VOLUME_FLOW_RATE
-
-    # last_24_hrs
-    entry = entity_registry.async_get_entity_id("sensor", DOMAIN, "last_24_hrs_1234")
-    state = hass.states.get(entry)
-    assert state.state == "20.4"
-    assert (
-        state.attributes["unit_of_measurement"] == UnitOfVolumeFlowRate.GALLONS_PER_DAY
-    )
-    assert state.attributes["device_class"] == SensorDeviceClass.VOLUME_FLOW_RATE
-
-    # last_30_days
-    entry = entity_registry.async_get_entity_id("sensor", DOMAIN, "last_30_days_1234")
-    state = hass.states.get(entry)
-    assert state.state == "150.8"
-    assert state.attributes["unit_of_measurement"] == "gal/mo"
-    assert "device_class" not in state.attributes
+    await snapshot_platform(hass, entity_registry, snapshot, config_entry.entry_id)
