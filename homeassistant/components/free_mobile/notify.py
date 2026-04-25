78459b150952b7caaac45dff14e667ad21b8e7c8
Denis Pitzalis <denics@free.fr>
2026-04-25 20:39:23 +0000

    Use unique issue ID for deprecated_yaml to avoid conflicts

--- homeassistant/components/free_mobile/notify.py
+++ homeassistant/components/free_mobile/notify.py
@@ -52,7 +52,7 @@ async def async_get_service(
         ir.async_create_issue(
             hass,
             HOMEASSISTANT_DOMAIN,
-            "deprecated_yaml",
+            f"deprecated_yaml_{DOMAIN}",
             breaks_in_ha_version="2026.7.0",
             is_fixable=False,
             issue_domain=DOMAIN,
@@ -68,7 +68,7 @@ async def async_get_service(
     ir.async_create_issue(
         hass,
         HOMEASSISTANT_DOMAIN,
-        "deprecated_yaml",
+        f"deprecated_yaml_{DOMAIN}",
         breaks_in_ha_version="2026.7.0",
         is_fixable=False,
         issue_domain=DOMAIN,
