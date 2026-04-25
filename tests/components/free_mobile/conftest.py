45f4b19e417f43f98fc653814ab5c930b82ad01c
Denis Pitzalis <denics@free.fr>
2026-04-25 20:22:07 +0000

    Remove unused configure_free_mobile_through_yaml and free_mobile_notification_entity fixtures

--- tests/components/free_mobile/conftest.py
+++ tests/components/free_mobile/conftest.py
@@ -6,10 +6,8 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 from homeassistant.components.free_mobile.const import DOMAIN
-from homeassistant.components.notify import DOMAIN as NOTIFY_DOMAIN
 from homeassistant.const import CONF_ACCESS_TOKEN, CONF_USERNAME
 from homeassistant.core import HomeAssistant
-from homeassistant.setup import async_setup_component
 
 from tests.common import MockConfigEntry
 
@@ -50,39 +48,3 @@ def mock_freesms_config_entry(hass: HomeAssistant) -> MockConfigEntry:
         domain=DOMAIN,
         data=CONF_INPUT,
     )
-
-
-@pytest.fixture
-async def configure_free_mobile_through_yaml(
-    hass: HomeAssistant, mock_freesms: MagicMock
-) -> None:
-    """Configure the notify domain with YAML for the Free Mobile platform."""
-    await async_setup_component(
-        hass,
-        NOTIFY_DOMAIN,
-        {
-            NOTIFY_DOMAIN: [
-                {
-                    "name": DOMAIN,
-                    "platform": DOMAIN,
-                    CONF_USERNAME: TEST_USERNAME,
-                    CONF_ACCESS_TOKEN: TEST_ACCESS_TOKEN,
-                },
-            ]
-        },
-    )
-    await hass.async_block_till_done()
-
-
-@pytest.fixture
-async def free_mobile_notification_entity(
-    hass: HomeAssistant,
-    mock_freesms: MagicMock,
-    mock_freesms_config_entry: MockConfigEntry,
-) -> MockConfigEntry:
-    """Configure a Free Mobile Notification Entity."""
-    mock_freesms_config_entry.add_to_hass(hass)
-    await hass.config_entries.async_setup(mock_freesms_config_entry.entry_id)
-    await hass.async_block_till_done()
-
-    return mock_freesms_config_entry
