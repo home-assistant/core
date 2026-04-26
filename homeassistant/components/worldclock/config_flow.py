f676a58595ffa8d68279937c1b24ba7767f113ed
G Johansson <goran.johansson@shiftit.se>
2026-04-26 18:33:46 +0000

    Remove name from worldclock config flow

--- homeassistant/components/worldclock/config_flow.py
+++ homeassistant/components/worldclock/config_flow.py
@@ -3,12 +3,12 @@
 from __future__ import annotations
 
 from collections.abc import Mapping
-from typing import Any, cast
+from typing import Any
 import zoneinfo
 
 import voluptuous as vol
 
-from homeassistant.const import CONF_NAME, CONF_TIME_ZONE
+from homeassistant.const import CONF_TIME_ZONE
 from homeassistant.helpers.schema_config_entry_flow import (
     SchemaCommonFlowHandler,
     SchemaConfigFlowHandler,
@@ -19,7 +19,6 @@ from homeassistant.helpers.selector import (
     SelectSelector,
     SelectSelectorConfig,
     SelectSelectorMode,
-    TextSelector,
 )
 
 from .const import CONF_TIME_FORMAT, DEFAULT_NAME, DEFAULT_TIME_STR_FORMAT, DOMAIN
@@ -55,7 +54,6 @@ async def get_schema(handler: SchemaCommonFlowHandler) -> vol.Schema:
     )
     return vol.Schema(
         {
-            vol.Required(CONF_NAME, default=DEFAULT_NAME): TextSelector(),
             vol.Required(CONF_TIME_ZONE): SelectSelector(
                 SelectSelectorConfig(
                     options=get_timezones, mode=SelectSelectorMode.DROPDOWN, sort=True
@@ -101,4 +99,4 @@ class WorldclockConfigFlowHandler(SchemaConfigFlowHandler, domain=DOMAIN):
 
     def async_config_entry_title(self, options: Mapping[str, Any]) -> str:
         """Return config entry title."""
-        return cast(str, options[CONF_NAME])
+        return DEFAULT_NAME
