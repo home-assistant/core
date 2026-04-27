73f09671ccc74c147294e5791b66da8b0fb36d2b
Tomáš Ženčák <tomas.zencak@seznam.cz>
2026-04-27 22:55:12 +0200

    Make prek happy

--- homeassistant/components/zwave_me/light.py
+++ homeassistant/components/zwave_me/light.py
@@ -65,8 +65,7 @@ class ZWaveMeRGB(ZWaveMeEntity, LightEntity):
     def _transition_to_duration(transition: float) -> int:
         if transition < 127:
             return round(transition)
-        else:
-            return min(127, round((transition) / 60)) + 127
+        return min(127, round((transition) / 60)) + 127
 
     @override
     def turn_off(self, **kwargs: Any) -> None:
