e35804e74362901c8a6640ded1f4bf64ecff45ab
Glenn Waters <glenn@watrs.ca>
2026-04-28 11:06:12 -0400

    Add ouput turn on action
    Supports length of time to turn output on for.

--- homeassistant/components/elkm1/switch.py
+++ homeassistant/components/elkm1/switch.py
@@ -9,14 +9,25 @@ from elkm1_lib.elements import Element
 from elkm1_lib.elk import Elk
 from elkm1_lib.outputs import Output
 from elkm1_lib.thermostats import Thermostat
+import voluptuous as vol
 
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers import entity_platform
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+from homeassistant.helpers.typing import VolDictType
 
 from . import ElkM1ConfigEntry
+from .const import ATTR_VALUE
 from .entity import ElkAttachedEntity, ElkEntity, create_elk_entities
 from .models import ELKM1Data
+
+SERVICE_SWITCH_OUTPUT_TURN_ON = "switch_output_turn_on"
+
+ELK_OUTPUT_TURN_ON_SERVICE_SCHEMA: VolDictType = {
+    vol.Required(ATTR_VALUE): vol.All(vol.Coerce(int), vol.Range(0, 65535))
+}
 
 
 async def async_setup_entry(
@@ -33,6 +44,14 @@ async def async_setup_entry(
         elk_data, elk.thermostats, "thermostat", ElkThermostatEMHeat, entities
     )
     async_add_entities(entities)
+
+    platform = entity_platform.async_get_current_platform()
+
+    platform.async_register_entity_service(
+        SERVICE_SWITCH_OUTPUT_TURN_ON,
+        ELK_OUTPUT_TURN_ON_SERVICE_SCHEMA,
+        "async_switch_output_turn_on",
+    )
 
 
 class ElkOutput(ElkAttachedEntity, SwitchEntity):
@@ -52,6 +71,13 @@ class ElkOutput(ElkAttachedEntity, SwitchEntity):
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off the output."""
         self._element.turn_off()
+
+    async def async_switch_output_turn_on(self, value: int | None = None) -> None:
+        """Set the value of a counter on the panel."""
+        if not isinstance(self, ElkOutput):
+            raise HomeAssistantError("supported only on ElkM1 Outputs")
+        if value is not None:
+            self._element.turn_on(value)
 
 
 class ElkThermostatEMHeat(ElkEntity, SwitchEntity):
