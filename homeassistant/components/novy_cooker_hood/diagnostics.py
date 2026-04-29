97caea5528b000f59714a6fae42362bb9b532840
Paul Bottein <paul.bottein@gmail.com>
2026-04-29 23:08:28 +0200

    Add diagnostics to Novy Cooker Hood

---
+++ homeassistant/components/novy_cooker_hood/diagnostics.py
@@ -0,0 +1,16 @@
+"""Diagnostics support for the Novy Cooker Hood integration."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+    return {"config_entry": config_entry.as_dict()}
---
+++ tests/components/novy_cooker_hood/test_diagnostics.py
@@ -0,0 +1,28 @@
+"""Tests for the diagnostics provided by the Novy Cooker Hood integration."""
+
+from __future__ import annotations
+
+from syrupy.assertion import SnapshotAssertion
+from syrupy.filters import props
+
+from homeassistant.core import HomeAssistant
+
+from tests.common import MockConfigEntry
+from tests.components.diagnostics import get_diagnostics_for_config_entry
+from tests.typing import ClientSessionGenerator
+
+
+async def test_diagnostics(
+    hass: HomeAssistant,
+    hass_client: ClientSessionGenerator,
+    init_novy_cooker_hood: MockConfigEntry,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test diagnostics."""
+    assert await get_diagnostics_for_config_entry(
+        hass, hass_client, init_novy_cooker_hood
+    ) == snapshot(
+        exclude=props(
+            "created_at", "modified_at", "entry_id", "unique_id", "transmitter"
+        )
+    )
