180e5ce28f2b54b39289236f4c60cedd7215a858
FIls0010 <a1867444@adelaide.edu.au>
2026-05-02 11:22:50 +0000

    Fix mypy no-any-return in youtube access_token property

--- homeassistant/components/youtube/api.py
+++ homeassistant/components/youtube/api.py
@@ -1,5 +1,7 @@
 """API for YouTube bound to Home Assistant OAuth."""
 
+from typing import cast
+
 from youtubeaio.types import AuthScope
 from youtubeaio.youtube import YouTube
 
@@ -26,7 +28,7 @@ class AsyncConfigEntryAuth:
     @property
     def access_token(self) -> str:
         """Return the access token."""
-        return self.oauth_session.token[CONF_ACCESS_TOKEN]  # type: ignore[no-any-return]
+        return cast(str, self.oauth_session.token[CONF_ACCESS_TOKEN])
 
     async def check_and_refresh_token(self) -> str:
         """Check the token."""
