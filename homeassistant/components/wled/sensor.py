f51feccc12a23b562d13c3312ff5f8925b8da6a4
Kamil Breguła <mik-laj@users.noreply.github.com>
2026-05-04 21:02:15 +0200

    Use SensorDeviceClass.UPTIME in WLED (#169708)
    Co-authored-by: mik-laj <12058428+mik-laj@users.noreply.github.com>

--- homeassistant/components/wled/sensor.py
+++ homeassistant/components/wled/sensor.py
@@ -68,7 +68,7 @@ SENSORS: tuple[WLEDSensorEntityDescription, ...] = (
     WLEDSensorEntityDescription(
         key="uptime",
         translation_key="uptime",
-        device_class=SensorDeviceClass.TIMESTAMP,
+        device_class=SensorDeviceClass.UPTIME,
         entity_category=EntityCategory.DIAGNOSTIC,
         entity_registry_enabled_default=False,
         value_fn=lambda device: utcnow() - device.info.uptime,
