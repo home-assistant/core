943c0eb8c3b38710cf251ecdaea509619b1f040c
G Johansson <goran.johansson@shiftit.se>
2026-05-04 19:34:17 +0200

    Fix config flow validation in Nord Pool (#169751)

--- homeassistant/components/nordpool/config_flow.py
+++ homeassistant/components/nordpool/config_flow.py
@@ -56,6 +56,8 @@ DATA_SCHEMA = vol.Schema(
 
 async def test_api(hass: HomeAssistant, user_input: dict[str, Any]) -> dict[str, str]:
     """Test fetch data from Nord Pool."""
+    if not user_input.get(CONF_AREAS):
+        return {CONF_AREAS: "no_areas"}
     client = NordPoolClient(async_get_clientsession(hass))
     try:
         await client.async_get_delivery_period(
