ea6036b5b05569fca63acda40652ded9c6e89136
Admin <211177151+kw6423@users.noreply.github.com>
2026-05-04 16:25:54 +0000

    Restore OwnTracks custom device tracker attributes

--- homeassistant/components/owntracks/const.py
+++ homeassistant/components/owntracks/const.py
@@ -1,4 +1,12 @@
 """Constants for OwnTracks."""
 
-DOMAIN = "owntracks"
-ATTR_UPDATE_TIMESTAMP = "update_timestamp"
+from typing import Final
+
+ATTR_ADDRESS: Final = "address"
+ATTR_BATTERY_STATUS: Final = "battery_status"
+ATTR_COURSE: Final = "course"
+ATTR_TID: Final = "tid"
+ATTR_UPDATE_TIMESTAMP: Final = "update_timestamp"
+ATTR_VELOCITY: Final = "velocity"
+
+DOMAIN: Final = "owntracks"
