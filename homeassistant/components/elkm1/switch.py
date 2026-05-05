46558261663bce2b86d5733d67812abba99e5b4e
Glenn Waters <glenn@watrs.ca>
2026-05-04 20:47:43 -0400

    Split off turn_on_for action

--- homeassistant/components/elkm1/switch.py
+++ homeassistant/components/elkm1/switch.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-from datetime import timedelta
 from typing import Any
 
 from elkm1_lib.const import ThermostatMode, ThermostatSetting
@@ -10,28 +9,14 @@ from elkm1_lib.elements import Element
 from elkm1_lib.elk import Elk
 from elkm1_lib.outputs import Output
 from elkm1_lib.thermostats import Thermostat
-import voluptuous as vol
 
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers import config_validation as cv, entity_platform
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
-from homeassistant.helpers.typing import VolDictType
 
 from . import ElkM1ConfigEntry
-from .const import ATTR_DURATION
 from .entity import ElkAttachedEntity, ElkEntity, create_elk_entities
 from .models import ELKM1Data
-
-SERVICE_SWITCH_OUTPUT_TURN_ON_FOR = "switch_output_turn_on_for"
-
-ELK_OUTPUT_TURN_ON_FOR_SERVICE_SCHEMA: VolDictType = {
-    vol.Required(ATTR_DURATION): vol.All(
-        cv.time_period,
-        vol.Range(min=timedelta(seconds=0), max=timedelta(seconds=65535)),
-    ),
-}
 
 
 async def async_setup_entry(
@@ -48,14 +33,6 @@ async def async_setup_entry(
         elk_data, elk.thermostats, "thermostat", ElkThermostatEMHeat, entities
     )
     async_add_entities(entities)
-
-    platform = entity_platform.async_get_current_platform()
-
-    platform.async_register_entity_service(
-        SERVICE_SWITCH_OUTPUT_TURN_ON_FOR,
-        ELK_OUTPUT_TURN_ON_FOR_SERVICE_SCHEMA,
-        "async_switch_output_turn_on_for",
-    )
 
 
 class ElkOutput(ElkAttachedEntity, SwitchEntity):
@@ -75,10 +52,6 @@ class ElkOutput(ElkAttachedEntity, SwitchEntity):
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off the output."""
         self._element.turn_off()
-
-    async def async_switch_output_turn_on_for(self, duration: timedelta) -> None:
-        """Turn on an output for specified length of time."""
-        self._element.turn_on(int(duration.total_seconds()))
 
 
 class ElkThermostatEMHeat(ElkEntity, SwitchEntity):
@@ -108,9 +81,3 @@ class ElkThermostatEMHeat(ElkEntity, SwitchEntity):
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off the output."""
         self._elk_set(ThermostatMode.EMERGENCY_HEAT)
-
-    async def async_switch_output_turn_on_for(
-        self, duration: int | None = None
-    ) -> None:
-        """Turn on an output for specified length of time: not supported for thermostat."""
-        raise HomeAssistantError("supported only on ElkM1 output switch entities")
