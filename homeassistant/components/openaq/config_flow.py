7de9b1fac751158359ef2e0f3c09fbea571cb6fd
Jeff Stein <jeffstein@gmail.com>
2026-05-05 10:55:41 -0600

    Remove future annotations from OpenAQ

--- homeassistant/components/openaq/config_flow.py
+++ homeassistant/components/openaq/config_flow.py
@@ -1,7 +1,5 @@
 """Config flow for the OpenAQ integration."""
 
-from __future__ import annotations
-
 from dataclasses import dataclass
 import logging
 from math import inf
--- homeassistant/components/openaq/coordinator.py
+++ homeassistant/components/openaq/coordinator.py
@@ -1,7 +1,5 @@
 """Data coordinator for the OpenAQ integration."""
 
-from __future__ import annotations
-
 from collections.abc import Mapping, Sequence
 from dataclasses import dataclass
 from datetime import timedelta
