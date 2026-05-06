75c6f4b2010f19fb04acc21efabd99250750b8d0
epenet <6771947+epenet@users.noreply.github.com>
2026-05-06 06:01:50 +0000

    Cleanup unused code in Tuya util

--- homeassistant/components/tuya/util.py
+++ homeassistant/components/tuya/util.py
@@ -7,27 +7,6 @@ from homeassistant.exceptions import ServiceValidationError
 from .const import DOMAIN, DPCode
 
 
-def get_dpcode(
-    device: CustomerDevice, dpcodes: str | tuple[str, ...] | None
-) -> str | None:
-    """Get the first matching DPCode from the device or return None."""
-    if dpcodes is None:
-        return None
-
-    if not isinstance(dpcodes, tuple):
-        dpcodes = (dpcodes,)
-
-    for dpcode in dpcodes:
-        if (
-            dpcode in device.function
-            or dpcode in device.status
-            or dpcode in device.status_range
-        ):
-            return dpcode
-
-    return None
-
-
 class ActionDPCodeNotFoundError(ServiceValidationError):
     """Custom exception for action DP code not found errors."""
 
