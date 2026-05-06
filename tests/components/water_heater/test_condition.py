d09f050a87b0ef05a5074f57d6077af5f348ea6f
Erik Montnemery <erik@montnemery.com>
2026-05-06 14:44:19 +0200

    Exclude incompatible water_heater entities from water_heater automations (#169904)

--- tests/components/water_heater/test_condition.py
+++ tests/components/water_heater/test_condition.py
@@ -228,6 +228,7 @@ async def test_water_heater_state_condition_behavior_all(
             "eco",
             ATTR_TEMPERATURE,
             threshold_unit=UnitOfTemperature.CELSIUS,
+            attribute_required=True,
         ),
     ],
 )
@@ -267,6 +268,7 @@ async def test_water_heater_numerical_condition_behavior_any(
             "eco",
             ATTR_TEMPERATURE,
             threshold_unit=UnitOfTemperature.CELSIUS,
+            attribute_required=True,
         ),
     ],
 )
