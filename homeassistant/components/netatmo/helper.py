f93a9b6e2a78b160934841e1daac014e1d02358d
Erik Montnemery <erik@montnemery.com>
2026-05-06 08:46:53 +0200

    Update homeassistant/components/netatmo/helper.py

--- homeassistant/components/netatmo/helper.py
+++ homeassistant/components/netatmo/helper.py
@@ -6,14 +6,6 @@ from uuid import UUID, uuid4
 from pyatmo.modules.device_types import DeviceType as NetatmoDeviceType
 
 
-# The stringified DeviceType members historically in pyatmo were rendered as 'DeviceType.<name>'.
-# This rendering has been changed (StrEnum), this below helper does that manually to avoid breaking
-# unique IDs for existing installations
-def device_type_to_str(device_type: NetatmoDeviceType) -> str:
-    """Convert a device type to a string."""
-# The stringified DeviceType members historically in pyatmo were rendered as 'DeviceType.<name>'.
-# This rendering has been changed (StrEnum), this below helper does that manually to avoid breaking
-# unique IDs for existing installations
 def device_type_to_str(device_type: NetatmoDeviceType) -> str:
     """Convert a device type to a string.
 
