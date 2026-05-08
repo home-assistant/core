298ff459e2b7f964f1f522b3a664efdef58ac6e5
Jeff Stein <jeffstein@gmail.com>
2026-05-08 16:01:19 -0600

    Address OpenAQ review feedback

--- homeassistant/components/openaq/sensor.py
+++ homeassistant/components/openaq/sensor.py
@@ -14,14 +14,9 @@ from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from homeassistant.helpers.typing import StateType
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from homeassistant.util.unit_conversion import DistanceConverter
 
 from .const import ATTRIBUTION, DOMAIN
-from .coordinator import (
-    OpenAQConfigEntry,
-    OpenAQDataUpdateCoordinator,
-    OpenAQLocationData,
-)
+from .coordinator import OpenAQConfigEntry, OpenAQDataUpdateCoordinator
 
 DISTANCE_FROM_HOME = "distance_from_home"
 
@@ -34,63 +29,54 @@ class OpenAQSensorEntityDescription(SensorEntityDescription):
 SENSOR_DESCRIPTIONS: dict[str, OpenAQSensorEntityDescription] = {
     "pm1": OpenAQSensorEntityDescription(
         key="pm1",
-        translation_key="pm1",
         device_class=SensorDeviceClass.PM1,
         state_class=SensorStateClass.MEASUREMENT,
         suggested_display_precision=1,
     ),
     "pm25": OpenAQSensorEntityDescription(
         key="pm25",
-        translation_key="pm25",
         device_class=SensorDeviceClass.PM25,
         state_class=SensorStateClass.MEASUREMENT,
         suggested_display_precision=1,
     ),
     "pm10": OpenAQSensorEntityDescription(
         key="pm10",
-        translation_key="pm10",
         device_class=SensorDeviceClass.PM10,
         state_class=SensorStateClass.MEASUREMENT,
         suggested_display_precision=1,
     ),
     "co": OpenAQSensorEntityDescription(
         key="co",
-        translation_key="co",
         device_class=SensorDeviceClass.CO,
         state_class=SensorStateClass.MEASUREMENT,
         suggested_display_precision=2,
     ),
     "co2": OpenAQSensorEntityDescription(
         key="co2",
-        translation_key="co2",
         device_class=SensorDeviceClass.CO2,
         state_class=SensorStateClass.MEASUREMENT,
         suggested_display_precision=1,
     ),
     "no2": OpenAQSensorEntityDescription(
         key="no2",
-        translation_key="no2",
         device_class=SensorDeviceClass.NITROGEN_DIOXIDE,
         state_class=SensorStateClass.MEASUREMENT,
         suggested_display_precision=1,
     ),
     "o3": OpenAQSensorEntityDescription(
         key="o3",
-        translation_key="o3",
         device_class=SensorDeviceClass.OZONE,
         state_class=SensorStateClass.MEASUREMENT,
         suggested_display_precision=1,
     ),
     "so2": OpenAQSensorEntityDescription(
         key="so2",
-        translation_key="so2",
         device_class=SensorDeviceClass.SULPHUR_DIOXIDE,
         state_class=SensorStateClass.MEASUREMENT,
         suggested_display_precision=1,
     ),
     "no": OpenAQSensorEntityDescription(
         key="no",
-        translation_key="no",
         device_class=SensorDeviceClass.NITROGEN_MONOXIDE,
         state_class=SensorStateClass.MEASUREMENT,
         suggested_display_precision=1,
@@ -109,18 +95,6 @@ SENSOR_DESCRIPTIONS: dict[str, OpenAQSensorEntityDescription] = {
     ),
 }
 
-DISTANCE_SENSOR_DESCRIPTION = OpenAQSensorEntityDescription(
-    key=DISTANCE_FROM_HOME,
-    translation_key=DISTANCE_FROM_HOME,
-    name="Distance from Home Assistant",
-    device_class=SensorDeviceClass.DISTANCE,
-    entity_category=EntityCategory.DIAGNOSTIC,
-    entity_registry_enabled_default=False,
-    native_unit_of_measurement=UnitOfLength.KILOMETERS,
-    state_class=SensorStateClass.MEASUREMENT,
-    suggested_display_precision=1,
-)
-
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -135,9 +109,18 @@ async def async_setup_entry(
                 for parameter in coordinator.data.measurements
                 if parameter in SENSOR_DESCRIPTIONS
             ]
-            + [OpenAQSensor(coordinator, DISTANCE_SENSOR_DESCRIPTION)],
+            + [OpenAQDistanceSensor(coordinator)],
             config_subentry_id=subentry_id,
         )
+
+
+def _device_info(coordinator: OpenAQDataUpdateCoordinator) -> DeviceInfo:
+    """Return device info for an OpenAQ location."""
+    return DeviceInfo(
+        identifiers={(DOMAIN, str(coordinator.location_id))},
+        name=coordinator.data.name,
+        entry_type=DeviceEntryType.SERVICE,
+    )
 
 
 class OpenAQSensor(CoordinatorEntity[OpenAQDataUpdateCoordinator], SensorEntity):
@@ -156,25 +139,11 @@ class OpenAQSensor(CoordinatorEntity[OpenAQDataUpdateCoordinator], SensorEntity)
         super().__init__(coordinator)
         self.entity_description = entity_description
         self._attr_unique_id = f"{coordinator.location_id}_{entity_description.key}"
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, str(coordinator.location_id))},
-            name=coordinator.data.name,
-            entry_type=DeviceEntryType.SERVICE,
-        )
+        self._attr_device_info = _device_info(coordinator)
 
     @property
     def native_value(self) -> StateType:
         """Return the sensor value."""
-        if self.entity_description.key == DISTANCE_FROM_HOME:
-            distance_to_home = self.coordinator.data.distance_to_home
-            if distance_to_home is None:
-                return None
-            return DistanceConverter.convert(
-                distance_to_home,
-                UnitOfLength.METERS,
-                self.hass.config.units.length_unit,
-            )
-
         measurement = self.coordinator.data.measurements.get(
             self.entity_description.key
         )
@@ -183,9 +152,6 @@ class OpenAQSensor(CoordinatorEntity[OpenAQDataUpdateCoordinator], SensorEntity)
     @property
     def native_unit_of_measurement(self) -> str | None:
         """Return the native unit of measurement."""
-        if self.entity_description.key == DISTANCE_FROM_HOME:
-            return self.hass.config.units.length_unit
-
         measurement = self.coordinator.data.measurements.get(
             self.entity_description.key
         )
@@ -194,4 +160,25 @@ class OpenAQSensor(CoordinatorEntity[OpenAQDataUpdateCoordinator], SensorEntity)
         return measurement.unit
 
 
-__all__ = ["SENSOR_DESCRIPTIONS", "OpenAQLocationData", "OpenAQSensor"]
+class OpenAQDistanceSensor(SensorEntity):
+    """Representation of a static OpenAQ distance sensor."""
+
+    _attr_attribution = ATTRIBUTION
+    _attr_device_class = SensorDeviceClass.DISTANCE
+    _attr_entity_category = EntityCategory.DIAGNOSTIC
+    _attr_entity_registry_enabled_default = False
+    _attr_has_entity_name = True
+    _attr_name = "Distance from Home Assistant"
+    _attr_native_unit_of_measurement = UnitOfLength.KILOMETERS
+    _attr_should_poll = False
+    _attr_suggested_display_precision = 1
+    _attr_translation_key = DISTANCE_FROM_HOME
+
+    def __init__(self, coordinator: OpenAQDataUpdateCoordinator) -> None:
+        """Initialize the sensor."""
+        self._attr_unique_id = f"{coordinator.location_id}_{DISTANCE_FROM_HOME}"
+        self._attr_device_info = _device_info(coordinator)
+        distance_to_home = coordinator.data.distance_to_home
+        self._attr_native_value = (
+            None if distance_to_home is None else distance_to_home / 1000
+        )
