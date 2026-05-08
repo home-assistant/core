744d26a227c2fd252fe3968f6b29b2c74c0c419a
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2026-05-08 18:34:52 +0000

    Start flow for first topic subentry in ntfy integration

--- homeassistant/components/ntfy/config_flow.py
+++ homeassistant/components/ntfy/config_flow.py
@@ -18,10 +18,13 @@ from yarl import URL
 
 from homeassistant import data_entry_flow
 from homeassistant.config_entries import (
+    SOURCE_USER,
     ConfigEntry,
     ConfigFlow,
     ConfigFlowResult,
     ConfigSubentryFlow,
+    FlowType,
+    SubentryFlowContext,
     SubentryFlowResult,
 )
 from homeassistant.const import (
@@ -54,6 +57,7 @@ from .const import (
     DOMAIN,
     SECTION_AUTH,
     SECTION_FILTER,
+    SUBENTRY_TYPE_TOPIC,
 )
 
 _LOGGER = logging.getLogger(__name__)
@@ -164,7 +168,7 @@ class NtfyConfigFlow(ConfigFlow, domain=DOMAIN):
         cls, config_entry: ConfigEntry
     ) -> dict[str, type[ConfigSubentryFlow]]:
         """Return subentries supported by this integration."""
-        return {"topic": TopicSubentryFlowHandler}
+        return {SUBENTRY_TYPE_TOPIC: TopicSubentryFlowHandler}
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -228,6 +232,18 @@ class NtfyConfigFlow(ConfigFlow, domain=DOMAIN):
             ),
             errors=errors,
         )
+
+    async def async_on_create_entry(self, result: ConfigFlowResult) -> ConfigFlowResult:
+        """Start subentry flow after creating main entry."""
+        subentry_result = await self.hass.config_entries.subentries.async_init(
+            (result["result"].entry_id, SUBENTRY_TYPE_TOPIC),
+            context=SubentryFlowContext(source=SOURCE_USER),
+        )
+        result["next_flow"] = (
+            FlowType.CONFIG_SUBENTRIES_FLOW,
+            subentry_result["flow_id"],
+        )
+        return result
 
     async def async_step_reauth(
         self, entry_data: Mapping[str, Any]
