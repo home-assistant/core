9abd5f484041bcafc874bac80e47f2786460e8bd
justin <justin@boyd.net.au>
2026-05-09 14:30:43 +0000

    trying a two stage approach

--- homeassistant/components/airtouch5/__init__.py
+++ homeassistant/components/airtouch5/__init__.py
@@ -135,13 +135,11 @@ async def async_migrate_entry(hass: HomeAssistant, entry: Airtouch5ConfigEntry) 
                 entity.unique_id,
                 new_unique_id,
             )
-            await hass.config_entries.async_reload(entry.entry_id)
-            hass.config_entries.async_update_entry(
-                entry,
-                unique_id=str(airtouch_device.system_id),
-                data=new_data,
-                minor_version=2,
-            )
+        hass.config_entries.async_update_entry(
+            entry,
+            data=new_data,
+            minor_version=2,
+        )
     # device_registry = dr.async_get(hass)
     # update_device_id(airtouch_device, device_registry)
 
