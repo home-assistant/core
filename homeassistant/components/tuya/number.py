08a73d1bd3884d6be284252c65a142a514c92f56
epenet <6771947+epenet@users.noreply.github.com>
2026-05-11 15:40:15 +0000

    Prioritize native Tuya unit of measurement

--- homeassistant/components/tuya/number.py
+++ homeassistant/components/tuya/number.py
@@ -529,41 +529,42 @@ class TuyaNumberEntity(TuyaEntity, NumberEntity):
         # Logic to ensure the set device class and API received Unit Of Measurement
         # match Home Assistants requirements.
         if (
-            self.device_class is not None
-            and not self.device_class.startswith(DOMAIN)
-            and self.entity_description.native_unit_of_measurement is None
+            (device_class := self.device_class) is None
+            or device_class.startswith(DOMAIN)
             # we do not need to check mappings if the API UOM is allowed
-            and self.native_unit_of_measurement
-            not in NUMBER_DEVICE_CLASS_UNITS[self.device_class]
+            or self.native_unit_of_measurement
+            in NUMBER_DEVICE_CLASS_UNITS[device_class]
         ):
-            # We cannot have a device class, if the UOM isn't set or the
-            # device class cannot be found in the validation mapping.
-            if (
-                self.native_unit_of_measurement is None
-                or self.device_class not in DEVICE_CLASS_UNITS
-            ):
-                LOGGER.debug(
-                    "Device class %s ignored for incompatible unit %s in number entity %s",
-                    self.device_class,
-                    self.native_unit_of_measurement,
-                    self.unique_id,
-                )
-                self._attr_device_class = None
-                return
+            return
 
-            uoms = DEVICE_CLASS_UNITS[self.device_class]
-            uom = uoms.get(self.native_unit_of_measurement) or uoms.get(
-                self.native_unit_of_measurement.lower()
+        # We cannot have a device class, if the UOM isn't set or the
+        # device class cannot be found in the validation mapping.
+        if (
+            self.native_unit_of_measurement is None
+            or device_class not in DEVICE_CLASS_UNITS
+        ):
+            LOGGER.debug(
+                "Device class %s ignored for incompatible unit %s in number entity %s",
+                device_class,
+                self.native_unit_of_measurement,
+                self.unique_id,
             )
+            self._attr_device_class = None
+            return
 
-            # Unknown unit of measurement, device class should not be used.
-            if uom is None:
-                self._attr_device_class = None
-                return
+        uoms = DEVICE_CLASS_UNITS[device_class]
+        uom = uoms.get(self.native_unit_of_measurement) or uoms.get(
+            self.native_unit_of_measurement.lower()
+        )
 
-            # Found unit of measurement, use the standardized Unit
-            # Use the target conversion unit (if set)
-            self._attr_native_unit_of_measurement = uom.unit
+        # Unknown unit of measurement, device class should not be used.
+        if uom is None:
+            self._attr_device_class = None
+            return
+
+        # Found unit of measurement, use the standardized Unit
+        # Use the target conversion unit (if set)
+        self._attr_native_unit_of_measurement = uom.unit
 
     @property
     def native_value(self) -> float | None:
--- homeassistant/components/tuya/sensor.py
+++ homeassistant/components/tuya/sensor.py
@@ -1730,44 +1730,44 @@ class TuyaSensorEntity(TuyaEntity, SensorEntity):
         # Logic to ensure the set device class and API received Unit Of Measurement
         # match Home Assistants requirements.
         if (
-            self.device_class is not None
-            and self.device_class != SensorDeviceClass.ENUM
-            and not self.device_class.startswith(DOMAIN)
-            and self.entity_description.native_unit_of_measurement is None
+            (device_class := self.device_class) is None
+            or device_class is SensorDeviceClass.ENUM
+            or device_class.startswith(DOMAIN)
             # we do not need to check mappings if the API UOM is allowed
-            and self.native_unit_of_measurement
-            not in SENSOR_DEVICE_CLASS_UNITS[self.device_class]
+            or self.native_unit_of_measurement
+            in SENSOR_DEVICE_CLASS_UNITS[device_class]
         ):
-            # We cannot have a device class, if the UOM isn't set or the
-            # device class cannot be found in the validation mapping.
-            if (
-                self.native_unit_of_measurement is None
-                or self.device_class not in DEVICE_CLASS_UNITS
-            ):
-                LOGGER.debug(
-                    "Device class %s ignored for incompatible unit %s in sensor entity %s",
-                    self.device_class,
-                    self.native_unit_of_measurement,
-                    self.unique_id,
-                )
-                self._attr_device_class = None
-                self._attr_suggested_unit_of_measurement = None
-                return
-
-            uoms = DEVICE_CLASS_UNITS[self.device_class]
-            uom = uoms.get(self.native_unit_of_measurement) or uoms.get(
-                self.native_unit_of_measurement.lower()
+            return
+        # We cannot have a device class, if the UOM isn't set or the
+        # device class cannot be found in the validation mapping.
+        if (
+            self.native_unit_of_measurement is None
+            or device_class not in DEVICE_CLASS_UNITS
+        ):
+            LOGGER.debug(
+                "Device class %s ignored for incompatible unit %s in sensor entity %s",
+                device_class,
+                self.native_unit_of_measurement,
+                self.unique_id,
             )
+            self._attr_device_class = None
+            self._attr_suggested_unit_of_measurement = None
+            return
 
-            # Unknown unit of measurement, device class should not be used.
-            if uom is None:
-                self._attr_device_class = None
-                self._attr_suggested_unit_of_measurement = None
-                return
+        uoms = DEVICE_CLASS_UNITS[device_class]
+        uom = uoms.get(self.native_unit_of_measurement) or uoms.get(
+            self.native_unit_of_measurement.lower()
+        )
 
-            # Found unit of measurement, use the standardized Unit
-            # Use the target conversion unit (if set)
-            self._attr_native_unit_of_measurement = uom.unit
+        # Unknown unit of measurement, device class should not be used.
+        if uom is None:
+            self._attr_device_class = None
+            self._attr_suggested_unit_of_measurement = None
+            return
+
+        # Found unit of measurement, use the standardized Unit
+        # Use the target conversion unit (if set)
+        self._attr_native_unit_of_measurement = uom.unit
 
     @property
     def native_value(self) -> StateType:
