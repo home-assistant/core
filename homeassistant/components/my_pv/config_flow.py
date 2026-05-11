fe5c53707c7b0b7ae835fe5895578f85ab7f4182
Rogier van Staveren <rogier@batoid.com>
2026-05-11 14:27:44 +0200

    Refactor config flow

--- homeassistant/components/my_pv/config_flow.py
+++ homeassistant/components/my_pv/config_flow.py
@@ -74,26 +74,15 @@ class MyPVConfigFlow(ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle discovery confirmation."""
-        errors: dict[str, str] = {}
-
-        password = None
         password_needed = False
-        password_valid = False
-        if user_input and (password := user_input.get(CONF_PASSWORD)):
-            password_needed = True
 
         # Check if we can connect to the device
-        device = await MyPVLocalDevice(self._host, password)
+        device = await MyPVLocalDevice(self._host)
         try:
             if not await device.connect():
                 return self.async_abort(reason="cannot_connect")
-            if password_needed:
-                password_valid = True
         except MyPVAuthenticationError:
-            if password_needed:
-                errors[CONF_PASSWORD] = "invalid_password"
             password_needed = True
-            password_valid = False
         finally:
             await device.disconnect()
 
@@ -101,20 +90,19 @@ class MyPVConfigFlow(ConfigFlow, domain=DOMAIN):
         # Update host ip address when device is already configured and abort.
         self._abort_if_unique_id_configured(updates={CONF_HOST: self._host})
 
+        if password_needed:
+            self._device_model = device.model
+            return await self.async_step_discovery_auth()
+
         title = f"my-PV {device.model}"
 
-        if user_input is None:
-            _LOGGER.debug("my-PV on %s is not yet configured", self._host)
-        elif not password_needed or password_valid:
+        if user_input is not None:
             data = {
                 CONF_HOST: self._host,
             }
-
-            if password:
-                data[CONF_PASSWORD] = password
-
             return self.async_create_entry(title=title, data=data)
 
+        _LOGGER.debug("my-PV on %s is not yet configured", self._host)
         self.context.update(
             {
                 "title_placeholders": {
@@ -123,14 +111,57 @@ class MyPVConfigFlow(ConfigFlow, domain=DOMAIN):
             }
         )
 
-        data_schema = None
-        if password_needed:
-            data_schema = AUTH_SCHEMA
-        else:
-            self._set_confirm_only()
-
+        self._set_confirm_only()
         return self.async_show_form(
             step_id="discovery_confirm",
+            description_placeholders=self.context["title_placeholders"],
+        )
+
+    async def async_step_discovery_auth(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle discovery password authentication."""
+        errors: dict[str, str] = {}
+        data_schema = AUTH_SCHEMA
+
+        if user_input is not None:
+            host = self._host
+            password = user_input.get(CONF_PASSWORD)
+
+            # Check if we can connect to the device
+            device = await MyPVLocalDevice(host, password)
+            try:
+                if not await device.connect():
+                    errors[CONF_BASE] = "cannot_connect"
+            except MyPVAuthenticationError:
+                errors[CONF_PASSWORD] = "invalid_password"
+            finally:
+                await device.disconnect()
+
+            if not errors:
+                await self.async_set_unique_id(device.serial_number)
+                self._abort_if_unique_id_configured()
+
+                title = f"my-PV {device.model}"
+                data = {
+                    CONF_HOST: host,
+                    CONF_PASSWORD: password,
+                }
+                return self.async_create_entry(title=title, data=data)
+
+            # Combine user input with schema.
+            data_schema = self.add_suggested_values_to_schema(data_schema, user_input)
+
+        self.context.update(
+            {
+                "title_placeholders": {
+                    "name": f"my-PV {self._device_model}",
+                }
+            }
+        )
+
+        return self.async_show_form(
+            step_id="discovery_auth",
             data_schema=data_schema,
             errors=errors,
             description_placeholders=self.context["title_placeholders"],
