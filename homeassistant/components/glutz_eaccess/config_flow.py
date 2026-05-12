2ec0621396758089f868e931b38460028c981338
miitchpls <mitchgrek@gmail.com>
2026-05-12 12:26:27 +0200

    Fix auth errors swallowed during invitation confirmation

--- homeassistant/components/glutz_eaccess/config_flow.py
+++ homeassistant/components/glutz_eaccess/config_flow.py
@@ -1,7 +1,6 @@
 """Config flow for the Glutz eAccess integration."""
 
 from collections.abc import Mapping
-import logging
 from typing import TYPE_CHECKING, Any
 from urllib.parse import urlparse
 
@@ -24,18 +23,7 @@ from .const import DOMAIN
 if TYPE_CHECKING:
     from homeassistant.config_entries import ConfigFlowResult
 
-_LOGGER = logging.getLogger(__name__)
-
 DEFAULT_TITLE = "Glutz eAccess"
-
-
-async def _resolve_system_info(api: GlutzAPI) -> dict[str, str]:
-    """Fetch system info, returning an empty dict on transient errors."""
-    try:
-        return await api.get_system_info()
-    except (GlutzAuthError, GlutzConnectionError) as err:
-        _LOGGER.warning("Could not fetch system info: %s", err)
-        return {}
 
 
 def _is_valid_password(pwd: str) -> bool:
@@ -203,21 +191,28 @@ class GlutzConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     api = GlutzAPI(
                         async_get_clientsession(self.hass), full_host, email, password
                     )
-                    info = await _resolve_system_info(api)
-                    system_id = info.get("id") or self._invitation.get("system_id")
-                    if not system_id:
+                    try:
+                        await api.get_access_points()
+                        info = await api.get_system_info()
+                    except GlutzAuthError:
+                        errors["base"] = "invalid_auth"
+                    except GlutzConnectionError:
                         errors["base"] = "cannot_connect"
                     else:
-                        await self.async_set_unique_id(system_id)
-                        self._abort_if_unique_id_configured()
-                        return self.async_create_entry(
-                            title=info.get("name") or DEFAULT_TITLE,
-                            data={
-                                CONF_HOST: full_host,
-                                CONF_USERNAME: email,
-                                CONF_PASSWORD: password,
-                            },
-                        )
+                        system_id = info.get("id") or self._invitation.get("system_id")
+                        if not system_id:
+                            errors["base"] = "cannot_connect"
+                        else:
+                            await self.async_set_unique_id(system_id)
+                            self._abort_if_unique_id_configured()
+                            return self.async_create_entry(
+                                title=info.get("name") or DEFAULT_TITLE,
+                                data={
+                                    CONF_HOST: full_host,
+                                    CONF_USERNAME: email,
+                                    CONF_PASSWORD: password,
+                                },
+                            )
 
         return self.async_show_form(
             step_id="invitation_confirm",
