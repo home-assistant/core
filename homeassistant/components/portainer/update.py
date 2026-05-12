452c0c0590f1e720c241cdbde0710e5cd3d27598
Erwin Douna <e.douna@gmail.com>
2026-05-12 18:53:45 +0200

    Remove import

--- homeassistant/components/portainer/update.py
+++ homeassistant/components/portainer/update.py
@@ -1,7 +1,5 @@
 """Support for Portainer container updates."""
 
-from __future__ import annotations
-
 from collections.abc import Awaitable, Callable
 from dataclasses import dataclass
 from datetime import timedelta
