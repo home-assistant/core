4081936989a31bc239ec9c3d9c205d4b3b799ab4
Jeff Stein <jeffstein@gmail.com>
2026-05-13 12:09:10 -0600

    Simplify OpenAQ location search choices

--- homeassistant/components/openaq/const.py
+++ homeassistant/components/openaq/const.py
@@ -8,11 +8,9 @@ DOMAIN = "openaq"
 LOGGER = logging.getLogger(__package__)
 
 ATTRIBUTION: Final = "Data provided by OpenAQ"
-CONF_LIMIT: Final = "limit"
 CONF_LOCATION_ID: Final = "location_id"
 CONF_RADIUS: Final = "radius"
 
-DEFAULT_LOCATION_LIMIT: Final = 25
 DEFAULT_RADIUS: Final = 10000
 MAX_RADIUS: Final = 25000
 SUBENTRY_TYPE_LOCATION: Final = "location"
