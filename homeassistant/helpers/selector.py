b926e1ac3789490a3887199cf21e54d85227c53e
G Johansson <goran.johansson@shiftit.se>
2026-05-13 16:42:47 +0200

    Fix ObjectSelector when using other selectors (#170453)

--- homeassistant/helpers/selector.py
+++ homeassistant/helpers/selector.py
@@ -1698,7 +1698,11 @@ class ObjectSelector(Selector[ObjectSelectorConfig]):
                 if field_data.get("required") and field not in _config:
                     raise vol.Invalid(f"Field {field} is required")
                 if field in _config:
-                    selector(field_data["selector"])(_config[field])  # type: ignore[operator]
+                    field_selector = field_data["selector"]
+                    if isinstance(field_selector, Selector):
+                        field_selector(_config[field])  # type: ignore[operator]
+                    else:
+                        selector(field_selector)(_config[field])  # type: ignore[operator]
 
             for key in _config:
                 if key not in self.config["fields"]:
