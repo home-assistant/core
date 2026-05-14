6aa0f01e7a626bf06e0638d94dac87ca90e6ca5f
Paulus Schoutsen <balloob@gmail.com>
2026-05-14 17:02:25 -0400

    Apply suggestion from @balloob

--- homeassistant/components/lutron_caseta/light.py
+++ homeassistant/components/lutron_caseta/light.py
@@ -82,8 +82,9 @@ async def async_setup_entry(
 
 
 class LutronCasetaLight(LutronCasetaUpdatableEntity, LightEntity):
-    """Representation of a Lutron light, including dimmable, white tune,
-    and spectrum/color tune.
+    """Representation of a Lutron light.
+
+    Including dimmable, white tune, and spectrum/color tune.
     """
 
     _attr_supported_features = LightEntityFeature.TRANSITION
