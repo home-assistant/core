8528f9f80847b857a294a74a081361b114927998
Crocmagnon <gabriel@augendre.info>
2026-05-14 13:01:24 +0200

    data grand lyon: add binary sensor for station status (#170522)

--- homeassistant/components/data_grand_lyon/__init__.py
+++ homeassistant/components/data_grand_lyon/__init__.py
@@ -8,7 +8,7 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .coordinator import DataGrandLyonConfigEntry, DataGrandLyonCoordinator
 
-PLATFORMS: list[Platform] = [Platform.SENSOR]
+PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.SENSOR]
 
 
 async def async_setup_entry(
