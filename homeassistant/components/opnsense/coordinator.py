4a53f3beb1d7a081a5ea83907404fba019de0e1a
Kevin McCormack <kevin@mccormack.tech>
2026-05-15 02:58:16 +0000

    Fix lint

--- homeassistant/components/opnsense/coordinator.py
+++ homeassistant/components/opnsense/coordinator.py
@@ -49,6 +49,7 @@ class OPNsenseDeviceTrackerCoordinator(DataUpdateCoordinator[DeviceDetailsByMAC]
         self.client = client
         self.interfaces = interfaces
         self.tracked_devices: dict[str, OPNsenseDeviceTrackerEntity] = {}
+
     def _get_mac_addrs(self, devices: list[DeviceDetails]) -> DeviceDetailsByMAC:
         """Create dict with mac address keys from list of devices."""
         out_devices: DeviceDetailsByMAC = {}
