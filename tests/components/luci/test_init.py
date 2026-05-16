a499c2814c9f193cb9e07dd224974bc9f2282fe9
kristbaum <max@kristenonline.de>
2026-05-16 14:54:09 +0000

    Move to init.py

---
+++ tests/components/luci/test_init.py
@@ -0,0 +1,21 @@
+"""Tests for the luci integration."""
+
+import pytest
+
+from homeassistant.core import HomeAssistant
+
+from tests.common import MockConfigEntry
+
+
+@pytest.mark.usefixtures("mock_luci_client")
+async def test_unload_entry(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test unloading a config entry."""
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert await hass.config_entries.async_unload(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
