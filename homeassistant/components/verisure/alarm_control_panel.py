d2f8529519a740a44c8324e996a4ab74737a4002
Franck Nijhof <git@frenck.dev>
2026-05-17 20:05:56 +0000

    Fix Verisure alarm crash when cloud rejects arm/disarm command

--- homeassistant/components/verisure/alarm_control_panel.py
+++ homeassistant/components/verisure/alarm_control_panel.py
@@ -9,6 +9,7 @@ from homeassistant.components.alarm_control_panel import (
     CodeFormat,
 )
 from homeassistant.core import HomeAssistant, callback
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
@@ -63,6 +64,11 @@ class VerisureAlarm(
             self.coordinator.verisure.request, command_data
         )
         LOGGER.debug("Verisure set arm state %s", state)
+        if arm_state is None or "data" not in arm_state:
+            raise HomeAssistantError(
+                translation_domain=DOMAIN,
+                translation_key="arm_state_failed",
+            )
         result = None
         attempts = 0
         while result is None:
