16bd21ebe794486ba16d554f8814b01ea93aaacc
Taraman17 <mozilla@adrario.de>
2026-05-17 16:33:46 +0000

    drop unrelated changes in test_button

--- tests/components/homee/test_button.py
+++ tests/components/homee/test_button.py
@@ -23,22 +23,15 @@ async def platforms() -> AsyncGenerator[None]:
         yield
 
 
-async def setup_mock_button(
-    hass: HomeAssistant, mock_config_entry: MockConfigEntry, mock_homee: MagicMock
-) -> None:
-    """Setups a node with buttons for the tests."""
-    mock_homee.nodes = [build_mock_node("buttons.json")]
-    mock_homee.get_node_by_id.return_value = mock_homee.nodes[0]
-    await setup_integration(hass, mock_config_entry)
-
-
 async def test_button_press(
     hass: HomeAssistant,
     mock_homee: MagicMock,
     mock_config_entry: MockConfigEntry,
 ) -> None:
     """Test press button service."""
-    await setup_mock_button(hass, mock_config_entry, mock_homee)
+    mock_homee.nodes = [build_mock_node("buttons.json")]
+    mock_homee.get_node_by_id.return_value = mock_homee.nodes[0]
+    await setup_integration(hass, mock_config_entry)
 
     await hass.services.async_call(
         BUTTON_DOMAIN,
@@ -58,6 +51,8 @@ async def test_button_snapshot(
     snapshot: SnapshotAssertion,
 ) -> None:
     """Test the multisensor snapshot."""
-    await setup_mock_button(hass, mock_config_entry, mock_homee)
+    mock_homee.nodes = [build_mock_node("buttons.json")]
+    mock_homee.get_node_by_id.return_value = mock_homee.nodes[0]
+    await setup_integration(hass, mock_config_entry)
 
     await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
