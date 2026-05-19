e3c1bbf0753de043dc4d41373bf24010bc7db78f
Chris <1105672+firstof9@users.noreply.github.com>
2026-05-19 13:14:54 -0700

    refactor: remove redundant CONF_ID constant from OpenEVSE integration (#171366)

--- homeassistant/components/openevse/const.py
+++ homeassistant/components/openevse/const.py
@@ -1,7 +1,5 @@
 """Constants for the OpenEVSE integration."""
 
-# pylint: disable-next=home-assistant-duplicate-const
-CONF_ID = "id"
 CONF_SERIAL = "serial"
 DOMAIN = "openevse"
 INTEGRATION_TITLE = "OpenEVSE"
