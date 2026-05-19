d47a95d45a4b0eed7d6cd5a18bdfc9b89f686374
Jan-Philipp Benecke <jan-philipp@bnck.me>
2026-05-19 19:34:52 +0200

    Use try/finaly

--- tests/components/jewish_calendar/conftest.py
+++ tests/components/jewish_calendar/conftest.py
@@ -99,24 +99,24 @@ def results(
     # If results are generated, by using the HDate library, we need to set the language
     set_language(language)
 
-    if isinstance(request.param, dict):
-        result = {
-            key: value.replace(tzinfo=tz_info)
-            if isinstance(value, dt.datetime)
-            else value
-            for key, value in request.param.items()
-        }
-        if "attr" in result and isinstance(result["attr"], dict):
-            result["attr"] = {
-                key: value() if callable(value) else value
-                for key, value in result["attr"].items()
+    try:
+        if isinstance(request.param, dict):
+            result = {
+                key: value.replace(tzinfo=tz_info)
+                if isinstance(value, dt.datetime)
+                else value
+                for key, value in request.param.items()
             }
-        return result
+            if "attr" in result and isinstance(result["attr"], dict):
+                result["attr"] = {
+                    key: value() if callable(value) else value
+                    for key, value in result["attr"].items()
+                }
+            return result
 
-    # Reset language
-    set_language(previous_language)
-
-    return request.param
+        return request.param
+    finally:
+        set_language(previous_language)
 
 
 @pytest.fixture
