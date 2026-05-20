a2d64ae924413d9847e586428d875005bc9326cb
orandasoft <orandasoft@users.noreply.github.com>
2026-05-20 13:54:16 +0900

    Add constants for Virtual Remote integration

---
+++ homeassistant/components/virtual_remote/const.py
@@ -0,0 +1,10 @@
+"""Constants for the Virtual Remote integration."""
+
+DOMAIN = "virtual_remote"
+
+CONF_VIRTUAL_REMOTES = "virtual_remotes"
+CONF_INFRARED_ENTITY_ID = "infrared_entity_id"
+CONF_COMMANDS = "commands"
+
+DEFAULT_DELAY_SECS = 0.4
+DEFAULT_NUM_REPEATS = 1
