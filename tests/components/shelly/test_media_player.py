d71e861ee417ed51dbd553323f94533afa24ad80
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-05-20 13:18:16 +0000

    Tests

--- tests/components/shelly/test_media_player.py
+++ tests/components/shelly/test_media_player.py
@@ -408,6 +408,56 @@ async def test_get_image_http_base64_decode_error(
     assert isinstance(content, bytes)
 
 
+async def test_get_image_url_invalid_thumb_string(
+    hass: HomeAssistant,
+    mock_rpc_device: Mock,
+    monkeypatch: pytest.MonkeyPatch,
+    hass_client_no_auth: ClientSessionGenerator,
+) -> None:
+    """Test get image via http command with invalid thumb string."""
+    status = deepcopy(mock_rpc_device.status)
+    status["media"] = STATUS_AUDIO_FILE
+    status["media"]["playback"]["media_meta"]["thumb"] = "data invalid"
+    monkeypatch.setattr(mock_rpc_device, "status", status)
+
+    await init_integration(hass, 2, model=MODEL_WALL_DISPLAY)
+
+    state = hass.states.get(ENTITY_ID)
+    assert "entity_picture_local" not in state.attributes
+
+    client = await hass_client_no_auth()
+
+    resp = await client.get(state.attributes["entity_picture"])
+    content = await resp.read()
+
+    assert isinstance(content, bytes)
+
+
+async def test_get_image_url_invalid_mime_type(
+    hass: HomeAssistant,
+    mock_rpc_device: Mock,
+    monkeypatch: pytest.MonkeyPatch,
+    hass_client_no_auth: ClientSessionGenerator,
+) -> None:
+    """Test get image via http command with invalid MIME type."""
+    status = deepcopy(mock_rpc_device.status)
+    status["media"] = STATUS_AUDIO_FILE
+    status["media"]["playback"]["media_meta"]["thumb"] = "data:video/mpg;base64,0"
+    monkeypatch.setattr(mock_rpc_device, "status", status)
+
+    await init_integration(hass, 2, model=MODEL_WALL_DISPLAY)
+
+    state = hass.states.get(ENTITY_ID)
+    assert "entity_picture_local" not in state.attributes
+
+    client = await hass_client_no_auth()
+
+    resp = await client.get(state.attributes["entity_picture"])
+    content = await resp.read()
+
+    assert isinstance(content, bytes)
+
+
 async def test_rpc_media_player_browse_media_root(
     hass: HomeAssistant,
     mock_rpc_device: Mock,
