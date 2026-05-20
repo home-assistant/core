1654e477bca6a13e7d2ea48def358a5415cf956b
orandasoft <orandasoft@users.noreply.github.com>
2026-05-20 14:26:57 +0900

    Refactor constants for Virtual Remote integration

--- homeassistant/components/virtual_remote/const.py
+++ homeassistant/components/virtual_remote/const.py
@@ -2,9 +2,11 @@
 
 DOMAIN = "virtual_remote"
 
-CONF_VIRTUAL_REMOTES = "virtual_remotes"
 CONF_INFRARED_ENTITY_ID = "infrared_entity_id"
-CONF_COMMANDS = "commands"
+CONF_REMOTE_COMMANDS = "commands"
+CONF_REMOTE_ID = "id"
+CONF_REMOTE_NAME = "name"
+CONF_VIRTUAL_REMOTES = "virtual_remotes"
 
 DEFAULT_DELAY_SECS = 0.4
 DEFAULT_NUM_REPEATS = 1
