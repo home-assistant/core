100aad5cc4eecee3036a07a64105b5067f121e86
Jeff Stein <jeffstein@gmail.com>
2026-05-22 11:43:50 -0600

    Address OpenAQ review feedback

--- homeassistant/components/openaq/const.py
+++ homeassistant/components/openaq/const.py
@@ -3,6 +3,16 @@
 import logging
 from typing import Final
 
+import httpx
+from openaq import (
+    ApiKeyMissingError,
+    ForbiddenError,
+    HTTPRateLimitError,
+    NotAuthorizedError,
+    RateLimitError,
+)
+from openaq.shared.exceptions import APIError, OpenAQError
+
 DOMAIN = "openaq"
 
 LOGGER = logging.getLogger(__package__)
@@ -12,3 +22,12 @@ CONF_LOCATION_ID: Final = "location_id"
 
 MAX_RADIUS: Final = 25000
 SUBENTRY_TYPE_LOCATION: Final = "location"
+
+OPENAQ_AUTH_EXCEPTIONS: Final = (
+    ApiKeyMissingError,
+    ForbiddenError,
+    NotAuthorizedError,
+)
+OPENAQ_RATE_LIMIT_EXCEPTIONS: Final = (HTTPRateLimitError, RateLimitError)
+OPENAQ_API_EXCEPTIONS: Final = (APIError, httpx.HTTPError)
+OPENAQ_UPDATE_EXCEPTIONS: Final = (APIError, OpenAQError, httpx.HTTPError)
