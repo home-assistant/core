642f75d2aaf2dfcf3153139a9b885137fd93a6c2
zhuxionghui@lepro.io <zhuxionghui@lepro.io>
2026-05-22 09:44:11 +0000

    pro config

--- homeassistant/components/lepro/const.py
+++ homeassistant/components/lepro/const.py
@@ -6,6 +6,7 @@ CONF_ACCOUNT = "account"
 CONF_API_HOST = "api_host"
 
 REGION_API_URL = "https://api-eu-iot.lepro.com/user/region"
+# REGION_API_URL = "https://api-fe-iot-beta.lepro.com/user/region"
 # REGION_API_URL = "http://192.168.30.3:9999/user/region"
 
 OAUTH2_SCOPES = "device.all user.read"
