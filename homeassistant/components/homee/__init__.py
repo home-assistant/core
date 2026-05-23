c5c31fa8b3ac585f7b76728bf2df89b8ea189c56
Taraman17 <mozilla@adrario.de>
2026-05-23 20:00:01 +0000

    translate exceptions in __init__.py

--- homeassistant/components/homee/__init__.py
+++ homeassistant/components/homee/__init__.py
@@ -54,10 +54,20 @@ async def async_setup_entry(hass: HomeAssistant, entry: HomeeConfigEntry) -> boo
     try:
         await homee.get_access_token()
     except HomeeConnectionFailedException as exc:
-        raise ConfigEntryNotReady(f"Connection to Homee failed: {exc.reason}") from exc
+        raise ConfigEntryNotReady(
+            translation_domain=DOMAIN,
+            translation_key="connection_failed",
+            translation_placeholders={
+                "error_reason": str(exc.reason),
+            },
+        ) from exc
     except HomeeAuthFailedException as exc:
         raise ConfigEntryAuthFailed(
-            f"Authentication to Homee failed: {exc.reason}"
+            translation_domain=DOMAIN,
+            translation_key="auth_failed",
+            translation_placeholders={
+                "error_reason": str(exc.reason),
+            },
         ) from exc
 
     hass.loop.create_task(homee.run())
