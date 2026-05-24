20c05aab2be90feec6d29bf6aae5d09f761e3336
Max Michels <6703026+maxmichels@users.noreply.github.com>
2026-05-24 19:29:24 +0200

    Replace duplicate constants in google_generative_ai_conversation with homeassistant.const imports (#172050)

--- tests/components/google_generative_ai_conversation/test_diagnostics.py
+++ tests/components/google_generative_ai_conversation/test_diagnostics.py
@@ -8,7 +8,6 @@ from homeassistant.components.google_generative_ai_conversation.const import (
     CONF_HARASSMENT_BLOCK_THRESHOLD,
     CONF_HATE_BLOCK_THRESHOLD,
     CONF_MAX_TOKENS,
-    CONF_PROMPT,
     CONF_RECOMMENDED,
     CONF_SEXUAL_BLOCK_THRESHOLD,
     CONF_TEMPERATURE,
@@ -21,6 +20,7 @@ from homeassistant.components.google_generative_ai_conversation.const import (
     RECOMMENDED_TOP_K,
     RECOMMENDED_TOP_P,
 )
+from homeassistant.const import CONF_PROMPT
 from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
