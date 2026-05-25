373db2b48b0dfd723083419fc37a7848538e0006
J. Nick Koston <nick@koston.org>
2026-05-25 14:47:09 -0500

    Restore mopeka sensor entity data across reloads (#172178)

--- homeassistant/components/mopeka/sensor.py
+++ homeassistant/components/mopeka/sensor.py
@@ -123,7 +123,9 @@ async def async_setup_entry(
             MopekaBluetoothSensorEntity, async_add_entities
         )
     )
-    entry.async_on_unload(coordinator.async_register_processor(processor))
+    entry.async_on_unload(
+        coordinator.async_register_processor(processor, SensorEntityDescription)
+    )
 
 
 class MopekaBluetoothSensorEntity(
