4c109db6a8d94e989830186cabb1258c91eba90a
J. Nick Koston <nick@koston.org>
2026-05-25 14:52:21 -0500

    Restore sensorpro sensor entity data across reloads (#172182)

--- homeassistant/components/sensorpro/sensor.py
+++ homeassistant/components/sensorpro/sensor.py
@@ -123,7 +123,9 @@ async def async_setup_entry(
             SensorProBluetoothSensorEntity, async_add_entities
         )
     )
-    entry.async_on_unload(coordinator.async_register_processor(processor))
+    entry.async_on_unload(
+        coordinator.async_register_processor(processor, SensorEntityDescription)
+    )
 
 
 class SensorProBluetoothSensorEntity(
