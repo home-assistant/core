d754826b02eda441b30f4e1170d54d832feea284
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2026-05-26 00:42:47 +0000

    Remove artificial throttling of push updates in EDL21 integration

--- homeassistant/components/edl21/sensor.py
+++ homeassistant/components/edl21/sensor.py
@@ -1,7 +1,6 @@
 """Support for EDL21 Smart Meters."""
 
 from collections.abc import Mapping
-from datetime import timedelta
 from typing import Any
 
 from sml import SmlGetListResponse
@@ -38,8 +37,6 @@ from .const import (
     LOGGER,
     SIGNAL_EDL21_TELEGRAM,
 )
-
-MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
 
 # OBIS format: A-B:C.D.E*F
 SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
@@ -391,7 +388,6 @@ class EDL21Entity(SensorEntity):
         self._electricity_id = electricity_id
         self._obis = obis
         self._telegram = telegram
-        self._min_time = MIN_TIME_BETWEEN_UPDATES
         self._last_update = utcnow()
         self._async_remove_dispatcher = None
         self.entity_description = entity_description
@@ -414,12 +410,7 @@ class EDL21Entity(SensorEntity):
             if self._telegram == telegram:
                 return
 
-            now = utcnow()
-            if now - self._last_update < self._min_time:
-                return
-
             self._telegram = telegram
-            self._last_update = now
             self.async_write_ha_state()
 
         self._async_remove_dispatcher = async_dispatcher_connect(
