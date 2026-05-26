63182719e222ecc7b8c34a162650f686565a3f28
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2026-05-26 22:41:12 +0000

    changes

--- homeassistant/components/smtp/__init__.py
+++ homeassistant/components/smtp/__init__.py
@@ -30,8 +30,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         )
     )
 
-    await hass.config_entries.async_forward_entry_setups(entry, [])
-
     entry.async_on_unload(entry.add_update_listener(_async_update_listener))
 
     return True
@@ -44,4 +42,4 @@ async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> Non
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, [])
+    return True
