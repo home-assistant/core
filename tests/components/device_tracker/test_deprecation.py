cfeac578ce30f8fdc762a48086569e41cf784973
Petro <mhpetrosino@yahoo.com>
2026-05-26 12:52:23 +0000

    address comments

--- tests/components/device_tracker/test_deprecation.py
+++ tests/components/device_tracker/test_deprecation.py
@@ -22,21 +22,47 @@ def test_all() -> None:
 
 
 @pytest.mark.parametrize(
-    ("alias_name", "replacement"),
+    ("alias_name", "replacement", "replacement_name"),
     [
-        ("BaseTrackerEntity", BaseTrackerEntity),
-        ("ScannerEntity", ScannerEntity),
-        ("SourceType", SourceType),
-        ("TrackerEntity", TrackerEntity),
-        ("TrackerEntityDescription", TrackerEntityDescription),
+        (
+            "BaseTrackerEntity",
+            BaseTrackerEntity,
+            "homeassistant.components.device_tracker.BaseTrackerEntity",
+        ),
+        (
+            "ScannerEntity",
+            ScannerEntity,
+            "homeassistant.components.device_tracker.ScannerEntity",
+        ),
+        (
+            "SourceType",
+            SourceType,
+            "homeassistant.components.device_tracker.SourceType",
+        ),
+        (
+            "TrackerEntity",
+            TrackerEntity,
+            "homeassistant.components.device_tracker.TrackerEntity",
+        ),
+        (
+            "TrackerEntityDescription",
+            TrackerEntityDescription,
+            "homeassistant.components.device_tracker.TrackerEntityDescription",
+        ),
     ],
 )
 def test_deprecated_config_entry_aliases(
     caplog: pytest.LogCaptureFixture,
     alias_name: str,
     replacement: Any,
+    replacement_name: str,
 ) -> None:
     """Test deprecated config_entry aliases."""
     import_and_test_deprecated_alias(
-        caplog, config_entry, alias_name, replacement, "2027.6"
+        caplog,
+        config_entry,
+        alias_name,
+        replacement,
+        "2027.6",
+        replacement_name=replacement_name,
     )
