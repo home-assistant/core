56f3831d6fb7c301f4bcd30edd9270a6593d91f8
starkillerOG <starkiller.og@gmail.com>
2026-05-27 12:41:32 +0200

    Reolink battery fast start (#171840)

--- homeassistant/components/reolink/const.py
+++ homeassistant/components/reolink/const.py
@@ -7,6 +7,7 @@ DOMAIN = "reolink"
 CONF_USE_HTTPS = "use_https"
 CONF_BC_PORT = "baichuan_port"
 CONF_BC_ONLY = "baichuan_only"
+CONF_BC_CONNECT = "baichuan_connection"
 CONF_SUPPORTS_PRIVACY_MODE = "privacy_mode_supported"
 CONF_FIRMWARE_CHECK_TIME = "firmware_check_time"
 
