64eac712b79bdcbfb2381c4ba7988c6bd5a2452c
Bartlomiej Kobus <bartlomiej.kobus@blebox.eu>
2026-05-29 13:05:20 +0000

    Add icon translations for Blebox integration

--- homeassistant/components/blebox/button.py
+++ homeassistant/components/blebox/button.py
@@ -2,12 +2,22 @@
 
 import blebox_uniapi.button
 
-from homeassistant.components.button import ButtonEntity
+from homeassistant.components.button import ButtonEntity, ButtonEntityDescription
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from . import BleBoxConfigEntry
 from .entity import BleBoxEntity
+
+BUTTON_TYPES: dict[str, ButtonEntityDescription] = {
+    "up": ButtonEntityDescription(key="up", translation_key="up"),
+    "down": ButtonEntityDescription(key="down", translation_key="down"),
+    "fav": ButtonEntityDescription(key="fav", translation_key="fav"),
+    "open": ButtonEntityDescription(key="open", translation_key="open"),
+    "close": ButtonEntityDescription(key="close", translation_key="close"),
+}
+
+_DEFAULT_BUTTON = ButtonEntityDescription(key="button")
 
 
 async def async_setup_entry(
@@ -29,21 +39,14 @@ class BleBoxButtonEntity(BleBoxEntity[blebox_uniapi.button.Button], ButtonEntity
     def __init__(self, feature: blebox_uniapi.button.Button) -> None:
         """Initialize a BleBox button feature."""
         super().__init__(feature)
-        self._attr_icon = self.get_icon()
+        self.entity_description = self._get_description()
 
-    def get_icon(self) -> str | None:
-        """Return icon for endpoint."""
-        if "up" in self._feature.query_string:
-            return "mdi:arrow-up-circle"
-        if "down" in self._feature.query_string:
-            return "mdi:arrow-down-circle"
-        if "fav" in self._feature.query_string:
-            return "mdi:heart-circle"
-        if "open" in self._feature.query_string:
-            return "mdi:arrow-up-circle"
-        if "close" in self._feature.query_string:
-            return "mdi:arrow-down-circle"
-        return None
+    def _get_description(self) -> ButtonEntityDescription:
+        """Return the description matching this button's query string."""
+        for key, description in BUTTON_TYPES.items():
+            if key in self._feature.query_string:
+                return description
+        return _DEFAULT_BUTTON
 
     async def async_press(self) -> None:
         """Handle the button press."""
