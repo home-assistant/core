4d8cc12278a1fefd5e34e46498efba34087baf31
Michael <35783820+mib1185@users.noreply.github.com>
2026-06-01 19:39:28 +0200

    Use proper user-agent to fetch feeds (#172655)

--- tests/components/feedreader/const.py
+++ tests/components/feedreader/const.py
@@ -4,9 +4,10 @@ from homeassistant.components.feedreader.const import (
     CONF_MAX_ENTRIES,
     DEFAULT_MAX_ENTRIES,
 )
-from homeassistant.const import CONF_URL
+from homeassistant.const import APPLICATION_NAME, CONF_URL, __version__ as ha_version
 
 URL = "http://some.rss.local/rss_feed.xml"
+USER_AGENT = f"{APPLICATION_NAME}/{ha_version}"
 FEED_TITLE = "RSS Sample"
 VALID_CONFIG_DEFAULT = {CONF_URL: URL, CONF_MAX_ENTRIES: DEFAULT_MAX_ENTRIES}
 VALID_CONFIG_100 = {CONF_URL: URL, CONF_MAX_ENTRIES: 100}
