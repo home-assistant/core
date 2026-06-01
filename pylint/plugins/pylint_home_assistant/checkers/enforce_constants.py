b197bb0972e67a1612bef503e55101c7bd08d977
epenet <6771947+epenet@users.noreply.github.com>
2026-06-01 06:09:19 +0000

    Improve

--- pylint/plugins/pylint_home_assistant/checkers/enforce_constants.py
+++ pylint/plugins/pylint_home_assistant/checkers/enforce_constants.py
@@ -1,8 +1,19 @@
-"""Plugin to enforce type hints on specific functions."""
+"""Plugin to ensure correct use of constants."""
 
 from astroid import nodes
 from pylint.checkers import BaseChecker
 from pylint.lint import PyLinter
+
+_FUNCTION_CHECKS: list[tuple[str, int | None, str]] = [
+    ("async_setup_component", 1, "domain"),
+    ("async_mock_service", 1, "domain"),
+    ("MockConfigEntry", None, "domain"),
+]
+_METHOD_CHECKS: list[tuple[str, str, int | None, str]] = [
+    ("hass.services", "async_call", 0, "domain"),
+    ("hass.services", "call", 0, "domain"),
+    ("hass.config_entries.flow", "async_init", 0, "handler"),
+]
 
 
 class HassEnforceConstantsChecker(BaseChecker):
@@ -12,9 +23,9 @@ class HassEnforceConstantsChecker(BaseChecker):
     priority = -1
     msgs = {
         "C7414": (
-            "Argument %s should be a DOMAIN constant or a domain variable in %s",
-            "hass-argument-domain-constant",
-            "Used when method argument should be a DOMAIN constant.",
+            "Argument %s should be a domain constant or variable in %s",
+            "hass-domain-argument",
+            "Used when function/method argument should be a domain constant/variable.",
         ),
     }
 
@@ -32,20 +43,28 @@ class HassEnforceConstantsChecker(BaseChecker):
             return
 
         if isinstance(node.func, nodes.Attribute):
-            if (
-                node.func.attrname in {"async_call", "call"}
-            ) and node.func.expr.as_string() == "hass.services":
-                self._ensure_domain_argument(node, arg_position=0, kwarg_name="domain")
-                return
+            for method_source, method_name, arg_position, kwarg_name in _METHOD_CHECKS:
+                if (
+                    node.func.attrname == method_name
+                    and node.func.expr.as_string() == method_source
+                ):
+                    self._ensure_domain_argument(
+                        node, arg_position=arg_position, kwarg_name=kwarg_name
+                    )
+                    return
         if isinstance(node.func, nodes.Name):
-            if node.func.name == "async_setup_component":
-                self._ensure_domain_argument(node, arg_position=1, kwarg_name="domain")
+            for func_name, arg_position, kwarg_name in _FUNCTION_CHECKS:
+                if node.func.name == func_name:
+                    self._ensure_domain_argument(
+                        node, arg_position=arg_position, kwarg_name=kwarg_name
+                    )
+                    return
 
     def _ensure_domain_argument(
-        self, call_node: nodes.Call, *, arg_position: int, kwarg_name: str
+        self, call_node: nodes.Call, *, arg_position: int | None, kwarg_name: str
     ) -> None:
 
-        if len(call_node.args) > arg_position:
+        if arg_position is not None and len(call_node.args) > arg_position:
             self._ensure_domain_argument_node(call_node, call_node.args[arg_position])
 
         for keyword in call_node.keywords:
@@ -56,21 +75,31 @@ class HassEnforceConstantsChecker(BaseChecker):
     def _ensure_domain_argument_node(
         self, call_node: nodes.Call, arg_node: nodes.Argument | nodes.Keyword
     ) -> None:
+        """Ensure the argument node is a domain constant or variable.
+
+        We allow:
+         - DOMAIN constant (or x.DOMAIN attribute)
+         - domain variable (or x.domain attribute)
+         - constants ending with _DOMAIN
+         - variables ending with _domain
+        """
         match arg_node:
             case nodes.Attribute():
-                if arg_node.attrname != "DOMAIN":
+                if arg_node.attrname in {"DOMAIN", "domain"}:
                     self.add_message(
-                        "hass-argument-domain-constant",
+                        "hass-domain-argument",
                         node=arg_node,
                         args=(arg_node.as_string(), call_node.func.as_string()),
                     )
                 return
             case nodes.Name():
-                if not arg_node.name.endswith("DOMAIN") and not arg_node.name.endswith(
-                    "domain"
+                if (
+                    (node_name := arg_node.name) not in {"DOMAIN", "domain"}
+                    and not node_name.endswith("_DOMAIN")
+                    and not node_name.endswith("_domain")
                 ):
                     self.add_message(
-                        "hass-argument-domain-constant",
+                        "hass-domain-argument",
                         node=arg_node,
                         args=(arg_node.as_string(), call_node.func.as_string()),
                     )
