e7c16b5206684a95c69fc3ae5d5867f6b79cbb0d
Chris <1105672+firstof9@users.noreply.github.com>
2026-06-03 10:37:55 -0700

    Add binary sensor platform to openevse (#172924)

--- homeassistant/components/openevse/__init__.py
+++ homeassistant/components/openevse/__init__.py
@@ -11,7 +11,7 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from .const import DOMAIN
 from .coordinator import OpenEVSEConfigEntry, OpenEVSEDataUpdateCoordinator
 
-PLATFORMS = [Platform.NUMBER, Platform.SENSOR]
+PLATFORMS = [Platform.BINARY_SENSOR, Platform.NUMBER, Platform.SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: OpenEVSEConfigEntry) -> bool:
