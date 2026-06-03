a4604b746369fb67ecd404f092ebaeaa9ede33a4
Roy Walker <roy.walker777@gmail.com>
2026-06-03 02:30:00 +0000

    One more time.

--- tests/components/derivative/test_config_flow.py
+++ tests/components/derivative/test_config_flow.py
@@ -41,6 +41,7 @@ async def test_config_flow(hass: HomeAssistant, platform) -> None:
                 "time_window": {"seconds": 0},
                 "unit_time": "min",
                 "max_sub_interval": {"minutes": 1},
+                "replace_unavailable": False,
             },
         )
         await hass.async_block_till_done()
@@ -55,6 +56,7 @@ async def test_config_flow(hass: HomeAssistant, platform) -> None:
         "time_window": {"seconds": 0.0},
         "unit_time": "min",
         "max_sub_interval": {"minutes": 1.0},
+        "replace_unavailable": False,
     }
     assert len(mock_setup_entry.mock_calls) == 1
 
@@ -67,6 +69,7 @@ async def test_config_flow(hass: HomeAssistant, platform) -> None:
         "time_window": {"seconds": 0.0},
         "unit_time": "min",
         "max_sub_interval": {"minutes": 1.0},
+        "replace_unavailable": False,
     }
     assert config_entry.title == "My derivative"
 
@@ -141,6 +144,7 @@ async def test_options(
         "source": "sensor.valid",
         "time_window": {"seconds": 10.0},
         "unit_time": "h",
+        "replace_unavailable": False,
     }
     assert config_entry.data == {}
     assert dict(config_entry.options) == {
@@ -149,6 +153,7 @@ async def test_options(
         "source": "sensor.valid",
         "time_window": {"seconds": 10.0},
         "unit_time": "h",
+        "replace_unavailable": False,
     }
     assert config_entry.title == "My derivative"
 
