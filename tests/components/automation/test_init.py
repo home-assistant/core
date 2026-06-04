3bef3f913f9aac4c9e185fb187896988d40ebe67
epenet <6771947+epenet@users.noreply.github.com>
2026-06-04 14:01:51 +0200

    Use DOMAIN constant in test (hass.states.async_entity_ids) (#173009)

--- tests/components/automation/test_init.py
+++ tests/components/automation/test_init.py
@@ -1777,7 +1777,7 @@ async def test_automation_bad_config_validation(
     assert issues[0]["translation_placeholders"]["error"].startswith(details)
 
     # Make sure both automations are setup
-    assert set(hass.states.async_entity_ids("automation")) == {
+    assert set(hass.states.async_entity_ids(DOMAIN)) == {
         "automation.bad_automation",
         "automation.good_automation",
     }
