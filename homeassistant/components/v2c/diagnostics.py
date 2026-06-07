21fecf6a8c40e0f2478adb7340a0c59468a4f250
Diogo Gomes <dgomes@ua.pt>
2026-06-07 21:52:23 +0100

    fix mypy

--- homeassistant/components/v2c/diagnostics.py
+++ homeassistant/components/v2c/diagnostics.py
@@ -22,10 +22,13 @@ async def async_get_config_entry_diagnostics(
 
     coordinator_data = coordinator.evse.data
     evse_raw_data = coordinator.evse.raw_data
+    assert evse_raw_data is not None
+    raw_content = evse_raw_data["content"]
+    assert isinstance(raw_content, bytes)
 
     return {
         "config_entry": async_redact_data(entry.as_dict(), TO_REDACT),
         "data": str(coordinator_data),
-        "raw_data": evse_raw_data["content"].decode("utf-8"),  # type: ignore[attr-defined]
+        "raw_data": raw_content.decode("utf-8"),
         "host_status": evse_raw_data["status_code"],
     }
