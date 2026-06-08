2e39229e8e2a6c035fa3210372a1aebb7c227ccf
Glenn Waters <glenn@watrs.ca>
2026-06-08 10:16:42 -0400

    Environment Canada integration: add get_alerts action (#172393)

--- homeassistant/components/environment_canada/__init__.py
+++ homeassistant/components/environment_canada/__init__.py
@@ -8,9 +8,12 @@ from env_canada import ECAirQuality, ECMap, ECWeather
 from homeassistant.const import CONF_LANGUAGE, CONF_LATITUDE, CONF_LONGITUDE, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.typing import ConfigType
 
-from .const import CONF_STATION
+from .const import CONF_STATION, DOMAIN
 from .coordinator import ECConfigEntry, ECDataUpdateCoordinator, ECRuntimeData
+from .services import async_setup_services
 
 DEFAULT_RADAR_UPDATE_INTERVAL = timedelta(minutes=5)
 DEFAULT_WEATHER_UPDATE_INTERVAL = timedelta(minutes=5)
@@ -18,6 +21,14 @@ DEFAULT_WEATHER_UPDATE_INTERVAL = timedelta(minutes=5)
 PLATFORMS = [Platform.CAMERA, Platform.SENSOR, Platform.WEATHER]
 
 _LOGGER = logging.getLogger(__name__)
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Environment Canada services."""
+    async_setup_services(hass)
+    return True
 
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ECConfigEntry) -> bool:
