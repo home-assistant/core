c8be2c1afcda87b41ece3ea38ff11bc1f69a81ba
Joakim Plate <elupus@ecce.se>
2026-06-09 14:42:38 +0200

    Ensure we provide strings to vol.In for philips js (#173313)

--- homeassistant/components/philips_js/config_flow.py
+++ homeassistant/components/philips_js/config_flow.py
@@ -45,8 +45,8 @@ USER_SCHEMA = vol.Schema(
         ): str,
         vol.Required(
             CONF_API_VERSION,
-            default=1,
-        ): vol.In([1, 5, 6]),
+            default="1",
+        ): vol.In(["1", "5", "6"]),
     }
 )
 
@@ -223,7 +223,7 @@ class PhilipsJSConfigFlow(ConfigFlow, domain=DOMAIN):
             self._current = user_input
             try:
                 await self._async_attempt_prepare(
-                    user_input[CONF_HOST], user_input[CONF_API_VERSION], False
+                    user_input[CONF_HOST], int(user_input[CONF_API_VERSION]), False
                 )
             except GeneralFailure as exc:
                 LOGGER.error(exc)
