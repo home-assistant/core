1788b04fe52d3b8e005be2fa3637eb8dbf546ba8
James Myatt <james@jamesmyatt.co.uk>
2026-06-10 21:36:08 +0100

    Fix docstrings in shopping_list (#173462)

--- homeassistant/components/shopping_list/todo.py
+++ homeassistant/components/shopping_list/todo.py
@@ -51,7 +51,7 @@ class ShoppingTodoListEntity(TodoListEntity):
         )
 
     async def async_update_todo_item(self, item: TodoItem) -> None:
-        """Update an item to the To-do list."""
+        """Update an item in the To-do list."""
         data = {
             "name": item.summary,
             "complete": item.status == TodoItemStatus.COMPLETED,
@@ -64,7 +64,7 @@ class ShoppingTodoListEntity(TodoListEntity):
             ) from err
 
     async def async_delete_todo_items(self, uids: list[str]) -> None:
-        """Add an item to the To-do list."""
+        """Delete items from the To-do list."""
         await self._data.async_remove_items(set(uids))
 
     async def async_move_todo_item(
