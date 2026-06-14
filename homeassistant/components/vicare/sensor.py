d215afebb5f020524f246202f069616256b6bbec
Christian Lackas <christian@lackas.net>
2026-06-14 14:16:07 +0200

    Migrate ViCare sensor platform to DataUpdateCoordinator

--- homeassistant/components/vicare/sensor.py
+++ homeassistant/components/vicare/sensor.py
@@ -36,6 +36,8 @@ from homeassistant.const import (
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+from homeassistant.helpers.typing import StateType
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import (
     VICARE_BAR,
@@ -47,6 +49,7 @@ from .const import (
     VICARE_W,
     VICARE_WH,
 )
+from .coordinator import ViCareCoordinator
 from .entity import ViCareEntity
 from .types import ViCareConfigEntry, ViCareDevice, ViCareRequiredKeysMixin
 from .utils import (
@@ -1497,9 +1500,11 @@ def _build_entities(
 
     entities: list[ViCareSensor] = []
     for device in device_list:
+        assert device.coordinator is not None
         # add device entities
         entities.extend(
             ViCareSensor(
+                device.coordinator,
                 description,
                 get_device_serial(device.api),
                 device.config,
@@ -1519,6 +1524,7 @@ def _build_entities(
         ):
             entities.extend(
                 ViCareSensor(
+                    device.coordinator,
                     description,
                     get_device_serial(device.api),
                     device.config,
@@ -1542,19 +1548,18 @@ async def async_setup_entry(
         await hass.async_add_executor_job(
             _build_entities,
             config_entry.runtime_data.devices,
-        ),
-        # run update to have device_class set depending on unit_of_measurement
-        True,
+        )
     )
 
 
-class ViCareSensor(ViCareEntity, SensorEntity):
+class ViCareSensor(CoordinatorEntity[ViCareCoordinator], ViCareEntity, SensorEntity):
     """Representation of a ViCare sensor."""
 
     entity_description: ViCareSensorEntityDescription
 
     def __init__(
         self,
+        coordinator: ViCareCoordinator,
         description: ViCareSensorEntityDescription,
         device_serial: str | None,
         device_config: PyViCareDeviceConfig,
@@ -1562,30 +1567,37 @@ class ViCareSensor(ViCareEntity, SensorEntity):
         component: PyViCareHeatingDeviceComponent | None = None,
     ) -> None:
         """Initialize the sensor."""
-        super().__init__(
-            description.key, device_serial, device_config, device, component
+        CoordinatorEntity.__init__(self, coordinator)
+        ViCareEntity.__init__(
+            self, description.key, device_serial, device_config, device, component
         )
         self.entity_description = description
 
     @property
-    def available(self) -> bool:
-        """Return True if entity is available."""
-        return self._attr_native_value is not None
+    def native_value(self) -> StateType:
+        """Return the state of the sensor."""
+        with suppress(PyViCareNotSupportedFeatureError):
+            return self.entity_description.value_getter(self._api)
+        return None
 
-    def update(self) -> None:
-        """Update state of sensor."""
-        vicare_unit = None
-        with self.vicare_api_handler(), suppress(PyViCareNotSupportedFeatureError):
-            self._attr_native_value = self.entity_description.value_getter(self._api)
+    @property
+    def native_unit_of_measurement(self) -> str | None:
+        """Return the unit of measurement of the sensor."""
+        if self.entity_description.unit_getter is None:
+            return self.entity_description.native_unit_of_measurement
+        with suppress(PyViCareNotSupportedFeatureError):
+            vicare_unit = self.entity_description.unit_getter(self._api)
+            if vicare_unit is not None:
+                return VICARE_UNIT_TO_HA_UNIT.get(vicare_unit)
+        return self.entity_description.native_unit_of_measurement
 
-            if self.entity_description.unit_getter:
-                vicare_unit = self.entity_description.unit_getter(self._api)
-
-        if vicare_unit is not None:
-            if (
-                device_class := VICARE_UNIT_TO_DEVICE_CLASS.get(vicare_unit)
-            ) is not None:
-                self._attr_device_class = device_class
-            self._attr_native_unit_of_measurement = VICARE_UNIT_TO_HA_UNIT.get(
-                vicare_unit
-            )
+    @property
+    def device_class(self) -> SensorDeviceClass | None:
+        """Return the device class of the sensor."""
+        if self.entity_description.unit_getter is None:
+            return self.entity_description.device_class
+        with suppress(PyViCareNotSupportedFeatureError):
+            vicare_unit = self.entity_description.unit_getter(self._api)
+            if vicare_unit is not None and vicare_unit in VICARE_UNIT_TO_DEVICE_CLASS:
+                return VICARE_UNIT_TO_DEVICE_CLASS[vicare_unit]
+        return self.entity_description.device_class
