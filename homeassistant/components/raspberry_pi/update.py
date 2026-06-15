747b58a282e95525a4eaaa203c3d244e29c16241
Jan Čermák <sairon@sairon.cz>
2026-06-15 18:45:53 +0200

    Drop unused _LOGGER

--- homeassistant/components/raspberry_pi/update.py
+++ homeassistant/components/raspberry_pi/update.py
@@ -1,7 +1,5 @@
 """Raspberry Pi firmware update entity."""
 
-import logging
-
 from aiohasupervisor import SupervisorError
 
 from homeassistant.components.hassio import HassioNotReadyError, get_os_info
@@ -20,8 +18,6 @@ from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from .const import DOMAIN
 from .hardware import BOARD_NAMES, MODELS
-
-_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(
