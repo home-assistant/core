6b18f49e32477a7f78deba37ed106a3dce21d3b5
Åke S - Piper <ake@strandberg.eu>
2026-06-16 10:55:41 +0000

    Use username as config entry title

--- homeassistant/components/aqvify/config_flow.py
+++ homeassistant/components/aqvify/config_flow.py
@@ -59,7 +59,8 @@ class AqvifyConfigFlow(ConfigFlow, domain=DOMAIN):
                         self._get_reconfigure_entry(), data_updates=user_input
                     )
                 self._abort_if_unique_id_configured()
-                return self.async_create_entry(title="Aqvify", data=user_input)
+                title = account_data.name or "Aqvify"
+                return self.async_create_entry(title=title, data=user_input)
 
         return self.async_show_form(
             step_id="user",
