25ab09ead67c4b6585016dc729a75084a8794690
tr4nt0r <tr4nt0r@uber.space>
2026-06-17 15:02:03 +0000

    Add snapshots

--- tests/components/rpi_power/test_binary_sensor.py
+++ tests/components/rpi_power/test_binary_sensor.py
@@ -5,6 +5,7 @@ import logging
 from unittest.mock import MagicMock
 
 import pytest
+from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.components.rpi_power import binary_sensor
 from homeassistant.components.rpi_power.binary_sensor import (
@@ -12,12 +13,19 @@ from homeassistant.components.rpi_power.binary_sensor import (
     DESCRIPTION_UNDER_VOLTAGE,
 )
 from homeassistant.components.rpi_power.const import DOMAIN
+from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import STATE_OFF, STATE_ON
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 from homeassistant.setup import async_setup_component
 from homeassistant.util import dt as dt_util
 
-from tests.common import MockConfigEntry, async_fire_time_changed, patch
+from tests.common import (
+    MockConfigEntry,
+    async_fire_time_changed,
+    patch,
+    snapshot_platform,
+)
 
 ENTITY_ID = "binary_sensor.raspberry_pi_power_status"
 
@@ -71,3 +79,23 @@ async def test_new_detected(
         logging.DEBUG,
         DESCRIPTION_NORMALIZED,
     ) in caplog.record_tuples
+
+
+async def test_setup(
+    hass: HomeAssistant,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Snapshot test states of binary sensor platform."""
+    with patch(
+        "homeassistant.components.rpi_power.binary_sensor.new_under_voltage", get=True
+    ):
+        config_entry = MockConfigEntry(domain=DOMAIN, entry_id="12345")
+        config_entry.add_to_hass(hass)
+
+        await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+
+    assert config_entry.state is ConfigEntryState.LOADED
+
+    await snapshot_platform(hass, entity_registry, snapshot, config_entry.entry_id)
