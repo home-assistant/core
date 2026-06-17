2a1eb586b076f8553c5db3a9179384ebb31637fa
tr4nt0r <tr4nt0r@uber.space>
2026-06-17 15:58:04 +0000

    test before setup

--- homeassistant/components/rpi_power/__init__.py
+++ homeassistant/components/rpi_power/__init__.py
@@ -1,14 +1,26 @@
 """The Raspberry Pi Power Supply Checker integration."""
 
+from rpi_bad_power import new_under_voltage
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryError
+
+from .const import DOMAIN
 
 PLATFORMS = [Platform.BINARY_SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Raspberry Pi Power Supply Checker from a config entry."""
+
+    if await hass.async_add_executor_job(new_under_voltage) is None:
+        raise ConfigEntryError(
+            translation_domain=DOMAIN,
+            translation_key="under_voltage_not_supported",
+        )
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
