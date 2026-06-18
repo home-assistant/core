bffc677454e6bc96a0e8b55082b9f20b40616792
Raphael Hehl <7577984+RaHehl@users.noreply.github.com>
2026-06-18 15:23:14 +0200

    Remove UniFi Protect Doorlock support (#174196)

--- tests/components/unifiprotect/conftest.py
+++ tests/components/unifiprotect/conftest.py
@@ -18,7 +18,6 @@ from uiprotect.data import (
     Camera,
     Chime,
     CloudAccount,
-    Doorlock,
     Light,
     Liveview,
     Sensor,
@@ -120,7 +119,6 @@ def bootstrap_fixture(nvr: NVR):
     data["viewers"] = []
     data["liveviews"] = []
     data["events"] = []
-    data["doorlocks"] = []
     data["chimes"] = []
     data["aiports"] = []
 
@@ -393,29 +391,6 @@ def sensor_all_fixture(sensor: Sensor):
     all_sensor.motion_settings.is_enabled = True
 
     return all_sensor
-
-
-@pytest.fixture(name="doorlock")
-def doorlock_fixture():
-    """Mock UniFi Protect Doorlock device."""
-
-    # disable pydantic validation so mocking can happen
-    Doorlock.model_config["validate_assignment"] = False
-
-    data = load_json_object_fixture("sample_doorlock.json", DOMAIN)
-    yield Doorlock.from_unifi_dict(**data)
-
-    Doorlock.model_config["validate_assignment"] = True
-
-
-@pytest.fixture
-def unadopted_doorlock(doorlock: Doorlock):
-    """Mock UniFi Protect Light device (unadopted)."""
-
-    no_doorlock = doorlock.model_copy()
-    no_doorlock.name = "Unadopted Lock"
-    no_doorlock.is_adopted = False
-    return no_doorlock
 
 
 @pytest.fixture
