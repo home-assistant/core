db2d53de2d6fb1a04a10f019ee418ac4377a6f45
Erwin Douna <e.douna@gmail.com>
2026-06-19 08:41:48 +0200

    MELCloud Home add constant in diagnostics (#174219)

--- homeassistant/components/melcloud_home/diagnostics.py
+++ homeassistant/components/melcloud_home/diagnostics.py
@@ -3,7 +3,7 @@
 from typing import Any
 
 from homeassistant.components.diagnostics import async_redact_data
-from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
+from homeassistant.const import CONF_EMAIL, CONF_PASSWORD, CONF_UNIQUE_ID
 from homeassistant.core import HomeAssistant
 
 from .coordinator import MelCloudHomeConfigEntry
@@ -14,7 +14,7 @@ TO_REDACT = [
     "first_name",
     "last_name",
     "title",
-    "unique_id",
+    CONF_UNIQUE_ID,
     "id",
 ]
 
