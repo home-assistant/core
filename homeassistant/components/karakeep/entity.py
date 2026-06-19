eaef9cb54a79ef34f815c6eca20dbda903b04f7a
sli-cka <flo.mang@gmail.com>
2026-06-19 19:11:03 +0200

    fetch version once during setup

--- homeassistant/components/karakeep/entity.py
+++ homeassistant/components/karakeep/entity.py
@@ -23,5 +23,5 @@ class KarakeepEntity(CoordinatorEntity[KarakeepDataUpdateCoordinator]):
             manufacturer="Karakeep",
             entry_type=DeviceEntryType.SERVICE,
             configuration_url=url,
-            sw_version=coordinator.data.version,
+            sw_version=coordinator.version,
         )
