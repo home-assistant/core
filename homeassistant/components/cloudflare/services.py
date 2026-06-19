86cc1f22c3715c2e3ada3c6f145d71634995c08b
some-random-climber <293766853+some-random-climber@users.noreply.github.com>
2026-06-19 12:51:19 +0000

    Use service helper

--- homeassistant/components/cloudflare/services.py
+++ homeassistant/components/cloudflare/services.py
@@ -1,27 +1,18 @@
 """Services for cloudflare."""
 
 from homeassistant.core import HomeAssistant, ServiceCall, callback
-from homeassistant.exceptions import ServiceValidationError
+from homeassistant.helpers import service
 
 from .const import DOMAIN, SERVICE_UPDATE_RECORDS
 from .coordinator import CloudflareConfigEntry
 
 
-def get_config_entry(call: ServiceCall) -> CloudflareConfigEntry:
-    """Retrieve current config entry."""
-    entries: list[CloudflareConfigEntry] = (
-        call.hass.config_entries.async_loaded_entries(DOMAIN)
-    )
-    if not entries:
-        raise ServiceValidationError(
-            translation_domain=DOMAIN, translation_key="config_entry_not_loaded"
-        )
-    return entries[0]
-
-
 async def update_records_service(call: ServiceCall) -> None:
     """Set up service for manual trigger."""
-    await get_config_entry(call).runtime_data.async_request_refresh()
+    entry: CloudflareConfigEntry = service.async_get_config_entry(
+        call.hass, DOMAIN, None
+    )
+    await entry.runtime_data.async_request_refresh()
 
 
 @callback
