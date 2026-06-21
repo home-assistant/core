8db948042dd864ea087c53ef2a22d3e3348dab8c
Michael Dluhosch <michael.dluhosch@gmail.com>
2026-06-21 18:54:33 +0000

    Improve config flow description and fix test

--- tests/components/theben_conexa/conftest.py
+++ tests/components/theben_conexa/conftest.py
@@ -10,6 +10,6 @@ import pytest
 def mock_setup_entry() -> Generator[AsyncMock]:
     """Override async_setup_entry."""
     with patch(
-        "homeassistant.components.thebenconexasmgw.async_setup_entry", return_value=True
+        "homeassistant.components.theben_conexa.async_setup_entry", return_value=True
     ) as mock_setup_entry:
         yield mock_setup_entry
