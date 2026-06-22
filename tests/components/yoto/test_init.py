0ee53830aa13a2d7b00c7cfb02dbad854ec83f12
Paul Bottein <paul.bottein@gmail.com>
2026-06-22 20:46:13 +0200

    Add dynamic device support to the switch platform

--- tests/components/yoto/test_init.py
+++ tests/components/yoto/test_init.py
@@ -356,6 +356,7 @@ async def test_dynamic_device_added(
     assert hass.states.get("time.playroom_yoto_day_mode_start") is not None
     assert hass.states.get("number.playroom_yoto_day_mode_brightness") is not None
     assert hass.states.get("select.playroom_yoto_day_mode_color") is not None
+    assert hass.states.get("switch.playroom_yoto_bluetooth_pairing") is not None
     mock_yoto_client.subscribe_player_events.assert_called_once_with("player-2")
 
 
