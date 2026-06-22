5522a5b0c5cfb01e1ae248b9741545c5d0321208
Michael Dluhosch <michael.dluhosch@gmail.com>
2026-06-22 17:04:53 +0000

    Remove device name and formatted serial number as per device

--- homeassistant/components/theben_conexa/entity.py
+++ homeassistant/components/theben_conexa/entity.py
@@ -16,12 +16,24 @@ class ConexaSMGWEntity(CoordinatorEntity[SmgwSensorCoordinator]):
     def __init__(self, coordinator: SmgwSensorCoordinator) -> None:
         """Initialize the Base entity."""
         super().__init__(coordinator)
+        raw_serial = coordinator.gateway_info.smgwID.upper()
+        # For example convert ETHE0213456789 to E THE02 1345 6789:
+        # First char (device type like E for electricity)
+        # A block of 3 chars THE (for theben) and 2 digits for production batch
+        # two blocks of 4 digits to make it unique
+        formatted_serial = " ".join(
+            [
+                raw_serial[:1],
+                raw_serial[1:6],
+                *(raw_serial[i : i + 4] for i in range(6, len(raw_serial), 4)),
+            ]
+        )
+
         self._attr_device_info = DeviceInfo(
-            name=coordinator.gateway_info.smgwID.upper(),
             identifiers={(DOMAIN, coordinator.gateway_info.smgwID)},
             manufacturer="Theben AG",
             model="CONEXA 3.0 Smart Meter Gateway",
             sw_version=coordinator.gateway_info.firmwareVersion,
-            serial_number=coordinator.gateway_info.smgwID,
+            serial_number=formatted_serial,
             configuration_url=f"https://{coordinator.config_entry.data[CONF_HOST]}",
         )
