eabf26f316c6603d815ccd4a26982c08abb560ce
tr4nt0r <tr4nt0r@uber.space>
2026-06-23 23:28:55 +0000

    Refactor Steam coordinator

--- homeassistant/components/steam_online/config_flow.py
+++ homeassistant/components/steam_online/config_flow.py
@@ -166,7 +166,6 @@ class SteamOptionsFlowHandler(OptionsFlowWithReload):
     ) -> ConfigFlowResult:
         """Manage Steam options."""
         if user_input is not None:
-            await self.hass.config_entries.async_unload(self.config_entry.entry_id)
             for _id in self.options[CONF_ACCOUNTS]:
                 if _id not in user_input[CONF_ACCOUNTS] and (
                     entity_id := er.async_get(self.hass).async_get_entity_id(
