518d964379cc7ed90683951b8ffbe9ae3fe4f4dc
Fabian Munkes <105975993+fmunkes@users.noreply.github.com>
2026-06-24 01:04:44 +0200

    Add username parameter to play media action in Music Assistant (#174486)

--- homeassistant/components/music_assistant/media_player.py
+++ homeassistant/components/music_assistant/media_player.py
@@ -6,6 +6,7 @@ from contextlib import suppress
 import os
 from typing import TYPE_CHECKING, Any, override
 
+from music_assistant_models.auth import UserRole
 from music_assistant_models.constants import PLAYER_CONTROL_NONE
 from music_assistant_models.enums import (
     EventType,
@@ -456,35 +457,74 @@ class MusicAssistantPlayer(MusicAssistantEntity, MediaPlayerEntity):
         enqueue: MediaPlayerEnqueue | QueueOption | None = None,
         radio_mode: bool | None = None,
         media_type: str | None = None,
+        username: str | None = None,
     ) -> None:
         """Send the play_media command to the media player."""
+        # verify username availability
+        if username is not None:
+            users = await self.mass.auth.list_users()
+            available_usernames = [
+                user.username
+                for user in users
+                if user.enabled and user.role != UserRole.GUEST
+            ]
+            if username not in available_usernames:
+                raise ServiceValidationError(
+                    translation_domain=DOMAIN,
+                    translation_key="invalid_username",
+                    translation_placeholders={
+                        "username": username,
+                        "available_usernames": ", ".join(available_usernames),
+                    },
+                )
+
         media_uris: list[str] = []
         item: MediaItemType | ItemMapping | None = None
         # work out (all) uri(s) to play
         for media_id_str in media_id:
-            # URL or URI string
-            if "://" in media_id_str:
-                media_uris.append(media_id_str)
-                continue
-            # try content id as library id
-            if media_type and media_id_str.isnumeric():
-                with suppress(MediaNotFoundError):
-                    item = await self.mass.music.get_item(
-                        MediaType(media_type), media_id_str, "library"
-                    )
-                    if isinstance(item, MediaItemType | ItemMapping) and item.uri:
-                        media_uris.append(item.uri)
+            assert self.mass.server_info  # for type checking
+            # pre schema 33: verify_item_uri does not exist as API method
+            # with schema 33: only local files have to be verified
+            if self.mass.server_info.schema_version < 33:
+                # URL or URI string
+                if "://" in media_id_str:
+                    media_uris.append(media_id_str)
                     continue
-            # try local accessible filename
-            elif await asyncio.to_thread(os.path.isfile, media_id_str):
-                media_uris.append(media_id_str)
-                continue
+                # try content id as library id
+                if media_type and media_id_str.isnumeric():
+                    with suppress(MediaNotFoundError):
+                        item = await self.mass.music.get_item(
+                            MediaType(media_type), media_id_str, "library"
+                        )
+                        if isinstance(item, MediaItemType | ItemMapping) and item.uri:
+                            media_uris.append(item.uri)
+                        continue
+                # try local accessible filename
+                elif await asyncio.to_thread(os.path.isfile, media_id_str):
+                    media_uris.append(media_id_str)
+                    continue
+            else:
+                media_id_verify_str = media_id_str
+                if media_type and media_id_str.isnumeric():
+                    # construct in library uri as replacement for pre 33 isnumeric path
+                    media_id_verify_str = (
+                        f"library://{MediaType(media_type).value}/{media_id_str}"
+                    )
+                if await self.mass.music.verify_item_uri(
+                    uri=media_id_verify_str, username=username
+                ):
+                    media_uris.append(media_id_verify_str)
+                    continue
+                if await asyncio.to_thread(os.path.isfile, media_id_str):
+                    media_uris.append(media_id_str)
+                    continue
             # last resort: search for media item by name/search
             if item := await self.mass.music.get_item_by_name(
                 name=media_id_str,
                 artist=artist,
                 album=album,
                 media_type=MediaType(media_type) if media_type else None,
+                username=username,
             ):
                 if TYPE_CHECKING:
                     assert item.uri is not None
@@ -508,6 +548,7 @@ class MusicAssistantPlayer(MusicAssistantEntity, MediaPlayerEntity):
             media=media_uris,
             option=self._convert_queueoption_to_media_player_enqueue(enqueue),
             radio_mode=radio_mode or False,
+            username=username,
         )
 
     @catch_musicassistant_error
