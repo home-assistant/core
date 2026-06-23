Lamarqe/ha_core

add mesh info service request

homeassistant/components/fritz/services.py
@@ -11,10 +11,18 @@ from fritzconnection.core.exceptions import (
 from fritzconnection.lib.fritzwlan import DEFAULT_PASSWORD_LENGTH
 import voluptuous as vol
 
-from homeassistant.core import HomeAssistant, ServiceCall, callback
+from homeassistant.const import ATTR_CONFIG_ENTRY_ID
+from homeassistant.core import (
+    HomeAssistant,
+    ServiceCall,
+    ServiceResponse,
+    SupportsResponse,
+    callback,
+)
 from homeassistant.exceptions import HomeAssistantError, ServiceValidationError
 from homeassistant.helpers.service import (
     async_extract_config_entry_ids,
+    async_get_config_entry,
     async_register_admin_service,
 )
 
@@ -39,6 +47,8 @@ SERVICE_SCHEMA_DIAL = vol.Schema(
         vol.Required("max_ring_seconds"): vol.Range(min=1, max=300),
     }
 )
+
+SERVICE_GET_MESH_INFO = "get_mesh_info"
 
 
 async def _async_set_guest_wifi_password(service_call: ServiceCall) -> None:
@@ -117,6 +127,17 @@ async def _async_dial(service_call: ServiceCall) -> None:
             ) from ex
 
 
+async def _async_get_mesh_info(call: ServiceCall) -> ServiceResponse:
+    """Return the most recent mesh info for targeted config entries."""
+    config_entry: FritzConfigEntry = async_get_config_entry(
+        call.hass, DOMAIN, call.data[ATTR_CONFIG_ENTRY_ID]
+    )
+    return {
+        "mesh_topology": config_entry.runtime_data.mesh_topology_raw,
+        "hosts_attributes": config_entry.runtime_data.hosts_attributes_raw,
+    }
+
+
 @callback
 def async_setup_services(hass: HomeAssistant) -> None:
     """Set up services for Fritz integration."""
@@ -129,3 +150,9 @@ def async_setup_services(hass: HomeAssistant) -> None:
         SERVICE_SCHEMA_SET_GUEST_WIFI_PW,
     )
     hass.services.async_register(DOMAIN, SERVICE_DIAL, _async_dial, SERVICE_SCHEMA_DIAL)
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_GET_MESH_INFO,
+        _async_get_mesh_info,
+        supports_response=SupportsResponse.ONLY,
+    )

home-assistant/core

homeassistant/components/fritz/services.py
@@ -11,10 +11,18 @@ from fritzconnection.core.exceptions import (
 from fritzconnection.lib.fritzwlan import DEFAULT_PASSWORD_LENGTH
 import voluptuous as vol
 
-from homeassistant.core import HomeAssistant, ServiceCall, callback
+from homeassistant.const import ATTR_CONFIG_ENTRY_ID
+from homeassistant.core import (
+    HomeAssistant,
+    ServiceCall,
+    ServiceResponse,
+    SupportsResponse,
+    callback,
+)
 from homeassistant.exceptions import HomeAssistantError, ServiceValidationError
 from homeassistant.helpers.service import (
     async_extract_config_entry_ids,
+    async_get_config_entry,
     async_register_admin_service,
 )
 
@@ -37,6 +45,13 @@ SERVICE_SCHEMA_DIAL = vol.Schema(
         vol.Required("device_id"): str,
         vol.Required("number"): str,
         vol.Required("max_ring_seconds"): vol.Range(min=1, max=300),
+    }
+)
+
+SERVICE_GET_MESH_INFO = "get_mesh_info"
+SERVICE_SCHEMA_GET_MESH_INFO = vol.Schema(
+    {
+        vol.Required(ATTR_CONFIG_ENTRY_ID): str,
     }
 )
 
@@ -117,6 +132,17 @@ async def _async_dial(service_call: ServiceCall) -> None:
             ) from ex
 
 
+async def _async_get_mesh_info(service_call: ServiceCall) -> ServiceResponse:
+    """Return the most recent mesh info for targeted config entry."""
+    config_entry: FritzConfigEntry = async_get_config_entry(
+        service_call.hass, DOMAIN, service_call.data[ATTR_CONFIG_ENTRY_ID]
+    )
+    return {
+        "mesh_topology": config_entry.runtime_data.mesh_topology_raw,
+        "hosts_attributes": config_entry.runtime_data.hosts_attributes_raw,
+    }
+
+
 @callback
 def async_setup_services(hass: HomeAssistant) -> None:
     """Set up services for Fritz integration."""
@@ -129,3 +155,10 @@ def async_setup_services(hass: HomeAssistant) -> None:
         SERVICE_SCHEMA_SET_GUEST_WIFI_PW,
     )
     hass.services.async_register(DOMAIN, SERVICE_DIAL, _async_dial, SERVICE_SCHEMA_DIAL)
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_GET_MESH_INFO,
+        _async_get_mesh_info,
+        SERVICE_SCHEMA_GET_MESH_INFO,
+        supports_response=SupportsResponse.ONLY,
+    )