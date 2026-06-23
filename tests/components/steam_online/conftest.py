e6d5a103a92fd857fcd1280dddd072528fcd5e3b
tr4nt0r <tr4nt0r@uber.space>
2026-06-23 09:45:37 +0000

    JSON fixtures

--- tests/components/steam_online/conftest.py
+++ tests/components/steam_online/conftest.py
@@ -7,16 +7,9 @@ import pytest
 
 from homeassistant.components.steam_online.const import DOMAIN
 
-from . import (
-    ACCOUNT_1,
-    ACCOUNT_2,
-    ACCOUNT_NAME_1,
-    ACCOUNT_NAME_2,
-    CONF_DATA,
-    CONF_OPTIONS,
-)
+from . import ACCOUNT_1, CONF_DATA, CONF_OPTIONS
 
-from tests.common import MockConfigEntry, patch
+from tests.common import MockConfigEntry, load_json_object_fixture, patch
 
 
 @pytest.fixture(name="config_entry")
@@ -56,57 +49,17 @@ def mock_steam_api() -> Generator[MagicMock]:
         client = MagicMock()
         mock_client.return_value = client
 
-        client.GetFriendList.return_value = {
-            "friendslist": {"friends": [{"steamid": ACCOUNT_2}]}
-        }
-        client.GetSteamLevel.return_value = {"response": {"player_level": 10}}
-        client.GetOwnedGames.return_value = {
-            "response": {"game_count": 1},
-            "games": [
-                {
-                    "appid": 20900,
-                    "img_icon_url": "746d1cd48fb2e57d579b05b6e9eccba95859e549",
-                },
-            ],
-        }
-        client.GetPlayerSummaries.return_value = {
-            "response": {
-                "players": {
-                    "player": [
-                        {
-                            "steamid": ACCOUNT_1,
-                            "communityvisibilitystate": 1,
-                            "profilestate": 1,
-                            "personaname": ACCOUNT_NAME_1,
-                            "profileurl": "https://steamcommunity.com/profiles/123456789/",
-                            "avatar": "https://avatars.steamstatic.com/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb.jpg",
-                            "avatarmedium": "https://avatars.steamstatic.com/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg",
-                            "avatarfull": "https://avatars.steamstatic.com/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg",
-                            "avatarhash": "fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb",
-                            "lastlogoff": 1775409487,
-                            "personastate": 1,
-                            "realname": "John Dough",
-                            "personastateflags": 0,
-                            "gameextrainfo": "The Witcher: Enhanced Edition",
-                            "gameid": "20900",
-                        },
-                        {
-                            "steamid": ACCOUNT_2,
-                            "communityvisibilitystate": 1,
-                            "profilestate": 1,
-                            "personaname": ACCOUNT_NAME_2,
-                            "profileurl": "https://steamcommunity.com/profiles/987654321/",
-                            "avatar": "https://avatars.steamstatic.com/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb.jpg",
-                            "avatarmedium": "https://avatars.steamstatic.com/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg",
-                            "avatarfull": "https://avatars.steamstatic.com/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg",
-                            "avatarhash": "fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb",
-                            "lastlogoff": 1775409487,
-                            "personastate": 2,
-                            "personastateflags": 0,
-                        },
-                    ]
-                }
-            }
-        }
+        client.GetFriendList.return_value = load_json_object_fixture(
+            "GetFriendList.json", DOMAIN
+        )
+        client.GetSteamLevel.return_value = load_json_object_fixture(
+            "GetSteamLevel.json", DOMAIN
+        )
+        client.GetOwnedGames.return_value = load_json_object_fixture(
+            "GetOwnedGames.json", DOMAIN
+        )
+        client.GetPlayerSummaries.return_value = load_json_object_fixture(
+            "GetPlayerSummaries.json", DOMAIN
+        )
 
         yield mock_client
