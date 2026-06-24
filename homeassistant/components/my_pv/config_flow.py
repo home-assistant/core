bff7be69427c5bfa1a27c305cd90c671888ab137
Rogier van Staveren <rogier@batoid.com>
2026-06-24 22:52:03 +0200

    abort early and avoid an unnecessary connection for known devices.

--- homeassistant/components/my_pv/config_flow.py
+++ homeassistant/components/my_pv/config_flow.py
@@ -53,6 +53,14 @@ class MyPVConfigFlow(ConfigFlow, domain=DOMAIN):
             discovery_info.ip_address,
         )
 
+        serial_number = discovery_info.properties.get("serialno")
+        if serial_number:
+            await self.async_set_unique_id(serial_number)
+            # Update host ip address when device is already configured and abort.
+            self._abort_if_unique_id_configured(
+                updates={CONF_HOST: discovery_info.ip_address}
+            )
+
         self._host = str(discovery_info.ip_address)
 
         return await self.async_step_discovery_confirm()
