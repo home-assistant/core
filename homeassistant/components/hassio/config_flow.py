af897381723aaf02ff8f34e1fde483e8e6c9dcb9
Mike Degatano <michael.degatano@gmail.com>
2026-06-25 21:10:35 +0000

    Refactor hassio to persist state in config entry

--- homeassistant/components/hassio/config_flow.py
+++ homeassistant/components/hassio/config_flow.py
@@ -3,8 +3,14 @@
 from typing import Any
 
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.const import HASSIO_USER_NAME
 
-from .const import DOMAIN
+from .const import (
+    DATA_HASSIO_SUPERVISOR_USER,
+    DEFAULT_UPDATE_OPTIONS,
+    DOMAIN,
+    ENTRY_DATA_USER,
+)
 
 
 class HassIoConfigFlow(ConfigFlow, domain=DOMAIN):
@@ -16,4 +22,12 @@ class HassIoConfigFlow(ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle the initial step."""
-        return self.async_create_entry(title="Supervisor", data={})
+        data: dict[str, Any] = {}
+        if (user := self.hass.data.get(DATA_HASSIO_SUPERVISOR_USER)) is not None:
+            data[ENTRY_DATA_USER] = user.id
+
+        return self.async_create_entry(
+            title=HASSIO_USER_NAME,
+            data=data,
+            options=DEFAULT_UPDATE_OPTIONS,
+        )
