ba068e59972283715b6161e5c2d1ec1362e932cc
Greg Haines <greg@greghaines.net>
2026-06-28 18:15:57 -0600

    Redact altitude, latitude, and longitude from diagnostics

--- homeassistant/components/centriconnect/diagnostics.py
+++ homeassistant/components/centriconnect/diagnostics.py
@@ -2,9 +2,12 @@
 
 from typing import Any
 
+from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.core import HomeAssistant
 
 from . import CentriConnectConfigEntry
+
+TO_REDACT = {"Latitude", "Longitude", "Altitude"}
 
 
 async def async_get_config_entry_diagnostics(
@@ -19,5 +22,5 @@ async def async_get_config_entry_diagnostics(
             "hardware_version": coord.device_info.hardware_version,
             "lte_version": coord.device_info.lte_version,
         },
-        "tank_data": coord.data.raw_data.copy(),
+        "tank_data": async_redact_data(coord.data.raw_data, TO_REDACT),
     }
