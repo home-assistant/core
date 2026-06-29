e0f5c99fdf823daa0cc1cf4817b0758d27a646a3
Franck Nijhof <frenck@frenck.nl>
2026-06-29 15:33:51 +0000

    Activate the probatio shim for script entrypoints

--- script/__init__.py
+++ script/__init__.py
@@ -1,1 +1,8 @@
 """Home Assistant scripts."""
+
+from probatio.compat import install_as_voluptuous
+
+# Scripts such as hassfest (and the libraries they import) use voluptuous. Alias it
+# to probatio before anything imports it, the same as homeassistant/__init__.py does
+# for the application itself. This must run before the first `import voluptuous`.
+install_as_voluptuous()
