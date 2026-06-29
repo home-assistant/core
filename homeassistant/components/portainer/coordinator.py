28bf1d8cce4e9272c0995a61ac0929b428b5ff78
Erwin Douna <e.douna@gmail.com>
2026-06-29 11:04:02 +0200

    Portainer add utility.py for duplicate code (#175082)

--- homeassistant/components/portainer/coordinator.py
+++ homeassistant/components/portainer/coordinator.py
@@ -34,6 +34,7 @@ from homeassistant.exceptions import ConfigEntryAuthFailed
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 from .const import DOMAIN
+from .util import sanitize_container_name
 
 type PortainerConfigEntry = ConfigEntry[PortainerCoordinator]
 
@@ -263,7 +264,7 @@ class PortainerCoordinator(
 
             # Map containers, started and stopped
             for container in containers:
-                container_name = self._get_container_name(container.names[0])
+                container_name = sanitize_container_name(container.names[0])
                 prev_container = (
                     prev_endpoint.containers.get(container_name)
                     if prev_endpoint
@@ -313,7 +314,7 @@ class PortainerCoordinator(
                 container_stats = dict(
                     zip(
                         (
-                            self._get_container_name(container.names[0])
+                            sanitize_container_name(container.names[0])
                             for container in active_containers
                         ),
                         await asyncio.gather(
@@ -430,10 +431,6 @@ class PortainerCoordinator(
             ]
             for stack_callback in self.new_stacks_callbacks:
                 stack_callback(new_stack_data)
-
-    def _get_container_name(self, container_name: str) -> str:
-        """Sanitize to get a proper container name."""
-        return container_name.replace("/", " ").strip()
 
 
 class PortainerDockerDiskSpaceCoordinator(
