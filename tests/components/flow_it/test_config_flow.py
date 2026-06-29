d5bb2f387988352f62b7b2df50c291d7949bf637
Alberto Geniola <albertogeniola@gmail.com>
2026-06-29 09:00:56 +0200

    Add missing type annotations.

--- tests/components/flow_it/test_config_flow.py
+++ tests/components/flow_it/test_config_flow.py
@@ -18,10 +18,10 @@ pytestmark = pytest.mark.usefixtures("mock_setup_entry")
 
 
 def _get_mock_vmc(
-    info_hostname="Flow-it Device",
-    state_name="00:11:22:33:44:55",
-    exception=None,
-):
+    info_hostname: str = "Flow-it Device",
+    state_name: str = "00:11:22:33:44:55",
+    exception: Exception | None = None,
+) -> AsyncMock:
     """Return a mock FlowItVMCMachine context manager."""
     mock_vmc = AsyncMock()
     mock_vmc.get_info.return_value.hostname = info_hostname
