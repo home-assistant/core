7310783d8bde8117d60f4e01f226d878846d0e27
Hamish West <hamish@hamishwest.xyz>
2026-06-30 16:23:52 +0930

    Fix test expecting non-translated string

--- tests/components/gatus/test_coordinator.py
+++ tests/components/gatus/test_coordinator.py
@@ -44,6 +44,6 @@ async def test_coordinator_client_error(
                 )
             ),
         ),
-        pytest.raises(UpdateFailed, match="update_failed"),
+        pytest.raises(UpdateFailed, match="Error communicating with Gatus API"),
     ):
         await coordinator._async_update_data()
