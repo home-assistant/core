cf59d70b96742b80640c14fba03ab4761ea78159
Alberto Geniola <albertogeniola@gmail.com>
2026-07-01 16:07:48 +0200

    Remove reauth step logic.

--- homeassistant/components/flow_it/config_flow.py
+++ homeassistant/components/flow_it/config_flow.py
@@ -1,6 +1,5 @@
 """Config flow for Flow-it integration."""
 
-from collections.abc import Mapping
 import logging
 from typing import Any, override
 
@@ -160,65 +159,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     self._discovery_info[CONF_HOST].removesuffix(".local"),
                 )
             },
-        )
-
-    async def async_step_reauth(
-        self, entry_data: Mapping[str, Any]
-    ) -> ConfigFlowResult:
-        """Handle reauthorization request."""
-        return await self.async_step_reauth_confirm()
-
-    async def async_step_reauth_confirm(
-        self, user_input: dict[str, Any] | None = None
-    ) -> ConfigFlowResult:
-        """Handle a reauthorization flow."""
-        errors: dict[str, str] = {}
-        reauth_entry = self._get_reauth_entry()
-
-        if user_input is not None:
-            data = {
-                CONF_HOST: reauth_entry.data[CONF_HOST],
-                CONF_USERNAME: user_input[CONF_USERNAME],
-                CONF_PASSWORD: user_input[CONF_PASSWORD],
-            }
-
-            try:
-                await validate_input(self.hass, data)
-            except FlowItAuthError:
-                errors["base"] = "invalid_auth"
-            except FlowItConnectionError:
-                errors["base"] = "cannot_connect"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
-            else:
-                return self.async_update_reload_and_abort(
-                    reauth_entry,
-                    data={**reauth_entry.data, **user_input},
-                )
-
-        data_schema = vol.Schema(
-            {
-                vol.Required(
-                    CONF_USERNAME, default=reauth_entry.data[CONF_USERNAME]
-                ): TextSelector(
-                    TextSelectorConfig(
-                        type=TextSelectorType.TEXT, autocomplete="username"
-                    )
-                ),
-                vol.Required(CONF_PASSWORD): TextSelector(
-                    TextSelectorConfig(
-                        type=TextSelectorType.PASSWORD, autocomplete="current-password"
-                    )
-                ),
-            }
-        )
-
-        return self.async_show_form(
-            step_id="reauth_confirm",
-            data_schema=data_schema,
-            errors=errors,
-            description_placeholders={"name": reauth_entry.title},
         )
 
     @override
