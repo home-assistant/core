ebbdb85bde42a5cb777fc3e89514961cd95ce771
tr4nt0r <tr4nt0r@uber.space>
2026-07-02 16:04:12 +0000

    Add 13-key

--- tests/components/led_infrared/test_light.py
+++ tests/components/led_infrared/test_light.py
@@ -53,42 +53,110 @@ async def test_setup(
 
 
 @pytest.mark.parametrize(
-    ("service", "service_data", "expected_code"),
+    ("service", "service_data", "expected_codes"),
     [
-        (SERVICE_TURN_ON, {}, TweenLightLEDStripCode.ON),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "flash"}, TweenLightLEDStripCode.FLASH),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "strobe"}, TweenLightLEDStripCode.STROBE),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "fade"}, TweenLightLEDStripCode.FADE),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "smooth"}, TweenLightLEDStripCode.SMOOTH),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "red"}, TweenLightLEDStripCode.RED),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "green"}, TweenLightLEDStripCode.GREEN),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "blue"}, TweenLightLEDStripCode.BLUE),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "white"}, TweenLightLEDStripCode.WHITE),
+        (SERVICE_TURN_ON, {}, [TweenLightLEDStripCode.ON]),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "flash"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.FLASH],
+        ),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "strobe"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.STROBE],
+        ),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "fade"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.FADE],
+        ),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "smooth"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.SMOOTH],
+        ),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "red"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.RED],
+        ),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "green"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.GREEN],
+        ),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "blue"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.BLUE],
+        ),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "white"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.WHITE],
+        ),
         (
             SERVICE_TURN_ON,
             {ATTR_EFFECT: "orange_red"},
-            TweenLightLEDStripCode.ORANGE_RED,
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.ORANGE_RED],
         ),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "tomato"}, TweenLightLEDStripCode.TOMATO),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "tomato"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.TOMATO],
+        ),
         (
             SERVICE_TURN_ON,
             {ATTR_EFFECT: "light_green"},
-            TweenLightLEDStripCode.LIGHT_GREEN,
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.LIGHT_GREEN],
         ),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "sky_blue"}, TweenLightLEDStripCode.SKY_BLUE),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "cyan"}, TweenLightLEDStripCode.CYAN),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "sky_blue"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.SKY_BLUE],
+        ),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "cyan"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.CYAN],
+        ),
         (
             SERVICE_TURN_ON,
             {ATTR_EFFECT: "rebecca_purple"},
-            TweenLightLEDStripCode.REBECCA_PURPLE,
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.REBECCA_PURPLE],
         ),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "orange"}, TweenLightLEDStripCode.ORANGE),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "turquoise"}, TweenLightLEDStripCode.TURQUOISE),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "purple"}, TweenLightLEDStripCode.PURPLE),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "yellow"}, TweenLightLEDStripCode.YELLOW),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "dark_cyan"}, TweenLightLEDStripCode.DARK_CYAN),
-        (SERVICE_TURN_ON, {ATTR_EFFECT: "plum"}, TweenLightLEDStripCode.PLUM),
-        (SERVICE_TURN_OFF, {}, TweenLightLEDStripCode.OFF),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "orange"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.ORANGE],
+        ),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "turquoise"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.TURQUOISE],
+        ),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "purple"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.PURPLE],
+        ),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "yellow"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.YELLOW],
+        ),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "dark_cyan"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.DARK_CYAN],
+        ),
+        (
+            SERVICE_TURN_ON,
+            {ATTR_EFFECT: "plum"},
+            [TweenLightLEDStripCode.ON, TweenLightLEDStripCode.PLUM],
+        ),
+        (SERVICE_TURN_OFF, {}, [TweenLightLEDStripCode.OFF]),
     ],
 )
 @pytest.mark.usefixtures("mock_infrared_receiver_entity", "led_strip_codes")
@@ -98,7 +166,7 @@ async def test_light_actions(
     mock_infrared_emitter_entity: MockInfraredEmitterEntity,
     service: str,
     service_data: dict[str, str],
-    expected_code: TweenLightLEDStripCode,
+    expected_codes: list[TweenLightLEDStripCode],
 ) -> None:
     """Test light actions."""
 
@@ -115,5 +183,5 @@ async def test_light_actions(
         blocking=True,
     )
 
-    assert len(mock_infrared_emitter_entity.send_command_calls) == 1
-    assert mock_infrared_emitter_entity.send_command_calls[0] == expected_code
+    assert len(mock_infrared_emitter_entity.send_command_calls) == len(expected_codes)
+    assert mock_infrared_emitter_entity.send_command_calls == expected_codes
