37304537489f174caab1a17d5e4e23cef1b70386
Erwin Douna <e.douna@gmail.com>
2026-07-03 12:27:38 +0200

    Move Shopping List services to async_setup

--- homeassistant/components/shopping_list/__init__.py
+++ homeassistant/components/shopping_list/__init__.py
@@ -55,6 +55,7 @@ SERVICE_SORT_SCHEMA = vol.Schema(
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Initialize the shopping list."""
+    async_register_services(hass)
 
     if DOMAIN not in config:
         return True
@@ -62,6 +63,106 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     hass.async_create_task(_async_setup(hass))
 
     return True
+
+
+@callback
+def async_register_services(hass: HomeAssistant) -> None:
+    """Register shopping list services."""
+
+    async def add_item_service(call: ServiceCall) -> None:
+        """Add an item with `name`."""
+        await _get_shopping_data(hass).async_add(call.data[ATTR_NAME])
+
+    async def remove_item_service(call: ServiceCall) -> None:
+        """Remove the first item with matching `name`."""
+        data = _get_shopping_data(hass)
+        name = call.data[ATTR_NAME]
+
+        try:
+            item = [item for item in data.items if item["name"] == name][0]
+        # pylint: disable-next=home-assistant-action-swallowed-exception
+        except IndexError:
+            _LOGGER.error("Removing of item failed: %s cannot be found", name)
+        else:
+            await data.async_remove(str(item["id"]))
+
+    async def complete_item_service(call: ServiceCall) -> None:
+        """Mark the first item with matching `name` as completed."""
+        name = call.data[ATTR_NAME]
+        try:
+            await _get_shopping_data(hass).async_complete(name)
+        # pylint: disable-next=home-assistant-action-swallowed-exception
+        except NoMatchingShoppingListItem:
+            _LOGGER.error("Completing of item failed: %s cannot be found", name)
+
+    async def incomplete_item_service(call: ServiceCall) -> None:
+        """Mark the first item with matching `name` as incomplete."""
+        data = _get_shopping_data(hass)
+        name = call.data[ATTR_NAME]
+
+        try:
+            item = [item for item in data.items if item["name"] == name][0]
+        # pylint: disable-next=home-assistant-action-swallowed-exception
+        except IndexError:
+            _LOGGER.error("Restoring of item failed: %s cannot be found", name)
+        else:
+            await data.async_update(str(item["id"]), {"name": name, "complete": False})
+
+    async def complete_all_service(call: ServiceCall) -> None:
+        """Mark all items in the list as complete."""
+        await _get_shopping_data(hass).async_update_list({"complete": True})
+
+    async def incomplete_all_service(call: ServiceCall) -> None:
+        """Mark all items in the list as incomplete."""
+        await _get_shopping_data(hass).async_update_list({"complete": False})
+
+    async def clear_completed_items_service(call: ServiceCall) -> None:
+        """Clear all completed items from the list."""
+        await _get_shopping_data(hass).async_clear_completed()
+
+    async def sort_list_service(call: ServiceCall) -> None:
+        """Sort all items by name."""
+        await _get_shopping_data(hass).async_sort(call.data[ATTR_REVERSE])
+
+    hass.services.async_register(
+        DOMAIN, SERVICE_ADD_ITEM, add_item_service, schema=SERVICE_ITEM_SCHEMA
+    )
+    hass.services.async_register(
+        DOMAIN, SERVICE_REMOVE_ITEM, remove_item_service, schema=SERVICE_ITEM_SCHEMA
+    )
+    hass.services.async_register(
+        DOMAIN, SERVICE_COMPLETE_ITEM, complete_item_service, schema=SERVICE_ITEM_SCHEMA
+    )
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_INCOMPLETE_ITEM,
+        incomplete_item_service,
+        schema=SERVICE_ITEM_SCHEMA,
+    )
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_COMPLETE_ALL,
+        complete_all_service,
+        schema=SERVICE_LIST_SCHEMA,
+    )
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_INCOMPLETE_ALL,
+        incomplete_all_service,
+        schema=SERVICE_LIST_SCHEMA,
+    )
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_CLEAR_COMPLETED_ITEMS,
+        clear_completed_items_service,
+        schema=SERVICE_LIST_SCHEMA,
+    )
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_SORT,
+        sort_list_service,
+        schema=SERVICE_SORT_SCHEMA,
+    )
 
 
 async def _async_setup(hass: HomeAssistant) -> None:
@@ -88,112 +189,8 @@ async def async_setup_entry(
     hass: HomeAssistant, config_entry: ShoppingListConfigEntry
 ) -> bool:
     """Set up shopping list from config flow."""
-
-    async def add_item_service(call: ServiceCall) -> None:
-        """Add an item with `name`."""
-        await config_entry.runtime_data.async_add(call.data[ATTR_NAME])
-
-    async def remove_item_service(call: ServiceCall) -> None:
-        """Remove the first item with matching `name`."""
-        data = config_entry.runtime_data
-        name = call.data[ATTR_NAME]
-
-        try:
-            item = [item for item in data.items if item["name"] == name][0]
-        # pylint: disable-next=home-assistant-action-swallowed-exception
-        except IndexError:
-            _LOGGER.error("Removing of item failed: %s cannot be found", name)
-        else:
-            await data.async_remove(str(item["id"]))
-
-    async def complete_item_service(call: ServiceCall) -> None:
-        """Mark the first item with matching `name` as completed."""
-        name = call.data[ATTR_NAME]
-        try:
-            await config_entry.runtime_data.async_complete(name)
-        # pylint: disable-next=home-assistant-action-swallowed-exception
-        except NoMatchingShoppingListItem:
-            _LOGGER.error("Completing of item failed: %s cannot be found", name)
-
-    async def incomplete_item_service(call: ServiceCall) -> None:
-        """Mark the first item with matching `name` as incomplete."""
-        data = config_entry.runtime_data
-        name = call.data[ATTR_NAME]
-
-        try:
-            item = [item for item in data.items if item["name"] == name][0]
-        # pylint: disable-next=home-assistant-action-swallowed-exception
-        except IndexError:
-            _LOGGER.error("Restoring of item failed: %s cannot be found", name)
-        else:
-            await data.async_update(str(item["id"]), {"name": name, "complete": False})
-
-    async def complete_all_service(call: ServiceCall) -> None:
-        """Mark all items in the list as complete."""
-        await data.async_update_list({"complete": True})
-
-    async def incomplete_all_service(call: ServiceCall) -> None:
-        """Mark all items in the list as incomplete."""
-        await data.async_update_list({"complete": False})
-
-    async def clear_completed_items_service(call: ServiceCall) -> None:
-        """Clear all completed items from the list."""
-        await data.async_clear_completed()
-
-    async def sort_list_service(call: ServiceCall) -> None:
-        """Sort all items by name."""
-        await data.async_sort(call.data[ATTR_REVERSE])
-
     data = config_entry.runtime_data = ShoppingData(hass)
     await data.async_load()
-
-    # pylint: disable-next=home-assistant-service-registered-in-setup-entry
-    hass.services.async_register(
-        DOMAIN, SERVICE_ADD_ITEM, add_item_service, schema=SERVICE_ITEM_SCHEMA
-    )
-    # pylint: disable-next=home-assistant-service-registered-in-setup-entry
-    hass.services.async_register(
-        DOMAIN, SERVICE_REMOVE_ITEM, remove_item_service, schema=SERVICE_ITEM_SCHEMA
-    )
-    # pylint: disable-next=home-assistant-service-registered-in-setup-entry
-    hass.services.async_register(
-        DOMAIN, SERVICE_COMPLETE_ITEM, complete_item_service, schema=SERVICE_ITEM_SCHEMA
-    )
-    # pylint: disable-next=home-assistant-service-registered-in-setup-entry
-    hass.services.async_register(
-        DOMAIN,
-        SERVICE_INCOMPLETE_ITEM,
-        incomplete_item_service,
-        schema=SERVICE_ITEM_SCHEMA,
-    )
-    # pylint: disable-next=home-assistant-service-registered-in-setup-entry
-    hass.services.async_register(
-        DOMAIN,
-        SERVICE_COMPLETE_ALL,
-        complete_all_service,
-        schema=SERVICE_LIST_SCHEMA,
-    )
-    # pylint: disable-next=home-assistant-service-registered-in-setup-entry
-    hass.services.async_register(
-        DOMAIN,
-        SERVICE_INCOMPLETE_ALL,
-        incomplete_all_service,
-        schema=SERVICE_LIST_SCHEMA,
-    )
-    # pylint: disable-next=home-assistant-service-registered-in-setup-entry
-    hass.services.async_register(
-        DOMAIN,
-        SERVICE_CLEAR_COMPLETED_ITEMS,
-        clear_completed_items_service,
-        schema=SERVICE_LIST_SCHEMA,
-    )
-    # pylint: disable-next=home-assistant-service-registered-in-setup-entry
-    hass.services.async_register(
-        DOMAIN,
-        SERVICE_SORT,
-        sort_list_service,
-        schema=SERVICE_SORT_SCHEMA,
-    )
 
     hass.http.register_view(ShoppingListView)
     hass.http.register_view(CreateShoppingListItemView)
