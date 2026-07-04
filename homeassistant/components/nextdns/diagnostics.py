ce64ea894cbc3c9144bbc33b21b61a8491da0cbf
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-07-04 12:56:02 +0200

    Do not use subentry.title as a key in diagnostics

--- homeassistant/components/nextdns/diagnostics.py
+++ homeassistant/components/nextdns/diagnostics.py
@@ -17,17 +17,20 @@ async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, config_entry: NextDnsConfigEntry
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
-    profiles_data = {}
+    profiles_data: list[dict[str, Any]] = []
     for subentry_id, profile_data in config_entry.runtime_data.profiles.items():
         subentry = config_entry.subentries[subentry_id]
-        profiles_data[subentry.title] = {
-            "dnssec_coordinator_data": asdict(profile_data.dnssec.data),
-            "encryption_coordinator_data": asdict(profile_data.encryption.data),
-            "ip_versions_coordinator_data": asdict(profile_data.ip_versions.data),
-            "protocols_coordinator_data": asdict(profile_data.protocols.data),
-            "settings_coordinator_data": asdict(profile_data.settings.data),
-            "status_coordinator_data": asdict(profile_data.status.data),
-        }
+        profiles_data.append(
+            {
+                "subentry_title": subentry.title,
+                "dnssec_coordinator_data": asdict(profile_data.dnssec.data),
+                "encryption_coordinator_data": asdict(profile_data.encryption.data),
+                "ip_versions_coordinator_data": asdict(profile_data.ip_versions.data),
+                "protocols_coordinator_data": asdict(profile_data.protocols.data),
+                "settings_coordinator_data": asdict(profile_data.settings.data),
+                "status_coordinator_data": asdict(profile_data.status.data),
+            }
+        )
 
     return {
         "config_entry": async_redact_data(config_entry.as_dict(), TO_REDACT),
