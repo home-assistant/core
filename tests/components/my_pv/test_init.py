d77dbc63dbcffa015432c32074dbb3cd8a02fd54
Rogier van Staveren <rogier@batoid.com>
2026-07-06 14:12:31 +0200

    Add test for failed first refresh

--- tests/components/my_pv/test_init.py
+++ tests/components/my_pv/test_init.py
@@ -1,11 +1,13 @@
 """Test the my-PV init."""
 
-from unittest.mock import AsyncMock
+from unittest.mock import AsyncMock, patch
 
+from my_pv.exceptions import MyPVAuthenticationError
 import pytest
 
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import UpdateFailed
 
 from tests.common import MockConfigEntry
 
@@ -38,6 +40,43 @@ async def test_async_setup_entry_cannot_connect(
     await hass.async_block_till_done()
 
     assert mock_config_entry.state is ConfigEntryState.SETUP_RETRY
+
+
+async def test_async_setup_entry_auth_error(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_my_pv_client: AsyncMock,
+) -> None:
+    """Test successful setup of a config entry."""
+    mock_config_entry.add_to_hass(hass)
+
+    mock_my_pv_client.connect.side_effect = MyPVAuthenticationError()
+
+    assert not await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert mock_config_entry.state is ConfigEntryState.SETUP_ERROR
+
+
+@pytest.mark.usefixtures("mock_my_pv_client")
+async def test_async_setup_entry_failed_first_refresh(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_my_pv_client: AsyncMock,
+) -> None:
+    """Test successful setup of a config entry."""
+    mock_config_entry.add_to_hass(hass)
+
+    with (
+        patch(
+            "homeassistant.components.my_pv.MyPVCoordinator.async_config_entry_first_refresh",
+            side_effect=UpdateFailed(),
+        ),
+    ):
+        assert not await hass.config_entries.async_setup(mock_config_entry.entry_id)
+        await hass.async_block_till_done()
+
+    assert mock_config_entry.state is ConfigEntryState.SETUP_ERROR
 
 
 @pytest.mark.usefixtures("mock_my_pv_client")
