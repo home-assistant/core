945392142c5f8b90c472a7dcde8b1d8fc07fa1a3
Rogier van Staveren <rogier@batoid.com>
2026-07-06 13:16:36 +0200

    Use usefixtures when possible

--- tests/components/my_pv/test_init.py
+++ tests/components/my_pv/test_init.py
@@ -1,24 +1,19 @@
 """Test the my-PV init."""
 
-from unittest.mock import AsyncMock
+import pytest
 
-from homeassistant.components.my_pv.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
-
-from . import ELWA2_SERIAL_NUMBER
 
 from tests.common import MockConfigEntry
 
 
+@pytest.mark.usefixtures("mock_my_pv_client")
 async def test_async_setup_entry_success(
     hass: HomeAssistant,
-    mock_my_pv_client: AsyncMock,
+    mock_config_entry: MockConfigEntry,
 ) -> None:
     """Test successful setup of a config entry."""
-    mock_config_entry = MockConfigEntry(
-        domain=DOMAIN, unique_id=ELWA2_SERIAL_NUMBER, data={"host": "127.0.0.1"}
-    )
     mock_config_entry.add_to_hass(hass)
 
     assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
@@ -27,14 +22,12 @@ async def test_async_setup_entry_success(
     assert mock_config_entry.state is ConfigEntryState.LOADED
 
 
+@pytest.mark.usefixtures("mock_my_pv_client")
 async def test_async_unload_entry(
     hass: HomeAssistant,
-    mock_my_pv_client: AsyncMock,
+    mock_config_entry: MockConfigEntry,
 ) -> None:
     """Test unloading a config entry."""
-    mock_config_entry = MockConfigEntry(
-        domain=DOMAIN, unique_id=ELWA2_SERIAL_NUMBER, data={"host": "127.0.0.1"}
-    )
     mock_config_entry.add_to_hass(hass)
 
     assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
