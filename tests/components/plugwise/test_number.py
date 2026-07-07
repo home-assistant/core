b8dcd355410dd8addda20ff1419d7cd52cf13c51
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-07-07 17:32:08 +0200

    Remove 2nd obsolete number testcase

--- tests/components/plugwise/test_number.py
+++ tests/components/plugwise/test_number.py
@@ -65,31 +65,3 @@ async def test_adam_temperature_offset_out_of_bounds_change(
             },
             blocking=True,
         )
-
-
-@pytest.mark.parametrize("chosen_env", ["m_adam_heating"], indirect=True)
-@pytest.mark.parametrize("cooling_present", [False], indirect=True)
-async def test_adam_dhw_setpoint_change(
-    hass: HomeAssistant,
-    mock_smile_adam_heat_cool: MagicMock,
-    init_integration: MockConfigEntry,
-) -> None:
-    """Test changing of number entities."""
-    state = hass.states.get("number.opentherm_domestic_hot_water_setpoint")
-    assert state
-    assert float(state.state) == 60.0
-
-    await hass.services.async_call(
-        NUMBER_DOMAIN,
-        SERVICE_SET_VALUE,
-        {
-            ATTR_ENTITY_ID: "number.opentherm_domestic_hot_water_setpoint",
-            ATTR_VALUE: 55,
-        },
-        blocking=True,
-    )
-
-    assert mock_smile_adam_heat_cool.set_number.call_count == 1
-    mock_smile_adam_heat_cool.set_number.assert_called_with(
-        "056ee145a816487eaa69243c3280f8bf", "max_dhw_temperature", 55.0
-    )
