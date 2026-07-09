db1a319e61fbce1ea0ccb3b11b3f5a1f3a804560
David-Wu1119 <133224895+David-Wu1119@users.noreply.github.com>
2026-07-10 02:00:46 +0800

    Migrate ruuvi_gateway to use entry.runtime_data

--- homeassistant/components/ruuvi_gateway/__init__.py
+++ homeassistant/components/ruuvi_gateway/__init__.py
@@ -12,12 +12,16 @@ from .models import RuuviGatewayRuntimeData
 
 _LOGGER = logging.getLogger(DOMAIN)
 
+type RuuviGatewayConfigEntry = ConfigEntry[RuuviGatewayRuntimeData]
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: RuuviGatewayConfigEntry
+) -> bool:
     """Set up Ruuvi Gateway from a config entry."""
     coordinator = RuuviGatewayUpdateCoordinator(hass, entry, _LOGGER)
     scanner, unload_scanner = async_connect_scanner(hass, entry, coordinator)
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = RuuviGatewayRuntimeData(
+    entry.runtime_data = RuuviGatewayRuntimeData(
         update_coordinator=coordinator,
         scanner=scanner,
     )
@@ -25,9 +29,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(
+    hass: HomeAssistant, entry: RuuviGatewayConfigEntry
+) -> bool:
     """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, []):
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, [])
