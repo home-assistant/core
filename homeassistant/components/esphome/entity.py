sakthisudarshan/jscpd3

esphome: feat: allow ble connection

homeassistant/components/esphome/entity.py
@@ -28,6 +28,7 @@ from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
+from .config_flow import format_mac
 from .const import DOMAIN
 
 # Import config flow so that it's added to the registry
@@ -131,7 +132,18 @@ def async_static_info_updated(
         else:
             # Entity now belongs to the main device
             new_device = dev_reg.async_get_device(
-                connections={(dr.CONNECTION_NETWORK_MAC, device_info.mac_address)}
+                connections={
+                    (
+                        dr.CONNECTION_BLUETOOTH
+                        if entry_data.connection_type == "ble"
+                        else dr.CONNECTION_NETWORK_MAC,
+                        format_mac(
+                            device_info.mac_address
+                            if entry_data.connection_type == "tcp"
+                            else entry_data.host
+                        ),
+                    )
+                }
             )
 
         if new_device:
@@ -368,7 +380,18 @@ class EsphomeEntity(EsphomeBaseEntity, Generic[_InfoT, _StateT]):  # noqa: UP046
         else:
             # Entity belongs to the main device
             self._attr_device_info = DeviceInfo(
-                connections={(dr.CONNECTION_NETWORK_MAC, device_info.mac_address)}
+                connections={
+                    (
+                        dr.CONNECTION_BLUETOOTH
+                        if entry_data.connection_type == "ble"
+                        else dr.CONNECTION_NETWORK_MAC,
+                        format_mac(
+                            device_info.mac_address
+                            if entry_data.connection_type == "tcp"
+                            else entry_data.host
+                        ),
+                    )
+                }
             )
 
     async def async_added_to_hass(self) -> None:
@@ -505,7 +528,18 @@ class EsphomeAssistEntity(EsphomeBaseEntity):
             f"{device_info.mac_address}-{self.entity_description.key}"
         )
         self._attr_device_info = DeviceInfo(
-            connections={(dr.CONNECTION_NETWORK_MAC, device_info.mac_address)}
+            connections={
+                (
+                    dr.CONNECTION_BLUETOOTH
+                    if entry_data.connection_type == "ble"
+                    else dr.CONNECTION_NETWORK_MAC,
+                    format_mac(
+                        device_info.mac_address
+                        if entry_data.connection_type == "tcp"
+                        else entry_data.host
+                    ),
+                )
+            }
         )
 
     async def async_added_to_hass(self) -> None:

homeassistant/components/esphome/entry_data.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass, field
 from functools import partial
 import logging
 from operator import delitem
-from typing import TYPE_CHECKING, Any, Final, TypedDict, cast
+from typing import TYPE_CHECKING, Any, Final, Literal, TypedDict, cast
 
 from aioesphomeapi import (
     COMPONENT_TYPE_TO_INFO,
@@ -138,6 +138,7 @@ class RuntimeEntryData:
     entry_id: str
     title: str
     client: APIClient
+    connection_type: Literal["tcp", "ble"]
     store: ESPHomeStorage
     state: defaultdict[type[EntityState], dict[int, EntityState]] = field(
         default_factory=lambda: defaultdict(dict)
@@ -191,6 +192,7 @@ class RuntimeEntryData:
     entity_removal_callbacks: dict[EntityInfoKey, list[CALLBACK_TYPE]] = field(
         default_factory=dict
     )
+    host: str | None = None
 
     @property
     def name(self) -> str:

homeassistant/components/esphome/update.py
@@ -19,7 +19,7 @@ from homeassistant.components.update import (
 from homeassistant.core import CALLBACK_TYPE, HomeAssistant, callback
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import device_registry as dr
-from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.device_registry import DeviceInfo, format_mac
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util.enum import try_parse_enum
@@ -122,7 +122,16 @@ class ESPHomeDashboardUpdateEntity(
         self._attr_unique_id = entry_data.device_info.mac_address
         self._attr_device_info = DeviceInfo(
             connections={
-                (dr.CONNECTION_NETWORK_MAC, entry_data.device_info.mac_address)
+                (
+                    dr.CONNECTION_BLUETOOTH
+                    if entry_data.connection_type == "ble"
+                    else dr.CONNECTION_NETWORK_MAC,
+                    format_mac(
+                        entry_data.device_info.mac_address
+                        if entry_data.connection_type == "tcp"
+                        else entry_data.host
+                    ),
+                )
             }
         )
         self._install_lock = asyncio.Lock()

home-assistant/core

homeassistant/components/esphome/entity.py
@@ -29,6 +29,7 @@ from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
+from .config_flow import format_mac
 from .const import DOMAIN
 
 # Import config flow so that it's added to the registry
@@ -127,7 +128,18 @@ def async_static_info_updated(
         else:
             # Entity now belongs to the main device
             new_device = dev_reg.async_get_device(
-                connections={(dr.CONNECTION_NETWORK_MAC, device_info.mac_address)}
+                connections={
+                    (
+                        dr.CONNECTION_BLUETOOTH
+                        if entry_data.transport == "ble"
+                        else dr.CONNECTION_NETWORK_MAC,
+                        format_mac(
+                            device_info.mac_address
+                            if entry_data.transport == "ip"
+                            else entry_data.host
+                        ),
+                    )
+                }
             )
 
         if new_device:
@@ -369,7 +381,18 @@ class EsphomeEntity(EsphomeBaseEntity, Generic[_InfoT, _StateT]):  # noqa: UP046
         else:
             # Entity belongs to the main device
             self._attr_device_info = DeviceInfo(
-                connections={(dr.CONNECTION_NETWORK_MAC, device_info.mac_address)}
+                connections={
+                    (
+                        dr.CONNECTION_BLUETOOTH
+                        if entry_data.transport == "ble"
+                        else dr.CONNECTION_NETWORK_MAC,
+                        format_mac(
+                            device_info.mac_address
+                            if entry_data.transport == "ip"
+                            else entry_data.host
+                        ),
+                    )
+                }
             )
 
     @override
@@ -507,7 +530,18 @@ class EsphomeAssistEntity(EsphomeBaseEntity):
             f"{device_info.mac_address}-{self.entity_description.key}"
         )
         self._attr_device_info = DeviceInfo(
-            connections={(dr.CONNECTION_NETWORK_MAC, device_info.mac_address)}
+            connections={
+                (
+                    dr.CONNECTION_BLUETOOTH
+                    if entry_data.transport == "ble"
+                    else dr.CONNECTION_NETWORK_MAC,
+                    format_mac(
+                        device_info.mac_address
+                        if entry_data.transport == "ip"
+                        else entry_data.host
+                    ),
+                )
+            }
         )
 
     @override

homeassistant/components/esphome/entry_data.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass, field
 from functools import partial
 import logging
 from operator import delitem
-from typing import TYPE_CHECKING, Any, Final, TypedDict, cast
+from typing import TYPE_CHECKING, Any, Final, Literal, TypedDict, cast
 
 from aioesphomeapi import (
     COMPONENT_TYPE_TO_INFO,
@@ -122,6 +122,7 @@ class RuntimeEntryData:
     entry_id: str
     title: str
     client: APIClient
+    transport: Literal["ip", "ble"]
     store: ESPHomeStorage
     state: defaultdict[type[EntityState], dict[int, EntityState]] = field(
         default_factory=lambda: defaultdict(dict)
@@ -177,6 +178,7 @@ class RuntimeEntryData:
     entity_removal_callbacks: dict[EntityInfoKey, list[CALLBACK_TYPE]] = field(
         default_factory=dict
     )
+    host: str | None = None
 
     @property
     def name(self) -> str:

homeassistant/components/esphome/update.py
@@ -19,7 +19,7 @@ from homeassistant.components.update import (
 from homeassistant.core import CALLBACK_TYPE, HomeAssistant, callback
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import device_registry as dr
-from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.device_registry import DeviceInfo, format_mac
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util.enum import try_parse_enum
@@ -122,7 +122,16 @@ class ESPHomeDashboardUpdateEntity(
         self._attr_unique_id = entry_data.device_info.mac_address
         self._attr_device_info = DeviceInfo(
             connections={
-                (dr.CONNECTION_NETWORK_MAC, entry_data.device_info.mac_address)
+                (
+                    dr.CONNECTION_BLUETOOTH
+                    if entry_data.transport == "ble"
+                    else dr.CONNECTION_NETWORK_MAC,
+                    format_mac(
+                        entry_data.device_info.mac_address
+                        if entry_data.transport == "ip"
+                        else entry_data.host
+                    ),
+                )
             }
         )
         self._install_lock = asyncio.Lock()