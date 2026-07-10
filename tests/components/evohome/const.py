883a51c4bb4476fc84d4fd438b48f07f602ed80d
David Bonnes <zxdavb@bonnes.me>
2026-07-10 05:44:24 +0000

    tweak multi-location fixtures

--- tests/components/evohome/const.py
+++ tests/components/evohome/const.py
@@ -15,9 +15,9 @@ TEST_INSTALLS: Final = (
     "h099625",  # RoundThermostat
     "h139906",  # zone with null schedule
     "h157546",  # tcs with long 8-digit system_id
-    "sys_004",  # RoundModulation; multi-location account (location_idx=0 is used)
-    "sys_006",  # multi-location account (location_idx=0 is used)
+    "sys_004",  # RoundModulation; multi-location account, empty gateway
+    "sys_006",  # multi-location account, lots of faults
 )
 #   "botched",  # as default: but with activeFaults, ghost zones & unknown types
 
-TEST_INSTALLS_WITH_DHW: Final = ("default", "botched")
+TEST_INSTALLS_WITH_DHW: Final = ("default", "botched", "sys_006")
