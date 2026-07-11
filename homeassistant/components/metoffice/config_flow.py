9df83448f905c2dc651fb51c5500b7ce0f399a75
Tom Carpenter <t.carpenter@leeds.ac.uk>
2026-07-11 17:48:55 +0000

    Revert custom forecast name ...

--- homeassistant/components/metoffice/config_flow.py
+++ homeassistant/components/metoffice/config_flow.py
@@ -80,8 +80,7 @@ class MetOfficeConfigFlow(ConfigFlow, domain=DOMAIN):
             errors = result["errors"]
 
             if not errors:
-                if user_input[CONF_NAME] is None:
-                    user_input[CONF_NAME] = result["site_name"]
+                user_input[CONF_NAME] = result["site_name"]
                 return self.async_create_entry(
                     title=user_input[CONF_NAME], data=user_input
                 )
@@ -95,7 +94,6 @@ class MetOfficeConfigFlow(ConfigFlow, domain=DOMAIN):
                 vol.Required(
                     CONF_LONGITUDE, default=self.hass.config.longitude
                 ): cv.longitude,
-                vol.Optional(CONF_NAME, default=None): str,
             },
         )
 
