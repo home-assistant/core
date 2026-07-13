ef9ef35d7d9998256b71d75efe1f750c36cea91a
Raphael Hehl <7577984+RaHehl@users.noreply.github.com>
2026-07-13 12:23:53 +0200

    Migrate UniFi Protect camera enumeration to the public API (#176267)

--- homeassistant/components/unifiprotect/utils.py
+++ homeassistant/components/unifiprotect/utils.py
@@ -11,7 +11,7 @@ from aiohttp import CookieJar
 from uiprotect import ProtectApiClient
 from uiprotect.data import (
     Bootstrap,
-    CameraChannel,
+    ChannelQuality,
     Light,
     LightModeEnableType,
     LightModeType,
@@ -134,14 +134,12 @@ def async_create_api_client(
 
 
 @callback
-def get_camera_base_name(channel: CameraChannel) -> str:
-    """Get base name for cameras channel."""
+def get_camera_base_name(quality: ChannelQuality) -> str:
+    """Get base name for a camera's RTSPS quality channel."""
 
-    camera_name = channel.name
-    if channel.name != "Package Camera":
-        camera_name = f"{channel.name} resolution channel"
-
-    return camera_name
+    if quality is ChannelQuality.PACKAGE:
+        return "Package Camera"
+    return f"{quality.value.title()} resolution channel"
 
 
 def async_ufp_instance_command[_EntityT, **_P](
