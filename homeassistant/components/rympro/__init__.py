0dda5bb703c5c87b36b26118e71a0cf47ec5f13e
Maor <maorcc@users.noreply.github.com>
2026-07-14 14:58:42 +0300

    Retry rympro setup on OperationError instead of failing permanently (#176478)

--- homeassistant/components/rympro/__init__.py
+++ homeassistant/components/rympro/__init__.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from pyrympro import CannotConnectError, RymPro, UnauthorizedError
+from pyrympro import CannotConnectError, OperationError, RymPro, UnauthorizedError
 
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD, CONF_TOKEN, Platform
 from homeassistant.core import HomeAssistant
@@ -22,13 +22,15 @@ async def async_setup_entry(hass: HomeAssistant, entry: RymProConfigEntry) -> bo
     rympro.set_token(data[CONF_TOKEN])
     try:
         await rympro.account_info()
-    except CannotConnectError as error:
+    except (CannotConnectError, OperationError) as error:
         raise ConfigEntryNotReady from error
     except UnauthorizedError:
         try:
             token = await rympro.login(data[CONF_EMAIL], data[CONF_PASSWORD], "ha")
         except UnauthorizedError as error:
             raise ConfigEntryAuthFailed from error
+        except CannotConnectError as error:
+            raise ConfigEntryNotReady from error
         hass.config_entries.async_update_entry(
             entry,
             data={**data, CONF_TOKEN: token},
