53488fc2274cfc8fda3785c960d282bbc4fa1734
WaterCryst Developers <Mathias.Hoertnagl@watercryst.com>
2026-07-15 11:16:00 +0000

    Remove obsolete constants from WATERCryst integration

--- homeassistant/components/watercryst/const.py
+++ homeassistant/components/watercryst/const.py
@@ -3,16 +3,4 @@
 DOMAIN = "watercryst"
 DEFAULT_NAME = "BIOCAT Device"
 
-# CONF_DATA_SOURCE = "data_source"
 CONF_BSN = "bsn"
-CONF_ESN = "esn"
-CONF_LINE = "line"
-CONF_SERIES = "series"
-CONF_FW_VERSION = "fw_version"
-CONF_HW_VERSION = "hw_version"
-CONF_LATEST_FW_VERSION = "latest_fw_version"
-CONF_SYSTEM_MAC = "system_mac"
-CONF_BLE_MAC = "ble_mac"
-
-# DATA_SOURCE_CLOUD = "data_source_cloud"
-# DATA_SOURCE_LOCAL = "data_source_local"
