992fc3e90393d933efc7ccaf736eadebcdce931e
Petro <mhpetrosino@yahoo.com>
2026-07-15 13:13:15 +0000

    Add restore to light template entities

--- tests/components/template/test_light.py
+++ tests/components/template/test_light.py
@@ -37,6 +37,7 @@ from .conftest import (
     ConfigurationStyle,
     TemplatePlatformSetup,
     assert_action,
+    assert_state_and_attributes,
     async_get_flow_preview_state,
     async_trigger,
     make_test_action,
@@ -44,12 +45,15 @@ from .conftest import (
     setup_and_test_nested_unique_id,
     setup_and_test_unique_id,
     setup_entity,
+    setup_mock_template_entity_restore_state,
+    setup_restore_template_entity,
 )
 
 from tests.common import MockConfigEntry
 from tests.typing import WebSocketGenerator
 
 TEST_STATE_ENTITY_ID = "light.test_state"
+TEST_SENSOR_STATE_ENTITY_ID = "sensor.test_state"
 TEST_AVAILABILITY_ENTITY = "binary_sensor.availability"
 
 TEST_LIGHT = TemplatePlatformSetup(
@@ -57,6 +61,7 @@ TEST_LIGHT = TemplatePlatformSetup(
     "test_light",
     make_test_trigger(
         TEST_STATE_ENTITY_ID,
+        TEST_SENSOR_STATE_ENTITY_ID,
         TEST_AVAILABILITY_ENTITY,
     ),
 )
@@ -1893,3 +1898,210 @@ async def test_flow_preview(
     )
 
     assert state["state"] == STATE_ON
+
+
+@pytest.mark.parametrize(
+    "style", [ConfigurationStyle.MODERN, ConfigurationStyle.TRIGGER]
+)
+@pytest.mark.parametrize(
+    "config",
+    [
+        {
+            "state": "{{ state_attr('sensor.test_state', 'is_on') }}",
+            "turn_on": [],
+            "turn_off": [],
+            "level": "{{ state_attr('sensor.test_state', 'brightness') }}",
+            "set_level": [],
+            "temperature": "{{ state_attr('sensor.test_state', 'color_temp_kelvin') }}",
+            "set_temperature": [],
+            "effect_list": "{{ state_attr('sensor.test_state', 'effect_list') }}",
+            "effect": "{{ state_attr('sensor.test_state', 'effect') }}",
+            "set_effect": [],
+            "hs": "{{ state_attr('sensor.test_state', 'hs_color') }}",
+            "set_hs": [],
+            "min_mireds": "{{ state_attr('sensor.test_state', 'max_color_temp_kelvin') }}",
+            "max_mireds": "{{ state_attr('sensor.test_state', 'min_color_temp_kelvin') }}",
+            "rgb": "{{ state_attr('sensor.test_state', 'rgb_color') }}",
+            "set_rgb": [],
+            "rgbw": "{{ state_attr('sensor.test_state', 'rgbw_color') }}",
+            "set_rgbw": [],
+            "rgbww": "{{ state_attr('sensor.test_state', 'rgbww_color') }}",
+            "set_rgbww": [],
+            "xy": "{{ state_attr('sensor.test_state', 'xy_color') }}",
+            "set_xy": [],
+        },
+    ],
+)
+@pytest.mark.parametrize(
+    (
+        "saved_state",
+        "saved_extra_data",
+        "initial_state",
+        "initial_attributes",
+    ),
+    [
+        (
+            STATE_OFF,
+            {
+                "is_on": False,
+                "brightness": None,
+                "color_mode": "color_temp",
+                "color_temp_kelvin": None,
+                "effect_list": None,
+                "effect": None,
+                "hs_color": None,
+                "max_color_temp_kelvin": 6535,
+                "min_color_temp_kelvin": 2000,
+                "rgb_color": None,
+                "rgbw_color": None,
+                "rgbww_color": None,
+                "supported_color_modes": ["color_temp"],
+                "xy_color": None,
+            },
+            STATE_OFF,
+            {
+                "brightness": None,
+                "color_mode": None,
+                "color_temp_kelvin": None,
+                "effect_list": None,
+                "effect": None,
+                "max_color_temp_kelvin": 6535,
+                "min_color_temp_kelvin": 2000,
+                "rgb_color": None,
+                "rgbw_color": None,
+                "rgbww_color": None,
+                "supported_color_modes": [ColorMode.COLOR_TEMP],
+                "xy_color": None,
+            },
+        ),
+        (
+            STATE_UNAVAILABLE,
+            {
+                "is_on": False,
+                "brightness": None,
+                "color_mode": None,
+                "color_temp_kelvin": None,
+                "effect_list": None,
+                "effect": None,
+                "hs_color": None,
+                "max_color_temp_kelvin": 6535,
+                "min_color_temp_kelvin": 2000,
+                "rgb_color": None,
+                "rgbw_color": None,
+                "rgbww_color": None,
+                "supported_color_modes": ["onoff"],
+                "xy_color": None,
+            },
+            STATE_UNKNOWN,
+            {
+                "brightness": None,
+                "color_mode": None,
+                "color_temp_kelvin": None,
+                "effect_list": None,
+                "effect": None,
+                "max_color_temp_kelvin": 6535,
+                "min_color_temp_kelvin": 2000,
+                "rgb_color": None,
+                "rgbw_color": None,
+                "rgbww_color": None,
+                "supported_color_modes": [
+                    ColorMode.COLOR_TEMP,
+                    ColorMode.HS,
+                    ColorMode.RGB,
+                    ColorMode.RGBW,
+                    ColorMode.RGBWW,
+                    ColorMode.XY,
+                ],
+                "xy_color": None,
+            },
+        ),
+        (
+            STATE_UNKNOWN,
+            {
+                "is_on": False,
+                "brightness": None,
+                "color_mode": None,
+                "color_temp_kelvin": None,
+                "effect_list": None,
+                "effect": None,
+                "hs_color": None,
+                "max_color_temp_kelvin": 6535,
+                "min_color_temp_kelvin": 2000,
+                "rgb_color": None,
+                "rgbw_color": None,
+                "rgbww_color": None,
+                "supported_color_modes": ["onoff"],
+                "xy_color": None,
+            },
+            STATE_UNKNOWN,
+            {
+                "brightness": None,
+                "color_mode": None,
+                "color_temp_kelvin": None,
+                "effect_list": None,
+                "effect": None,
+                "max_color_temp_kelvin": 6535,
+                "min_color_temp_kelvin": 2000,
+                "rgb_color": None,
+                "rgbw_color": None,
+                "rgbww_color": None,
+                "supported_color_modes": [
+                    ColorMode.COLOR_TEMP,
+                    ColorMode.HS,
+                    ColorMode.RGB,
+                    ColorMode.RGBW,
+                    ColorMode.RGBWW,
+                    ColorMode.XY,
+                ],
+                "xy_color": None,
+            },
+        ),
+    ],
+)
+async def test_restore_state(
+    hass: HomeAssistant,
+    config: ConfigType,
+    style: ConfigurationStyle,
+    saved_state: str,
+    saved_extra_data: dict | None,
+    initial_state: str,
+    initial_attributes: ConfigType,
+) -> None:
+    """Test restoring trigger template light."""
+
+    restored_attributes = {  # These should be ignored
+        "current_position": 5,
+        "current_tilt_position": 5,
+    }
+
+    setup_mock_template_entity_restore_state(
+        hass,
+        TEST_LIGHT,
+        saved_state,
+        saved_extra_data=saved_extra_data,
+        saved_attributes=restored_attributes,
+    )
+
+    await setup_restore_template_entity(
+        hass,
+        TEST_LIGHT,
+        style,
+        config,
+        "state_attr('sensor.test_state', 'is_on') is true",
+    )
+
+    assert_state_and_attributes(
+        hass,
+        TEST_LIGHT,
+        initial_state,
+        initial_attributes,
+    )
+
+    await async_trigger(
+        hass,
+        "sensor.test_state",
+        "anything",
+        {"is_on": True},
+    )
+
+    assert_state_and_attributes(hass, TEST_LIGHT, STATE_ON)
