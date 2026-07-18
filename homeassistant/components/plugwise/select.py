8718ba67852c0e40a8f807b613aac83ca4c2c999
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-07-18 10:03:52 +0200

    Fix typo

--- homeassistant/components/plugwise/select.py
+++ homeassistant/components/plugwise/select.py
@@ -132,7 +132,7 @@ class PlugwiseSelectEntity(PlugwiseEntity, SelectEntity):
         """Change to the selected entity option.
 
         Appliance ID (= device_id) is required for the dhw_mode select
-        Locattion ID is required for the thermostat-schedule and zone_profile selects.
+        Location ID is required for the thermostat-schedule and zone_profile selects.
         STATE_ON is required for the thermostat-schedule select.
         """
         await self.coordinator.api.set_select(
