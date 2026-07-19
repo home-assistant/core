fe3b11ffd98441dc9689af68ea09bf8e09149b00
mib1185 <mail@mib85.de>
2026-07-19 11:09:06 +0000

    add tests

---
+++ tests/components/librenms/test_binary_sensor.py
@@ -0,0 +1,30 @@
+"""Test the LibreNMS binary sensor platform."""
+
+from unittest.mock import Mock, patch
+
+import pytest
+from syrupy.assertion import SnapshotAssertion
+
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
+
+from . import setup_integration
+
+from tests.common import MockConfigEntry, snapshot_platform
+
+
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_sensors(
+    hass: HomeAssistant,
+    entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
+    mock_librenms: Mock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test the LibreNMS binary sensor platform."""
+
+    with patch("homeassistant.components.librenms.PLATFORMS", [Platform.BINARY_SENSOR]):
+        await setup_integration(hass, mock_config_entry)
+
+    await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
