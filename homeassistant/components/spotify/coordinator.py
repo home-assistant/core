ad65210e015989cf3f4466b29390ca48eec013c1
Wolfgang Koeppner-Bures <wolfgang.bures@bures.at>
2026-07-21 01:38:49 +0200

    fix(spotify): Don't await async_start_reauth - it returns None
    async_start_reauth() schedules the reauth flow but doesn't return
    a coroutine. Remove await keyword and just call it directly.
    
    Build ID incremented to 20260721-003.

--- homeassistant/components/spotify/coordinator.py
+++ homeassistant/components/spotify/coordinator.py
@@ -32,7 +32,7 @@ from homeassistant.util import dt as dt_util
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
-BUILD_ID = "20260721-002"  # Increment for each deployment
+BUILD_ID = "20260721-003"  # Increment for each deployment
 
 
 type SpotifyConfigEntry = ConfigEntry[SpotifyData]
@@ -106,7 +106,7 @@ class SpotifyCoordinator(DataUpdateCoordinator[SpotifyCoordinatorData]):
                 BUILD_ID,
                 self.config_entry.title,
             )
-            await self.config_entry.async_start_reauth(self.hass)
+            self.config_entry.async_start_reauth(self.hass)
             raise ConfigEntryAuthFailed(
                 translation_domain=DOMAIN,
                 translation_key="oauth2_token_reauth_required",
@@ -138,7 +138,7 @@ class SpotifyCoordinator(DataUpdateCoordinator[SpotifyCoordinatorData]):
                 BUILD_ID,
                 self.config_entry.title,
             )
-            await self.config_entry.async_start_reauth(self.hass)
+            self.config_entry.async_start_reauth(self.hass)
             raise ConfigEntryAuthFailed(
                 translation_domain=DOMAIN,
                 translation_key="oauth2_token_reauth_required",
@@ -235,7 +235,7 @@ class SpotifyDeviceCoordinator(DataUpdateCoordinator[list[Device]]):
                 BUILD_ID,
                 self.config_entry.title,
             )
-            await self.config_entry.async_start_reauth(self.hass)
+            self.config_entry.async_start_reauth(self.hass)
             raise ConfigEntryAuthFailed(
                 translation_domain=DOMAIN,
                 translation_key="oauth2_token_reauth_required",
