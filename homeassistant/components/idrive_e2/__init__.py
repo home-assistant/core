5260bc7496d5adc60de46680928d41ca40ed5ee7
Patrick Vorgers <patrick@vorgers-stolle.nl>
2026-07-27 20:56:10 +0000

    Warmup loader caches to remove blocking listdir calls

--- homeassistant/components/idrive_e2/__init__.py
+++ homeassistant/components/idrive_e2/__init__.py
@@ -4,6 +4,7 @@ import logging
 from typing import Any, cast
 
 from aiobotocore.client import AioBaseClient as S3Client
+from aiobotocore.config import AioConfig
 from aiobotocore.session import AioSession
 from aiohttp import ClientError as AiohttpClientError
 from botocore.exceptions import ClientError, ConnectionError
@@ -50,6 +51,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: IDriveE2ConfigEntry) -> 
             endpoint_url=entry.data[CONF_ENDPOINT_URL],
             aws_secret_access_key=entry.data[CONF_SECRET_ACCESS_KEY],
             aws_access_key_id=entry.data[CONF_ACCESS_KEY_ID],
+            config=AioConfig(warm_up_loader_caches=True),
         ).__aenter__()
         await cast(Any, client).head_bucket(Bucket=entry.data[CONF_BUCKET])
     except ClientError as err:
