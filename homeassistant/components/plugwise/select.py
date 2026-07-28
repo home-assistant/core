efc8b35368089de1ab690dd8ef1be950468c3412
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-07-28 17:47:14 +0200

    Revert select changes

--- homeassistant/components/plugwise/select.py
+++ homeassistant/components/plugwise/select.py
@@ -113,7 +113,11 @@ async def async_setup_entry(
             PlugwiseSelectEntity(coordinator, device_id, description)
             for device_id in coordinator.new_devices
             for description in SELECT_TYPES
-            if coordinator.data[device_id].get(description.key)
+            if (device := coordinator.data[device_id]).get(description.key)
+            and (
+                description.key not in (DHW_MODE, SELECT_DHW_MODE)
+                or len(device[description.options_key]) > 2
+            )
         )
 
     _add_entities()
