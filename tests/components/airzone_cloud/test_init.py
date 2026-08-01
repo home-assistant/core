a1642e6f5b35c425a6d29fb0ebc0df93cfaa5cc8
Erik Montnemery <erik@montnemery.com>
2026-08-01 11:13:28 +0200

    Fix via_device race in airzone_cloud (#177707)

--- tests/components/airzone_cloud/test_init.py
+++ tests/components/airzone_cloud/test_init.py
@@ -7,8 +7,9 @@ from aioairzone_cloud.exceptions import AirzoneTimeout
 from homeassistant.components.airzone_cloud.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
 
-from .util import CONFIG
+from .util import CONFIG, WS_ID, WS_ID_AIDOO, async_init_integration
 
 from tests.common import MockConfigEntry
 
@@ -52,6 +53,51 @@ async def test_unload_entry(hass: HomeAssistant) -> None:
         await hass.config_entries.async_unload(config_entry.entry_id)
         await hass.async_block_till_done()
         assert config_entry.state is ConfigEntryState.NOT_LOADED
+
+
+async def test_device_via_device(
+    hass: HomeAssistant,
+    device_registry: dr.DeviceRegistry,
+) -> None:
+    """Test that child devices are linked to their via_device parents."""
+    await async_init_integration(hass)
+
+    config_entry = hass.config_entries.async_entries(DOMAIN)[0]
+
+    ws_device = device_registry.async_get_device_by_identifier(
+        (DOMAIN, WS_ID), config_entry.entry_id
+    )
+    assert ws_device is not None
+    assert ws_device.via_device_id is None
+
+    ws_aidoo_device = device_registry.async_get_device_by_identifier(
+        (DOMAIN, WS_ID_AIDOO), config_entry.entry_id
+    )
+    assert ws_aidoo_device is not None
+
+    system_device = device_registry.async_get_device_by_identifier(
+        (DOMAIN, "system1"), config_entry.entry_id
+    )
+    assert system_device is not None
+    assert system_device.via_device_id == ws_device.id
+
+    zone_device = device_registry.async_get_device_by_identifier(
+        (DOMAIN, "zone1"), config_entry.entry_id
+    )
+    assert zone_device is not None
+    assert zone_device.via_device_id == system_device.id
+
+    dhw_device = device_registry.async_get_device_by_identifier(
+        (DOMAIN, "dhw1"), config_entry.entry_id
+    )
+    assert dhw_device is not None
+    assert dhw_device.via_device_id == ws_device.id
+
+    aidoo_device = device_registry.async_get_device_by_identifier(
+        (DOMAIN, "aidoo1"), config_entry.entry_id
+    )
+    assert aidoo_device is not None
+    assert aidoo_device.via_device_id == ws_aidoo_device.id
 
 
 async def test_init_api_timeout(hass: HomeAssistant) -> None:
