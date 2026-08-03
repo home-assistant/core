0dc700497c38353a76e7380adff046818a055f46
Alan Murray <github@atmurray.net>
2026-08-03 18:42:15 +1000

    Update helpers.py
    Remove superfluous comment

--- homeassistant/components/acmeda/helpers.py
+++ homeassistant/components/acmeda/helpers.py
@@ -27,7 +27,6 @@ def async_add_acmeda_entities(
     hub = config_entry.runtime_data
     LOGGER.debug("Looking for new %s on: %s", entity_class.__name__, hub.host)
 
-    # If not setup
     if hub.api is None:
         return
 
