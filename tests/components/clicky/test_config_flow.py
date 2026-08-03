b708b42af3d4f2f89860a420319e634bdf017a56
avclicky <766041+avclicky@users.noreply.github.com>
2026-08-03 13:47:46 -0700

    Remove direct InvalidAuth test

--- tests/components/clicky/test_config_flow.py
+++ tests/components/clicky/test_config_flow.py
@@ -5,7 +5,6 @@ from unittest.mock import AsyncMock, patch
 from pyclicky import AuthenticationError, ConnectionError
 
 from homeassistant import config_entries
-from homeassistant.components.clicky.config_flow import InvalidAuth
 from homeassistant.components.clicky.const import CONF_SITE_ID, CONF_SITEKEY, DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
@@ -48,34 +47,6 @@ async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
         CONF_SITEKEY: TEST_SITEKEY,
     }
     assert len(mock_setup_entry.mock_calls) == 1
-
-
-async def test_form_invalid_auth(
-    hass: HomeAssistant, mock_setup_entry: AsyncMock
-) -> None:
-    """Test we handle invalid auth."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-    client = AsyncMock()
-
-    with patch(
-        "homeassistant.components.clicky.config_flow.ClickyClient",
-        side_effect=InvalidAuth,
-    ) as mock_lib:
-        client = mock_lib.return_value
-        client.query = AsyncMock()
-
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                CONF_SITE_ID: TEST_SITE_ID,
-                CONF_SITEKEY: TEST_SITEKEY,
-            },
-        )
-
-    assert result["type"] is FlowResultType.FORM
-    assert result["errors"] == {"base": "invalid_auth"}
 
 
 async def test_form_unknown(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
