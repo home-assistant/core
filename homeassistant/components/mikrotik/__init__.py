5380eee8599948cc65990dc98440c09bd50f302a
Simone Chemelli <simone.chemelli@gmail.com>
2026-08-03 13:12:06 +0000

    Add select platform to Mikrotik

--- homeassistant/components/mikrotik/__init__.py
+++ homeassistant/components/mikrotik/__init__.py
@@ -19,6 +19,7 @@ from .coordinator import (
 PLATFORMS = [
     Platform.BUTTON,
     Platform.DEVICE_TRACKER,
+    Platform.SELECT,
     Platform.SENSOR,
     Platform.SWITCH,
     Platform.UPDATE,
---
+++ homeassistant/components/mikrotik/select.py
@@ -0,0 +1,65 @@
+"""Support for Mikrotik routers select entities."""
+
+from typing import Final, override
+
+from homeassistant.components.select import SelectEntity, SelectEntityDescription
+from homeassistant.const import EntityCategory
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+
+from .coordinator import MikrotikConfigEntry, mikrotik_config_entry_errors
+from .entity import MikrotikDeviceEntity
+
+PARALLEL_UPDATES = 0
+
+
+SELECTS: Final = (
+    SelectEntityDescription(
+        key="poe-out",
+        translation_key="poe_out",
+        entity_category=EntityCategory.CONFIG,
+        options=["off", "auto-on", "forced-on"],
+    ),
+)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: MikrotikConfigEntry,
+    async_add_entities: AddConfigEntryEntitiesCallback,
+) -> None:
+    """Set up select entities based on a config entry."""
+    coordinator = entry.runtime_data
+
+    select_list = [
+        MikrotikSelectEntity(entry, coordinator, switch_desc, interface)
+        for switch_desc in SELECTS
+        for interface in coordinator.api.interfaces
+        if interface.get(switch_desc.key) is not None
+    ]
+
+    async_add_entities(select_list)
+
+
+class MikrotikSelectEntity(MikrotikDeviceEntity, SelectEntity):
+    """Representation of a select entity."""
+
+    @override
+    async def async_added_to_hass(self) -> None:
+        """Restore last known option."""
+        await super().async_added_to_hass()
+        self._attr_current_option = self._interface.get("poe-out", "off")
+
+    @override
+    async def async_select_option(self, option: str) -> None:
+        """Change the selected option."""
+
+        with mikrotik_config_entry_errors():
+            await self.hass.async_add_executor_job(
+                self.coordinator.api.command,
+                "/interface/ethernet/poe/set",
+                {".id": self._interface[".id"], "poe-out": option},
+            )
+
+        self._attr_current_option = option
+        self.async_write_ha_state()
