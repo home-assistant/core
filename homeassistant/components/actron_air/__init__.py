0d5e61854616fa69a414635824ce6adc76841cf1
Kurt Chrisford <92524101+kclif9@users.noreply.github.com>
2026-08-04 23:28:55 +1000

    Add binary sensor platform to actron_air integration (#178148)

--- homeassistant/components/actron_air/__init__.py
+++ homeassistant/components/actron_air/__init__.py
@@ -15,7 +15,12 @@ from .coordinator import (
     ActronAirSystemCoordinator,
 )
 
-PLATFORMS = [Platform.CLIMATE, Platform.SENSOR, Platform.SWITCH]
+PLATFORMS = [
+    Platform.BINARY_SENSOR,
+    Platform.CLIMATE,
+    Platform.SENSOR,
+    Platform.SWITCH,
+]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ActronAirConfigEntry) -> bool:
