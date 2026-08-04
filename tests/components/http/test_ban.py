d622b0e8657da8ecff56f62caea2411d78ca7582
Jakob Schlyter <jakob@kirei.se>
2026-08-04 20:33:03 +0200

    Verify migration success, compare correct data

--- tests/components/http/test_ban.py
+++ tests/components/http/test_ban.py
@@ -6,14 +6,17 @@ import logging
 import os
 from unittest.mock import AsyncMock, Mock, patch
 
+import aiofiles
 from aiohttp import web
 from aiohttp.web_exceptions import HTTPUnauthorized
 from aiohttp.web_middlewares import middleware
 import pytest
+import yaml
 
 from homeassistant.components.http.ban import (
     KEY_BAN_MANAGER,
     KEY_FAILED_LOGIN_ATTEMPTS,
+    LEGACY_IP_BANS_FILE,
     IpBanManager,
     ban_middleware,
     process_success_login,
@@ -425,15 +428,26 @@ async def test_ban_migration(
     setup_bans(hass, app, 5)
     set_real_ip = mock_real_ip(app)
 
-    with patch(
-        "homeassistant.components.http.ban.load_yaml_config_file",
-        return_value={
-            banned_ip: {"banned_at": "2016-11-16T19:20:03"} for banned_ip in BANNED_IPS
-        },
-    ):
-        client = await aiohttp_client(app)
+    legacy_ip_bans_file_path = hass.config.path(LEGACY_IP_BANS_FILE)
+
+    # Create a legacy ban file with banned IPs
+    async with aiofiles.open(legacy_ip_bans_file_path, "w") as fp:
+        await fp.write(
+            yaml.dump(
+                {
+                    banned_ip: {"banned_at": "2016-11-16T19:20:03"}
+                    for banned_ip in BANNED_IPS
+                }
+            )
+        )
+
+    client = await aiohttp_client(app)
 
     for remote_addr in BANNED_IPS:
         set_real_ip(remote_addr)
         resp = await client.get("/")
         assert resp.status == HTTPStatus.FORBIDDEN
+
+    # Legacy file should be removed after migration
+    with pytest.raises(FileNotFoundError):
+        os.stat(legacy_ip_bans_file_path)
