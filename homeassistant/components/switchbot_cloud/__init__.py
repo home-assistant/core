ff557d6462d10e78e6f49b41c2be0d09ab15a090
Samuel Xiao <40679757+XiaoLing-git@users.noreply.github.com>
2026-08-06 16:34:39 +0800

    Switchbot Cloud: Unify coding style [Water Detector] (#178324)

--- homeassistant/components/switchbot_cloud/__init__.py
+++ homeassistant/components/switchbot_cloud/__init__.py
@@ -210,12 +210,6 @@ async def make_device_data(
             hass, entry, api, device, coordinators_by_id
         )
         devices_data.fans.append((device, coordinator))
-    if isinstance(device, Device) and device.device_type == "Water Detector":
-        coordinator = await coordinator_for_device(
-            hass, entry, api, device, coordinators_by_id, True
-        )
-        devices_data.binary_sensors.append((device, coordinator))
-        devices_data.sensors.append((device, coordinator))
 
     if isinstance(device, Device) and device.device_type in [
         "Curtain",
