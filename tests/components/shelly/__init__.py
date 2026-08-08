661e6d612ed967f99acf99843c093ba6fd78deb9
Erik <erik@montnemery.com>
2026-08-08 18:15:12 +0200

    Improve handling of via_device in Shelly tests

--- tests/components/shelly/__init__.py
+++ tests/components/shelly/__init__.py
@@ -182,13 +182,14 @@ def register_sub_device(
     device_registry: DeviceRegistry, config_entry: ConfigEntry, unique_id: str
 ) -> DeviceEntry:
     """Register Shelly sub-device."""
-    main_device = device_registry.async_get_device_by_identifier(
-        (DOMAIN, MOCK_MAC), config_entry.entry_id
+    main_device = device_registry.async_get_device_by_connection(
+        (CONNECTION_NETWORK_MAC, MOCK_MAC), config_entry.entry_id
     )
+    assert main_device is not None
     return device_registry.async_get_or_create(
         config_entry_id=config_entry.entry_id,
         identifiers={(DOMAIN, f"{MOCK_MAC}-{unique_id}")},
-        via_device_id=main_device.id if main_device else None,
+        via_device_id=main_device.id,
     )
 
 
