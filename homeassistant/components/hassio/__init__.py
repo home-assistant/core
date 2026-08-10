41265ed8c9247889482f60db0f27f771250ebf60
Stefan Agner <stefan@agner.ch>
2026-08-10 19:43:19 +0200

    Drop redundant eager_start argument

--- homeassistant/components/hassio/__init__.py
+++ homeassistant/components/hassio/__init__.py
@@ -349,8 +349,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
         hass.async_create_task(
             hass.config_entries.flow.async_init(
                 DOMAIN, context={"source": SOURCE_SYSTEM}, data={}
-            ),
-            eager_start=True,
+            )
         )
     return True
 
