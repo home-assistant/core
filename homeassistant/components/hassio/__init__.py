da0183ae8665490971c9bcf22b5dabc6da8691dc
Stefan Agner <stefan@agner.ch>
2026-08-11 12:41:15 +0200

    Name the config entry flow task

--- homeassistant/components/hassio/__init__.py
+++ homeassistant/components/hassio/__init__.py
@@ -349,7 +349,8 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
         hass.async_create_task(
             hass.config_entries.flow.async_init(
                 DOMAIN, context={"source": SOURCE_SYSTEM}, data={}
-            )
+            ),
+            "hassio system config entry flow",
         )
     return True
 
