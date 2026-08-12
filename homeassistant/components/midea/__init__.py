6bbf53f8392c4d943f0aae2f20ddd622255bd885
Simone Chemelli <simone.chemelli@gmail.com>
2026-08-12 13:46:46 +0000

    Add humidifier platform to Midea

--- homeassistant/components/midea/__init__.py
+++ homeassistant/components/midea/__init__.py
@@ -20,7 +20,7 @@ from homeassistant.exceptions import ConfigEntryError, ConfigEntryNotReady
 from .const import CONF_KEY, CONF_SUBTYPE
 from .entity import MideaConfigEntry
 
-_PLATFORMS: list[Platform] = [Platform.CLIMATE]
+_PLATFORMS: list[Platform] = [Platform.CLIMATE, Platform.HUMIDIFIER]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: MideaConfigEntry) -> bool:
