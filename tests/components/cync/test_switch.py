d532e58afce86fe43f08b4b61a97606cf2489b27
Brian Williams <williamsbm2000@comcast.net>
2026-08-13 15:46:18 +0000

    Improve Cync discovery coverage

--- tests/components/cync/test_switch.py
+++ tests/components/cync/test_switch.py
@@ -114,34 +114,6 @@ async def test_dual_outlet_control_targets_selected_outlet(
     right_device.turn_off.assert_not_awaited()
 
 
-@pytest.mark.parametrize(
-    ("unique_id", "expected_mesh_id", "expected_mesh_group_id"),
-    [
-        (DUAL_OUTLET_LEFT_UNIQUE_ID, 6, 1),
-        (DUAL_OUTLET_RIGHT_UNIQUE_ID, 6, 2),
-    ],
-)
-async def test_dual_outlet_pycync_command_routing(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    unique_id: str,
-    expected_mesh_id: int,
-    expected_mesh_group_id: int,
-) -> None:
-    """Test pycync routes each outlet through its mesh group ID."""
-    await setup_integration(hass, mock_config_entry)
-
-    device = mock_config_entry.runtime_data.data[unique_id]
-    command_client = AsyncMock()
-    device._command_client = command_client
-
-    await device.turn_off()
-
-    assert device.mesh_reference_id == expected_mesh_id
-    assert device.mesh_group_id == expected_mesh_group_id
-    command_client.set_power_state.assert_awaited_once_with(device, False)
-
-
 async def test_initial_state_is_unknown_until_device_update(
     hass: HomeAssistant,
     mock_config_entry: MockConfigEntry,
