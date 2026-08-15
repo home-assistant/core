249ece5018527c6aa105423b2c4e61d40cda5ec8
jptstar <jptstar@me.com>
2026-08-15 07:24:25 +0200

    Remove unused TSUN config flow import

--- homeassistant/components/tsun/config_flow.py
+++ homeassistant/components/tsun/config_flow.py
@@ -15,7 +15,6 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.config_entries import ConfigFlowResult
 from homeassistant.const import CONF_HOST, CONF_PORT
-from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import (
