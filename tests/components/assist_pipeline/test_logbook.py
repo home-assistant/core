179705f9f2c3fc99dbeaa55ffd00b519c274549e
Erik <erik@montnemery.com>
2026-08-17 10:35:04 +0200

    Add support for child devices to assist_pipeline logbook augmentation

--- tests/components/assist_pipeline/test_logbook.py
+++ tests/components/assist_pipeline/test_logbook.py
@@ -42,3 +42,67 @@ async def test_recording_event(
     assert (
         event[logbook.LOGBOOK_ENTRY_MESSAGE] == "My Satellite captured an audio sample"
     )
+
+
+async def test_recording_event_child_device(
+    hass: HomeAssistant, init_components, device_registry: dr.DeviceRegistry
+) -> None:
+    """Test recording event fired for a child device."""
+    hass.config.components.add("recorder")
+    assert await async_setup_component(hass, "logbook", {})
+    await hass.async_block_till_done()
+
+    entry = MockConfigEntry()
+    entry.add_to_hass(hass)
+    satellite_device = device_registry.async_get_or_create(
+        config_entry_id=entry.entry_id,
+        connections=set(),
+        identifiers={("demo", "satellite-1234")},
+    )
+    child_device = device_registry.async_get_or_create_child(
+        config_entry_id=entry.entry_id,
+        identifiers={("demo", "satellite-1234-child")},
+        parent_device_id=satellite_device.id,
+        name="My Child Satellite",
+    )
+
+    event = mock_humanify(
+        hass,
+        [
+            MockRow(
+                assist_pipeline.EVENT_RECORDING,
+                {ATTR_DEVICE_ID: child_device.id},
+            ),
+        ],
+    )[0]
+
+    assert event[logbook.LOGBOOK_ENTRY_NAME] == "My Child Satellite"
+    assert (
+        event[logbook.LOGBOOK_ENTRY_MESSAGE]
+        == "My Child Satellite captured an audio sample"
+    )
+
+
+async def test_recording_event_unknown_device(
+    hass: HomeAssistant, init_components
+) -> None:
+    """Test recording event for an unknown device does not raise."""
+    hass.config.components.add("recorder")
+    assert await async_setup_component(hass, "logbook", {})
+    await hass.async_block_till_done()
+
+    event = mock_humanify(
+        hass,
+        [
+            MockRow(
+                assist_pipeline.EVENT_RECORDING,
+                {ATTR_DEVICE_ID: "non-existing-device"},
+            ),
+        ],
+    )[0]
+
+    assert event[logbook.LOGBOOK_ENTRY_NAME] == "Unknown device"
+    assert (
+        event[logbook.LOGBOOK_ENTRY_MESSAGE]
+        == "Unknown device captured an audio sample"
+    )
