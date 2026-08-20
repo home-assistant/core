10889a3842500ca8d72242ef0e67ae6126dee643
Nicolas Cazottes <nicolas@cazottes.net>
2026-08-20 21:33:14 +0000

    Fix test to split user flow init and data submission - 2nd occurence

--- tests/components/chacon_dio/test_config_flow.py
+++ tests/components/chacon_dio/test_config_flow.py
@@ -70,9 +70,15 @@ async def test_errors(
     mock_dio_chacon_client.get_user_id.side_effect = exception
 
     result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={"source": SOURCE_USER},
-        data={
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "user"
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={
             CONF_USERNAME: "nada",
             CONF_PASSWORD: "nadap",
         },
