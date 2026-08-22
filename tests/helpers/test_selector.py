3e7e09550201517f8fe9f86c5d5bfa4af194e4bb
nokside <276099665+nokside@users.noreply.github.com>
2026-08-22 14:02:10 +0000

    Fix object selector default test indentation

--- tests/helpers/test_selector.py
+++ tests/helpers/test_selector.py
@@ -1382,7 +1382,7 @@ def test_object_selector_schema(schema, valid_selections, invalid_selections) ->
 )
 def test_object_selector_field_default(default: object) -> None:
     """Test object selector field default."""
-validated = selector.validate_selector(
+    validated = selector.validate_selector(
         {
             "object": {
                 "fields": {
