daf5af447d177f8704a932aee590ac234fa32a61
nokside <276099665+nokside@users.noreply.github.com>
2026-08-22 12:28:03 +0000

    Add default support to object selector fields

--- homeassistant/helpers/selector.py
+++ homeassistant/helpers/selector.py
@@ -1660,6 +1660,7 @@ class NumericThresholdSelector(Selector[NumericThresholdSelectorConfig]):
 class ObjectSelectorField(TypedDict, total=False):
     """Class to represent an object selector fields dict."""
 
+    default: Any
     label: str
     required: bool
     selector: Required[Selector | dict[str, Any]]
@@ -1686,6 +1687,7 @@ class ObjectSelector(Selector[ObjectSelectorConfig]):
             vol.Optional("fields"): {
                 str: {
                     vol.Required("selector"): vol.Any(Selector, validate_selector),
+                    vol.Optional("default"): cv.match_all,
                     vol.Optional("required"): bool,
                     vol.Optional("label"): str,
                 }
