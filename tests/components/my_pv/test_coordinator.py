95399cf3a63947bb6d17233d1bd3c58208b0273f
rrooggiieerr <rogier@batoid.com>
2026-08-24 15:40:02 +0200

    Refactor coordinator test

--- tests/components/my_pv/test_coordinator.py
+++ tests/components/my_pv/test_coordinator.py
@@ -2,179 +2,70 @@
 
 from unittest.mock import AsyncMock
 
-from my_pv.exceptions import MyPVAuthenticationError, MyPVConnectionError
+from freezegun.api import FrozenDateTimeFactory
 import pytest
 
-from homeassistant.components.my_pv.coordinator import MyPVCoordinator
+from homeassistant.components.my_pv.coordinator import UPDATE_INTERVAL
+from homeassistant.const import STATE_UNAVAILABLE
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryAuthFailed, HomeAssistantError
-from homeassistant.helpers.update_coordinator import UpdateFailed
 
-from tests.common import MockConfigEntry
+from tests.common import MockConfigEntry, async_fire_time_changed
 
 
-async def test_coordinator_refresh(
+@pytest.mark.usefixtures("mock_my_pv_client")
+async def test_coordinator_update_data(
     hass: HomeAssistant,
+    freezer: FrozenDateTimeFactory,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test a coordinator update."""
+    mock_config_entry.add_to_hass(hass)
+
+    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
+
+    # Test successful setup and first data fetch
+    await hass.async_block_till_done()
+    states = hass.states.async_all()
+    assert False not in [state.state != STATE_UNAVAILABLE for state in states]
+
+    # Test successful data fetch
+    freezer.tick(UPDATE_INTERVAL)
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+    states = hass.states.async_all()
+    assert False not in [state.state != STATE_UNAVAILABLE for state in states]
+
+
+async def test_coordinator_update_data_not_connected(
+    hass: HomeAssistant,
+    freezer: FrozenDateTimeFactory,
     mock_config_entry: MockConfigEntry,
     mock_my_pv_client: AsyncMock,
 ) -> None:
-    """Test coordinator refresh."""
-    coordinator = MyPVCoordinator(hass, mock_config_entry, mock_my_pv_client)
+    """Test coordinator update when client not connected."""
+    mock_config_entry.add_to_hass(hass)
 
-    await coordinator.async_refresh()
-    assert coordinator.last_update_success
+    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
 
+    # Test successful setup and first data fetch
+    await hass.async_block_till_done()
+    states = hass.states.async_all()
+    assert False not in [state.state != STATE_UNAVAILABLE for state in states]
 
-async def test_coordinator_refresh_not_connected(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_my_pv_client: AsyncMock,
-) -> None:
-    """Test coordinator refresh when client not connected."""
-    coordinator = MyPVCoordinator(hass, mock_config_entry, mock_my_pv_client)
-
-    await coordinator.async_refresh()
-    assert coordinator.last_update_success
-
+    # Test states get unavailable when not connected
+    freezer.tick(UPDATE_INTERVAL)
     mock_my_pv_client.connected = False
-    mock_my_pv_client.connect.return_value = False
+    mock_my_pv_client.connect = AsyncMock(return_value=False)
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+    states = hass.states.async_all()
+    assert False not in [state.state == STATE_UNAVAILABLE for state in states]
 
-    await coordinator.async_refresh()
-
-    assert not coordinator.last_update_success
-    assert isinstance(coordinator.last_exception, UpdateFailed)
-
-
-async def test_coordinator_refresh_connection_error(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_my_pv_client: AsyncMock,
-) -> None:
-    """Test coordinator refresh when client has connection error."""
-    coordinator = MyPVCoordinator(hass, mock_config_entry, mock_my_pv_client)
-
-    await coordinator.async_refresh()
-    assert coordinator.last_update_success
-
+    # Test successful data fetch
+    freezer.tick(UPDATE_INTERVAL)
     mock_my_pv_client.connected = True
-    mock_my_pv_client.fetch_data.side_effect = MyPVConnectionError()
-
-    await coordinator.async_refresh()
-
-    assert not coordinator.last_update_success
-    assert isinstance(coordinator.last_exception, UpdateFailed)
-
-
-async def test_coordinator_refresh_fetch_data_false(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_my_pv_client: AsyncMock,
-) -> None:
-    """Test coordinator refresh when client fetch data returns False."""
-    coordinator = MyPVCoordinator(hass, mock_config_entry, mock_my_pv_client)
-
-    await coordinator.async_refresh()
-    assert coordinator.last_update_success
-
-    mock_my_pv_client.connected = True
-    mock_my_pv_client.fetch_data.return_value = False
-
-    await coordinator.async_refresh()
-
-    assert not coordinator.last_update_success
-    assert isinstance(coordinator.last_exception, UpdateFailed)
-
-
-async def test_coordinator_refresh_authentication_error(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_my_pv_client: AsyncMock,
-) -> None:
-    """Test coordinator refresh when client has authentication error."""
-    coordinator = MyPVCoordinator(hass, mock_config_entry, mock_my_pv_client)
-
-    await coordinator.async_refresh()
-    assert coordinator.last_update_success
-
-    mock_my_pv_client.connected = False
-    mock_my_pv_client.connect.side_effect = MyPVAuthenticationError()
-
-    await coordinator.async_refresh()
-
-    assert not coordinator.last_update_success
-    assert isinstance(coordinator.last_exception, ConfigEntryAuthFailed)
-
-
-async def test_coordinator_set_value(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_my_pv_client: AsyncMock,
-) -> None:
-    """Test coordinator setting value."""
-    coordinator = MyPVCoordinator(hass, mock_config_entry, mock_my_pv_client)
-
-    await coordinator.async_refresh()
-    assert coordinator.last_update_success
-
-    result = await coordinator.set_target_temperature(35)
-    assert result
-
-
-async def test_coordinator_set_value_not_connected(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_my_pv_client: AsyncMock,
-) -> None:
-    """Test coordinator setting value when client not connected."""
-    coordinator = MyPVCoordinator(hass, mock_config_entry, mock_my_pv_client)
-
-    await coordinator.async_refresh()
-    assert coordinator.last_update_success
-
-    mock_my_pv_client.connected = False
-    mock_my_pv_client.connect.return_value = False
-
-    with (
-        pytest.raises(HomeAssistantError),
-    ):
-        await coordinator.set_target_temperature(35)
-
-
-async def test_coordinator_set_value_connection_error(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_my_pv_client: AsyncMock,
-) -> None:
-    """Test coordinator setting value when client has connection error."""
-    coordinator = MyPVCoordinator(hass, mock_config_entry, mock_my_pv_client)
-
-    await coordinator.async_refresh()
-    assert coordinator.last_update_success
-
-    mock_my_pv_client.connected = True
-    mock_my_pv_client.set_target_temperature.side_effect = MyPVConnectionError()
-
-    with (
-        pytest.raises(HomeAssistantError),
-    ):
-        await coordinator.set_target_temperature(35)
-
-
-async def test_coordinator_set_value_authentication_error(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_my_pv_client: AsyncMock,
-) -> None:
-    """Test coordinator setting value when client has authentication error."""
-    coordinator = MyPVCoordinator(hass, mock_config_entry, mock_my_pv_client)
-
-    await coordinator.async_refresh()
-    assert coordinator.last_update_success
-
-    mock_my_pv_client.connected = False
-    mock_my_pv_client.connect.side_effect = MyPVAuthenticationError()
-
-    with (
-        pytest.raises(ConfigEntryAuthFailed),
-    ):
-        await coordinator.set_target_temperature(35)
+    mock_my_pv_client.connect = AsyncMock(return_value=True)
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+    states = hass.states.async_all()
+    assert False not in [state.state != STATE_UNAVAILABLE for state in states]
