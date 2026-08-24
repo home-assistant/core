7307de11964c9a72070be05d636d4d4ad0b33965
karwosts <karwosts@gmail.com>
2026-08-24 22:44:57 +0000

    Add collection_image.shuffle service

--- homeassistant/components/collection_image/__init__.py
+++ homeassistant/components/collection_image/__init__.py
@@ -3,8 +3,26 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv, service
+
+from .const import DOMAIN
 
 PLATFORMS: list[Platform] = [Platform.IMAGE]
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigEntry) -> bool:
+    """Set up is called when Home Assistant is loading our component."""
+    service.async_register_platform_entity_service(
+        hass,
+        DOMAIN,
+        "shuffle",
+        entity_domain="image",
+        schema={},
+        func="get_next_image",
+    )
+    return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
