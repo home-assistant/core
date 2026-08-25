c70b2245794001a0ca34ac0c1836aab3e9d43782
rrooggiieerr <rogier@batoid.com>
2026-08-25 11:30:08 +0200

    Parameterize the duplicated DHCP and Zeroconf discovery cases.

--- tests/components/my_pv/test_config_flow.py
+++ tests/components/my_pv/test_config_flow.py
@@ -10,7 +10,7 @@ from homeassistant import config_entries
 from homeassistant.components.my_pv.const import DOMAIN
 from homeassistant.const import CONF_HOST, CONF_PASSWORD
 from homeassistant.core import HomeAssistant
-from homeassistant.data_entry_flow import FlowResultType
+from homeassistant.data_entry_flow import BaseServiceInfo, FlowResultType
 from homeassistant.helpers.service_info.dhcp import DhcpServiceInfo
 from homeassistant.helpers.service_info.zeroconf import ZeroconfServiceInfo
 
@@ -264,61 +264,106 @@ async def test_step_dhcp(
     assert result["result"].unique_id == ELWA2_SERIAL_NUMBER
 
 
+@pytest.mark.parametrize(
+    ("source", "data"),
+    [
+        (
+            config_entries.SOURCE_DHCP,
+            DHCP_DISCOVERY,
+        ),
+        (
+            config_entries.SOURCE_ZEROCONF,
+            ZEROCONF_DISCOVERY,
+        ),
+    ],
+)
 @pytest.mark.usefixtures("mock_my_pv_client")
-async def test_step_dhcp_already_configured(
+async def test_step_discovery_already_configured(
     hass: HomeAssistant,
     mock_config_entry: MockConfigEntry,
+    source: str,
+    data: BaseServiceInfo,
 ) -> None:
-    """Test for DHCP discovery that is already configured."""
+    """Test for discovery that is already configured."""
     mock_config_entry.add_to_hass(hass)
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={
-            "source": config_entries.SOURCE_DHCP,
+            "source": source,
         },
-        data=DHCP_DISCOVERY,
+        data=data,
     )
 
     assert result["type"] is FlowResultType.ABORT
     assert result["reason"] == "already_configured"
 
 
-async def test_step_dhcp_cannot_connect(
+@pytest.mark.parametrize(
+    ("source", "data"),
+    [
+        (
+            config_entries.SOURCE_DHCP,
+            DHCP_DISCOVERY,
+        ),
+        (
+            config_entries.SOURCE_ZEROCONF,
+            ZEROCONF_DISCOVERY,
+        ),
+    ],
+)
+async def test_step_discovery_cannot_connect(
     hass: HomeAssistant,
     mock_my_pv_client: AsyncMock,
+    source: str,
+    data: BaseServiceInfo,
 ) -> None:
-    """Test for DHCP discovery that can not connect."""
+    """Test for discovery that can not connect."""
 
     mock_my_pv_client.connect.return_value = False
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={
-            "source": config_entries.SOURCE_DHCP,
+            "source": source,
         },
-        data=DHCP_DISCOVERY,
+        data=data,
     )
 
     assert result["type"] is FlowResultType.ABORT
     assert result["reason"] == "cannot_connect"
 
 
+@pytest.mark.parametrize(
+    ("source", "data"),
+    [
+        (
+            config_entries.SOURCE_DHCP,
+            DHCP_DISCOVERY,
+        ),
+        (
+            config_entries.SOURCE_ZEROCONF,
+            ZEROCONF_DISCOVERY,
+        ),
+    ],
+)
 @pytest.mark.usefixtures("mock_setup_entry")
-async def test_step_dhcp_auth(
+async def test_step_discovery_auth(
     hass: HomeAssistant,
     mock_my_pv_client: AsyncMock,
+    source: str,
+    data: BaseServiceInfo,
 ) -> None:
-    """Test for DHCP discovery that requires a password."""
+    """Test for discovery that requires a password."""
 
     mock_my_pv_client.connect.side_effect = MyPVAuthenticationError()
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={
-            "source": config_entries.SOURCE_DHCP,
+            "source": source,
         },
-        data=DHCP_DISCOVERY,
+        data=data,
     )
 
     assert result["type"] is FlowResultType.FORM
@@ -340,20 +385,35 @@ async def test_step_dhcp_auth(
     assert result["result"].unique_id == ELWA2_SERIAL_NUMBER
 
 
-async def test_step_dhcp_auth_wrong_password(
+@pytest.mark.parametrize(
+    ("source", "data"),
+    [
+        (
+            config_entries.SOURCE_DHCP,
+            DHCP_DISCOVERY,
+        ),
+        (
+            config_entries.SOURCE_ZEROCONF,
+            ZEROCONF_DISCOVERY,
+        ),
+    ],
+)
+async def test_step_discovery_auth_wrong_password(
     hass: HomeAssistant,
     mock_my_pv_client: AsyncMock,
+    source: str,
+    data: BaseServiceInfo,
 ) -> None:
-    """Test for DHCP discovery with an incorrect password."""
+    """Test for discovery with an incorrect password."""
 
     mock_my_pv_client.connect.side_effect = MyPVAuthenticationError()
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={
-            "source": config_entries.SOURCE_DHCP,
+            "source": source,
         },
-        data=DHCP_DISCOVERY,
+        data=data,
     )
 
     assert result["type"] is FlowResultType.FORM
@@ -373,127 +433,6 @@ async def test_step_dhcp_auth_wrong_password(
     result = await hass.config_entries.flow.async_configure(
         result["flow_id"],
         {CONF_PASSWORD: "test-password"},
-    )
-
-    assert result["type"] is FlowResultType.CREATE_ENTRY
-    assert result["title"] == "my-PV AC ELWA 2 0000000000"
-    assert result["data"] == {
-        CONF_HOST: "127.0.0.1",
-        CONF_PASSWORD: "test-password",
-    }
-    assert result["result"].unique_id == ELWA2_SERIAL_NUMBER
-
-
-@pytest.mark.usefixtures("mock_setup_entry")
-async def test_step_zeroconf(
-    hass: HomeAssistant,
-    mock_my_pv_client: AsyncMock,
-) -> None:
-    """Test for Zeroconf discovery that requires a password."""
-
-    mock_my_pv_client.connect.side_effect = MyPVAuthenticationError()
-
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={
-            "source": config_entries.SOURCE_ZEROCONF,
-        },
-        data=ZEROCONF_DISCOVERY,
-    )
-
-    assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "discovery_auth"
-    assert not result["errors"]
-
-    mock_my_pv_client.connect.side_effect = None
-
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"], {CONF_PASSWORD: "test-password"}
-    )
-
-    assert result["type"] is FlowResultType.CREATE_ENTRY
-    assert result["title"] == "my-PV AC ELWA 2 0000000000"
-    assert result["data"] == {
-        CONF_HOST: "127.0.0.1",
-        CONF_PASSWORD: "test-password",
-    }
-    assert result["result"].unique_id == ELWA2_SERIAL_NUMBER
-
-
-async def test_step_zeroconf_already_configured(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_my_pv_client: AsyncMock,
-) -> None:
-    """Test for Zeroconf discovery that is already configured."""
-    mock_config_entry.add_to_hass(hass)
-
-    mock_my_pv_client.connect.side_effect = MyPVAuthenticationError()
-
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={
-            "source": config_entries.SOURCE_ZEROCONF,
-        },
-        data=ZEROCONF_DISCOVERY,
-    )
-
-    assert result["type"] is FlowResultType.ABORT
-    assert result["reason"] == "already_configured"
-
-
-async def test_step_zeroconf_cannot_connect(
-    hass: HomeAssistant,
-    mock_my_pv_client: AsyncMock,
-) -> None:
-    """Test for Zeroconf discovery that can not connect."""
-
-    mock_my_pv_client.connect.return_value = False
-
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={
-            "source": config_entries.SOURCE_ZEROCONF,
-        },
-        data=ZEROCONF_DISCOVERY,
-    )
-
-    assert result["type"] is FlowResultType.ABORT
-    assert result["reason"] == "cannot_connect"
-
-
-async def test_step_zeroconf_wrong_password(
-    hass: HomeAssistant,
-    mock_my_pv_client: AsyncMock,
-) -> None:
-    """Test for Zeroconf discovery with an incorrect password."""
-
-    mock_my_pv_client.connect.side_effect = MyPVAuthenticationError()
-
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={
-            "source": config_entries.SOURCE_ZEROCONF,
-        },
-        data=ZEROCONF_DISCOVERY,
-    )
-
-    assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "discovery_auth"
-    assert not result["errors"]
-
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"], {CONF_PASSWORD: "wrong-password"}
-    )
-
-    assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "discovery_auth"
-    assert result["errors"]["password"] == "invalid_password"
-
-    mock_my_pv_client.connect.side_effect = None
-
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"], {CONF_PASSWORD: "test-password"}
     )
 
     assert result["type"] is FlowResultType.CREATE_ENTRY
