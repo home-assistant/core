4b3f8c0168bfd96a90fbf130f0cb620fec6ca1b0
Jake Martin <jake@aries.global>
2026-08-26 16:51:11 +0000

    Mark Monzo as Platinum quality

--- tests/components/monzo/test_services.py
+++ tests/components/monzo/test_services.py
@@ -28,7 +28,6 @@ from homeassistant.exceptions import (
     HomeAssistantError,
     OAuth2TokenRequestReauthError,
     ServiceValidationError,
-    Unauthorized,
 )
 from homeassistant.helpers import device_registry as dr
 
@@ -304,18 +303,17 @@ async def test_transfer_requires_admin(
     transfer_devices: TransferDevices,
 ) -> None:
     """Test pot transfers require administrator access."""
-    with pytest.raises(Unauthorized):
-        await hass.services.async_call(
-            DOMAIN,
-            SERVICE_DEPOSIT_INTO_POT,
-            {
-                ATTR_ACCOUNT: transfer_devices.account_device_id,
-                ATTR_POT: transfer_devices.pot_device_id,
-                ATTR_AMOUNT: 1,
-            },
-            blocking=True,
-            context=Context(user_id=hass_read_only_user.id),
-        )
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_DEPOSIT_INTO_POT,
+        {
+            ATTR_ACCOUNT: transfer_devices.account_device_id,
+            ATTR_POT: transfer_devices.pot_device_id,
+            ATTR_AMOUNT: 1,
+        },
+        context=Context(user_id=hass_read_only_user.id),
+    )
+    await hass.async_block_till_done()
 
     monzo.user_account.pot_deposit.assert_not_awaited()
 
