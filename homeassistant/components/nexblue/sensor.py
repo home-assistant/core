db3fb6022c8324521a6e31f3cdbf6fecd5b65400
Joost Lekkerkerker <joostlek@outlook.com>
2026-08-26 14:59:22 +0200

    Apply suggestion from @joostlek

--- homeassistant/components/nexblue/sensor.py
+++ homeassistant/components/nexblue/sensor.py
@@ -263,7 +263,7 @@ class NexBlueStatusSensor(
         """Return whether this charger is currently reachable."""
         return (
             super().available
-            and self.coordinator.data.get(self._serial_number) is not None
+            and self._serial_number in self.coordinator.data
         )
 
     @property
