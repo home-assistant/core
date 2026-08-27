ddf11071c8ac9e5da9bd9f1498065a4ed0f53f6b
Erwin Douna <e.douna@gmail.com>
2026-08-27 13:50:43 +0000

    Extend

--- homeassistant/components/portainer/coordinator.py
+++ homeassistant/components/portainer/coordinator.py
@@ -264,6 +264,174 @@ class PortainerCoordinator(
                     self.portainer.docker_system_df(endpoint.id, verbose=True),
                     self.portainer.get_volumes(endpoint.id),
                 )
+
+                stack_requests = [self.portainer.get_stacks(endpoint_id=endpoint.id)]
+                swarm_id = (
+                    docker_info.swarm.cluster.get("ID")
+                    if docker_info.swarm
+                    and docker_info.swarm.control_available
+                    and docker_info.swarm.cluster
+                    else None
+                )
+                if swarm_id:
+                    stack_requests.append(
+                        self.portainer.get_stacks(
+                            endpoint_id=endpoint.id, swarm_id=swarm_id
+                        )
+                    )
+
+                stacks = [
+                    stack
+                    for result in await asyncio.gather(*stack_requests)
+                    for stack in result
+                ]
+
+                prev_endpoint = self.data.get(endpoint.id) if self.data else None
+                container_map: dict[str, PortainerContainerData] = {}
+                stack_map: dict[str, PortainerStackData] = {
+                    stack.name: PortainerStackData(stack=stack, container_count=0)
+                    for stack in stacks
+                }
+
+                container_names = [
+                    sanitize_container_name(container.names[0])
+                    for container in containers
+                ]
+                container_inspects = dict(
+                    zip(
+                        container_names,
+                        await asyncio.gather(
+                            *(
+                                self.portainer.inspect_container(
+                                    endpoint.id, container.id
+                                )
+                                for container in containers
+                            )
+                        ),
+                        strict=False,
+                    )
+                )
+                local_images = dict(
+                    zip(
+                        container_inspects,
+                        await asyncio.gather(
+                            *(
+                                self._get_local_image(
+                                    endpoint.id, str(container_inspect.image)
+                                )
+                                for container_inspect in container_inspects.values()
+                            )
+                        ),
+                        strict=False,
+                    )
+                )
+
+                # Map containers, started and stopped
+                for container in containers:
+                    container_name = sanitize_container_name(container.names[0])
+                    prev_container = (
+                        prev_endpoint.containers.get(container_name)
+                        if prev_endpoint
+                        else None
+                    )
+
+                    container_inspect = container_inspects[container_name]
+                    local_image = local_images[container_name]
+
+                    image_status = (
+                        (
+                            result.status
+                            if (
+                                result := self.watcher.results.get(
+                                    (endpoint.id, container.id)
+                                )
+                            )
+                            else None
+                        )
+                        if self.watcher
+                        else None
+                    )
+
+                    # Check if container belongs to a stack via docker compose label
+                    stack_name: str | None = (
+                        container.labels.get("com.docker.compose.project")
+                        or container.labels.get("com.docker.stack.namespace")
+                        if container.labels
+                        else None
+                    )
+                    if stack_name and (stack_data := stack_map.get(stack_name)):
+                        stack_data.container_count += 1
+
+                    container_map[container_name] = PortainerContainerData(
+                        container=container,
+                        container_inspect=container_inspect,
+                        local_image=local_image,
+                        stats=None,
+                        stats_pre=prev_container.stats if prev_container else None,
+                        image_status=image_status,
+                        stack=stack_map[stack_name].stack
+                        if stack_name and stack_name in stack_map
+                        else None,
+                    )
+
+                volume_usage_map = {
+                    item["Name"]: item
+                    for item in (docker_system_df.volume_disk_usage.items or [])
+                }
+                volume_map: dict[str, PortainerVolumeData] = {}
+                for volume in volumes:
+                    if item := volume_usage_map.get(volume.name):
+                        volume.usage_data = DockerVolumeUsageData(
+                            size=item["UsageData"]["Size"],
+                            ref_count=item["UsageData"]["RefCount"],
+                        )
+                    volume_map[volume.name] = PortainerVolumeData(volume=volume)
+
+                # Separately fetch stats for active containers
+                active_containers = [
+                    container
+                    for container in containers
+                    if container.state
+                    in (DockerContainerState.RUNNING, DockerContainerState.PAUSED)
+                ]
+                if active_containers:
+                    container_stats = dict(
+                        zip(
+                            (
+                                sanitize_container_name(container.names[0])
+                                for container in active_containers
+                            ),
+                            await asyncio.gather(
+                                *(
+                                    self.portainer.container_stats(
+                                        endpoint_id=endpoint.id,
+                                        container_id=container.id,
+                                    )
+                                    for container in active_containers
+                                )
+                            ),
+                            strict=False,
+                        )
+                    )
+
+                    # Now assign stats to the containers
+                    for container_name, stats in container_stats.items():
+                        container_map[container_name].stats = stats
+
+                self._container_ids_by_endpoint[endpoint.id] = {
+                    data.container.id: name for name, data in container_map.items()
+                }
+
+                mapped_endpoints[endpoint.id] = PortainerCoordinatorData(
+                    id=endpoint.id,
+                    name=endpoint.name,
+                    endpoint=endpoint,
+                    containers=container_map,
+                    docker_version=docker_version,
+                    docker_info=docker_info,
+                    volumes=volume_map,
+                    stacks=stack_map,
+                )
             except PortainerTimeoutError:
                 _LOGGER.warning(
                     "Timed out fetching data for endpoint: %s (ID: %d). Skipping data fetch",
@@ -271,171 +439,6 @@ class PortainerCoordinator(
                     endpoint.id,
                 )
                 continue
-
-            stack_requests = [self.portainer.get_stacks(endpoint_id=endpoint.id)]
-            swarm_id = (
-                docker_info.swarm.cluster.get("ID")
-                if docker_info.swarm
-                and docker_info.swarm.control_available
-                and docker_info.swarm.cluster
-                else None
-            )
-            if swarm_id:
-                stack_requests.append(
-                    self.portainer.get_stacks(
-                        endpoint_id=endpoint.id, swarm_id=swarm_id
-                    )
-                )
-
-            stacks = [
-                stack
-                for result in await asyncio.gather(*stack_requests)
-                for stack in result
-            ]
-
-            prev_endpoint = self.data.get(endpoint.id) if self.data else None
-            container_map: dict[str, PortainerContainerData] = {}
-            stack_map: dict[str, PortainerStackData] = {
-                stack.name: PortainerStackData(stack=stack, container_count=0)
-                for stack in stacks
-            }
-
-            container_names = [
-                sanitize_container_name(container.names[0]) for container in containers
-            ]
-            container_inspects = dict(
-                zip(
-                    container_names,
-                    await asyncio.gather(
-                        *(
-                            self.portainer.inspect_container(endpoint.id, container.id)
-                            for container in containers
-                        )
-                    ),
-                    strict=False,
-                )
-            )
-            local_images = dict(
-                zip(
-                    container_inspects,
-                    await asyncio.gather(
-                        *(
-                            self._get_local_image(
-                                endpoint.id, str(container_inspect.image)
-                            )
-                            for container_inspect in container_inspects.values()
-                        )
-                    ),
-                    strict=False,
-                )
-            )
-
-            # Map containers, started and stopped
-            for container in containers:
-                container_name = sanitize_container_name(container.names[0])
-                prev_container = (
-                    prev_endpoint.containers.get(container_name)
-                    if prev_endpoint
-                    else None
-                )
-
-                container_inspect = container_inspects[container_name]
-                local_image = local_images[container_name]
-
-                image_status = (
-                    (
-                        result.status
-                        if (
-                            result := self.watcher.results.get(
-                                (endpoint.id, container.id)
-                            )
-                        )
-                        else None
-                    )
-                    if self.watcher
-                    else None
-                )
-
-                # Check if container belongs to a stack via docker compose label
-                stack_name: str | None = (
-                    container.labels.get("com.docker.compose.project")
-                    or container.labels.get("com.docker.stack.namespace")
-                    if container.labels
-                    else None
-                )
-                if stack_name and (stack_data := stack_map.get(stack_name)):
-                    stack_data.container_count += 1
-
-                container_map[container_name] = PortainerContainerData(
-                    container=container,
-                    container_inspect=container_inspect,
-                    local_image=local_image,
-                    stats=None,
-                    stats_pre=prev_container.stats if prev_container else None,
-                    image_status=image_status,
-                    stack=stack_map[stack_name].stack
-                    if stack_name and stack_name in stack_map
-                    else None,
-                )
-
-            volume_usage_map = {
-                item["Name"]: item
-                for item in (docker_system_df.volume_disk_usage.items or [])
-            }
-            volume_map: dict[str, PortainerVolumeData] = {}
-            for volume in volumes:
-                if item := volume_usage_map.get(volume.name):
-                    volume.usage_data = DockerVolumeUsageData(
-                        size=item["UsageData"]["Size"],
-                        ref_count=item["UsageData"]["RefCount"],
-                    )
-                volume_map[volume.name] = PortainerVolumeData(volume=volume)
-
-            # Separately fetch stats for active containers
-            active_containers = [
-                container
-                for container in containers
-                if container.state
-                in (DockerContainerState.RUNNING, DockerContainerState.PAUSED)
-            ]
-            if active_containers:
-                container_stats = dict(
-                    zip(
-                        (
-                            sanitize_container_name(container.names[0])
-                            for container in active_containers
-                        ),
-                        await asyncio.gather(
-                            *(
-                                self.portainer.container_stats(
-                                    endpoint_id=endpoint.id,
-                                    container_id=container.id,
-                                )
-                                for container in active_containers
-                            )
-                        ),
-                        strict=False,
-                    )
-                )
-
-                # Now assign stats to the containers
-                for container_name, stats in container_stats.items():
-                    container_map[container_name].stats = stats
-
-            self._container_ids_by_endpoint[endpoint.id] = {
-                data.container.id: name for name, data in container_map.items()
-            }
-
-            mapped_endpoints[endpoint.id] = PortainerCoordinatorData(
-                id=endpoint.id,
-                name=endpoint.name,
-                endpoint=endpoint,
-                containers=container_map,
-                docker_version=docker_version,
-                docker_info=docker_info,
-                volumes=volume_map,
-                stacks=stack_map,
-            )
 
         self._async_add_remove_endpoints(mapped_endpoints)
         self._async_sync_event_listeners(mapped_endpoints)
@@ -714,5 +717,15 @@ class PortainerDockerDiskSpaceCoordinator(
         for endpoint in endpoints:
             if endpoint.status == EndpointStatus.DOWN:
                 continue
-            results[endpoint.id] = await self.portainer.docker_system_df(endpoint.id)
+            try:
+                results[endpoint.id] = await self.portainer.docker_system_df(
+                    endpoint.id
+                )
+            except PortainerTimeoutError:
+                _LOGGER.warning(
+                    "Timed out fetching DF data for endpoint: %s (ID: %d). Skipping data fetch",
+                    endpoint.name,
+                    endpoint.id,
+                )
+                continue
         return results
