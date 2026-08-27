73207e4b211f777cc777f26822d4ffdaca805d39
Ash Hughes <spirit.returned@gmail.com>
2026-08-27 23:02:58 +0000

    Check that changing get_dst return value changes switch state.

--- tests/components/frontier_silicon/test_switch.py
+++ tests/components/frontier_silicon/test_switch.py
@@ -1,14 +1,23 @@
 """Test the Frontier Silicon switch entity."""
 
+from datetime import timedelta
 from unittest.mock import AsyncMock
 
+from freezegun.api import FrozenDateTimeFactory
+
 from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
-from homeassistant.const import ATTR_ENTITY_ID, SERVICE_TURN_OFF, SERVICE_TURN_ON
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    SERVICE_TURN_OFF,
+    SERVICE_TURN_ON,
+    STATE_OFF,
+    STATE_ON,
+)
 from homeassistant.core import HomeAssistant
 
 from . import setup_integration
 
-from tests.common import MockConfigEntry
+from tests.common import MockConfigEntry, async_fire_time_changed
 
 DST_SWITCH_ENTITY_ID = "switch.name_of_the_device_daylight_saving_time"
 
@@ -44,3 +53,30 @@ async def test_dst_switch(
 
     mock_afsapi.set_dst.assert_awaited_with(False)
     mock_afsapi.set_dst.reset_mock()
+
+
+async def test_dst_switch_get(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    mock_afsapi: AsyncMock,
+    freezer: FrozenDateTimeFactory,
+) -> None:
+    """Test that switch state reflects get_dst result."""
+
+    await setup_integration(hass, config_entry)
+
+    # Turn DST switch on and advance time to trigger a poll
+    mock_afsapi.get_dst.return_value = True
+    freezer.tick(timedelta(seconds=10))
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+
+    assert hass.states.get(DST_SWITCH_ENTITY_ID).state == STATE_ON
+
+    # Turn DST switch off and advance time to trigger a poll
+    mock_afsapi.get_dst.return_value = False
+    freezer.tick(timedelta(seconds=60))
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+
+    assert hass.states.get(DST_SWITCH_ENTITY_ID).state == STATE_OFF
