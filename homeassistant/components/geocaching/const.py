2038fafc0af14db903fc131b95c121ff7f4e6bc6
lampje25 <48517037+lampje25@users.noreply.github.com>
2026-08-29 11:45:24 +0000

    Match geocaching trackable limit to API

--- homeassistant/components/geocaching/const.py
+++ homeassistant/components/geocaching/const.py
@@ -13,7 +13,7 @@ CONF_TRACKABLE_CODES: Final = "trackable_codes"
 SUBENTRY_TYPE_TRACKED_CACHE: Final = "tracked_cache"
 
 MAX_TRACKED_CACHES: Final = 50
-MAX_TRACKED_TRACKABLES: Final = 50
+MAX_TRACKED_TRACKABLES: Final = 10
 LOGGER = logging.getLogger(__package__)
 UPDATE_INTERVAL = timedelta(hours=1)
 
