88fa047bcaeb82e09048b6341188b280fca55a1e
karwosts <karwosts@gmail.com>
2026-09-01 12:25:42 +0000

    Refactor tests into more reusable fixtures

--- tests/components/collection_image/test_config_flow.py
+++ tests/components/collection_image/test_config_flow.py
@@ -1,68 +1,44 @@
 """Test the Collection Image config flow."""
 
-from unittest.mock import patch
+from unittest.mock import AsyncMock, patch
+
+import pytest
 
 from homeassistant import config_entries
 from homeassistant.components.collection_image.const import DOMAIN
-from homeassistant.components.media_player import BrowseMedia, MediaClass
-from homeassistant.components.media_source import BrowseMediaSource
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
+from .const import (
+    MOCK_MEDIA_DIR_URI_1,
+    MOCK_MEDIA_DIR_URI_BROWSE_ERROR,
+    MOCK_MEDIA_DIR_URI_EMPTY,
+)
 
-async def _assert_successful_configure(
-    hass: HomeAssistant, previous_step: config_entries.ConfigFlowResult
-) -> None:
-    with (
-        patch(
-            "homeassistant.components.collection_image.async_setup_entry",
-            return_value=True,
-        ) as mock_setup_entry,
-        patch(
-            "homeassistant.components.collection_image.config_flow.async_browse_media",
-            return_value=BrowseMediaSource(
-                domain=None,
-                identifier=None,
-                media_class="",
-                media_content_type="",
-                title="My pictures",
-                can_play=False,
-                can_expand=True,
-                children=[
-                    BrowseMedia(
-                        media_class=MediaClass.IMAGE,
-                        media_content_id="media-source://mymedia/photo",
-                        media_content_type="image/png",
-                        title="a picture",
-                        can_play=True,
-                        can_expand=False,
-                    ),
-                ],
-            ),
-        ),
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            previous_step["flow_id"],
-            {
-                "media": {
-                    "media_content_id": "media-source://mymedia",
-                    "media_content_type": "",
-                },
-            },
-        )
 
-    assert result.get("type") is FlowResultType.CREATE_ENTRY
-    assert result.get("title") == "My pictures collection"
-    assert result.get("data") == {
+@pytest.fixture
+def mock_setup_entry():
+    """Mock collection_image setup successfully."""
+
+    with patch(
+        "homeassistant.components.collection_image.async_setup_entry",
+        new=AsyncMock(return_value=True),
+    ) as mock_setup:
+        yield mock_setup
+
+
+def _data_from_uri(uri: str) -> dict:
+    return {
         "media": {
-            "media_content_id": "media-source://mymedia",
+            "media_content_id": uri,
             "media_content_type": "",
-        },
+        }
     }
-    assert len(mock_setup_entry.mock_calls) == 1
 
 
-async def test_config_flow(hass: HomeAssistant) -> None:
+async def test_config_flow(
+    hass: HomeAssistant, mock_media_source, mock_setup_entry
+) -> None:
     """Test the config flow."""
 
     result = await hass.config_entries.flow.async_init(
@@ -71,11 +47,41 @@ async def test_config_flow(hass: HomeAssistant) -> None:
     assert result.get("type") is FlowResultType.FORM
     assert result.get("errors") == {}
 
-    await _assert_successful_configure(hass, result)
+    data = _data_from_uri(MOCK_MEDIA_DIR_URI_1)
+    expected_title = "My pictures collection"
+
+    result = await hass.config_entries.flow.async_configure(result["flow_id"], data)
+
+    assert result.get("type") is FlowResultType.CREATE_ENTRY
+    assert result.get("title") == expected_title
+    assert result.get("data") == data
+    assert len(mock_setup_entry.mock_calls) == 1
 
 
-async def test_config_flow_with_error(hass: HomeAssistant) -> None:
-    """Test the config flow with an invalid directory."""
+@pytest.mark.parametrize(
+    ("uri", "error", "placeholders"),
+    [
+        (
+            MOCK_MEDIA_DIR_URI_EMPTY,
+            "selected_media_no_images",
+            {},
+        ),
+        (
+            MOCK_MEDIA_DIR_URI_BROWSE_ERROR,
+            "failed_browse",
+            {"error": "Mock directory failed to browse"},
+        ),
+    ],
+)
+async def test_config_flow_error(
+    hass: HomeAssistant,
+    mock_media_source,
+    mock_setup_entry,
+    uri: str,
+    error: str,
+    placeholders: dict,
+) -> None:
+    """Test the config flow with an invalid media."""
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -83,79 +89,24 @@ async def test_config_flow_with_error(hass: HomeAssistant) -> None:
     assert result.get("type") is FlowResultType.FORM
     assert result.get("errors") == {}
 
-    with (
-        patch(
-            "homeassistant.components.collection_image.async_setup_entry",
-            return_value=True,
-        ) as mock_setup_entry,
-        patch(
-            "homeassistant.components.collection_image.config_flow.async_browse_media",
-            return_value=BrowseMediaSource(
-                domain=None,
-                identifier=None,
-                media_class="",
-                media_content_type="",
-                title="",
-                can_play=False,
-                can_expand=True,
-                children=[],
-            ),
-        ),
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "media": {
-                    "media_content_id": "media-source://mymedia_empty",
-                    "media_content_type": "",
-                },
-            },
-        )
-        await hass.async_block_till_done()
+    data = _data_from_uri(uri)
+    result = await hass.config_entries.flow.async_configure(result["flow_id"], data)
+    await hass.async_block_till_done()
 
     assert result.get("type") is FlowResultType.FORM
     assert result.get("title") is None
     assert result.get("data") is None
-    assert result.get("errors") == {"media": "selected_media_no_images"}
+    assert result.get("errors") == {"media": error}
+    assert result.get("description_placeholders") == placeholders
     assert len(mock_setup_entry.mock_calls) == 0
 
     # Try again successfully to ensure we can recover from errors
-    await _assert_successful_configure(hass, result)
+    data = _data_from_uri(MOCK_MEDIA_DIR_URI_1)
+    expected_title = "My pictures collection"
 
+    result = await hass.config_entries.flow.async_configure(result["flow_id"], data)
 
-async def test_config_flow_with_exception(hass: HomeAssistant) -> None:
-    """Test the config flow with a browse failure."""
-
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-    assert result.get("type") is FlowResultType.FORM
-    assert result.get("errors") == {}
-
-    with (
-        patch(
-            "homeassistant.components.collection_image.async_setup_entry",
-            return_value=True,
-        ) as mock_setup_entry,
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "media": {
-                    "media_content_id": "media-source://mymedia",
-                    "media_content_type": "",
-                },
-            },
-        )
-        await hass.async_block_till_done()
-
-    assert result.get("type") is FlowResultType.FORM
-    assert result.get("title") is None
-    assert result.get("data") is None
-    assert result.get("errors") == {"media": "failed_browse"}
-    assert result.get("description_placeholders") == {
-        "error": "Media Source not loaded"
-    }
-    assert len(mock_setup_entry.mock_calls) == 0
-
-    await _assert_successful_configure(hass, result)
+    assert result.get("type") is FlowResultType.CREATE_ENTRY
+    assert result.get("title") == expected_title
+    assert result.get("data") == data
+    assert len(mock_setup_entry.mock_calls) == 1
