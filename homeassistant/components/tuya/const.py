c966d4c431ed6320f5e23ef42811d191e0cb741c
Oleksandr B <oleksandr.borovets@gmail.com>
2026-09-08 14:39:26 +0300

    Add sensor platform to Tuya qccdz (EV charger)

--- homeassistant/components/tuya/const.py
+++ homeassistant/components/tuya/const.py
@@ -641,6 +641,7 @@ class DPCode(StrEnum):
     CH2O_VALUE = "ch2o_value"
     CH4_SENSOR_STATE = "ch4_sensor_state"
     CH4_SENSOR_VALUE = "ch4_sensor_value"
+    CHARGE_ENERGY_ONCE = "charge_energy_once"
     CHARGE_STATE = "charge_state"
     CHILD_LOCK = "child_lock"  # Child lock
     CISTERN = "cistern"
@@ -858,6 +859,7 @@ class DPCode(StrEnum):
     SENSOR_TEMPERATURE = "sensor_temperature"
     SHAKE = "shake"  # Oscillating
     SHOCK_STATE = "shock_state"  # Vibration status
+    SIGLE_PHASE_POWER = "sigle_phase_power"  # Single-phase power (Tuya's spelling)
     SIREN_SWITCH = "siren_switch"
     SITUATION_SET = "situation_set"
     SLEEP = "sleep"  # Sleep function
