b074419672c69cd64a4850266fd6736bbf8e0e46
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2026-09-08 18:23:46 -0400

    style(midea): format capability lookup

--- homeassistant/components/midea/switch.py
+++ homeassistant/components/midea/switch.py
@@ -125,9 +125,7 @@ async def async_setup_entry(
         and description.key in device.attributes
         and (
             description.key != "pump"
-            or (getattr(device, "capabilities", None) or {}).get(
-                description.key, False
-            )
+            or (getattr(device, "capabilities", None) or {}).get(description.key, False)
         )
     )
 
