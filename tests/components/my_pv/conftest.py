f50c085f98e019f794e38a0401c2179db94f6bf7
rrooggiieerr <rogier@batoid.com>
2026-09-09 18:21:37 +0200

    Add tests for sensor platform

--- tests/components/my_pv/conftest.py
+++ tests/components/my_pv/conftest.py
@@ -16,9 +16,55 @@ SETUP_CONFIGURATION = {
     "ww1target": {"step": 0.1, "unit": "°C", "min": 5.0, "max": 95.0}
 }
 
+DATA_CONFIGURATION = {
+    "cur_eth_mode": {
+        "options": {"0": "LAN", "1": "WLAN", "2": "AP"},
+        "type": "enumeration",
+    },
+    "freq": {"type": "number", "unit": "Hz"},
+    "power": {"type": "number", "unit": "W"},
+    "screen_mode_flag": {
+        "options": {
+            "0": "Standby",
+            "1": "Heat",
+            "2": "Boost",
+            "3": "Heating finished",
+            "4": "No connection / Disabled",
+            "5": "Error",
+            "6": "Block active",
+        },
+        "type": "enumeration",
+    },
+    "temp1": {"type": "number", "unit": "°C"},
+    "temp2": {"type": "number", "unit": "°C"},
+    "temp3": {"type": "number", "unit": "°C"},
+    "temp4": {"type": "number", "unit": "°C"},
+    "temp_ps": {"type": "number", "unit": "°C"},
+    "uptime": {"type": "number", "unit": "h"},
+    "volt_mains": {"type": "number", "unit": "V"},
+}
+
+DATA_VALUE = {
+    "cur_eth_mode": "0",
+    "freq": 49.965,
+    "power": 3445,
+    "screen_mode_flag": "4",
+    "temp1": 12.3,
+    "temp2": 23.4,
+    "temp3": 34.5,
+    "temp4": 45.6,
+    "temp_ps": 56.7,
+    "uptime": 2,
+    "volt_mains": 238,
+}
+
 
 def _setup_configuration_lookup(key):
     return SETUP_CONFIGURATION.get(key)
+
+
+def _data_value_lookup(key):
+    return DATA_VALUE.get(key)
 
 
 @pytest.fixture
@@ -74,5 +120,7 @@ def mock_my_pv_client() -> Generator[AsyncMock]:
         client.current_temperature = 54.3
         client.target_temperature = 62.1
         client.get_setup_configuration = Mock(side_effect=_setup_configuration_lookup)
+        client.get_data_configurations = Mock(return_value=DATA_CONFIGURATION)
+        client.get_data_value = Mock(side_effect=_data_value_lookup)
 
         yield client
---
+++ tests/components/my_pv/test_sensor.py
@@ -0,0 +1,67 @@
+"""Test the my-PV water heater."""
+
+from unittest.mock import AsyncMock, Mock, patch
+
+import pytest
+from syrupy.assertion import SnapshotAssertion
+
+from homeassistant.const import STATE_UNAVAILABLE, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
+
+from tests.common import MockConfigEntry, snapshot_platform
+
+
+@pytest.mark.usefixtures("mock_my_pv_client")
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_sensor(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Test successful setup of a water heater."""
+
+    with patch("homeassistant.components.my_pv.PLATFORMS", [Platform.SENSOR]):
+        mock_config_entry.add_to_hass(hass)
+
+        assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
+        await hass.async_block_till_done()
+
+    await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
+
+
+async def test_sensor_unavailable_not_connected(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_my_pv_client: AsyncMock,
+) -> None:
+    """Test if a sensor is unavailable when not connected."""
+
+    mock_config_entry.add_to_hass(hass)
+
+    mock_my_pv_client.connected = False
+
+    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.my_pv_ac_elwa_2_temperature_1")
+    assert state.state == STATE_UNAVAILABLE
+
+
+async def test_sensor_unavailable_data_value_none(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_my_pv_client: AsyncMock,
+) -> None:
+    """Test if a sensor is unavailable when data value is None."""
+
+    mock_config_entry.add_to_hass(hass)
+
+    mock_my_pv_client.get_data_value = Mock(return_value=None)
+
+    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.my_pv_ac_elwa_2_temperature_1")
+    assert state.state == STATE_UNAVAILABLE
