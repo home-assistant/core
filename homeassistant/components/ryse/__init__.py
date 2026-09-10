135fb1699a0be730359a6192dd0502300a0d535e
MOHAMED Kallel <mohamed@helloryse.com>
2026-09-10 12:17:39 +0000

    Fix review: Successful unload currently calls the device’s unpair() method twice

--- homeassistant/components/ryse/__init__.py
+++ homeassistant/components/ryse/__init__.py
@@ -94,7 +94,4 @@ async def async_setup_entry(hass: HomeAssistant, entry: RyseConfigEntry) -> bool
 
 async def async_unload_entry(hass: HomeAssistant, entry: RyseConfigEntry) -> bool:
     """Unload a RYSE config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        await entry.runtime_data.unpair()
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
