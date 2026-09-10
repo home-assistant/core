d14635e99c4907ad8106112dd33a221bacd02635
tr4nt0r <tr4nt0r@uber.space>
2026-09-10 15:09:01 +0000

    Move websocket registration to async_setup in HTML5 integration

---
+++ homeassistant/components/html5/websocket_api.py
@@ -0,0 +1,41 @@
+"""HTML5 Websocket API."""
+
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant.components import websocket_api
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.service import async_get_config_entry
+
+from .const import ATTR_VAPID_PUB_KEY, DOMAIN
+
+WS_TYPE_APPKEY = "notify/html5/appkey"
+SCHEMA_WS_APPKEY = websocket_api.BASE_COMMAND_MESSAGE_SCHEMA.extend(
+    {vol.Required("type"): WS_TYPE_APPKEY}
+)
+
+
+@callback
+def async_register_websocket_api(hass: HomeAssistant) -> None:
+    """Register the websocket API."""
+
+    websocket_api.async_register_command(hass, websocket_appkey)
+
+
+@websocket_api.websocket_command(
+    {
+        vol.Required("type"): WS_TYPE_APPKEY,
+    }
+)
+@websocket_api.async_response
+async def websocket_appkey(
+    hass: HomeAssistant,
+    connection: websocket_api.ActiveConnection,
+    msg: dict[str, Any],
+) -> None:
+    """Handle request for the VAPID public key."""
+    entry = async_get_config_entry(hass, DOMAIN, None)
+    connection.send_message(
+        websocket_api.result_message(msg["id"], entry.data[ATTR_VAPID_PUB_KEY])
+    )
