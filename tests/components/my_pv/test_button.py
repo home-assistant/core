416233ee61d13f6e4bfbfcf375bc754476b3c970
rrooggiieerr <rogier@batoid.com>
2026-09-10 13:56:11 +0200

    Add tests for button press errors

--- tests/components/my_pv/test_button.py
+++ tests/components/my_pv/test_button.py
@@ -2,12 +2,14 @@
 
 from unittest.mock import AsyncMock, patch
 
+from my_pv.exceptions import MyPVAuthenticationError, MyPVConnectionError
 import pytest
 from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.components.button import DOMAIN as BUTTON_DOMAIN, SERVICE_PRESS
 from homeassistant.const import STATE_UNAVAILABLE, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import entity_registry as er
 
 from tests.common import MockConfigEntry, snapshot_platform
@@ -62,6 +64,46 @@ async def test_button_press(
         assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
         await hass.async_block_till_done()
 
+    await hass.services.async_call(
+        BUTTON_DOMAIN,
+        SERVICE_PRESS,
+        {"entity_id": "button.my_pv_ac_elwa_2_restart"},
+        blocking=True,
+    )
+    mock_my_pv_client.send_command.assert_awaited_once_with("reboot_device", None)
+
+
+@pytest.mark.parametrize(
+    ("error"), [(MyPVConnectionError()), (MyPVAuthenticationError())]
+)
+async def test_button_press_error(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_my_pv_client: AsyncMock,
+    error,
+) -> None:
+    """Test successful press of a button."""
+
+    with patch("homeassistant.components.my_pv.PLATFORMS", [Platform.BUTTON]):
+        mock_config_entry.add_to_hass(hass)
+
+        assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
+        await hass.async_block_till_done()
+
+    mock_my_pv_client.send_command.side_effect = error
+    with (
+        pytest.raises(HomeAssistantError),
+    ):
+        await hass.services.async_call(
+            BUTTON_DOMAIN,
+            SERVICE_PRESS,
+            {"entity_id": "button.my_pv_ac_elwa_2_restart"},
+            blocking=True,
+        )
+    mock_my_pv_client.send_command.assert_awaited_once_with("reboot_device", None)
+
+    mock_my_pv_client.send_command.reset_mock()
+    mock_my_pv_client.send_command.side_effect = None
     await hass.services.async_call(
         BUTTON_DOMAIN,
         SERVICE_PRESS,
