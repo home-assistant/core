60f9040cafcdb35a2b01eb0afb3714ff45f862d0
rrooggiieerr <rogier@batoid.com>
2026-09-10 15:24:44 +0200

    Fix parameter type annotation

--- tests/components/my_pv/test_button.py
+++ tests/components/my_pv/test_button.py
@@ -120,7 +120,7 @@ async def test_button_press_send_command_throws_error(
     hass: HomeAssistant,
     mock_config_entry: MockConfigEntry,
     mock_my_pv_client: AsyncMock,
-    error: MyPVConnectionError,
+    error: MyPVConnectionError | MyPVAuthenticationError,
     expected_ha_error: type[HomeAssistantError],
 ) -> None:
     """Test for HomeAssistantError when send_command throws error."""
