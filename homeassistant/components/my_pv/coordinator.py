6b78e29bcdd583bbbb2545dd3a2ac7647aa34382
rrooggiieerr <rogier@batoid.com>
2026-09-10 12:56:47 +0200

    Add Button platform

--- homeassistant/components/my_pv/coordinator.py
+++ homeassistant/components/my_pv/coordinator.py
@@ -145,6 +145,13 @@ class MyPVCoordinator(DataUpdateCoordinator[None]):
         return result
 
     @_my_pv_connection
+    async def send_command(self, key, value: bool | float | str | None = None):
+        """Send command."""
+        result = await self.device.send_command(key, value)
+        self.async_update_listeners()
+        return result
+
+    @_my_pv_connection
     async def turn_on(self) -> bool:
         """Turn on the device."""
         result = await self.device.turn_on()
