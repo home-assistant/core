e78cdefecaf417eafc3d8655ccfa07d0493c01a0
Bastien Gautier <contact@bastiengautier.com>
2026-09-12 20:28:10 +0200

    Update breaking version to 2027.4.0 in issue.py

--- homeassistant/components/free_mobile/issue.py
+++ homeassistant/components/free_mobile/issue.py
@@ -23,7 +23,7 @@ def async_deprecate_yaml_issue(
             f"deprecated_yaml_{DOMAIN}",
             is_fixable=False,
             issue_domain=DOMAIN,
-            breaks_in_ha_version="2027.2.0",
+            breaks_in_ha_version="2027.4.0",
             severity=IssueSeverity.WARNING,
             translation_key="deprecated_yaml",
             translation_placeholders={
@@ -37,7 +37,7 @@ def async_deprecate_yaml_issue(
             hass,
             DOMAIN,
             f"deprecated_yaml_import_issue_error_{name}",
-            breaks_in_ha_version="2027.2.0",
+            breaks_in_ha_version="2027.4.0",
             is_fixable=False,
             severity=IssueSeverity.WARNING,
             translation_key="deprecated_yaml_import_issue_error",
