5553b786c0a64d8c50f57231ba6480d73b824387
Thomas55555 <thomas.protzner@gmail.com>
2026-09-14 20:05:35 +0200

    don't update dynamic

--- tests/components/husqvarna_automower/test_number.py
+++ tests/components/husqvarna_automower/test_number.py
@@ -125,34 +125,6 @@ async def test_number_workarea_cutting_height_legacy_registry_entry_removed(
     assert entity_registry.async_get(registry_entry.entity_id) is None
 
 
-@pytest.mark.usefixtures(
-    "entity_registry_enabled_by_default",
-    "mock_automower_client",
-)
-async def test_number_workarea_cutting_height_transition(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    values: dict[str, MowerAttributes],
-) -> None:
-    """Test dynamic work area cutting height entity creation and removal."""
-    entity_id = "number.garden_test_mower_1_front_lawn_cutting_height"
-    await setup_integration(hass, mock_config_entry)
-
-    assert hass.states.get(entity_id) is not None
-
-    values[TEST_MOWER_ID].work_areas[123456].use_global_cutting_height = True
-    mock_config_entry.runtime_data.async_set_updated_data(values)
-    await hass.async_block_till_done()
-
-    assert hass.states.get(entity_id) is None
-
-    values[TEST_MOWER_ID].work_areas[123456].use_global_cutting_height = False
-    mock_config_entry.runtime_data.async_set_updated_data(values)
-    await hass.async_block_till_done()
-
-    assert hass.states.get(entity_id) is not None
-
-
 @pytest.mark.usefixtures("entity_registry_enabled_by_default")
 async def test_number_snapshot(
     hass: HomeAssistant,
