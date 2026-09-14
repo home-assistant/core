a13bc18d22ae2239b6dae25b66ca5dfbb71714ec
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-09-14 17:34:22 +0200

    Change suggested unit to GB

--- tests/components/shelly/test_sensor.py
+++ tests/components/shelly/test_sensor.py
@@ -2336,7 +2336,7 @@ async def test_rpc_storage_fs_free_sensor(
     monkeypatch: pytest.MonkeyPatch,
 ) -> None:
     """Test RPC storage free space sensor."""
-    status = {"storage:0": {"present": True, "fs_free": 10485760}}
+    status = {"storage:0": {"present": True, "fs_free": 1048576000}}
     monkeypatch.setattr(mock_rpc_device, "status", status)
 
     config = {"storage:0": {"id": 0}}
@@ -2346,22 +2346,22 @@ async def test_rpc_storage_fs_free_sensor(
     await init_integration(hass, 4, model=MODEL_CAMERA)
 
     assert (state := hass.states.get(entity_id))
-    assert state.state == "10.48576"
-    assert state.attributes[ATTR_UNIT_OF_MEASUREMENT] == UnitOfInformation.MEGABYTES
+    assert state.state == "1.048576"
+    assert state.attributes[ATTR_UNIT_OF_MEASUREMENT] == UnitOfInformation.GIGABYTES
     assert state.attributes[ATTR_DEVICE_CLASS] == SensorDeviceClass.DATA_SIZE
     assert state.attributes[ATTR_STATE_CLASS] == SensorStateClass.MEASUREMENT
 
     assert (entry := entity_registry.async_get(entity_id))
     assert entry.unique_id == "123456789ABC-storage:0-storage_fs_free"
-    assert entry.unit_of_measurement == UnitOfInformation.MEGABYTES
+    assert entry.unit_of_measurement == UnitOfInformation.GIGABYTES
 
     mutate_rpc_device_status(
-        monkeypatch, mock_rpc_device, "storage:0", "fs_free", 20971520
+        monkeypatch, mock_rpc_device, "storage:0", "fs_free", 2097152000
     )
     mock_rpc_device.mock_update()
 
     assert (state := hass.states.get(entity_id))
-    assert state.state == "20.97152"
+    assert state.state == "2.097152"
 
 
 async def test_rpc_storage_fs_free_sensor_removal(
