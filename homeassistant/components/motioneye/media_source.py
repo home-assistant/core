86f6018c92b19dcd641e42ec32d8c1cb3ca21b87
Marcin Krawczyk <mkc@kramar.pl>
2026-09-14 21:52:58 +0200

    Handle malformed motionEye media proxy paths

--- homeassistant/components/motioneye/media_source.py
+++ homeassistant/components/motioneye/media_source.py
@@ -1,6 +1,7 @@
 """motionEye Media Source Implementation."""
 
 from base64 import urlsafe_b64decode, urlsafe_b64encode
+from binascii import Error
 from datetime import timedelta
 import logging
 from pathlib import PurePath
@@ -114,7 +115,7 @@ class MotionEyeMediaProxyView(HomeAssistantView):
         try:
             media_path = urlsafe_b64decode(path.encode("ascii")).decode("utf-8")
             camera = int(camera_id)
-        except ValueError, UnicodeDecodeError:
+        except (Error, ValueError, UnicodeDecodeError):
             return web.Response(status=400)
 
         data = await cast(Any, entry.runtime_data.client).async_get_media(
