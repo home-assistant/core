7ea1d5fc1a33e3dee805e5c29bddd12bfc73f351
salkotoro <114193957+salkotoro@users.noreply.github.com>
2026-09-15 09:39:38 +0000

    Fix host lists label

--- homeassistant/components/nmap_tracker/const.py
+++ homeassistant/components/nmap_tracker/const.py
@@ -19,7 +19,7 @@ NMAP_TRACKER_DATA: HassKey[NmapTrackedDevices] = HassKey(DOMAIN)
 # Interval in minutes to exclude devices from a scan while they are home
 CONF_HOME_INTERVAL: Final = "home_interval"
 CONF_OPTIONS: Final = "scan_options"
-CONF_HOSTS_LIST: Final = "hosts_list"
+CONF_HOSTS_LIST: Final = "hosts"
 CONF_HOSTS_EXCLUDE: Final = "hosts_exclude"
 CONF_MAC_EXCLUDE: Final = "mac_exclude"
 DEFAULT_OPTIONS: Final = "-n -sn -PR -T4 --min-rate 10 --host-timeout 5s"
