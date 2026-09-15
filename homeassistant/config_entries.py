f011a885c0092048052ab77d0afe08c67b3f2245
G Johansson <goran.johansson@shiftit.se>
2026-09-15 15:43:00 +0000

    Handle disabled entry

--- homeassistant/config_entries.py
+++ homeassistant/config_entries.py
@@ -2454,17 +2454,22 @@ class ConfigEntries:
         non-recoverable migration errors.
         """
         entry = self.async_get_known_entry(entry_id)
-        if entry.state is ConfigEntryState.MIGRATION_ERROR:
-            # Config entry was never loaded so we can set state and start setup to try again
-            entry._async_set_state(self.hass, ConfigEntryState.NOT_LOADED, None)  # noqa: SLF001
-            await self.async_setup(entry_id)
-            return
+        if entry.state is not ConfigEntryState.MIGRATION_ERROR:
+            raise OperationNotAllowed(
+                f"The config entry '{entry.title}' ({entry.domain}) with entry_id"
+                f" '{entry.entry_id}' cannot retry the migration as it is not in the"
+                f" state {ConfigEntryState.MIGRATION_ERROR} but is in the state {entry.state}"
+            )
+        if entry.disabled_by:
+            raise OperationNotAllowed(
+                f"The config entry '{entry.title}' ({entry.domain}) with entry_id"
+                f" '{entry.entry_id}' cannot retry the migration as it is disabled by"
+                f" {entry.disabled_by}. Please enable the config entry and retry."
+            )
 
-        raise OperationNotAllowed(
-            f"The config entry '{entry.title}' ({entry.domain}) with entry_id"
-            f" '{entry.entry_id}' cannot retry the migration as it is not in the"
-            f" state '{ConfigEntryState.MIGRATION_ERROR}' but is in the state {entry.state}"
-        )
+        # Config entry was never loaded so we can set state and start setup to try again
+        entry._async_set_state(self.hass, ConfigEntryState.NOT_LOADED, None)  # noqa: SLF001
+        await self.async_setup(entry_id)
 
     async def async_unload(self, entry_id: str, _lock: bool = True) -> bool:
         """Unload a config entry."""
