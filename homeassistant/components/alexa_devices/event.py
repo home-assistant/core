0916d6d38989449b8bb0e321420ab6a825a834c3
jamesonuk <1040621+jamesonuk@users.noreply.github.com>
2026-09-16 14:14:40 +0000

    add person details to voice event

--- homeassistant/components/alexa_devices/event.py
+++ homeassistant/components/alexa_devices/event.py
@@ -96,6 +96,8 @@ class AlexaVoiceEvent(AmazonEntity, EventEntity):
                 "intent": vocal_record.intent,
                 "voice_command": vocal_record.title,
                 "voice_reply": vocal_record.sub_title,
+                "person_first_name": vocal_record.person_first_name,
+                "person_type": vocal_record.person_type,
             },
         )
         self.async_write_ha_state()
