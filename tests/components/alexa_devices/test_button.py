892d392d76392bdcf8074c7eda650938d6e17076
jamesonuk <1040621+jamesonuk@users.noreply.github.com>
2026-09-16 13:40:06 +0000

    review comments

--- tests/components/alexa_devices/test_button.py
+++ tests/components/alexa_devices/test_button.py
@@ -13,8 +13,14 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 from homeassistant.util import slugify
 
-from . import setup_integration
-from .const import TEST_DEVICE_1, TEST_USERNAME
+from . import assert_device_removed_and_readded, setup_integration
+from .const import (
+    TEST_DEVICE_1,
+    TEST_DEVICE_1_SN,
+    TEST_DEVICE_2,
+    TEST_DEVICE_2_SN,
+    TEST_USERNAME,
+)
 
 from tests.common import MockConfigEntry, async_fire_time_changed, snapshot_platform
 
@@ -125,6 +131,24 @@ async def test_routine_removed_and_readded(
     await hass.async_block_till_done()
 
     assert hass.states.get(entity_id) is not None
+
+
+async def test_device_removed_and_readded(
+    hass: HomeAssistant,
+    freezer: FrozenDateTimeFactory,
+    mock_amazon_devices_client: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test device button is recreated when its device is removed and re-added."""
+    await assert_device_removed_and_readded(
+        hass,
+        freezer,
+        mock_amazon_devices_client,
+        mock_config_entry,
+        entity_id="button.echo_test_2_restart",
+        devices_with={TEST_DEVICE_1_SN: TEST_DEVICE_1, TEST_DEVICE_2_SN: TEST_DEVICE_2},
+        devices_without={TEST_DEVICE_1_SN: TEST_DEVICE_1},
+    )
 
 
 async def test_restart_button(
