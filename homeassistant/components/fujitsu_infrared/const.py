108b06c14c4e6302ac9f2b953d9b73b4a1717f17
Dr-Blank <64108942+Dr-Blank@users.noreply.github.com>
2026-09-16 10:51:41 +0000

    Add Fujitsu Infrared integration

---
+++ homeassistant/components/fujitsu_infrared/const.py
@@ -0,0 +1,9 @@
+"""Constants for the Fujitsu IR integration."""
+
+DOMAIN = "fujitsu_infrared"
+CONF_INFRARED_EMITTER_ENTITY_ID = "infrared_emitter_entity_id"
+CONF_INFRARED_RECEIVER_ENTITY_ID = "infrared_receiver_entity_id"
+CONF_HVAC_MODES = "hvac_modes"
+
+PROTOCOL_STANDARD = "standard"
+PROTOCOL_EXTENDED = "extended"
