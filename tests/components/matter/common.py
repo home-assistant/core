e0831cdf53a117125aeb6cc771d06c83f1ec15a8
Ludovic BOUÉ <lboue@users.noreply.github.com>
2026-09-17 13:43:30 +0200

    Update common.py

--- tests/components/matter/common.py
+++ tests/components/matter/common.py
@@ -55,6 +55,7 @@ FIXTURES = [
     "mock_battery_storage",
     "mock_chime",
     "mock_closure_covering_lift",
+    "mock_closure_roof_window",
     "mock_cooktop",
     "mock_dimmable_light",
     "mock_dimmable_plugin_unit",
