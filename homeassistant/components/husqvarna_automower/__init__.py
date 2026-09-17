0ed6980ce9f7fe911531b69ba13b82c686e0afd0
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2026-09-17 21:21:09 +0200

    Fix deferred migration cleanup

--- homeassistant/components/husqvarna_automower/__init__.py
+++ homeassistant/components/husqvarna_automower/__init__.py
@@ -12,6 +12,7 @@ from homeassistant.helpers import (
     entity_registry as er,
 )
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.util import dt as dt_util
 
 from . import api
 from .const import DOMAIN
