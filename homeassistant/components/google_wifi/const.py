1ddb0c94ffe65c7accf4cb9b11b16c1fbf6f7620
EReaso <136942618+EReaso@users.noreply.github.com>
2026-09-18 10:20:34 -0600

    Fix Google Wifi default host

--- homeassistant/components/google_wifi/const.py
+++ homeassistant/components/google_wifi/const.py
@@ -10,7 +10,7 @@ ATTR_LOCAL_IP = "local_ip"
 ATTR_NEW_VERSION = "new_version"
 ATTR_STATUS = "status"
 ATTR_UPTIME = "uptime"
-DEFAULT_HOST = "testwifi.here"
+DEFAULT_HOST = "192.168.86.2"
 DEFAULT_NAME = "google_wifi"
 ENDPOINT = "/api/v1/status"
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=1)
