1f6cfd98ff94ae3e06b43fdde95340156c94807a
mvn23 <schopdiedwaas@gmail.com>
2026-09-21 13:49:52 +0200

    Update integration cleanup to also reset the second heating circuit override

--- homeassistant/components/opentherm_gw/__init__.py
+++ homeassistant/components/opentherm_gw/__init__.py
@@ -105,6 +105,7 @@ class OpenThermGatewayHub:
     async def cleanup(self, event=None) -> None:
         """Reset overrides on the gateway."""
         await self.gateway.set_control_setpoint(0)
+        await self.gateway.set_control_setpoint_2(0)
         await self.gateway.set_max_relative_mod("-")
         await self.gateway.disconnect()
 
