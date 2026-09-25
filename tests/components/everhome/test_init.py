6d2998abd615dbaca1e3020a843b80b6622cca03
Raphael Betz <raphaelbetz@gmail.com>
2026-09-25 08:40:08 +0000

    add config entry load/unload test

--- tests/components/everhome/test_init.py
+++ tests/components/everhome/test_init.py
@@ -2,6 +2,8 @@
 
 from unittest.mock import AsyncMock
 
+import pytest
+
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
@@ -9,6 +11,24 @@ from homeassistant.core import HomeAssistant
 from . import setup_platform
 
 from tests.common import MockConfigEntry
+
+
+@pytest.mark.usefixtures("mock_everhome_client")
+async def test_load_unload_config_entry(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test loading and unloading the config entry."""
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert mock_config_entry.state is ConfigEntryState.LOADED
+
+    await hass.config_entries.async_unload(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert mock_config_entry.state is ConfigEntryState.NOT_LOADED
 
 
 async def test_connection_failed(
