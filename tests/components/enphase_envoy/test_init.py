a242ef90d70f987800969912fa452242ba3f51a1
Arie Catsman <catsmanac@outlook.com>
2026-09-26 08:40:56 +0000

    Update copied comment

--- tests/components/enphase_envoy/test_init.py
+++ tests/components/enphase_envoy/test_init.py
@@ -844,7 +844,7 @@ async def test_coordinator_interface_information_session_is_closed(
     async_fire_time_changed(hass)
     await hass.async_block_till_done(wait_background_tasks=True)
 
-    # verify no device found message in log
+    # verify client is closed message is in log
     assert "Client is closed when reading interface information" in caplog.text
 
 
